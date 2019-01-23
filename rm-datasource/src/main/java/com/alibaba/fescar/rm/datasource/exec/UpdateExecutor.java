5d36111edc28fb5742ec18af2908b3c3834db8d5
sherlock-hao <153004802@qq.com>
2019-01-23 11:57:03 +0800

    Update UpdateExecutor.java

--- rm-datasource/src/main/java/com/alibaba/fescar/rm/datasource/exec/UpdateExecutor.java
+++ rm-datasource/src/main/java/com/alibaba/fescar/rm/datasource/exec/UpdateExecutor.java
@@ -27,121 +27,131 @@ import com.alibaba.fescar.rm.datasource.ParametersHolder;
 import com.alibaba.fescar.rm.datasource.StatementProxy;
 import com.alibaba.fescar.rm.datasource.sql.SQLRecognizer;
 import com.alibaba.fescar.rm.datasource.sql.SQLUpdateRecognizer;
+import com.alibaba.fescar.rm.datasource.sql.druid.MySQLUpdateRecognizer;
 import com.alibaba.fescar.rm.datasource.sql.struct.Field;
 import com.alibaba.fescar.rm.datasource.sql.struct.TableMeta;
 import com.alibaba.fescar.rm.datasource.sql.struct.TableRecords;
 
 public class UpdateExecutor<T, S extends Statement> extends AbstractDMLBaseExecutor<T, S> {
 
-    public UpdateExecutor(StatementProxy statementProxy, StatementCallback statementCallback, SQLRecognizer sqlRecognizer) {
-        super(statementProxy, statementCallback, sqlRecognizer);
-    }
+	public UpdateExecutor(StatementProxy statementProxy, StatementCallback statementCallback,
+			SQLRecognizer sqlRecognizer) {
+		super(statementProxy, statementCallback, sqlRecognizer);
+	}
 
-    @Override
-    protected TableRecords beforeImage() throws SQLException {
-        SQLUpdateRecognizer visitor = (SQLUpdateRecognizer) sqlRecognizer;
-        TableMeta tmeta = getTableMeta();
-        List<String> updateColumns = visitor.getUpdateColumns();
+	@Override
+	protected TableRecords beforeImage() throws SQLException {
+		SQLUpdateRecognizer visitor = (SQLUpdateRecognizer) sqlRecognizer;
+		TableMeta tmeta = getTableMeta();
+		List<String> updateColumns = visitor.getUpdateColumns();
 
-        StringBuffer selectSQLAppender = new StringBuffer("SELECT ");
-        if (!tmeta.containsPK(updateColumns)) {
-            // PK should be included.
-            selectSQLAppender.append(tmeta.getPkName() + ", ");
-        }
-        for (int i = 0; i < updateColumns.size(); i++) {
-            selectSQLAppender.append(updateColumns.get(i));
-            if (i < (updateColumns.size() - 1)) {
-                selectSQLAppender.append(", ");
-            }
-        }
-        String whereCondition = null;
-        ArrayList<Object> paramAppender = new ArrayList<>();
-        if (statementProxy instanceof ParametersHolder) {
-            whereCondition = visitor.getWhereCondition((ParametersHolder) statementProxy, paramAppender);
-        } else {
-            whereCondition = visitor.getWhereCondition();
-        }
-        selectSQLAppender.append(" FROM " + tmeta.getTableName() + " WHERE " + whereCondition + " FOR UPDATE");
-        String selectSQL = selectSQLAppender.toString();
+		StringBuffer selectSQLAppender = new StringBuffer("SELECT ");
+		if (!tmeta.containsPK(updateColumns)) {
+			// PK should be included.
+			selectSQLAppender.append(tmeta.getPkName() + ", ");
+		}
+		for (int i = 0; i < updateColumns.size(); i++) {
+			selectSQLAppender.append(updateColumns.get(i));
+			if (i < (updateColumns.size() - 1)) {
+				selectSQLAppender.append(", ");
+			}
+		}
+		String whereCondition = null;
+		ArrayList<Object> paramAppender = new ArrayList<>();
+		if (statementProxy instanceof ParametersHolder) {
+			whereCondition = visitor.getWhereCondition((ParametersHolder) statementProxy, paramAppender);
+		} else {
+			whereCondition = visitor.getWhereCondition();
+		}
 
-        TableRecords beforeImage = null;
-        PreparedStatement ps = null;
-        Statement st = null;
-        ResultSet rs = null;
-        try {
-            if (paramAppender.isEmpty()) {
-                st = statementProxy.getConnection().createStatement();
-                rs = st.executeQuery(selectSQL);
-            } else {
-                ps = statementProxy.getConnection().prepareStatement(selectSQL);
-                for (int i = 0; i< paramAppender.size(); i++) {
-                    ps.setObject(i + 1, paramAppender.get(i));
-                }
-                rs = ps.executeQuery();
-            }
-            beforeImage = TableRecords.buildRecords(tmeta, rs);
+		if (sqlRecognizer instanceof MySQLUpdateRecognizer) {
+			MySQLUpdateRecognizer mySQLUpdateRecognizer = (MySQLUpdateRecognizer) sqlRecognizer;
+			selectSQLAppender.append(" FROM " + tmeta.getTableName() + " " + mySQLUpdateRecognizer.getTableAlias()
+					+ " WHERE " + whereCondition + " FOR UPDATE");
+		} else {
+			selectSQLAppender.append(" FROM " + tmeta.getTableName() + " WHERE " + whereCondition + " FOR UPDATE");
+		}
 
-        } finally {
-            if (rs != null) {
-                rs.close();
-            }
-            if (st != null) {
-                st.close();
-            }
-            if (ps != null) {
-                ps.close();
-            }
-        }
-        return beforeImage;
-    }
+		String selectSQL = selectSQLAppender.toString();
 
-    @Override
-    protected TableRecords afterImage(TableRecords beforeImage) throws SQLException {
-        SQLUpdateRecognizer visitor = (SQLUpdateRecognizer) sqlRecognizer;
-        TableMeta tmeta = getTableMeta();
-        if (beforeImage == null || beforeImage.size() == 0) {
-            return TableRecords.empty(getTableMeta());
-        }
-        List<String> updateColumns = visitor.getUpdateColumns();
+		TableRecords beforeImage = null;
+		PreparedStatement ps = null;
+		Statement st = null;
+		ResultSet rs = null;
+		try {
+			if (paramAppender.isEmpty()) {
+				st = statementProxy.getConnection().createStatement();
+				rs = st.executeQuery(selectSQL);
+			} else {
+				ps = statementProxy.getConnection().prepareStatement(selectSQL);
+				for (int i = 0; i < paramAppender.size(); i++) {
+					ps.setObject(i + 1, paramAppender.get(i));
+				}
+				rs = ps.executeQuery();
+			}
+			beforeImage = TableRecords.buildRecords(tmeta, rs);
 
-        StringBuffer selectSQLAppender = new StringBuffer("SELECT ");
-        if (!tmeta.containsPK(updateColumns)) {
-            // PK should be included.
-            selectSQLAppender.append(tmeta.getPkName() + ", ");
-        }
-        for (int i = 0; i < updateColumns.size(); i++) {
-            selectSQLAppender.append(updateColumns.get(i));
-            if (i < (updateColumns.size() - 1)) {
-                selectSQLAppender.append(", ");
-            }
-        }
-        List<Field> pkRows = beforeImage.pkRows();
-        selectSQLAppender.append(" FROM " + tmeta.getTableName() + " WHERE " + buildWhereConditionByPKs(pkRows) + " FOR UPDATE");
-        String selectSQL = selectSQLAppender.toString();
+		} finally {
+			if (rs != null) {
+				rs.close();
+			}
+			if (st != null) {
+				st.close();
+			}
+			if (ps != null) {
+				ps.close();
+			}
+		}
+		return beforeImage;
+	}
 
-        TableRecords afterImage = null;
-        PreparedStatement pst = null;
-        ResultSet rs = null;
-        try {
-            pst = statementProxy.getConnection().prepareStatement(selectSQL);
-            int index = 0;
-            for (Field pkField : pkRows) {
-                index++;
-                pst.setObject(index, pkField.getValue(), pkField.getType());
-            }
-            rs = pst.executeQuery();
-            afterImage = TableRecords.buildRecords(tmeta, rs);
+	@Override
+	protected TableRecords afterImage(TableRecords beforeImage) throws SQLException {
+		SQLUpdateRecognizer visitor = (SQLUpdateRecognizer) sqlRecognizer;
+		TableMeta tmeta = getTableMeta();
+		if (beforeImage == null || beforeImage.size() == 0) {
+			return TableRecords.empty(getTableMeta());
+		}
+		List<String> updateColumns = visitor.getUpdateColumns();
 
-        } finally {
-            if (rs != null) {
-                rs.close();
-            }
-            if (pst != null) {
-                pst.close();
-            }
-        }
-        return afterImage;
-    }
+		StringBuffer selectSQLAppender = new StringBuffer("SELECT ");
+		if (!tmeta.containsPK(updateColumns)) {
+			// PK should be included.
+			selectSQLAppender.append(tmeta.getPkName() + ", ");
+		}
+		for (int i = 0; i < updateColumns.size(); i++) {
+			selectSQLAppender.append(updateColumns.get(i));
+			if (i < (updateColumns.size() - 1)) {
+				selectSQLAppender.append(", ");
+			}
+		}
+		List<Field> pkRows = beforeImage.pkRows();
+		selectSQLAppender
+				.append(" FROM " + tmeta.getTableName() + " WHERE " + buildWhereConditionByPKs(pkRows) + " FOR UPDATE");
+		String selectSQL = selectSQLAppender.toString();
 
+		TableRecords afterImage = null;
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+		try {
+			pst = statementProxy.getConnection().prepareStatement(selectSQL);
+			int index = 0;
+			for (Field pkField : pkRows) {
+				index++;
+				pst.setObject(index, pkField.getValue(), pkField.getType());
+			}
+			rs = pst.executeQuery();
+			afterImage = TableRecords.buildRecords(tmeta, rs);
+
+		} finally {
+			if (rs != null) {
+				rs.close();
+			}
+			if (pst != null) {
+				pst.close();
+			}
+		}
+		return afterImage;
+	}
 
 }
