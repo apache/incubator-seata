579c7e5a7313c699cf47182ee414dffa3bd75eb1
sherlock-hao <153004802@qq.com>
2019-01-23 11:56:26 +0800

    Update SelectForUpdateExecutor.java

--- rm-datasource/src/main/java/com/alibaba/fescar/rm/datasource/exec/SelectForUpdateExecutor.java
+++ rm-datasource/src/main/java/com/alibaba/fescar/rm/datasource/exec/SelectForUpdateExecutor.java
@@ -28,93 +28,100 @@ import com.alibaba.fescar.rm.datasource.ParametersHolder;
 import com.alibaba.fescar.rm.datasource.StatementProxy;
 import com.alibaba.fescar.rm.datasource.sql.SQLRecognizer;
 import com.alibaba.fescar.rm.datasource.sql.SQLSelectRecognizer;
+import com.alibaba.fescar.rm.datasource.sql.druid.MySQLSelectForUpdateRecognizer;
 import com.alibaba.fescar.rm.datasource.sql.struct.TableRecords;
 
 public class SelectForUpdateExecutor<S extends Statement> extends BaseTransactionalExecutor<ResultSet, S> {
 
-    public SelectForUpdateExecutor(StatementProxy<S> statementProxy, StatementCallback<ResultSet, S> statementCallback, SQLRecognizer sqlRecognizer) {
-        super(statementProxy, statementCallback, sqlRecognizer);
-    }
+	public SelectForUpdateExecutor(StatementProxy<S> statementProxy, StatementCallback<ResultSet, S> statementCallback,
+			SQLRecognizer sqlRecognizer) {
+		super(statementProxy, statementCallback, sqlRecognizer);
+	}
 
-    @Override
-    public Object doExecute(Object... args) throws Throwable {
-        SQLSelectRecognizer recognizer = (SQLSelectRecognizer) sqlRecognizer;
+	@Override
+	public Object doExecute(Object... args) throws Throwable {
+		SQLSelectRecognizer recognizer = (SQLSelectRecognizer) sqlRecognizer;
 
-        Connection conn = statementProxy.getConnection();
-        ResultSet rs = null;
-        Savepoint sp = null;
-        LockRetryController lockRetryController = new LockRetryController();
-        boolean originalAutoCommit = conn.getAutoCommit();
+		Connection conn = statementProxy.getConnection();
+		ResultSet rs = null;
+		Savepoint sp = null;
+		LockRetryController lockRetryController = new LockRetryController();
+		boolean originalAutoCommit = conn.getAutoCommit();
 
-        StringBuffer selectSQLAppender = new StringBuffer("SELECT ");
-        selectSQLAppender.append(getTableMeta().getPkName());
-        selectSQLAppender.append(" FROM " + getTableMeta().getTableName());
-        String whereCondition = null;
-        ArrayList<Object> paramAppender = new ArrayList<>();
-        if (statementProxy instanceof ParametersHolder) {
-            whereCondition = recognizer.getWhereCondition((ParametersHolder) statementProxy, paramAppender);
-        } else {
-            whereCondition = recognizer.getWhereCondition();
-        }
-        if (!StringUtils.isEmpty(whereCondition)) {
-            selectSQLAppender.append(" WHERE " + whereCondition);
-        }
-        selectSQLAppender.append(" FOR UPDATE");
-        String selectPKSQL = selectSQLAppender.toString();
+		StringBuffer selectSQLAppender = new StringBuffer("SELECT ");
+		selectSQLAppender.append(getTableMeta().getPkName());
+		selectSQLAppender.append(" FROM " + getTableMeta().getTableName());
 
-        try {
-            if (originalAutoCommit) {
-                conn.setAutoCommit(false);
-            }
-            sp = conn.setSavepoint();
-            rs = statementCallback.execute(statementProxy.getTargetStatement(), args);
+		if (sqlRecognizer instanceof MySQLSelectForUpdateRecognizer) {
+			MySQLSelectForUpdateRecognizer mySQLSelectForUpdateRecognizer = (MySQLSelectForUpdateRecognizer) sqlRecognizer;
+			selectSQLAppender.append(mySQLSelectForUpdateRecognizer.getTableAlias() + " ");
+		}
+		String whereCondition = null;
+		ArrayList<Object> paramAppender = new ArrayList<>();
+		if (statementProxy instanceof ParametersHolder) {
+			whereCondition = recognizer.getWhereCondition((ParametersHolder) statementProxy, paramAppender);
+		} else {
+			whereCondition = recognizer.getWhereCondition();
+		}
+		if (!StringUtils.isEmpty(whereCondition)) {
+			selectSQLAppender.append(" WHERE " + whereCondition);
+		}
+		selectSQLAppender.append(" FOR UPDATE");
+		String selectPKSQL = selectSQLAppender.toString();
 
-            while (true) {
-                // Try to get global lock of those rows selected
-                Statement stPK = null;
-                PreparedStatement pstPK = null;
-                ResultSet rsPK = null;
-                try {
-                    if (paramAppender.isEmpty()) {
-                        stPK = statementProxy.getConnection().createStatement();
-                        rsPK = stPK.executeQuery(selectPKSQL);
-                    } else {
-                        pstPK = statementProxy.getConnection().prepareStatement(selectPKSQL);
-                        for (int i = 0; i < paramAppender.size(); i++) {
-                            pstPK.setObject(i + 1, paramAppender.get(i));
-                        }
-                        rsPK = pstPK.executeQuery();
-                    }
+		try {
+			if (originalAutoCommit) {
+				conn.setAutoCommit(false);
+			}
+			sp = conn.setSavepoint();
+			rs = statementCallback.execute(statementProxy.getTargetStatement(), args);
 
-                    TableRecords selectPKRows = TableRecords.buildRecords(getTableMeta(), rsPK);
-                    statementProxy.getConnectionProxy().checkLock(selectPKRows);
-                    break;
+			while (true) {
+				// Try to get global lock of those rows selected
+				Statement stPK = null;
+				PreparedStatement pstPK = null;
+				ResultSet rsPK = null;
+				try {
+					if (paramAppender.isEmpty()) {
+						stPK = statementProxy.getConnection().createStatement();
+						rsPK = stPK.executeQuery(selectPKSQL);
+					} else {
+						pstPK = statementProxy.getConnection().prepareStatement(selectPKSQL);
+						for (int i = 0; i < paramAppender.size(); i++) {
+							pstPK.setObject(i + 1, paramAppender.get(i));
+						}
+						rsPK = pstPK.executeQuery();
+					}
 
-                } catch (LockConflictException lce) {
-                    conn.rollback(sp);
-                    lockRetryController.sleep(lce);
+					TableRecords selectPKRows = TableRecords.buildRecords(getTableMeta(), rsPK);
+					statementProxy.getConnectionProxy().checkLock(selectPKRows);
+					break;
 
-                } finally {
-                    if (rsPK != null) {
-                        rsPK.close();
-                    }
-                    if (stPK != null) {
-                        stPK.close();
-                    }
-                    if (pstPK != null) {
-                        pstPK.close();
-                    }
-                }
-            }
+				} catch (LockConflictException lce) {
+					conn.rollback(sp);
+					lockRetryController.sleep(lce);
 
-        } finally {
-            if (sp != null) {
-                conn.releaseSavepoint(sp);
-            }
-            if (originalAutoCommit) {
-                conn.setAutoCommit(true);
-            }
-        }
-        return rs;
-    }
+				} finally {
+					if (rsPK != null) {
+						rsPK.close();
+					}
+					if (stPK != null) {
+						stPK.close();
+					}
+					if (pstPK != null) {
+						pstPK.close();
+					}
+				}
+			}
+
+		} finally {
+			if (sp != null) {
+				conn.releaseSavepoint(sp);
+			}
+			if (originalAutoCommit) {
+				conn.setAutoCommit(true);
+			}
+		}
+		return rs;
+	}
 }
