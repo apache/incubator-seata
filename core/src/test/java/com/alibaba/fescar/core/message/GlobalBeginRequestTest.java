edad0d080519c10ad827c37b7d0febf63e1393c9
xiajunhust <xiajun.0706@163.com>
2019-01-25 13:41:43 +0800

    add unit test to core module (#276)
    * release 0.1.1 (#178)
    
    * Remove redundant semicolons and modify non-standard names
    
    * remove duplicate RM init
    
    * issue #86 length of applicationData should be int
    
    * fix: fixed qualifier
    
    * Modify method description of DemoCode
    
    * Update ConfigurationKeys.java
    
    * config instance obtained by the factory class
    
    * fix typo of `retryable` as the annotation of Spring Retryable. add static code to init BranchStatus mapper to speed up get(int ordinal)
    
    * add template
    
    * add template
    
    * add template
    
    * fixes #117
    
    * fixes #114
    
    * fix issue #122
    
    * update template
    
    * fix file.RenameTo->Files.move
    
    * #73 add travis config
    
    * fix #121
    
    * #73 add travis config
    
    * fix #135 delete BranchSession main
    
    * fix #137 param position err
    
    * fix #142 delete defaultEventExecutorGroup
    
    * fix #142 delete defaultEventExecutorGroup (#144)
    
    * fix #139 netty heartbeat configurable
    
    * fixes #139 heartbeat configurable (#146)
    
    * fix #142 delete defaultEventExecutorGroup
    
    * fix #139 netty heartbeat configurable
    
    * FileTransactionStoreManager#closeFile() method remove redundant fileChannel.close() invoke (#140)
    
    * fix: When local branch change nothing, the RC will rise an NullpointerException (#155)
    
    * bugfix: when no record changed register localBranch will occur exception
    in TC
    
    * clean the environment
    
    * enhancement: when no changes in local branch let RC not throw an
    NullpointerException
    
    * change tabs to spaces
    
    * fix #150 use ServerBootstrap#childOption() to set SO_KEEPALIVE (#151)
    
    * FileTransactionStoreManager#closeFile() method remove redundant fileChannel.close() invoke
    
    * Netty SO_KEEPALIVE option not works on ServerBootstrap#option(), should replace with ServerBootstrap#childOption()
    
    * fix #149 (long)->Number.longValue
    
    *  fix #149 (long)->Number.longValue (#156)
    
    * fix #142 delete defaultEventExecutorGroup
    
    * fix #139 netty heartbeat configurable
    
    * fix #149 (long)->Number.longValue
    
    * remove mistake twitter icon (#165)
    
    * Update README.md
    
    * Update README.md
    
    * issue #110 bug fix for RM channel management (#169)
    
    * issue #110 fix RM channel management
    
    * fix merge mistake
    
    * issue #110 enhance: if no channel found on my application set, try other application on the same resource.
    
    * bug fix and enhance
    
    * issue #110 enhance the original fix
    
    * fix: Unify `undo_log` table name variable (#174)
    
    * ShowSql might be better when debugging to execute the demo to observe the data. (#164)
    
    issues #158
    
    * revert OrderServiceImpl
    
    * add toString method
    
    * add toString method
    
    * add toString method
    
    * add toString method
    
    * fix
    
    * 小优化-类型转换
    
    * 类型转换小优化
    
    * add unit test to fescar/core/message package
    
    * merge conflichts

---
+++ core/src/test/java/com/alibaba/fescar/core/message/GlobalBeginRequestTest.java
@@ -0,0 +1,69 @@
+/*
+ *  Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.alibaba.fescar.core.message;
+
+import com.alibaba.fescar.core.protocol.transaction.GlobalBeginRequest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+
+/**
+ * @author xiajun.0706@163.com
+ * @since 2019/1/24
+ */
+public class GlobalBeginRequestTest {
+
+  @Test
+  public void testToString() throws Exception{
+    GlobalBeginRequest globalBeginRequest = new GlobalBeginRequest();
+    globalBeginRequest.setTransactionName("tran 1");
+    System.out.println(globalBeginRequest.toString());
+
+    Assert.assertEquals("timeout=60000,transactionName=tran 1", globalBeginRequest.toString());
+  }
+
+  @Test
+  public void testEncode() throws Exception{
+    GlobalBeginRequest globalBeginRequest = new GlobalBeginRequest();
+    globalBeginRequest.setTransactionName("tran 1");
+
+    byte[] encodeResult = globalBeginRequest.encode();
+    String encodeResultStr = Arrays.toString(encodeResult);
+
+    Assert.assertEquals("[0, 0, -22, 96, 0, 6, 116, 114, 97, 110, 32, 49]", encodeResultStr);
+  }
+
+  @Test
+  public void testDecode() throws Exception{
+    GlobalBeginRequest globalBeginRequest = new GlobalBeginRequest();
+    globalBeginRequest.setTransactionName("tran 1");
+
+    byte[] encodeResult = globalBeginRequest.encode();
+
+    ByteBuffer byteBuffer = ByteBuffer.allocate(encodeResult.length);
+    byteBuffer.put(encodeResult);
+    byteBuffer.flip();
+
+    GlobalBeginRequest decodeGlobalBeginRequest = new GlobalBeginRequest();
+    decodeGlobalBeginRequest.decode(byteBuffer);
+    System.out.println(decodeGlobalBeginRequest);
+    Assert.assertEquals(globalBeginRequest.getTimeout(), decodeGlobalBeginRequest.getTimeout());
+    Assert.assertEquals(globalBeginRequest.getTransactionName(), decodeGlobalBeginRequest.getTransactionName());
+  }
+}
