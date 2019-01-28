c21d4c05c6ae474b918dccf03a61f7a0f9c305c8
tianjunwei <tianjunwei@mi-ya.com.cn>
2019-01-28 11:30:12 +0800

    dubbo-storage-service.xml中注入GlobalTransactionScanner已经初始化RMClientAT.init，初始化了两个AsyncWork线程

--- examples/src/main/java/com/alibaba/fescar/tm/dubbo/impl/StorageServiceImpl.java
+++ examples/src/main/java/com/alibaba/fescar/tm/dubbo/impl/StorageServiceImpl.java
@@ -52,11 +52,6 @@ public class StorageServiceImpl implements StorageService {
 
     public static void main(String[] args) throws Throwable {
 
-        String applicationId = "dubbo-demo-storage-service";
-        String txServiceGroup = "my_test_tx_group";
-
-        RMClientAT.init(applicationId, txServiceGroup);
-
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"dubbo-storage-service.xml"});
         context.getBean("service");
         JdbcTemplate jdbcTemplate = (JdbcTemplate) context.getBean("jdbcTemplate");
