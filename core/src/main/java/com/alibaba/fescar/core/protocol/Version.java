958084b50e0f3f2a070152e1236a84fb55fe7800
slievrly <slievrly@163.com>
2019-03-04 13:29:50 +0800

    fix #500 change version to 0.2.4-SNAPSHOT
    Signed-off-by: slievrly <slievrly@163.com>

--- core/src/main/java/com/alibaba/fescar/core/protocol/Version.java
+++ core/src/main/java/com/alibaba/fescar/core/protocol/Version.java
@@ -31,7 +31,7 @@ public class Version {
     /**
      * The constant CURRENT.
      */
-    public static final String CURRENT = "0.2.3";
+    public static final String CURRENT = "0.2.4";
 
     /**
      * The constant VERSION_MAP.
