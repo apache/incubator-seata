78523b34d91973062d368879d0f1cd8fd3273f07
zhaoyunxiang <hdzee19@gmail.com>
2019-03-04 22:29:48 +0800

    fix copyright and getBytes with charset utf-8

--- common/src/test/java/com/alibaba/fescar/common/util/BlobUtilsTest.java
+++ common/src/test/java/com/alibaba/fescar/common/util/BlobUtilsTest.java
@@ -1,27 +1,24 @@
 /*
+ *  Copyright 1999-2018 Alibaba Group Holding Ltd.
  *
- *  *  Copyright 1999-2018 Alibaba Group Holding Ltd.
- *  *
- *  *  Licensed under the Apache License, Version 2.0 (the "License");
- *  *  you may not use this file except in compliance with the License.
- *  *  You may obtain a copy of the License at
- *  *
- *  *       http://www.apache.org/licenses/LICENSE-2.0
- *  *
- *  *  Unless required by applicable law or agreed to in writing, software
- *  *  distributed under the License is distributed on an "AS IS" BASIS,
- *  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  *  See the License for the specific language governing permissions and
- *  *  limitations under the License.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
-
 package com.alibaba.fescar.common.util;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.InputStream;
+import java.nio.charset.Charset;
 import java.sql.SQLException;
 import javax.sql.rowset.serial.SerialBlob;
 import org.junit.Test;
@@ -34,12 +31,12 @@ public class BlobUtilsTest {
 
     @Test
     public void testString2blob() throws SQLException {
-        assertThat(BlobUtils.string2blob("123abca")).isEqualTo(new SerialBlob("123abca".getBytes()));
+        assertThat(BlobUtils.string2blob("123abc")).isEqualTo(new SerialBlob("123abc".getBytes(Charset.forName("UTF-8"))));
     }
 
     @Test
     public void testBlob2string() throws SQLException {
-        assertThat(BlobUtils.blob2string(new SerialBlob("123abckdle".getBytes()))).isEqualTo("123abckdle");
+        assertThat(BlobUtils.blob2string(new SerialBlob("123absent".getBytes(Charset.forName("UTF-8"))))).isEqualTo("123absent");
     }
 
     @Test
--- common/src/test/java/com/alibaba/fescar/common/util/NetUtilTest.java
+++ common/src/test/java/com/alibaba/fescar/common/util/NetUtilTest.java
@@ -1,28 +1,25 @@
 /*
+ *  Copyright 1999-2018 Alibaba Group Holding Ltd.
  *
- *  *  Copyright 1999-2018 Alibaba Group Holding Ltd.
- *  *
- *  *  Licensed under the Apache License, Version 2.0 (the "License");
- *  *  you may not use this file except in compliance with the License.
- *  *  You may obtain a copy of the License at
- *  *
- *  *       http://www.apache.org/licenses/LICENSE-2.0
- *  *
- *  *  Unless required by applicable law or agreed to in writing, software
- *  *  distributed under the License is distributed on an "AS IS" BASIS,
- *  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  *  See the License for the specific language governing permissions and
- *  *  limitations under the License.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
-
 package com.alibaba.fescar.common.util;
 
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.net.Inet4Address;
+import java.net.Inet6Address;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.net.UnknownHostException;
@@ -35,7 +32,7 @@ import org.junit.Test;
 public class NetUtilTest {
 
     private InetSocketAddress ipv4 = new InetSocketAddress(Inet4Address.getLocalHost().getHostName(), 3902);
-    private InetSocketAddress ipv6 = new InetSocketAddress(Inet4Address.getLocalHost().getHostName(), 3904);
+    private InetSocketAddress ipv6 = new InetSocketAddress(Inet6Address.getLocalHost().getHostName(), 3904);
 
     public NetUtilTest() throws UnknownHostException {
     }
