4130180d54bf72458a8488a0538b2a47b86cd5be
wu <xingfudeshi@vip.qq.com>
2019-03-06 13:05:28 +0800

    fixes test case.

--- server/src/test/java/com/alibaba/fescar/server/session/SessionHolderTest.java
+++ server/src/test/java/com/alibaba/fescar/server/session/SessionHolderTest.java
@@ -1,9 +1,14 @@
 package com.alibaba.fescar.server.session;
 
 
+import org.apache.commons.io.FileUtils;
 import org.testng.annotations.Test;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 /**
  * @author Wu
  * @date 2019/3/6
@@ -14,6 +19,11 @@ public class SessionHolderTest {
 
     @Test
     public void testInit() throws IOException {
-        SessionHolder.init("D:\\sessionStore");
+        String sessionStorePath=System.getProperty("user.dir")+ File.separator +"sessionStore";
+        File file=new File(sessionStorePath);
+        if(!file.exists()&&!file.isDirectory()){
+            file.mkdirs();
+        }
+        SessionHolder.init(sessionStorePath);
     }
 }
