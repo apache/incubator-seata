8ed020989d61e47b76445c844aecbf8a955cc5cc
isudox <isudox@gmail.com>
2019-03-21 20:19:20 +0800

    Refactor some functions and move duplicate code to abstract classes. (#487)

--- core/src/main/java/com/alibaba/fescar/core/rpc/netty/AbstractRpcRemotingClient.java
+++ core/src/main/java/com/alibaba/fescar/core/rpc/netty/AbstractRpcRemotingClient.java
@@ -252,7 +252,7 @@ public abstract class AbstractRpcRemotingClient extends AbstractRpcRemoting
             }
             super.destroy();
         } catch (Exception exx) {
-            LOGGER.error("shutdown error:" + exx.getMessage());
+            LOGGER.error("Failed to shutdown: {}", exx.getMessage());
         }
     }
 
@@ -262,7 +262,7 @@ public abstract class AbstractRpcRemotingClient extends AbstractRpcRemoting
             RpcMessage rpcMessage = (RpcMessage)msg;
             if (rpcMessage.getBody() == HeartbeatMessage.PONG) {
                 if (LOGGER.isDebugEnabled()) {
-                    LOGGER.debug("received PONG from " + ctx.channel().remoteAddress());
+                    LOGGER.debug("received PONG from {}", ctx.channel().remoteAddress());
                 }
                 return;
             }
@@ -277,7 +277,7 @@ public abstract class AbstractRpcRemotingClient extends AbstractRpcRemoting
                 MessageFuture future = futures.remove(msgId);
                 if (future == null) {
                     if (LOGGER.isInfoEnabled()) {
-                        LOGGER.info("msg:" + msgId + " is not found in futures.");
+                        LOGGER.info("msg: {} is not found in futures.", msgId);
                     }
                 } else {
                     future.setResultMessage(results.getMsgs()[i]);
@@ -286,6 +286,18 @@ public abstract class AbstractRpcRemotingClient extends AbstractRpcRemoting
             return;
         }
         super.channelRead(ctx, msg);
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        if (messageExecutor.isShutdown()) {
+            return;
+        }
+        if (LOGGER.isInfoEnabled()) {
+            LOGGER.info("channel inactive: {}", ctx.channel());
+        }
+        releaseChannel(ctx.channel(), NetUtil.toStringAddress(ctx.channel().remoteAddress()));
+        super.channelInactive(ctx);
     }
 
     /**
@@ -311,6 +323,27 @@ public abstract class AbstractRpcRemotingClient extends AbstractRpcRemoting
         if (clientMessageListener != null) {
             String remoteAddress = NetUtil.toStringAddress(ctx.channel().remoteAddress());
             clientMessageListener.onMessage(msgId, remoteAddress, msg, this);
+        }
+    }
+
+    protected void reconnect(String transactionServiceGroup) {
+        List<String> availList = null;
+        try {
+            availList = getAvailServerList(transactionServiceGroup);
+        } catch (Exception exx) {
+            LOGGER.error("Failed to get available servers: {}" + exx.getMessage());
+        }
+        if (CollectionUtils.isEmpty(availList)) {
+            LOGGER.error("no available server to connect.");
+            return;
+        }
+        for (String serverAddress : availList) {
+            try {
+                connect(serverAddress);
+            } catch (Exception e) {
+                LOGGER.error(FrameworkErrorCode.NetConnect.errCode,
+                        "can not connect to " + serverAddress + " cause:" + e.getMessage(), e);
+            }
         }
     }
 
@@ -350,6 +383,14 @@ public abstract class AbstractRpcRemotingClient extends AbstractRpcRemoting
      * @return the channel
      */
     protected abstract Channel connect(String serverAddress);
+
+    /**
+     * Release channel.
+     *
+     * @param channel       the channel
+     * @param serverAddress the server address
+     */
+    protected abstract void releaseChannel(Channel channel, String serverAddress);
 
     /**
      * Gets netty pool config.
--- core/src/main/java/com/alibaba/fescar/core/rpc/netty/TmRpcClient.java
+++ core/src/main/java/com/alibaba/fescar/core/rpc/netty/TmRpcClient.java
@@ -162,45 +162,12 @@ public final class TmRpcClient extends AbstractRpcRemotingClient {
             @Override
             public void run() {
                 try {
-                    reconnect();
+                    reconnect(transactionServiceGroup);
                 } catch (Exception ignore) {
                     LOGGER.error(ignore.getMessage());
                 }
             }
         }, healthCheckDelay, healthCheckPeriod, TimeUnit.SECONDS);
-    }
-
-    private void reconnect() {
-        List<String> availList = null;
-        try {
-            availList = getAvailServerList(transactionServiceGroup);
-        } catch (Exception exx) {
-            LOGGER.error(exx.getMessage());
-        }
-        if (CollectionUtils.isEmpty(availList)) {
-            LOGGER.error("no available server to connect.");
-            return;
-        }
-        for (String serverAddress : availList) {
-            try {
-                connect(serverAddress);
-            } catch (Exception e) {
-                LOGGER.error(FrameworkErrorCode.NetConnect.errCode,
-                    "can not connect to " + serverAddress + " cause:" + e.getMessage(), e);
-            }
-        }
-    }
-
-    @Override
-    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
-        if (messageExecutor.isShutdown()) {
-            return;
-        }
-        if (LOGGER.isInfoEnabled()) {
-            LOGGER.info("channel inactive:" + ctx.channel());
-        }
-        releaseChannel(ctx.channel(), NetUtil.toStringAddress(ctx.channel().remoteAddress()));
-        super.channelInactive(ctx);
     }
 
     @Override
@@ -210,7 +177,7 @@ public final class TmRpcClient extends AbstractRpcRemotingClient {
         Channel acquireChannel = connect(validAddress);
         Object result = super.sendAsyncRequestWithResponse(validAddress, acquireChannel, msg, timeout);
         if (result instanceof GlobalBeginResponse
-            && ((GlobalBeginResponse)result).getResultCode() == ResultCode.Failed) {
+                && ((GlobalBeginResponse) result).getResultCode() == ResultCode.Failed) {
             LOGGER.error("begin response error,release channel:" + acquireChannel);
             releaseChannel(acquireChannel, validAddress);
         }
@@ -273,14 +240,11 @@ public final class TmRpcClient extends AbstractRpcRemotingClient {
 
     }
 
-    /**
-     * Release channel.
-     *
-     * @param channel       the channel
-     * @param serverAddress the server address
-     */
-    public void releaseChannel(Channel channel, String serverAddress) {
-        if (null == channel || null == serverAddress) { return; }
+    @Override
+    protected void releaseChannel(Channel channel, String serverAddress) {
+        if (null == channel || null == serverAddress) {
+            return;
+        }
         try {
             Object connectLock = channelLocks.get(serverAddress);
             synchronized (connectLock) {
@@ -303,12 +267,6 @@ public final class TmRpcClient extends AbstractRpcRemotingClient {
         }
     }
 
-    /**
-     * Connect channel.
-     *
-     * @param serverAddress the server address
-     * @return the channel
-     */
     @Override
     protected Channel connect(String serverAddress) {
         Channel channelToServer = channels.get(serverAddress);
