a181c9a1dda3a9684b3c4e963aae240954050f62
李哲 <lizhe11@58ganji.com>
2019-04-02 16:59:30 +0800

    change pom

---
+++ fescar-scf/src/main/java/com/bj58/dao/proxy/DataSourceInit.java
@@ -0,0 +1,8 @@
+package com.bj58.dao.proxy;
+
+/**
+ * @author lizhe11 on 2019/4/1 14:24
+ * @version $Id: DataSourceInit.java, v 0.1 2019/4/1 14:24 lizhe11 Exp $
+ */
+public class DataSourceInit {
+}
