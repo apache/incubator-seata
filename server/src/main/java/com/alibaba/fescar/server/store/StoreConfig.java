35bd8b427816f2f7d12a29bb1afad1c7fbc70e06
lz <563868273@qq.com>
2019-04-03 15:48:04 +0800

    add session size configuration (#713)
    * compress
    
    * add compress
    
    * add compress
    
    * add compress
    
    * fix:review

---
+++ server/src/main/java/com/alibaba/fescar/server/store/StoreConfig.java
@@ -0,0 +1,50 @@
+/*
+ *  Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.alibaba.fescar.server.store;
+
+import com.alibaba.fescar.config.Configuration;
+import com.alibaba.fescar.config.ConfigurationFactory;
+
+/**
+ * @author lizhao
+ */
+public class StoreConfig {
+
+    private static Configuration fileConfiguration = ConfigurationFactory.getInstance();
+
+    private static final String STORE_PREFIX = "store.";
+
+    // default 16kb
+    private static final int DEFAULT_MAX_BRANCH_SESSION_SIZE = 1024 * 16;
+
+    // default 512b
+    public static final int DEFAULT_MAX_GLOBAL_SESSION_SIZE = 512;
+
+    // default 16kb
+    public static final int DEFAULT_WRITE_BUFFER_SIZE = 1024 * 16;
+
+    public static int getMaxBranchSessionSize() {
+        return fileConfiguration.getInt(STORE_PREFIX + "max-branch-session-size", DEFAULT_MAX_BRANCH_SESSION_SIZE);
+    }
+
+    public static int getMaxGlobalSessionSize() {
+        return fileConfiguration.getInt(STORE_PREFIX + "max-global-session-size", DEFAULT_MAX_GLOBAL_SESSION_SIZE);
+    }
+
+    public static int getFileWriteBufferCacheSize() {
+        return fileConfiguration.getInt(STORE_PREFIX + "file-write-buffer-cache-size", DEFAULT_WRITE_BUFFER_SIZE);
+    }
+}
