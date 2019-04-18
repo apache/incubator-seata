6d46907012ee6dc19de7fa517ef3b0f692907010
yuanguoyao <heizi.ygy@raycloud.com>
2019-04-18 10:26:04 +0800

    format code and add test

--- tm/src/test/java/com/alibaba/fescar/tm/api/transaction/TransactionInfoTest.java
+++ tm/src/test/java/com/alibaba/fescar/tm/api/transaction/TransactionInfoTest.java
@@ -1,3 +1,18 @@
+/*
+ *  Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package com.alibaba.fescar.tm.api.transaction;
 
 import com.alibaba.fastjson.JSON;
@@ -10,8 +25,6 @@ import java.io.IOException;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
@@ -20,8 +33,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class TransactionInfoTest {
 
-    private static final String EXCEPTION_NAME = "java.lang.Exception";
-    private static final String IO_EXCEPTION_NAME = "IOException";
+    private static final String IO_EXCEPTION_SHORT_NAME = "IOException";
     private static final String NAME = "test";
     private static final int TIME_OUT = 30000;
 
@@ -32,16 +44,27 @@ public class TransactionInfoTest {
     @Test
     public void testFieldGetSetFromJson() {
         String fromJson = "{\n" +
-                "\t\"name\":\""+NAME+"\",\n" +
-                "\t\"timeOut\":"+TIME_OUT+"\n" +
+                "\t\"name\":\""+ NAME +"\",\n" +
+                "\t\"rollbackRules\":[{\n" +
+                "\t\t\"exceptionName\":\""+ Exception.class.getName() +"\"\n" +
+                "\t},{\n" +
+                "\t\t\"exceptionName\":\""+ IllegalArgumentException.class.getName() +"\"\n" +
+                "\t},{\n" +
+                "\t\t\"exceptionName\":\""+ IO_EXCEPTION_SHORT_NAME +"\"\n" +
+                "\t},{\n" +
+                "\t\t\"exceptionName\":\""+ NullPointerException.class.getName() +"\"\n" +
+                "\t}],\n" +
+                "\t\"timeOut\":30000\n" +
                 "}";
         TransactionInfo fromTxInfo = JSON.parseObject(fromJson, TransactionInfo.class);
         assertThat(fromTxInfo.getTimeOut()).isEqualTo(TIME_OUT);
         assertThat(fromTxInfo.getName()).isEqualTo(NAME);
+        assertThat(fromTxInfo.getRollbackRules()).isEqualTo(getRollbackRules());
 
         TransactionInfo toTxInfo = new TransactionInfo();
         toTxInfo.setTimeOut(TIME_OUT);
         toTxInfo.setName(NAME);
+        toTxInfo.setRollbackRules(getRollbackRules());
         String toJson = JSON.toJSONString(toTxInfo, true);
         assertThat(fromJson).isEqualTo(toJson);
     }
@@ -50,27 +73,31 @@ public class TransactionInfoTest {
     @Test
     public void testRollBackOn() {
         TransactionInfo txInfo = new TransactionInfo();
+
         //default true
-        assertTrue(txInfo.rollBackOn(new IllegalArgumentException()));
-        assertTrue(txInfo.rollBackOn(new Exception()));
-        assertTrue(txInfo.rollBackOn(new IOException()));
-        assertTrue(txInfo.rollBackOn(new NullPointerException()));
+        assertThat(txInfo.rollbackOn(new IllegalArgumentException())).isTrue();
+        assertThat(txInfo.rollbackOn(new Exception())).isTrue();
+        assertThat(txInfo.rollbackOn(new IOException())).isTrue();
+        assertThat(txInfo.rollbackOn(new NullPointerException())).isTrue();
 
-        Set<RollbackRule> sets = new LinkedHashSet<>();
-        sets.add(new RollbackRule(EXCEPTION_NAME));
-        sets.add(new RollbackRule(IllegalArgumentException.class));
-        sets.add(new NoRollbackRule(IO_EXCEPTION_NAME));
-        sets.add(new NoRollbackRule(NullPointerException.class));
-
-
+        Set<RollbackRule> sets = getRollbackRules();
         txInfo.setRollbackRules(sets);
 
-        assertTrue(txInfo.rollBackOn(new IllegalArgumentException()));
-        assertTrue(txInfo.rollBackOn(new Exception()));
-        assertFalse(txInfo.rollBackOn(new IOException()));
-        assertFalse(txInfo.rollBackOn(new NullPointerException()));
+        assertThat(txInfo.rollbackOn(new IllegalArgumentException())).isTrue();
+        assertThat(txInfo.rollbackOn(new Exception())).isTrue();
+        assertThat(txInfo.rollbackOn(new IOException())).isFalse();
+        assertThat(txInfo.rollbackOn(new NullPointerException())).isFalse();
 
         // not found return true
-        assertTrue(txInfo.rollBackOn(new RuntimeException()));
+        assertThat(txInfo.rollbackOn(new RuntimeException())).isTrue();
+    }
+
+    private Set<RollbackRule> getRollbackRules() {
+        Set<RollbackRule> sets = new LinkedHashSet<>();
+        sets.add(new RollbackRule(Exception.class.getName()));
+        sets.add(new RollbackRule(IllegalArgumentException.class));
+        sets.add(new NoRollbackRule(IO_EXCEPTION_SHORT_NAME));
+        sets.add(new NoRollbackRule(NullPointerException.class));
+        return sets;
     }
 }
