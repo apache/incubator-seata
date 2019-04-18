1b723b1412f46af32b83659e437120ce85daf03b
leizhiyuan <leizhiyuan@gmail.com>
2019-04-18 13:21:20 +0800

    use assertThat

--- core/src/test/java/io/seata/core/protocol/MergeResultMessageTest.java
+++ core/src/test/java/io/seata/core/protocol/MergeResultMessageTest.java
@@ -19,10 +19,9 @@ package io.seata.core.protocol;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
 import io.seata.core.protocol.transaction.GlobalBeginResponse;
-import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Arrays;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * The type MergeResultMessage test.
@@ -35,20 +34,16 @@ public class MergeResultMessageTest {
     public void getAndSetMsgs() {
         MergeResultMessage mergeResultMessage = new MergeResultMessage();
         final AbstractResultMessage[] msgs = new AbstractResultMessage[1];
-        final GlobalBeginResponse globalBeginResponse = new GlobalBeginResponse();
-        globalBeginResponse.setXid("xid");
-        globalBeginResponse.setExtraData("data");
-        globalBeginResponse.setMsg("success");
-        globalBeginResponse.setResultCode(ResultCode.Success);
+        final GlobalBeginResponse globalBeginResponse = buildGlobalBeginResponse();
         msgs[0] = globalBeginResponse;
         mergeResultMessage.setMsgs(msgs);
-        Assert.assertEquals(globalBeginResponse, mergeResultMessage.getMsgs()[0]);
+        assertThat(globalBeginResponse).isEqualTo(mergeResultMessage.getMsgs()[0]);
     }
 
     @Test
     public void getTypeCode() {
         MergeResultMessage mergeResultMessage = new MergeResultMessage();
-        Assert.assertEquals(AbstractMessage.TYPE_SEATA_MERGE_RESULT, mergeResultMessage.getTypeCode());
+        assertThat(AbstractMessage.TYPE_SEATA_MERGE_RESULT).isEqualTo(mergeResultMessage.getTypeCode());
     }
 
     @Test
@@ -56,36 +51,37 @@ public class MergeResultMessageTest {
         byte[] expect = new byte[]{0, 0, 0, 17, 0, 1, 0, 2, 1, 0, 0, 3, 120, 105, 100, 0, 4, 100, 97, 116, 97};
         MergeResultMessage mergeResultMessage = new MergeResultMessage();
         final AbstractResultMessage[] msgs = new AbstractResultMessage[1];
-        final GlobalBeginResponse globalBeginResponse = new GlobalBeginResponse();
-        globalBeginResponse.setXid("xid");
-        globalBeginResponse.setExtraData("data");
-        globalBeginResponse.setMsg("success");
-        globalBeginResponse.setResultCode(ResultCode.Success);
+        final GlobalBeginResponse globalBeginResponse = buildGlobalBeginResponse();
         msgs[0] = globalBeginResponse;
         mergeResultMessage.setMsgs(msgs);
         byte[] result = mergeResultMessage.encode();
-        Assert.assertEquals(Arrays.toString(expect), Arrays.toString(result));
+        assertThat(expect).isEqualTo(result);
     }
 
     @Test
     public void decode() {
 
         byte[] result = new byte[]{0, 0, 0, 17, 0, 1, 0, 2, 1, 0, 0, 3, 120, 105, 100, 0, 4, 100, 97, 116, 97};
+        final GlobalBeginResponse globalBeginResponse = buildGlobalBeginResponse();
+        ByteBuf buffer = ByteBufAllocator.DEFAULT.buffer();
+        MergeResultMessage decodeResult = new MergeResultMessage();
+        decodeResult.decode(buffer.writeBytes(result));
+        GlobalBeginResponse decodeGlobalBeginResponse = (GlobalBeginResponse) decodeResult.getMsgs()[0];
+
+        assertThat(globalBeginResponse.getExtraData()).isEqualTo(decodeGlobalBeginResponse.getExtraData());
+        assertThat(globalBeginResponse.getXid()).isEqualTo(decodeGlobalBeginResponse.getXid());
+        assertThat(globalBeginResponse.getResultCode()).isEqualTo(decodeGlobalBeginResponse.getResultCode());
+        //msg won't be coded
+        assertThat(decodeGlobalBeginResponse.getMsg()).isNull();
+    }
+
+    private GlobalBeginResponse buildGlobalBeginResponse() {
         final GlobalBeginResponse globalBeginResponse = new GlobalBeginResponse();
         globalBeginResponse.setXid("xid");
         globalBeginResponse.setExtraData("data");
         globalBeginResponse.setMsg("success");
         globalBeginResponse.setResultCode(ResultCode.Success);
-        ByteBuf buffer = ByteBufAllocator.DEFAULT.buffer();
-        MergeResultMessage decodeResult = new MergeResultMessage();
-        decodeResult.decode(buffer.writeBytes(result));
-        GlobalBeginResponse decodeGlobalBeginResponse = (GlobalBeginResponse) decodeResult.getMsgs()[0];
-        Assert.assertEquals(globalBeginResponse.getExtraData(), decodeGlobalBeginResponse.getExtraData());
-        Assert.assertEquals(globalBeginResponse.getXid(), decodeGlobalBeginResponse.getXid());
-        //msg won't be coded
-        Assert.assertNull(decodeGlobalBeginResponse.getMsg());
-        Assert.assertEquals(globalBeginResponse.getResultCode(), decodeGlobalBeginResponse.getResultCode());
-
+        return globalBeginResponse;
     }
 
 }
