6ca4177b0ff1479b8fbe57705822375d16e9b590
leizhiyuan <leizhiyuan@gmail.com>
2019-04-25 11:11:38 +0800

    add

--- core/src/main/java/io/seata/core/protocol/convertor/BranchRegisterRequestConvertor.java
+++ core/src/main/java/io/seata/core/protocol/convertor/BranchRegisterRequestConvertor.java
@@ -4,7 +4,12 @@
  */
 package io.seata.core.protocol.convertor;
 
+import io.seata.core.model.BranchType;
+import io.seata.core.protocol.protobuf.AbstractMessageProto;
+import io.seata.core.protocol.protobuf.AbstractTransactionRequestProto;
 import io.seata.core.protocol.protobuf.BranchRegisterRequestProto;
+import io.seata.core.protocol.protobuf.BranchTypeProto;
+import io.seata.core.protocol.protobuf.MessageTypeProto;
 import io.seata.core.protocol.transaction.BranchRegisterRequest;
 
 /**
@@ -14,11 +19,34 @@ import io.seata.core.protocol.transaction.BranchRegisterRequest;
 public class BranchRegisterRequestConvertor implements PbConvertor<BranchRegisterRequest, BranchRegisterRequestProto> {
     @Override
     public BranchRegisterRequestProto convert2Proto(BranchRegisterRequest branchRegisterRequest) {
-        return null;
+        final short typeCode = branchRegisterRequest.getTypeCode();
+
+        final AbstractMessageProto abstractMessage = AbstractMessageProto.newBuilder().setMessageType(
+            MessageTypeProto.forNumber(typeCode)).build();
+
+        final AbstractTransactionRequestProto abstractTransactionRequestProto = AbstractTransactionRequestProto
+            .newBuilder().setAbstractMessage(
+                abstractMessage).build();
+
+        BranchRegisterRequestProto result = BranchRegisterRequestProto.newBuilder()
+            .setAbstractTransactionRequest(abstractTransactionRequestProto)
+            .setApplicationData(branchRegisterRequest.getApplicationData())
+            .setBranchType(BranchTypeProto.valueOf(branchRegisterRequest.getBranchType().name()))
+            .setLockKey(branchRegisterRequest.getLockKey())
+            .setResourceId(branchRegisterRequest.getResourceId())
+            .setXid(branchRegisterRequest.getXid())
+            .build();
+        return result;
     }
 
     @Override
     public BranchRegisterRequest convert2Model(BranchRegisterRequestProto branchRegisterRequestProto) {
-        return null;
+        BranchRegisterRequest branchRegisterRequest = new BranchRegisterRequest();
+        branchRegisterRequest.setApplicationData(branchRegisterRequestProto.getApplicationData());
+        branchRegisterRequest.setBranchType(BranchType.valueOf(branchRegisterRequestProto.getBranchType().name()));
+        branchRegisterRequest.setLockKey(branchRegisterRequestProto.getLockKey());
+        branchRegisterRequest.setResourceId(branchRegisterRequestProto.getResourceId());
+        branchRegisterRequest.setXid(branchRegisterRequestProto.getXid());
+        return branchRegisterRequest;
     }
 }
--- core/src/main/java/io/seata/core/protocol/convertor/BranchRegisterResponseConvertor.java
+++ core/src/main/java/io/seata/core/protocol/convertor/BranchRegisterResponseConvertor.java
@@ -4,21 +4,60 @@
  */
 package io.seata.core.protocol.convertor;
 
+import io.seata.core.exception.TransactionExceptionCode;
+import io.seata.core.protocol.ResultCode;
+import io.seata.core.protocol.protobuf.AbstractMessageProto;
+import io.seata.core.protocol.protobuf.AbstractResultMessageProto;
+import io.seata.core.protocol.protobuf.AbstractTransactionResponseProto;
 import io.seata.core.protocol.protobuf.BranchRegisterResponseProto;
+import io.seata.core.protocol.protobuf.MessageTypeProto;
+import io.seata.core.protocol.protobuf.ResultCodeProto;
+import io.seata.core.protocol.protobuf.TransactionExceptionCodeProto;
 import io.seata.core.protocol.transaction.BranchRegisterResponse;
 
 /**
  * @author bystander
  * @version : BranchRegisterResponseConvertor.java, v 0.1 2019年04月25日 08:49 bystander Exp $
  */
-public class BranchRegisterResponseConvertor implements PbConvertor<BranchRegisterResponse, BranchRegisterResponseProto> {
+public class BranchRegisterResponseConvertor
+    implements PbConvertor<BranchRegisterResponse, BranchRegisterResponseProto> {
     @Override
     public BranchRegisterResponseProto convert2Proto(BranchRegisterResponse branchRegisterResponse) {
-        return null;
+        final short typeCode = branchRegisterResponse.getTypeCode();
+
+        final AbstractMessageProto abstractMessage = AbstractMessageProto.newBuilder().setMessageType(
+            MessageTypeProto.forNumber(typeCode)).build();
+
+        final AbstractResultMessageProto abstractResultMessageProto = AbstractResultMessageProto.newBuilder().setMsg(
+            branchRegisterResponse.getMsg())
+            .setResultCode(ResultCodeProto.valueOf(branchRegisterResponse.getResultCode().name())).setAbstractMessage(
+                abstractMessage).build();
+
+        AbstractTransactionResponseProto abstractTransactionResponseProto = AbstractTransactionResponseProto
+            .newBuilder().setAbstractResultMessage(abstractResultMessageProto)
+            .setTransactionExceptionCode(
+                TransactionExceptionCodeProto.valueOf(branchRegisterResponse.getTransactionExceptionCode().name()))
+            .build();
+
+        BranchRegisterResponseProto result = BranchRegisterResponseProto.newBuilder().setAbstractTransactionResponse(
+            abstractTransactionResponseProto)
+            .setBranchId(branchRegisterResponse.getBranchId()).build();
+
+        return result;
     }
 
     @Override
     public BranchRegisterResponse convert2Model(BranchRegisterResponseProto branchRegisterResponseProto) {
-        return null;
+        BranchRegisterResponse branchRegisterResponse = new BranchRegisterResponse();
+        branchRegisterResponse.setBranchId(branchRegisterResponseProto.getBranchId());
+        final AbstractResultMessageProto abstractResultMessage = branchRegisterResponseProto
+            .getAbstractTransactionResponse().getAbstractResultMessage();
+        branchRegisterResponse.setMsg(
+            abstractResultMessage.getMsg());
+        branchRegisterResponse.setResultCode(ResultCode.valueOf(abstractResultMessage.getResultCode().name()));
+        branchRegisterResponse.setTransactionExceptionCode(TransactionExceptionCode.valueOf(
+            branchRegisterResponseProto.getAbstractTransactionResponse().getTransactionExceptionCode().name()));
+
+        return branchRegisterResponse;
     }
 }
