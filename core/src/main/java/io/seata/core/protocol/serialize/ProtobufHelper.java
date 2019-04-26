7c232519205a6b1f4681ea3890001206fb158c67
leizhiyuan <leizhiyuan@gmail.com>
2019-04-26 11:18:23 +0800

    exclude protobuf checkstyle and copyright

--- core/src/main/java/io/seata/core/protocol/serialize/ProtobufHelper.java
+++ core/src/main/java/io/seata/core/protocol/serialize/ProtobufHelper.java
@@ -1,23 +1,17 @@
-/**
- * Copyright Notice: This software is developed by Ant Small and Micro Financial Services Group Co., Ltd. This software
- * and
- * all the relevant information, including but not limited to any signs, images, photographs, animations, text,
- * interface design, audios and videos, and printed materials, are protected by copyright laws and other intellectual
- * property laws and treaties.
+/*
+ *  Copyright 1999-2019 Seata.io Group.
  *
- * The use of this software shall abide by the laws and regulations as well as Software Installation License
- * Agreement/Software Use Agreement updated from time to time. Without authorization from Ant Small and Micro Financial
- * Services Group Co., Ltd., no one may conduct the following actions:
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- * 1) reproduce, spread, present, set up a mirror of, upload, download this software;
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
- * 2) reverse engineer, decompile the source code of this software or try to find the source code in any other ways;
- *
- * 3) modify, translate and adapt this software, or develop derivative products, works, and services based on this
- * software;
- *
- * 4) distribute, lease, rent, sub-license, demise or transfer any rights in relation to this software, or authorize
- * the reproduction of this software on other’s computers.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package io.seata.core.protocol.serialize;
 
