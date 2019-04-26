07eac3feebe899acabeea1603af0daf8e1dea723
CAIS <1246846408@qq.com>
2019-04-26 21:00:32 +0800

    change the key type to `String` (#923)

--- core/src/main/java/io/seata/core/rpc/netty/TmRpcClient.java
+++ core/src/main/java/io/seata/core/rpc/netty/TmRpcClient.java
@@ -193,7 +193,8 @@ public final class TmRpcClient extends AbstractRpcRemotingClient {
                     LOGGER.info("channel" + ctx.channel() + " read idle.");
                 }
                 try {
-                    nettyClientKeyPool.invalidateObject(poolKeyMap.get(ctx.channel().remoteAddress()), ctx.channel());
+                    String serverAddress = NetUtil.toStringAddress(ctx.channel().remoteAddress());
+                    nettyClientKeyPool.invalidateObject(poolKeyMap.get(serverAddress), ctx.channel());
                 } catch (Exception exx) {
                     LOGGER.error(exx.getMessage());
                 } finally {
