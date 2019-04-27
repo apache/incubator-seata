a59f78c37cc15f973bd47172f294673586508f31
CoffeeLatte007 <563868273@qq.com>
2019-04-27 19:37:33 +0800

    fix when loop start,it will be parked

--- server/src/main/java/io/seata/server/session/GlobalSession.java
+++ server/src/main/java/io/seata/server/session/GlobalSession.java
@@ -514,7 +514,7 @@ public class GlobalSession implements SessionLifecycle, SessionStorable {
 
         public void lock() throws TransactionException {
             boolean flag;
-            int times = 0;
+            int times = 1;
             long beginTime = System.currentTimeMillis();
             long restTime = GLOBAL_SESSOION_LOCK_TIME_OUT_MILLS ;
             do {
