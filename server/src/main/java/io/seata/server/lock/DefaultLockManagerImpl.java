170d9c396efb5b712da7e15e2f094d6a81bed03d
jimin <slievrly@163.com>
2019-04-29 20:02:27 +0800

    refactor:move branchSession#unlock to LockManager (#945)

--- server/src/main/java/io/seata/server/lock/DefaultLockManagerImpl.java
+++ server/src/main/java/io/seata/server/lock/DefaultLockManagerImpl.java
@@ -16,16 +16,17 @@
 package io.seata.server.lock;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
+import io.netty.util.internal.ConcurrentSet;
 import io.seata.common.exception.ShouldNeverHappenException;
 import io.seata.common.util.StringUtils;
 import io.seata.core.exception.TransactionException;
 import io.seata.server.session.BranchSession;
-
-import io.netty.util.internal.ConcurrentSet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -152,5 +153,32 @@ public class DefaultLockManagerImpl implements LockManager {
     @Override
     public void cleanAllLocks() throws TransactionException {
         LOCK_MAP.clear();
+    }
+
+    @Override
+    public boolean releaseLock(BranchSession branchSession) throws TransactionException {
+        ConcurrentHashMap<Map<String, Long>, Set<String>> lockHolder = branchSession.getLockHolder();
+        if (lockHolder.size() == 0) {
+            return true;
+        }
+        Iterator<Entry<Map<String, Long>, Set<String>>> it = lockHolder.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<Map<String, Long>, Set<String>> entry = it.next();
+            Map<String, Long> bucket = entry.getKey();
+            Set<String> keys = entry.getValue();
+            synchronized (bucket) {
+                for (String key : keys) {
+                    Long v = bucket.get(key);
+                    if (v == null) {
+                        continue;
+                    }
+                    if (v.longValue() == branchSession.getTransactionId()) {
+                        bucket.remove(key);
+                    }
+                }
+            }
+        }
+        lockHolder.clear();
+        return true;
     }
 }
