3fd8de98218c880dc6a3afae1ede4aa39cfc9471
CoffeeLatte007 <563868273@qq.com>
2019-04-30 17:43:17 +0800

    fix unit bug

--- server/src/main/java/io/seata/server/session/GlobalSession.java
+++ server/src/main/java/io/seata/server/session/GlobalSession.java
@@ -27,6 +27,7 @@ import io.seata.server.store.StoreConfig;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.locks.LockSupport;
 
@@ -511,7 +512,7 @@ public class GlobalSession implements SessionLifecycle, SessionStorable {
 
         private static final int PARK_TIMES_BASE = 10;
 
-        private static final int PARK_TIMES_BASE_NANOS = 1 * 1000 * 1000;
+        private static final int PARK_TIMES_BASE_MILLS = 1 * 1000;
 
         public void lock() throws TransactionException {
             boolean flag;
@@ -525,9 +526,9 @@ public class GlobalSession implements SessionLifecycle, SessionStorable {
                 // Pause every PARK_TIMES_BASE times,yield the CPU
                 if (times % PARK_TIMES_BASE == 0){
                     // Exponential Backoff
-                    long backOffTime =  PARK_TIMES_BASE_NANOS << (times/PARK_TIMES_BASE);
+                    long backOffTime =  PARK_TIMES_BASE_MILLS << (times/PARK_TIMES_BASE);
                     long parkTime = backOffTime < restTime ? backOffTime : restTime;
-                    LockSupport.parkNanos(parkTime);
+                    LockSupport.parkNanos(TimeUnit.MILLISECONDS.toNanos(parkTime));
                 }
                 flag = this.globalSessionSpinLock.compareAndSet(true, false);
                 times++;
