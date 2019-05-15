ea38b450f0bf00fc80831fe7bb67d516f25224c9
unknown <you@example.com>
2019-05-15 16:41:29 +0300

    fixed variable capitalisation

--- core/src/test/java/io/seata/core/protocol/RegisterTMRequestTest.java
+++ core/src/test/java/io/seata/core/protocol/RegisterTMRequestTest.java
@@ -14,39 +14,36 @@ import org.junit.jupiter.api.*;
 
 public class RegisterTMRequestTest {
 
-	private static RegisterTMRequest RTMR;
+	private static RegisterTMRequest registerTMRequest;
 	private static final String APP_ID = "applicationId";
 	private static final String TSG = "transactionServiceGroup";
 	private static final String ED = "extraData";
-	
+
 	/** Constructor without arguments **/
 	@BeforeAll
 	public static void setupNull() {
-		RTMR = new RegisterTMRequest();
+		registerTMRequest = new RegisterTMRequest();
 	}
-	
+
 	/** Constructor with arguments **/
 	@BeforeAll
 	public static void setupWithValues() {
-		RTMR = new RegisterTMRequest(APP_ID, TSG, ED);
+		registerTMRequest = new RegisterTMRequest(APP_ID, TSG, ED);
 	}
-	
+
 	/** Test get type code **/
 	@Test
 	public void testGetTypeCode() {
-		Assertions.assertEquals(101, RTMR.getTypeCode());
+		Assertions.assertEquals(101, registerTMRequest.getTypeCode());
 	}
-	
+
 	/**
 	 * Test toString having all the parameters initialized to null
 	 */
 	@Test
 	public void testToStringNullValues() {
-		Assertions.assertEquals(
-				"RegisterTMRequest{" + "applicationId='" + null + '\'' +
-			            ", transactionServiceGroup='" + null + '\'' +
-			            '}',
-				RTMR.toString());
+		Assertions.assertEquals("RegisterTMRequest{" + "applicationId='" + null + '\'' + ", transactionServiceGroup='"
+				+ null + '\'' + '}', registerTMRequest.toString());
 	}
-		
+
 }
