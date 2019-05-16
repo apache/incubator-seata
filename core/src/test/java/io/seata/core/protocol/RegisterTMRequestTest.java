90ee5032dd57dbb968af0f37fa431f48da73731f
Kaiti <aikelthoma@gmail.com>
2019-05-17 00:39:32 +0300

    made const variable for 101 and added copyright

--- core/src/test/java/io/seata/core/protocol/RegisterTMRequestTest.java
+++ core/src/test/java/io/seata/core/protocol/RegisterTMRequestTest.java
@@ -1,6 +1,23 @@
+/*
+ *  Copyright 1999-2019 Seata.io Group.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package io.seata.core.protocol;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 /**
  * RegisterTMRequest Test
@@ -18,6 +35,7 @@ public class RegisterTMRequestTest {
 	private static final String APP_ID = "applicationId";
 	private static final String TSG = "transactionServiceGroup";
 	private static final String ED = "extraData";
+	private static final short TYPE_CODE = 101;
 
 	/** Constructor without arguments **/
 	@BeforeAll
@@ -34,7 +52,7 @@ public class RegisterTMRequestTest {
 	/** Test get type code **/
 	@Test
 	public void testGetTypeCode() {
-		Assertions.assertEquals(101, registerTMRequest.getTypeCode());
+		Assertions.assertEquals(TYPE_CODE, registerTMRequest.getTypeCode());
 	}
 
 	/**
