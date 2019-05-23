9f4b97a9cd07524f40432fc8c7fe47a043415f8a
cherrylzhao <zhaojunv3@126.com>
2019-05-24 00:14:12 +0800

    add ClientPoolKeyGenerator & implement RMClientPoolKeyGenerator.

---
+++ core/src/main/java/io/seata/core/rpc/netty/pool/ClientPoolKeyGenerator.java
@@ -0,0 +1,13 @@
+package io.seata.core.rpc.netty.pool;
+
+import io.seata.core.rpc.netty.NettyPoolKey;
+
+/**
+ * Client pool key generator.
+ *
+ * @author zhaojun
+ */
+public interface ClientPoolKeyGenerator {
+    
+    NettyPoolKey generate();
+}
