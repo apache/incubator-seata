b34fcd161bf9de93e9313a6f465318aa44bb4f32
Viken <wangfei1321@gmail.com>
2019-07-18 11:46:13 +0800

    add sql init comment

--- server/src/main/resources/db_undo_log.sql
+++ server/src/main/resources/db_undo_log.sql
@@ -1,5 +1,7 @@
 -- the table to store seata xid data
 -- 0.7.0+ add context
+-- you must to init this sql for you business databese. the seata server not need it.
+-- 此脚本必须初始化在你当前的业务数据库中，用于AT 模式XID记录。与server端无关（注：业务数据库）
 -- 注意此处0.3.0+ 增加唯一索引 ux_undo_log
 drop table `undo_log`;
 CREATE TABLE `undo_log` (
