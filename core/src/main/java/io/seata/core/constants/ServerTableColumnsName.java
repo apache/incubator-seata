5a9efc453b15980a6698e92e2ba794958a7e5876
zjinlei <75718677@qq.com>
2019-08-02 18:00:20 +0800

    add acquire lock failure log for db mode and table columns name defined as constants (#1364)

---
+++ core/src/main/java/io/seata/core/constants/ServerTableColumnsName.java
@@ -0,0 +1,201 @@
+/*
+ *  Copyright 1999-2019 Seata.io Group.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package io.seata.core.constants;
+
+/**
+ * server table columns name.
+ *
+ * @author zjinlei
+ */
+public class ServerTableColumnsName {
+
+    /**
+     * The constant global_table column name xid
+     */
+    public static final String GLOBAL_TABLE_XID = "xid";
+
+    /**
+     * The constant global_table column name transaction_id
+     */
+    public static final String GLOBAL_TABLE_TRANSACTION_ID = "transaction_id";
+
+    /**
+     * The constant global_table column name status
+     */
+    public static final String GLOBAL_TABLE_STATUS = "status";
+
+    /**
+     * The constant global_table column name application_id
+     */
+    public static final String GLOBAL_TABLE_APPLICATION_ID = "application_id";
+
+    /**
+     * The constant global_table column name transaction_service_group
+     */
+    public static final String GLOBAL_TABLE_TRANSACTION_SERVICE_GROUP = "transaction_service_group";
+
+    /**
+     * The constant global_table column name transaction_name
+     */
+    public static final String GLOBAL_TABLE_TRANSACTION_NAME = "transaction_name";
+
+    /**
+     * The constant global_table column name timeout
+     */
+    public static final String GLOBAL_TABLE_TIMEOUT = "timeout";
+
+    /**
+     * The constant global_table column name begin_time
+     */
+    public static final String GLOBAL_TABLE_BEGIN_TIME = "begin_time";
+
+    /**
+     * The constant global_table column name application_data
+     */
+    public static final String GLOBAL_TABLE_APPLICATION_DATA = "application_data";
+
+    /**
+     * The constant global_table column name gmt_create
+     */
+    public static final String GLOBAL_TABLE_GMT_CREATE = "gmt_create";
+
+    /**
+     * The constant global_table column name gmt_modified
+     */
+    public static final String GLOBAL_TABLE_GMT_MODIFIED = "gmt_modified";
+
+
+
+
+
+
+    /**
+     * The constant branch_table column name branch_id
+     */
+    public static final String BRANCH_TABLE_BRANCH_XID = "branch_id";
+
+    /**
+     * The constant branch_table column name xid
+     */
+    public static final String BRANCH_TABLE_XID = "xid";
+
+    /**
+     * The constant branch_table column name transaction_id
+     */
+    public static final String BRANCH_TABLE_TRANSACTION_ID = "transaction_id";
+
+    /**
+     * The constant branch_table column name resource_group_id
+     */
+    public static final String BRANCH_TABLE_RESOURCE_GROUP_ID = "resource_group_id";
+
+    /**
+     * The constant branch_table column name resource_id
+     */
+    public static final String BRANCH_TABLE_RESOURCE_ID = "resource_id";
+
+    /**
+     * The constant branch_table column name lock_key
+     */
+    public static final String BRANCH_TABLE_LOCK_KEY = "lock_key";
+
+    /**
+     * The constant branch_table column name branch_type
+     */
+    public static final String BRANCH_TABLE_BRANCH_TYPE = "branch_type";
+
+    /**
+     * The constant branch_table column name status
+     */
+    public static final String BRANCH_TABLE_STATUS = "status";
+
+    /**
+     * The constant branch_table column name begin_time
+     */
+    public static final String BRANCH_TABLE_BEGIN_TIME = "begin_time";
+
+    /**
+     * The constant branch_table column name application_data
+     */
+    public static final String BRANCH_TABLE_APPLICATION_DATA = "application_data";
+
+    /**
+     * The constant branch_table column name client_id
+     */
+    public static final String BRANCH_TABLE_CLIENT_ID = "client_id";
+
+    /**
+     * The constant branch_table column name gmt_create
+     */
+    public static final String BRANCH_TABLE_GMT_CREATE = "gmt_create";
+
+    /**
+     * The constant branch_table column name gmt_modified
+     */
+    public static final String BRANCH_TABLE_GMT_MODIFIED = "gmt_modified";
+
+
+
+
+
+
+    /**
+     * The constant lock_table column name row_key
+     */
+    public static final String LOCK_TABLE_ROW_KEY = "row_key";
+
+    /**
+     * The constant lock_table column name xid
+     */
+    public static final String LOCK_TABLE_XID = "xid";
+
+    /**
+     * The constant lock_table column name transaction_id
+     */
+    public static final String LOCK_TABLE_TRANSACTION_ID = "transaction_id";
+
+    /**
+     * The constant lock_table column name branch_id
+     */
+    public static final String LOCK_TABLE_BRANCH_ID = "branch_id";
+
+
+    /**
+     * The constant lock_table column name resource_id
+     */
+    public static final String LOCK_TABLE_RESOURCE_ID = "resource_id";
+
+    /**
+     * The constant lock_table column name table_name
+     */
+    public static final String LOCK_TABLE_TABLE_NAME = "table_name";
+
+    /**
+     * The constant lock_table column name pk
+     */
+    public static final String LOCK_TABLE_PK = "pk";
+
+    /**
+     * The constant lock_table column name gmt_create
+     */
+    public static final String LOCK_TABLE_GMT_CREATE = "gmt_create";
+
+    /**
+     * The constant lock_table column name gmt_modified
+     */
+    public static final String LOCK_TABLE_GMT_MODIFIED = "gmt_modified";
+
+}
