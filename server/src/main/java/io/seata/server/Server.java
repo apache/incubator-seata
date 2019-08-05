bbb7d3ecdeff959a73c6956b5c23c47f8cb911ec
zjinlei <75718677@qq.com>
2019-08-05 16:48:08 +0800

    priority start parameter

--- server/src/main/java/io/seata/server/Server.java
+++ server/src/main/java/io/seata/server/Server.java
@@ -59,6 +59,8 @@ public class Server {
         //initialize the parameter parser
         ParameterParser parameterParser = new ParameterParser(args);
 
+        System.setProperty(ConfigurationKeys.STORE_MODE, parameterParser.getStoreMode());
+
         RpcServer rpcServer = new RpcServer(WORKING_THREADS);
         //server host
         rpcServer.setHost(parameterParser.getHost());
