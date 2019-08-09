52bd0b37d8f9a553cb3e69a34fd84d17e10254c0
jsbxyyx <jsbxyyx@163.com>
2019-08-09 17:41:06 +0800

    feature: protocol kryo serializer

--- codec/seata-codec-kryo/src/main/java/io/seata/codec/kryo/KryoCodec.java
+++ codec/seata-codec-kryo/src/main/java/io/seata/codec/kryo/KryoCodec.java
@@ -1,22 +1,48 @@
+/*
+ *  Copyright 1999-2019 Seata.io Group.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package io.seata.codec.kryo;
 
 import io.seata.common.loader.LoadLevel;
 import io.seata.core.codec.Codec;
+import io.seata.core.protocol.AbstractMessage;
 
 /**
  * @author jsbxyyx
  */
-@LoadLevel(name = "KRYO", order = 0)
+@LoadLevel(name = "KRYO")
 public class KryoCodec implements Codec {
 
     @Override
     public <T> byte[] encode(T t) {
-        return new byte[0];
+        if (t == null || !(t instanceof AbstractMessage)) {
+            throw new IllegalArgumentException("message is null");
+        }
+        KryoSerializer kryoSerializer = KryoSerializerFactory.get();
+        byte[] bytes = kryoSerializer.serialize(t);
+        return bytes;
     }
 
     @Override
     public <T> T decode(byte[] bytes) {
-        return null;
+        if (bytes == null || bytes.length == 0) {
+            throw new IllegalArgumentException("bytes is null");
+        }
+        KryoSerializer kryoSerializer = KryoSerializerFactory.get();
+        T t = kryoSerializer.deserialize(bytes);
+        return t;
     }
 
 }
---
+++ codec/seata-codec-kryo/src/main/java/io/seata/codec/kryo/KryoSerializerFactory.java
@@ -0,0 +1,113 @@
+/*
+ *  Copyright 1999-2019 Seata.io Group.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package io.seata.codec.kryo;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import com.esotericsoftware.kryo.serializers.DefaultSerializers;
+import de.javakaffee.kryoserializers.ArraysAsListSerializer;
+import de.javakaffee.kryoserializers.BitSetSerializer;
+import de.javakaffee.kryoserializers.GregorianCalendarSerializer;
+import de.javakaffee.kryoserializers.JdkProxySerializer;
+import de.javakaffee.kryoserializers.RegexSerializer;
+import de.javakaffee.kryoserializers.URISerializer;
+import de.javakaffee.kryoserializers.UUIDSerializer;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.lang.reflect.InvocationHandler;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.net.URI;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.BitSet;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.LinkedList;
+import java.util.TreeSet;
+import java.util.UUID;
+import java.util.Vector;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.regex.Pattern;
+
+/**
+ * @author jsbxyyx
+ */
+public class KryoSerializerFactory {
+
+    private static final ThreadLocal<Kryo> HOLDER = new ThreadLocal<Kryo>() {
+        @Override
+        protected Kryo initialValue () {
+            return create();
+        }
+    };
+
+    public static KryoSerializer get() {
+        return new KryoSerializer(HOLDER.get());
+    }
+
+    public static void returnKryo() {}
+
+    private static Kryo create() {
+        Kryo kryo = new Kryo();
+        kryo.setRegistrationRequired(false);
+
+        // register serializer
+        kryo.register(Arrays.asList("").getClass(), new ArraysAsListSerializer());
+        kryo.register(GregorianCalendar.class, new GregorianCalendarSerializer());
+        kryo.register(InvocationHandler.class, new JdkProxySerializer());
+        kryo.register(BigDecimal.class, new DefaultSerializers.BigDecimalSerializer());
+        kryo.register(BigInteger.class, new DefaultSerializers.BigIntegerSerializer());
+        kryo.register(Pattern.class, new RegexSerializer());
+        kryo.register(BitSet.class, new BitSetSerializer());
+        kryo.register(URI.class, new URISerializer());
+        kryo.register(UUID.class, new UUIDSerializer());
+
+        // register commonly class
+        kryo.register(HashMap.class);
+        kryo.register(ArrayList.class);
+        kryo.register(LinkedList.class);
+        kryo.register(HashSet.class);
+        kryo.register(TreeSet.class);
+        kryo.register(Hashtable.class);
+        kryo.register(Date.class);
+        kryo.register(Calendar.class);
+        kryo.register(ConcurrentHashMap.class);
+        kryo.register(SimpleDateFormat.class);
+        kryo.register(GregorianCalendar.class);
+        kryo.register(Vector.class);
+        kryo.register(BitSet.class);
+        kryo.register(StringBuffer.class);
+        kryo.register(StringBuilder.class);
+        kryo.register(Object.class);
+        kryo.register(Object[].class);
+        kryo.register(String[].class);
+        kryo.register(byte[].class);
+        kryo.register(char[].class);
+        kryo.register(int[].class);
+        kryo.register(float[].class);
+        kryo.register(double[].class);
+        return kryo;
+    }
+
+}
