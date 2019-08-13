3dc43d6b600c71dd9c876245395efc25ab407684
jsbxyyx <jsbxyyx@163.com>
2019-08-13 15:28:27 +0800

    refactor: undolog kryo

--- rm-datasource/src/main/java/io/seata/rm/datasource/undo/parser/KryoUndoLogParser.java
+++ rm-datasource/src/main/java/io/seata/rm/datasource/undo/parser/KryoUndoLogParser.java
@@ -15,43 +15,9 @@
  */
 package io.seata.rm.datasource.undo.parser;
 
-import com.esotericsoftware.kryo.Kryo;
-import com.esotericsoftware.kryo.io.Input;
-import com.esotericsoftware.kryo.io.Output;
-import com.esotericsoftware.kryo.serializers.DefaultSerializers;
-import de.javakaffee.kryoserializers.ArraysAsListSerializer;
-import de.javakaffee.kryoserializers.BitSetSerializer;
-import de.javakaffee.kryoserializers.GregorianCalendarSerializer;
-import de.javakaffee.kryoserializers.JdkProxySerializer;
-import de.javakaffee.kryoserializers.RegexSerializer;
-import de.javakaffee.kryoserializers.URISerializer;
-import de.javakaffee.kryoserializers.UUIDSerializer;
 import io.seata.common.loader.LoadLevel;
 import io.seata.rm.datasource.undo.BranchUndoLog;
 import io.seata.rm.datasource.undo.UndoLogParser;
-
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.lang.reflect.InvocationHandler;
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.net.URI;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.BitSet;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Hashtable;
-import java.util.LinkedList;
-import java.util.TreeSet;
-import java.util.UUID;
-import java.util.Vector;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.regex.Pattern;
 
 /**
  * kryo serializer
@@ -61,13 +27,6 @@ import java.util.regex.Pattern;
 public class KryoUndoLogParser implements UndoLogParser {
 
     public static final String NAME = "kryo";
-
-    private static final ThreadLocal<Kryo> HOLDER = new ThreadLocal<Kryo>() {
-        @Override
-        protected Kryo initialValue () {
-            return create();
-        }
-    };
 
     @Override
     public String getName() {
@@ -81,78 +40,22 @@ public class KryoUndoLogParser implements UndoLogParser {
 
     @Override
     public byte[] encode(BranchUndoLog branchUndoLog) {
-        Kryo kryo = getKryo();
+        KryoSerializer kryoSerializer = KryoSerializerFactory.getInstance().get();
         try {
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            Output output = new Output(baos);
-            kryo.writeClassAndObject(output, branchUndoLog);
-            output.close();
-            return baos.toByteArray();
+            return kryoSerializer.serialize(branchUndoLog);
         } finally {
-            returnKryo(kryo);
+            KryoSerializerFactory.getInstance().returnKryo(kryoSerializer);
         }
     }
 
     @Override
     public BranchUndoLog decode(byte[] bytes) {
-        Kryo kryo = getKryo();
+        KryoSerializer kryoSerializer = KryoSerializerFactory.getInstance().get();
         try {
-            ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
-            Input input = new Input(bais);
-            input.close();
-            return (BranchUndoLog) kryo.readClassAndObject(input);
+            return kryoSerializer.deserialize(bytes);
         } finally {
-            returnKryo(kryo);
+            KryoSerializerFactory.getInstance().returnKryo(kryoSerializer);
         }
     }
 
-    private Kryo getKryo() {
-        return HOLDER.get();
-    }
-
-    private void returnKryo(Kryo kryo) {
-        // free or do nothing
-    }
-
-    private static Kryo create() {
-        Kryo kryo = new Kryo();
-        kryo.setRegistrationRequired(false);
-
-        // register serializer
-        kryo.register(Arrays.asList("").getClass(), new ArraysAsListSerializer());
-        kryo.register(GregorianCalendar.class, new GregorianCalendarSerializer());
-        kryo.register(InvocationHandler.class, new JdkProxySerializer());
-        kryo.register(BigDecimal.class, new DefaultSerializers.BigDecimalSerializer());
-        kryo.register(BigInteger.class, new DefaultSerializers.BigIntegerSerializer());
-        kryo.register(Pattern.class, new RegexSerializer());
-        kryo.register(BitSet.class, new BitSetSerializer());
-        kryo.register(URI.class, new URISerializer());
-        kryo.register(UUID.class, new UUIDSerializer());
-
-        // register commonly class
-        kryo.register(HashMap.class);
-        kryo.register(ArrayList.class);
-        kryo.register(LinkedList.class);
-        kryo.register(HashSet.class);
-        kryo.register(TreeSet.class);
-        kryo.register(Hashtable.class);
-        kryo.register(Date.class);
-        kryo.register(Calendar.class);
-        kryo.register(ConcurrentHashMap.class);
-        kryo.register(SimpleDateFormat.class);
-        kryo.register(GregorianCalendar.class);
-        kryo.register(Vector.class);
-        kryo.register(BitSet.class);
-        kryo.register(StringBuffer.class);
-        kryo.register(StringBuilder.class);
-        kryo.register(Object.class);
-        kryo.register(Object[].class);
-        kryo.register(String[].class);
-        kryo.register(byte[].class);
-        kryo.register(char[].class);
-        kryo.register(int[].class);
-        kryo.register(float[].class);
-        kryo.register(double[].class);
-        return kryo;
-    }
 }
