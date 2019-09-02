5f7670f7952f550ba8af5efc34df7ae510977d93
jsbxyyx <jsbxyyx@163.com>
2019-09-02 15:08:26 +0800

    optimize priority

--- rm-datasource/src/main/java/io/seata/rm/datasource/exec/InsertExecutor.java
+++ rm-datasource/src/main/java/io/seata/rm/datasource/exec/InsertExecutor.java
@@ -15,6 +15,14 @@
  */
 package io.seata.rm.datasource.exec;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import io.seata.common.exception.NotSupportYetException;
 import io.seata.common.exception.ShouldNeverHappenException;
 import io.seata.rm.datasource.PreparedStatementProxy;
@@ -29,14 +37,6 @@ import io.seata.rm.datasource.sql.struct.TableMeta;
 import io.seata.rm.datasource.sql.struct.TableRecords;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 /**
  * The type Insert executor.
@@ -73,7 +73,8 @@ public class InsertExecutor<T, S extends Statement> extends AbstractDMLBaseExecu
     @Override
     protected TableRecords afterImage(TableRecords beforeImage) throws SQLException {
         //Pk column exists or PK is just auto generated
-        List<Object> pkValues = containsPK() ? getPkValuesByColumn() : containsColumns() ? getPkValuesByAuto() : getPkValuesByColumn();
+        List<Object> pkValues = containsPK() ? getPkValuesByColumn() :
+                (containsColumns() ? getPkValuesByAuto() : getPkValuesByColumn());
 
         TableRecords afterImage = buildTableRecords(pkValues);
 
