e5f58bd2bd47d53e911d8dc085c94e0f4e57d9a0
zjinlei <75718677@qq.com>
2019-09-07 14:16:55 +0800

    optimize: once delete GlobalSession locks for db mode when commit success (#1574)

--- server/src/main/java/io/seata/server/lock/DefaultLockManager.java
+++ server/src/main/java/io/seata/server/lock/DefaultLockManager.java
@@ -15,14 +15,20 @@
  */
 package io.seata.server.lock;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import io.seata.common.util.CollectionUtils;
 import io.seata.common.util.StringUtils;
+import io.seata.config.Configuration;
+import io.seata.config.ConfigurationFactory;
+import io.seata.core.constants.ConfigurationKeys;
 import io.seata.core.exception.TransactionException;
 import io.seata.core.lock.Locker;
 import io.seata.core.lock.RowLock;
+import io.seata.core.store.StoreMode;
 import io.seata.server.session.BranchSession;
+import io.seata.server.session.GlobalSession;
 
 /**
  * The type Default lock manager.
@@ -33,6 +39,11 @@ import io.seata.server.session.BranchSession;
 public class DefaultLockManager extends AbstractLockManager {
 
     private static Locker locker = null;
+
+    /**
+     * The constant CONFIG.
+     */
+    protected static final Configuration CONFIG = ConfigurationFactory.getInstance();
 
     @Override
     public boolean acquireLock(BranchSession branchSession) throws TransactionException {
@@ -53,16 +64,46 @@ public class DefaultLockManager extends AbstractLockManager {
     @Override
     public boolean releaseLock(BranchSession branchSession) throws TransactionException {
         List<RowLock> locks = collectRowLocks(branchSession);
-        if (CollectionUtils.isEmpty(locks)) {
-            //no lock
-            return true;
-        }
         try {
-            return getLocker(branchSession).releaseLock(locks);
+            return this.doReleaseLock(locks, branchSession);
         } catch (Exception t) {
             LOGGER.error("unLock error, branchSession:" + branchSession, t);
             return false;
         }
+    }
+
+    @Override
+    public boolean releaseGlobalSessionLock(GlobalSession globalSession) throws TransactionException {
+        ArrayList<BranchSession> branchSessions = globalSession.getBranchSessions();
+        String storeMode = CONFIG.getConfig(ConfigurationKeys.STORE_MODE);
+        if (StoreMode.DB.name().equalsIgnoreCase(storeMode)) {
+            List<RowLock> locks = new ArrayList<>();
+            for (BranchSession branchSession : branchSessions) {
+                locks.addAll(collectRowLocks(branchSession));
+            }
+            try {
+                return this.doReleaseLock(locks, null);
+            } catch (Exception t) {
+                LOGGER.error("unLock globalSession error, xid:{}", globalSession.getXid(), t);
+                return false;
+            }
+        } else {
+            boolean releaseLockResult = true;
+            for (BranchSession branchSession : branchSessions) {
+                if (!this.releaseLock(branchSession)) {
+                    releaseLockResult = false;
+                }
+            }
+            return releaseLockResult;
+        }
+    }
+
+    private boolean doReleaseLock(List<RowLock> locks, BranchSession branchSession) {
+        if (CollectionUtils.isEmpty(locks)) {
+            //no lock
+            return true;
+        }
+        return getLocker(branchSession).releaseLock(locks);
     }
 
     @Override
