c757492ea4576be531a871d9ec9aef314c098b6d
ggndnn <ggndnn@163.com>
2019-09-19 18:11:04 +0800

    feature: support custom config and registry type (#1625)

---
+++ config/seata-config-custom/src/test/java/io/seata/config/CustomConfigurationTest.java
@@ -0,0 +1,44 @@
+/*
+ *  Copyright 1999-2019 Seata.io Group.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package io.seata.config;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * @author ggndnn
+ */
+public class CustomConfigurationTest {
+    @Test
+    public void testCustomConfigLoad() throws Exception {
+        Configuration<?> configuration = ConfigurationFactory.getInstance();
+        Assertions.assertTrue(configuration instanceof CustomConfigurationForTest);
+        Properties properties;
+        try (InputStream input = CustomConfigurationForTest.class.getClassLoader().getResourceAsStream("custom_for_test.properties")) {
+            properties = new Properties();
+            properties.load(input);
+        }
+        Assertions.assertNotNull(properties);
+        for (String name : properties.stringPropertyNames()) {
+            String value = properties.getProperty(name);
+            Assertions.assertNotNull(value);
+            Assertions.assertEquals(value, configuration.getConfig(name));
+        }
+    }
+}
---
+++ discovery/seata-discovery-custom/src/main/java/io/seata/discovery/registry/custom/CustomRegistryProvider.java
@@ -0,0 +1,53 @@
+/*
+ *  Copyright 1999-2019 Seata.io Group.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package io.seata.discovery.registry.custom;
+
+import io.seata.common.loader.EnhancedServiceLoader;
+import io.seata.common.loader.LoadLevel;
+import io.seata.common.util.StringUtils;
+import io.seata.config.ConfigurationFactory;
+import io.seata.discovery.registry.RegistryProvider;
+import io.seata.discovery.registry.RegistryService;
+import io.seata.discovery.registry.RegistryType;
+
+import java.util.stream.Stream;
+
+/**
+ * @author ggndnn
+ */
+@LoadLevel(name = "Custom")
+public class CustomRegistryProvider implements RegistryProvider {
+    private static final String FILE_CONFIG_KEY_PREFIX = "registry.custom.name";
+
+    private final String CUSTOM_NAME;
+
+    public CustomRegistryProvider() {
+        String name = ConfigurationFactory.CURRENT_FILE_INSTANCE.getConfig(FILE_CONFIG_KEY_PREFIX);
+        if (StringUtils.isBlank(name)) {
+            throw new IllegalArgumentException("name value of custom registry type must not be blank");
+        }
+        if (Stream.of(RegistryType.values())
+                .anyMatch(ct -> ct.name().equalsIgnoreCase(name))) {
+            throw new IllegalArgumentException(String.format("custom registry type name %s is not allowed", name));
+        }
+        CUSTOM_NAME = name;
+    }
+
+    @Override
+    public RegistryService provide() {
+        return EnhancedServiceLoader.load(RegistryProvider.class, CUSTOM_NAME).provide();
+    }
+}
