9576f56c188b3a1aa2b26d6e6b92fee2a708fd63
ccg <ccg2525628>
2019-10-08 09:59:04 +0800

    码升级到0.9.0快照

--- rm-datasource/src/main/java/io/seata/rm/datasource/undo/parser/KryoUndoLogParser.java
+++ rm-datasource/src/main/java/io/seata/rm/datasource/undo/parser/KryoUndoLogParser.java
@@ -15,9 +15,23 @@
  */
 package io.seata.rm.datasource.undo.parser;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import io.seata.common.loader.LoadLevel;
 import io.seata.rm.datasource.undo.BranchUndoLog;
 import io.seata.rm.datasource.undo.UndoLogParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * kryo serializer
@@ -25,6 +39,8 @@ import io.seata.rm.datasource.undo.UndoLogParser;
  */
 @LoadLevel(name = KryoUndoLogParser.NAME)
 public class KryoUndoLogParser implements UndoLogParser {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KryoUndoLogParser.class);
 
     public static final String NAME = "kryo";
 
@@ -60,6 +76,34 @@ public class KryoUndoLogParser implements UndoLogParser {
             return kryoSerializer.deserialize(bytes);
         } finally {
             KryoSerializerFactory.getInstance().returnKryo(kryoSerializer);
+        }
+    }
+
+
+    @Override
+    public String writeValueAsString(BranchUndoLog branchUndoLog) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            //类为空时，不要抛异常
+            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+//            objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+            objectMapper.getSerializerProvider().setNullValueSerializer(new JsonSerializer<Object>() {
+                @Override
+                public void serialize(Object value, JsonGenerator jgen, SerializerProvider provider) throws IOException, JsonProcessingException {
+                    jgen.writeObject("");// 输出孔字符串
+                }
+            });
+            objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+            SimpleModule simpleModule = new SimpleModule();
+            simpleModule.addSerializer(Number.class, ToStringSerializer.instance);
+            simpleModule.addSerializer(Date.class, ToStringSerializer.instance);
+            objectMapper.registerModule(simpleModule);
+
+            String context = objectMapper.writeValueAsString(branchUndoLog);
+            return context;
+        } catch (JsonProcessingException e) {
+            LOGGER.error("json encode exception, {}", e.getMessage(), e);
+            throw new RuntimeException(e);
         }
     }
 
