b98d9dd95df959e406f1031a54be6d086c516929
zhongfuhua <zhongfuhua@users.noreply.github.com>
2019-10-14 15:16:26 +0800

    Update TccActionInterceptor.java

--- spring/src/main/java/io/seata/spring/tcc/TccActionInterceptor.java
+++ spring/src/main/java/io/seata/spring/tcc/TccActionInterceptor.java
@@ -96,8 +96,8 @@ public class TccActionInterceptor implements MethodInterceptor {
                 return ret.get(Constants.TCC_METHOD_RESULT);
             } finally {
                 //recovery the context
-                RootContext.bind(xid);
                 RootContext.unbindType();
+                RootContext.bind(xid);
             }
         }
         return invocation.proceed();
