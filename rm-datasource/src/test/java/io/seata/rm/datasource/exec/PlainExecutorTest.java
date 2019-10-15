3ab062c3433e1ab5b0c319c5f9ba1c912542b699
will <349071347@qq.com>
2019-10-15 14:30:18 +0800

    increase PlainExecutor code coverage

--- rm-datasource/src/test/java/io/seata/rm/datasource/exec/PlainExecutorTest.java
+++ rm-datasource/src/test/java/io/seata/rm/datasource/exec/PlainExecutorTest.java
@@ -15,25 +15,17 @@
  */
 package io.seata.rm.datasource.exec;
 
-import java.lang.reflect.Field;
 import java.sql.SQLException;
 import java.sql.Types;
 import java.util.List;
-
 import com.alibaba.druid.mock.MockStatement;
 import com.alibaba.druid.mock.MockStatementBase;
 import com.alibaba.druid.pool.DruidDataSource;
-import com.alibaba.druid.sql.SQLUtils;
-import com.alibaba.druid.sql.ast.SQLStatement;
-import com.alibaba.druid.util.JdbcConstants;
 import com.google.common.collect.Lists;
 import io.seata.rm.datasource.ConnectionProxy;
 import io.seata.rm.datasource.DataSourceProxy;
 import io.seata.rm.datasource.StatementProxy;
 import io.seata.rm.datasource.mock.MockDriver;
-import io.seata.rm.datasource.sql.druid.MySQLDeleteRecognizer;
-import io.seata.rm.datasource.sql.struct.TableRecords;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -41,22 +33,22 @@ import org.junit.jupiter.api.Test;
  * @author will
  * @date 2019/9/18
  */
-public class DeleteExecutorTest {
+public class PlainExecutorTest {
 
-    private DeleteExecutor deleteExecutor;
+    private PlainExecutor plainExecutor;
 
     private StatementProxy statementProxy;
 
     @BeforeEach
-    public void init() throws SQLException, NoSuchFieldException, IllegalAccessException {
+    public void init() throws SQLException {
         List<String> returnValueColumnLabels = Lists.newArrayList("id", "name");
         Object[][] returnValue = new Object[][] {
             new Object[] {1, "Tom"},
             new Object[] {2, "Jack"},
         };
         Object[][] columnMetas = new Object[][] {
-            new Object[] {"", "", "table_delete_executor_test", "id", Types.INTEGER, "INTEGER", 64, 0, 10, 1, "", "", 0, 0, 64, 1, "NO", "YES"},
-            new Object[] {"", "", "table_delete_executor_test", "name", Types.VARCHAR, "VARCHAR", 64, 0, 10, 0, "", "", 0, 0, 64, 2, "YES", "NO"},
+            new Object[] {"", "", "table_plain_executor_test", "id", Types.INTEGER, "INTEGER", 64, 0, 10, 1, "", "", 0, 0, 64, 1, "NO", "YES"},
+            new Object[] {"", "", "table_plain_executor_test", "name", Types.VARCHAR, "VARCHAR", 64, 0, 10, 0, "", "", 0, 0, 64, 2, "YES", "NO"},
         };
         Object[][] indexMetas = new Object[][] {
             new Object[] {"PRIMARY", "id", false, "", 3, 1, "A", 34},
@@ -68,34 +60,16 @@ public class DeleteExecutorTest {
         dataSource.setDriver(mockDriver);
 
         DataSourceProxy dataSourceProxy = new DataSourceProxy(dataSource);
-        Field field = dataSourceProxy.getClass().getDeclaredField("dbType");
-        field.setAccessible(true);
-        field.set(dataSourceProxy, "mysql");
         ConnectionProxy connectionProxy = new ConnectionProxy(dataSourceProxy, dataSource.getConnection().getConnection());
         MockStatementBase mockStatement = new MockStatement(dataSource.getConnection().getConnection());
         statementProxy = new StatementProxy(connectionProxy, mockStatement);
-        String sql = "delete from t where id = 1";
-        List<SQLStatement> asts = SQLUtils.parseStatements(sql, JdbcConstants.MYSQL);
-        MySQLDeleteRecognizer recognizer = new MySQLDeleteRecognizer(sql, asts.get(0));
-        deleteExecutor = new DeleteExecutor(statementProxy, (statement, args) -> {
-            return null;
-        }, recognizer);
+
+        plainExecutor = new PlainExecutor(statementProxy, (statement, args) -> null);
     }
 
     @Test
-    public void testBeforeImage() throws SQLException {
-        Assertions.assertNotNull(deleteExecutor.beforeImage());
-
-        String sql = "delete from t";
-        List<SQLStatement> asts = SQLUtils.parseStatements(sql, JdbcConstants.MYSQL);
-        MySQLDeleteRecognizer recognizer = new MySQLDeleteRecognizer(sql, asts.get(0));
-        deleteExecutor = new DeleteExecutor(statementProxy, (statement, args) -> null, recognizer);
-        Assertions.assertNotNull(deleteExecutor.beforeImage());
+    public void testExecute() throws Throwable {
+        plainExecutor.execute(null);
     }
 
-    @Test
-    public void testAfterImage() throws SQLException {
-        TableRecords tableRecords = deleteExecutor.beforeImage();
-        Assertions.assertTrue(deleteExecutor.afterImage(tableRecords).size() == 0);
-    }
 }
