6467dcd40cc64615fcfa4af06abbbe6c7cc25c6e
slievrly <slievrly@163.com>
2019-10-30 15:01:11 +0800

    script: add some init script
    Signed-off-by: slievrly <slievrly@163.com>

---
+++ script/client/at/undo_log/mysql/undo_log.sql
@@ -0,0 +1,15 @@
+-- for AT mode you must to init this sql for you business databese. the seata server not need it.
+drop table `undo_log`;
+CREATE TABLE `undo_log` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `branch_id` bigint(20) NOT NULL,
+  `xid` varchar(100) NOT NULL,
+  `context` varchar(128) NOT NULL,
+  `rollback_info` longblob NOT NULL,
+  `log_status` int(11) NOT NULL,
+  `log_created` datetime NOT NULL,
+  `log_modified` datetime NOT NULL,
+  `ext` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ux_undo_log` (`xid`,`branch_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
