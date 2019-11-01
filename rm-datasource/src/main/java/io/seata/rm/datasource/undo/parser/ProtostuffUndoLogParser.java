804837acf1e048755c0388d42878eaabc92c1658
iapplejohn <czj4451@126.com>
2019-11-01 09:46:28 +0800

    bugfix: protostuff undo log get default content (#1856)

--- rm-datasource/src/main/java/io/seata/rm/datasource/undo/parser/ProtostuffUndoLogParser.java
+++ rm-datasource/src/main/java/io/seata/rm/datasource/undo/parser/ProtostuffUndoLogParser.java
@@ -60,7 +60,7 @@ public class ProtostuffUndoLogParser implements UndoLogParser {
 
     @Override
     public byte[] getDefaultContent() {
-        return new byte[0];
+        return encode(new BranchUndoLog());
     }
 
     @Override
