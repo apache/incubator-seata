978f4a396a4c19141f2b70916964f981d8c02930
leo <long_187@126.com>
2019-11-01 15:59:23 +0800

    bugfix: statemachine def can't store to db and provide query the state logs (#1810)

--- saga/seata-saga-engine/src/main/java/io/seata/saga/engine/strategy/StatusDecisionStrategy.java
+++ saga/seata-saga-engine/src/main/java/io/seata/saga/engine/strategy/StatusDecisionStrategy.java
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package io.seata.saga.engine;
+package io.seata.saga.engine.strategy;
 
 import io.seata.saga.proctrl.ProcessContext;
 import io.seata.saga.statelang.domain.StateMachineInstance;
