ef2c5127f22d9f430e2093d4b25001ba31874d54
yuhuangbin <danielyu96@163.com>
2019-11-06 21:03:48 +0800

    add log

--- config/seata-config-core/src/main/java/io/seata/config/FileConfiguration.java
+++ config/seata-config-core/src/main/java/io/seata/config/FileConfiguration.java
@@ -46,6 +46,8 @@ public class FileConfiguration extends AbstractConfiguration<ConfigChangeListene
 
     private final Config fileConfig;
 
+    private String fileName;
+
     private ExecutorService configOperateExecutor;
 
     private ExecutorService configChangeExecutor;
@@ -79,6 +81,7 @@ public class FileConfiguration extends AbstractConfiguration<ConfigChangeListene
      * @param name the name
      */
     public FileConfiguration(String name) {
+        this.fileName = name;
         if (null == name) {
             fileConfig = ConfigFactory.load();
         }
@@ -195,6 +198,7 @@ public class FileConfiguration extends AbstractConfiguration<ConfigChangeListene
                     setFailResult(configFuture);
                     return;
                 }
+                LOGGER.info("The file name of the operation is {}", fileName);
                 try {
                     if (configFuture.getOperation() == ConfigOperation.GET) {
                         String result = fileConfig.getString(configFuture.getDataId());
