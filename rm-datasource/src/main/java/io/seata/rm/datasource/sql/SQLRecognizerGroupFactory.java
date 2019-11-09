43bc2c344dd8f44ad4f60dcadcbe54a6ba4ddbb1
Hao Zhibei <haozhibei@inspur.com>
2019-11-09 14:38:09 +0800

    Restructure SQLRecognizer and UndoExecutor

--- rm-datasource/src/main/java/io/seata/rm/datasource/sql/SQLRecognizerGroupFactory.java
+++ rm-datasource/src/main/java/io/seata/rm/datasource/sql/SQLRecognizerGroupFactory.java
@@ -1,3 +1,18 @@
+/*
+ *  Copyright 1999-2019 Seata.io Group.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package io.seata.rm.datasource.sql;
 
 import io.seata.common.loader.EnhancedServiceLoader;
@@ -35,6 +50,6 @@ public class SQLRecognizerGroupFactory
     if (recognizerGroupMap.containsKey(dbType)) {
       return recognizerGroupMap.get(dbType);
     }
-    throw new RuntimeException(MessageFormat.format("now not support {0}", dbType));
+    throw new UnsupportedOperationException(MessageFormat.format("now not support {0}", dbType));
   }
 }
