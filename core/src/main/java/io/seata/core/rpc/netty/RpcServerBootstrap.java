76d00bfc4c4911b140ca89ea1e6631d7fe40b4e2
zhangchenghui <zhangchenghui.dev@gmail.com>
2019-11-24 21:27:21 +0800

    optimize: code style about import class use *.

--- core/src/main/java/io/seata/core/rpc/netty/RpcServerBootstrap.java
+++ core/src/main/java/io/seata/core/rpc/netty/RpcServerBootstrap.java
@@ -16,7 +16,13 @@
 package io.seata.core.rpc.netty;
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.*;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.WriteBufferWaterMark;
 import io.netty.channel.epoll.EpollEventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
