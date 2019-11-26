30271e641ad4622f932b70cb3f706cce2e41ca84
ruqinhu <542849951@qq.com>
2019-11-26 19:17:57 +0800

    Update TransactionInfo.java

--- tm/src/main/java/io/seata/tm/api/transaction/TransactionInfo.java
+++ tm/src/main/java/io/seata/tm/api/transaction/TransactionInfo.java
@@ -59,21 +59,16 @@ public final class TransactionInfo implements Serializable {
     }
 
     public boolean rollbackOn(Throwable ex) {
-
         RollbackRule winner = null;
-        int deepest = Integer.MAX_VALUE;
-
         if (CollectionUtils.isNotEmpty(rollbackRules)) {
             winner = NoRollbackRule.DEFAULT_NO_ROLLBACK_RULE;
             for (RollbackRule rule : this.rollbackRules) {
                 int depth = rule.getDepth(ex);
-                if (depth >= 0 && depth < deepest) {
-                    deepest = depth;
+                if (depth >= 0) {
                     winner = rule;
                 }
             }
         }
-
-        return winner == null || !(winner instanceof NoRollbackRule);
+        return !(winner instanceof NoRollbackRule);
     }
 }
