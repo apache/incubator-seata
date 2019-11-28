e3e13e723d057ce0137ee836f6bbb8460f8be9ac
will <349071347@qq.com>
2019-11-28 16:04:46 +0800

    create postgre sql spi

---
+++ rm-datasource/src/main/java/io/seata/rm/datasource/sql/druid/postgresql/PostgresqlOperateRecognizerHolder.java
@@ -0,0 +1,55 @@
+/*
+ *  Copyright 1999-2019 Seata.io Group.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package io.seata.rm.datasource.sql.druid.postgresql;
+
+import com.alibaba.druid.sql.ast.SQLStatement;
+
+import io.seata.rm.datasource.sql.SQLOperateRecognizerHolder;
+import io.seata.rm.datasource.sql.SQLRecognizer;
+
+/**
+ * The type PostgresqlOperateRecognizerHolder
+ * @author will.zjw
+ */
+public class PostgresqlOperateRecognizerHolder implements SQLOperateRecognizerHolder {
+
+    private static final String POSTGRE_SQL = "postgresql";
+
+    @Override
+    public SQLRecognizer getDeleteRecognizer(String sql, SQLStatement ast) {
+        return new PostgresqlDeleteRecognizer(sql, ast);
+    }
+
+    @Override
+    public SQLRecognizer getInsertRecognizer(String sql, SQLStatement ast) {
+        return new PostgresqlInsertRecognizer(sql, ast);
+    }
+
+    @Override
+    public SQLRecognizer getUpdateRecognizer(String sql, SQLStatement ast) {
+        return new PostgresqlUpdateRecognizer(sql, ast);
+    }
+
+    @Override
+    public SQLRecognizer getSelectForUpdateRecognizer(String sql, SQLStatement ast) {
+        return new PostgresqlSelectForUpdateRecognizer(sql, ast);
+    }
+
+    @Override
+    public String getDbType() {
+        return POSTGRE_SQL;
+    }
+}
