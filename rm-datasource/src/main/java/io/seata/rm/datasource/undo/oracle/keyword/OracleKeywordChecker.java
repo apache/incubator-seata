54c19df7d7494b9222416cc165874537f9d21f08
Hao Zhibei <837948266@qq.com>
2019-11-29 11:43:42 +0800

    remove unused import

--- rm-datasource/src/main/java/io/seata/rm/datasource/undo/oracle/keyword/OracleKeywordChecker.java
+++ rm-datasource/src/main/java/io/seata/rm/datasource/undo/oracle/keyword/OracleKeywordChecker.java
@@ -19,7 +19,6 @@ import com.alibaba.druid.util.JdbcConstants;
 import io.seata.rm.datasource.undo.KeywordChecker;
 
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
