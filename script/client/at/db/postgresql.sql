a95573fd1317d287126cc3f7636c2b1316e6df27
helloworlde <hellowoodes@gmail.com>
2019-12-08 09:46:09 +0800

    optimize: classify script to correspond directory

--- script/client/at/db/postgresql.sql
+++ script/client/at/db/postgresql.sql
@@ -9,7 +9,6 @@ CREATE TABLE IF NOT EXISTS public.undo_log
     log_status   INT          NOT NULL,
     log_created  TIMESTAMP(0) NOT NULL,
     log_modified TIMESTAMP(0) NOT NULL,
-    ext          VARCHAR(100) DEFAULT NULL,
     CONSTRAINT pk_undo_log PRIMARY KEY (id),
     CONSTRAINT ux_undo_log UNIQUE (xid, branch_id)
 );
