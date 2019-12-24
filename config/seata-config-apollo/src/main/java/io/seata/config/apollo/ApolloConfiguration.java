77c550301c7df6ddb6ec9ed712460dcdd3cd951c
zhangchenghui <zhangchenghui.dev@gmail.com>
2019-12-24 20:40:45 +0800

    feature: add apollo configuration namespace.

--- config/seata-config-apollo/src/main/java/io/seata/config/apollo/ApolloConfiguration.java
+++ config/seata-config-apollo/src/main/java/io/seata/config/apollo/ApolloConfiguration.java
@@ -52,6 +52,8 @@ public class ApolloConfiguration extends AbstractConfiguration {
     private static final String REGISTRY_TYPE = "apollo";
     private static final String APP_ID = "app.id";
     private static final String APOLLO_META = "apollo.meta";
+    private static final String NAMESPACE = "namespace";
+    private static final String DEFAULT_NAMESPACE = "application";
     private static final Configuration FILE_CONFIG = ConfigurationFactory.CURRENT_FILE_INSTANCE;
     private static volatile Config config;
     private ExecutorService configOperateExecutor;
@@ -66,7 +68,7 @@ public class ApolloConfiguration extends AbstractConfiguration {
         if (null == config) {
             synchronized (ApolloConfiguration.class) {
                 if (null == config) {
-                    config = ConfigService.getAppConfig();
+                    config = ConfigService.getConfig(FILE_CONFIG.getConfig(getApolloNamespaceKey(), DEFAULT_NAMESPACE));
                     configOperateExecutor = new ThreadPoolExecutor(CORE_CONFIG_OPERATE_THREAD,
                         MAX_CONFIG_OPERATE_THREAD, Integer.MAX_VALUE, TimeUnit.MILLISECONDS,
                         new LinkedBlockingQueue<>(),
@@ -179,6 +181,10 @@ public class ApolloConfiguration extends AbstractConfiguration {
 
     private static String getApolloAppIdFileKey() {
         return FILE_ROOT_CONFIG + FILE_CONFIG_SPLIT_CHAR + REGISTRY_TYPE + FILE_CONFIG_SPLIT_CHAR + APP_ID;
+    }
+
+    private static String getApolloNamespaceKey() {
+        return FILE_ROOT_CONFIG + FILE_CONFIG_SPLIT_CHAR + REGISTRY_TYPE + FILE_CONFIG_SPLIT_CHAR + NAMESPACE;
     }
 
     private ConfigurationChangeType getChangeType(PropertyChangeType changeType) {
