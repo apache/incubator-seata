b5b7c7ccab31627f3426006e7fb1cb6db4898e37
zhichen <zhichen.lc@alibaba-inc.com>
2020-01-10 15:58:01 +0800

    update: update Copyright

--- console/src/main/resources/static/console-fe/src/pages/Login/Login.tsx
+++ console/src/main/resources/static/console-fe/src/pages/Login/Login.tsx
@@ -1,3 +1,18 @@
+/**
+ * Copyright 1999-2019 Seata.io Group.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import React from 'react';
 import { Card, Form, Input, ConfigProvider, Field } from '@alicloud/console-components';
 import { withRouter } from 'react-router-dom';
@@ -57,7 +72,7 @@ class Login extends React.Component<PropsType> {
           }}
         >
           <div className="vertical-middle product-area">
-            <img className="product-logo" src="img/nacos.png" />
+            <img className="product-logo" src="img/seata_logo.png" />
             <p className="product-desc">
               an easy-to-use dynamic service discovery, configuration and service management
               platform for building cloud native applications
