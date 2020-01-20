ee66a9fd08d256da091221e6331a1ca89c614724
lorne.cl <long_187@126.com>
2020-01-20 17:15:03 +0800

    handing saga transaction timeout

--- saga/seata-saga-statelang/src/main/java/io/seata/saga/statelang/domain/StateMachineInstance.java
+++ saga/seata-saga-statelang/src/main/java/io/seata/saga/statelang/domain/StateMachineInstance.java
@@ -327,4 +327,11 @@ public interface StateMachineInstance {
      * @param serializedException the serialized exception
      */
     void setSerializedException(Object serializedException);
+
+    /**
+     * is timeout
+     * @param timeoutMillis
+     * @return
+     */
+    boolean isTimeout(int timeoutMillis);
 }
