512eab731cc548908db83dae6ef0328a80fb4deb
jaspercloud <276726581@qq.com>
2020-02-08 01:12:27 +0800

    gson timestampFactory

--- rm-datasource/src/main/java/io/seata/rm/datasource/undo/parser/GsonUndoLogParser.java
+++ rm-datasource/src/main/java/io/seata/rm/datasource/undo/parser/GsonUndoLogParser.java
@@ -16,10 +16,19 @@
 package io.seata.rm.datasource.undo.parser;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.TypeAdapter;
+import com.google.gson.TypeAdapterFactory;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
 import io.seata.common.Constants;
 import io.seata.common.loader.LoadLevel;
 import io.seata.rm.datasource.undo.BranchUndoLog;
 import io.seata.rm.datasource.undo.UndoLogParser;
+
+import java.io.IOException;
+import java.sql.Timestamp;
 
 /**
  * The type Json based undo log parser.
@@ -31,7 +40,16 @@ public class GsonUndoLogParser implements UndoLogParser {
 
     public static final String NAME = "gson";
 
-    private Gson gson = new Gson();
+    private static TypeAdapterFactory timestampFactory = new TimestampTypeAdapterFactory();
+
+    private static Gson gson;
+
+    static {
+        gson = new GsonBuilder()
+                .serializeNulls()
+                .registerTypeAdapterFactory(timestampFactory)
+                .create();
+    }
 
     @Override
     public String getName() {
@@ -53,5 +71,29 @@ public class GsonUndoLogParser implements UndoLogParser {
     public BranchUndoLog decode(byte[] bytes) {
         String text = new String(bytes, Constants.DEFAULT_CHARSET);
         return gson.fromJson(text, BranchUndoLog.class);
+    }
+
+    private static class TimestampTypeAdapterFactory implements TypeAdapterFactory {
+
+        @Override
+        public <T> TypeAdapter<T> create(Gson gson, TypeToken<T> type) {
+            if (type.getRawType() != Timestamp.class) {
+                return null;
+            }
+
+            final TypeAdapter<Long> dateTypeAdapter = gson.getAdapter(Long.class);
+            return (TypeAdapter<T>) new TypeAdapter<Timestamp>() {
+                @Override
+                public Timestamp read(JsonReader in) throws IOException {
+                    Long date = dateTypeAdapter.read(in);
+                    return date != null ? new Timestamp(date) : null;
+                }
+
+                @Override
+                public void write(JsonWriter out, Timestamp value) throws IOException {
+                    dateTypeAdapter.write(out, value.getTime());
+                }
+            };
+        }
     }
 }
