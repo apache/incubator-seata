46aec72c01ce0a9900b0b3ec3710e3a5c94e6379
FUNKYE <364176773@qq.com>
2020-02-12 00:41:01 +0800

    adjust calss structure

--- core/src/main/java/io/seata/core/constants/DubboConstants.java
+++ core/src/main/java/io/seata/core/constants/DubboConstants.java
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package io.seata.integration.dubbo;
+package io.seata.core.constants;
 
 /**
  * DubboConstants
