53f08166f91b3efcb164b3e2196e545a09a606cc
jaspercloud <276726581@qq.com>
2020-02-13 10:13:43 +0800

    feature: support postgresql(client and server) (#2200)
    Co-authored-by: will <349071347@qq.com>

---
+++ sqlparser/seata-sqlparser-druid/src/main/java/io/seata/sqlparser/druid/postgresql/PostgresqlDeleteRecognizer.java
@@ -0,0 +1,84 @@
+/*
+ *  Copyright 1999-2019 Seata.io Group.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package io.seata.sqlparser.druid.postgresql;
+
+import com.alibaba.druid.sql.ast.SQLExpr;
+import com.alibaba.druid.sql.ast.SQLStatement;
+import com.alibaba.druid.sql.ast.statement.SQLExprTableSource;
+import com.alibaba.druid.sql.dialect.postgresql.ast.stmt.PGDeleteStatement;
+import com.alibaba.druid.sql.dialect.postgresql.visitor.PGOutputVisitor;
+import io.seata.sqlparser.ParametersHolder;
+import io.seata.sqlparser.SQLDeleteRecognizer;
+import io.seata.sqlparser.SQLType;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author japsercloud
+ */
+public class PostgresqlDeleteRecognizer extends BasePostgresqlRecognizer implements SQLDeleteRecognizer {
+
+    private final PGDeleteStatement ast;
+
+    /**
+     * Instantiates a new Postgresql delete recognizer.
+     *
+     * @param originalSQL the original sql
+     * @param ast the ast
+     */
+    public PostgresqlDeleteRecognizer(String originalSQL, SQLStatement ast) {
+        super(originalSQL);
+        this.ast = (PGDeleteStatement) ast;
+    }
+
+    @Override
+    public SQLType getSQLType() {
+        return SQLType.DELETE;
+    }
+
+    @Override
+    public String getTableAlias() {
+        return ast.getTableSource().getAlias();
+    }
+
+    @Override
+    public String getTableName() {
+        StringBuilder sb = new StringBuilder();
+        PGOutputVisitor visitor = new PGOutputVisitor(sb) {
+
+            @Override
+            public boolean visit(SQLExprTableSource x) {
+                printTableSourceExpr(x.getExpr());
+                return false;
+            }
+        };
+        visitor.visit((SQLExprTableSource) ast.getTableSource());
+        return sb.toString();
+    }
+
+    @Override
+    public String getWhereCondition(final ParametersHolder parametersHolder,
+        final ArrayList<List<Object>> paramAppenderList) {
+        SQLExpr where = ast.getWhere();
+        return super.getWhereCondition(where, parametersHolder, paramAppenderList);
+    }
+
+    @Override
+    public String getWhereCondition() {
+        SQLExpr where = ast.getWhere();
+        return super.getWhereCondition(where);
+    }
+}
