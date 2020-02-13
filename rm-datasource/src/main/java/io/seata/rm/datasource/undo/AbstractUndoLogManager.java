3afd1180790da62b9a2151802ec0b2d89b96c116
jaspercloud <276726581@qq.com>
2020-02-13 10:13:43 +0800

    feature: support postgresql(client and server) (#2200)
    Co-authored-by: will <349071347@qq.com>

--- rm-datasource/src/main/java/io/seata/rm/datasource/undo/AbstractUndoLogManager.java
+++ rm-datasource/src/main/java/io/seata/rm/datasource/undo/AbstractUndoLogManager.java
@@ -15,20 +15,7 @@
  */
 package io.seata.rm.datasource.undo;
 
-import java.sql.Blob;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.SQLIntegrityConstraintViolationException;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
 import io.seata.common.Constants;
-import io.seata.common.util.BlobUtils;
 import io.seata.common.util.CollectionUtils;
 import io.seata.config.ConfigurationFactory;
 import io.seata.core.constants.ClientTableColumnsName;
@@ -42,6 +29,17 @@ import io.seata.rm.datasource.sql.struct.TableMeta;
 import io.seata.rm.datasource.sql.struct.TableMetaCacheFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import static io.seata.core.exception.TransactionExceptionCode.BranchRollbackFailed_Retriable;
 
@@ -265,8 +263,7 @@ public abstract class AbstractUndoLogManager implements UndoLogManager {
 
                     String contextString = rs.getString(ClientTableColumnsName.UNDO_LOG_CONTEXT);
                     Map<String, String> context = parseContext(contextString);
-                    Blob b = rs.getBlob(ClientTableColumnsName.UNDO_LOG_ROLLBACK_INFO);
-                    byte[] rollbackInfo = BlobUtils.blob2Bytes(b);
+                    byte[] rollbackInfo = getRollbackInfo(rs);
 
                     String serializer = context == null ? null : context.get(UndoLogConstants.SERIALIZER_KEY);
                     UndoLogParser parser = serializer == null ? UndoLogParserFactory.getInstance()
@@ -382,4 +379,13 @@ public abstract class AbstractUndoLogManager implements UndoLogManager {
      */
     protected abstract void insertUndoLogWithNormal(String xid, long branchId, String rollbackCtx,
                                                     byte[] undoLogContent, Connection conn) throws SQLException;
+
+    /**
+     * RollbackInfo to bytes
+     *
+     * @param rs
+     * @return
+     * @throws SQLException
+     */
+    protected abstract byte[] getRollbackInfo(ResultSet rs) throws SQLException;
 }
