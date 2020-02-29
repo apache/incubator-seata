cf8bbb61db84e6d8d25878f06ca2931bee438786
Hao Zhibei <837948266@qq.com>
2020-02-29 10:15:49 +0800

    set dbSessionMgr scope prototype

--- server/src/main/java/io/seata/server/session/db/DataBaseSessionManager.java
+++ server/src/main/java/io/seata/server/session/db/DataBaseSessionManager.java
@@ -15,6 +15,7 @@
  */
 package io.seata.server.session.db;
 
+import io.seata.common.loader.Scope;
 import java.util.Collection;
 import java.util.List;
 
@@ -46,7 +47,7 @@ import org.slf4j.LoggerFactory;
  *
  * @author zhangsen
  */
-@LoadLevel(name = "db")
+@LoadLevel(name = "db",scope = Scope.PROTOTYPE)
 public class DataBaseSessionManager extends AbstractSessionManager
     implements SessionManager, SessionLifecycleListener, Initialize, Reloadable {
 
