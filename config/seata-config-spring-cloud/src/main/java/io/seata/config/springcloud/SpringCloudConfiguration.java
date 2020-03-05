8ddda385d60aad82b1fefa4d3f682736a22af593
zjinlei <75718677@qq.com>
2020-03-05 20:01:19 +0800

    optimize: adjust the processing logic for unsupported listeners (#2354)

--- config/seata-config-spring-cloud/src/main/java/io/seata/config/springcloud/SpringCloudConfiguration.java
+++ config/seata-config-spring-cloud/src/main/java/io/seata/config/springcloud/SpringCloudConfiguration.java
@@ -21,10 +21,12 @@ import io.seata.common.holder.ObjectHolder;
 import io.seata.common.util.StringUtils;
 import io.seata.config.AbstractConfiguration;
 import io.seata.config.ConfigurationChangeListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 
 public class SpringCloudConfiguration extends AbstractConfiguration {
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpringCloudConfiguration.class);
     private static final String CONFIG_TYPE = "SpringCloudConfig";
     private static volatile SpringCloudConfiguration instance;
     private static final String PREFIX = "seata.";
@@ -61,31 +63,30 @@ public class SpringCloudConfiguration extends AbstractConfiguration {
 
     @Override
     public boolean putConfig(String dataId, String content, long timeoutMills) {
-        throw new UnsupportedOperationException();
+        return false;
     }
 
     @Override
     public boolean putConfigIfAbsent(String dataId, String content, long timeoutMills) {
-        throw new UnsupportedOperationException();
+        return false;
     }
 
     @Override
     public boolean removeConfig(String dataId, long timeoutMills) {
-        throw new UnsupportedOperationException();
+        return false;
     }
 
     @Override
     public void addConfigListener(String dataId, ConfigurationChangeListener listener) {
-        throw new UnsupportedOperationException();
+        LOGGER.warn("dynamic listening is not supported spring cloud config");
     }
 
     @Override
     public void removeConfigListener(String dataId, ConfigurationChangeListener listener) {
-        throw new UnsupportedOperationException();
     }
 
     @Override
     public Set<ConfigurationChangeListener> getConfigListeners(String dataId) {
-        throw new UnsupportedOperationException();
+        return null;
     }
 }
