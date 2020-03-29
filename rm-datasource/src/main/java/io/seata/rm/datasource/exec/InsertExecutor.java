318a33b591de44c17db579cfe5ecb5fbd8b7ef3a
zjinlei <75718677@qq.com>
2020-03-29 11:16:33 +0800

    import layout

--- rm-datasource/src/main/java/io/seata/rm/datasource/exec/InsertExecutor.java
+++ rm-datasource/src/main/java/io/seata/rm/datasource/exec/InsertExecutor.java
@@ -15,6 +15,14 @@
  */
 package io.seata.rm.datasource.exec;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import io.seata.common.exception.NotSupportYetException;
 import io.seata.common.exception.ShouldNeverHappenException;
 import io.seata.common.util.CollectionUtils;
@@ -31,14 +39,6 @@ import io.seata.sqlparser.struct.SqlSequenceExpr;
 import io.seata.sqlparser.util.JdbcConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 /**
  * The type Insert executor.
