2c1f175ad279fdc49a7688dc2af75142a4b9d852
zhangchenghui <zhangchenghui.dev@gmail.com>
2020-03-31 13:31:17 +0800

    fix insert space

--- core/src/main/java/io/seata/core/store/db/sql/lock/OceanbaseLockStoreSql.java
+++ core/src/main/java/io/seata/core/store/db/sql/lock/OceanbaseLockStoreSql.java
@@ -29,8 +29,8 @@ public class OceanbaseLockStoreSql extends AbstractLockStoreSql {
     /**
      * The constant INSERT_LOCK_SQL_OCEANBASE.
      */
-    private static final String INSERT_LOCK_SQL_OCEANBASE = "insert into " + LOCK_TABLE_PLACE_HOLD + "(" + ALL_COLUMNS + ")" +
-        "values (?, ?, ?, ?, ?, ?, ?, now(), now())";
+    private static final String INSERT_LOCK_SQL_OCEANBASE = "insert into " + LOCK_TABLE_PLACE_HOLD + "(" + ALL_COLUMNS + ")"
+        + "values (?, ?, ?, ?, ?, ?, ?, now(), now())";
 
     @Override
     public String getInsertLockSQL(String lockTable) {
