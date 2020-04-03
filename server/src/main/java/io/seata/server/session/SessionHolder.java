bab325a023e78a7e37a8b9e069171fc2692cb2df
FUNKYE <364176773@qq.com>
2020-04-03 13:51:07 +0800

    bugfix: fix bug of session store path value judgment (#2505)

--- server/src/main/java/io/seata/server/session/SessionHolder.java
+++ server/src/main/java/io/seata/server/session/SessionHolder.java
@@ -98,7 +98,7 @@ public class SessionHolder {
             //file store
             String sessionStorePath = CONFIG.getConfig(ConfigurationKeys.STORE_FILE_DIR,
                 DEFAULT_SESSION_STORE_FILE_DIR);
-            if (sessionStorePath == null) {
+            if (StringUtils.isBlank(sessionStorePath)) {
                 throw new StoreException("the {store.file.dir} is empty.");
             }
             ROOT_SESSION_MANAGER = EnhancedServiceLoader.load(SessionManager.class, StoreMode.FILE.getName(),
