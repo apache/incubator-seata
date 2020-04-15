988f81bcb74980414c9716bc22622fb378b60060
王良 <841369634@qq.com>
2020-04-15 12:11:40 +0800

    代码按p3c规范调整。

--- saga/seata-saga-processctrl/src/main/java/io/seata/saga/proctrl/ProcessUtil.java
+++ saga/seata-saga-processctrl/src/main/java/io/seata/saga/proctrl/ProcessUtil.java
@@ -6,7 +6,6 @@ import io.seata.core.model.BranchType;
 import io.seata.saga.statelang.domain.DomainConstants;
 import io.seata.saga.statelang.domain.StateMachineInstance;
 import io.seata.tm.api.GlobalTransaction;
-
 import java.util.Map;
 
 /**
@@ -16,75 +15,75 @@ import java.util.Map;
  */
 public class ProcessUtil {
 
-	private ProcessUtil() {
-	}
+    private ProcessUtil() {
+    }
 
-	/**
-	 * Gets xid from saga process context.
-	 *
-	 * @return the xid
-	 */
-	public static String getXidFromProcessContext(ProcessContext context) {
-		String xid = null;
-		Map<String, Object> contextVariable = (Map<String, Object>) context.getVariable(DomainConstants.VAR_NAME_STATEMACHINE_CONTEXT);
-		if (contextVariable != null && contextVariable.containsKey(DomainConstants.VAR_NAME_GLOBAL_TX)) {
-			GlobalTransaction globalTransaction = (GlobalTransaction) contextVariable.get(DomainConstants.VAR_NAME_GLOBAL_TX);
-			xid = globalTransaction.getXid();
-		} else {
-			StateMachineInstance smi = (StateMachineInstance) context.getVariable(DomainConstants.VAR_NAME_STATEMACHINE_INST);
-			if (smi != null) {
-				xid = smi.getId();
-			}
-		}
-		return xid;
-	}
+    /**
+     * Gets xid from saga process context.
+     *
+     * @return the xid
+     */
+    public static String getXidFromProcessContext(ProcessContext context) {
+        String xid = null;
+        Map<String, Object> contextVariable = (Map<String, Object>) context.getVariable(DomainConstants.VAR_NAME_STATEMACHINE_CONTEXT);
+        if (contextVariable != null && contextVariable.containsKey(DomainConstants.VAR_NAME_GLOBAL_TX)) {
+            GlobalTransaction globalTransaction = (GlobalTransaction) contextVariable.get(DomainConstants.VAR_NAME_GLOBAL_TX);
+            xid = globalTransaction.getXid();
+        } else {
+            StateMachineInstance smi = (StateMachineInstance) context.getVariable(DomainConstants.VAR_NAME_STATEMACHINE_INST);
+            if (smi != null) {
+                xid = smi.getId();
+            }
+        }
+        return xid;
+    }
 
-	/**
-	 * Run in the saga branch.
-	 *
-	 * @param context
-	 * @param runnable
-	 */
-	public static void runInSagaBranch(ProcessContext context, Runnable runnable) {
-		// get xidType
-		String xidType = RootContext.getXIDInterceptorType();
-		boolean isSagaBranch = xidType != null && xidType.endsWith(BranchType.SAGA.name());
+    /**
+     * Run in the saga branch.
+     *
+     * @param context
+     * @param runnable
+     */
+    public static void runInSagaBranch(ProcessContext context, Runnable runnable) {
+        // get xidType
+        String xidType = RootContext.getXIDInterceptorType();
+        boolean isSagaBranch = xidType != null && xidType.endsWith(BranchType.SAGA.name());
 
-		// before run
-		String xid = null;
-		boolean inGlobalTransaction = false;
-		if (!isSagaBranch) {
-			// unbind xid
-			xid = RootContext.unbind();
-			inGlobalTransaction = (xid != null);
-			if (xid == null) {
-				xid = getXidFromProcessContext(context);
-			}
+        // before run
+        String xid = null;
+        boolean inGlobalTransaction = false;
+        if (!isSagaBranch) {
+            // unbind xid
+            xid = RootContext.unbind();
+            inGlobalTransaction = (xid != null);
+            if (xid == null) {
+                xid = getXidFromProcessContext(context);
+            }
 
-			// bind xidType to saga
-			RootContext.bindInterceptorType(xid, BranchType.SAGA);
-		}
+            // bind xidType to saga
+            RootContext.bindInterceptorType(xid, BranchType.SAGA);
+        }
 
-		try {
-			runnable.run();
-		} finally {
-			// after run
-			if (!isSagaBranch) {
-				if (StringUtils.isNotBlank(xidType)) {
-					// bind xid
-					if (inGlobalTransaction) {
-						RootContext.bind(xid);
-					}
-					// bind xidType
-					RootContext.bindInterceptorType(xidType);
-				} else {
-					// unbind xid
-					RootContext.unbind();
-					// unbind xidType
-					RootContext.unbindInterceptorType();
-				}
-			}
-		}
-	}
+        try {
+            runnable.run();
+        } finally {
+            // after run
+            if (!isSagaBranch) {
+                if (StringUtils.isNotBlank(xidType)) {
+                    // bind xid
+                    if (inGlobalTransaction) {
+                        RootContext.bind(xid);
+                    }
+                    // bind xidType
+                    RootContext.bindInterceptorType(xidType);
+                } else {
+                    // unbind xid
+                    RootContext.unbind();
+                    // unbind xidType
+                    RootContext.unbindInterceptorType();
+                }
+            }
+        }
+    }
 
 }
