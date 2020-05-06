61b65c7f0cc78da041c89fc5db4dc5a9fd49ad12
JerryYin <superywj@foxmail.com>
2020-05-06 15:08:39 +0800

    revert file

--- integration/http/src/test/java/io/seata/integration/http/MockHttpServletRequest.java
+++ integration/http/src/test/java/io/seata/integration/http/MockHttpServletRequest.java
@@ -17,12 +17,13 @@ package io.seata.integration.http;
 
 import io.seata.core.context.RootContext;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.security.Principal;
-import java.util.Collection;
 import java.util.Enumeration;
 import java.util.Locale;
 import java.util.Map;
@@ -168,31 +169,6 @@ public class MockHttpServletRequest implements HttpServletRequest {
     }
 
     @Override
-    public boolean authenticate(HttpServletResponse httpServletResponse) throws IOException, ServletException {
-        return false;
-    }
-
-    @Override
-    public void login(String s, String s1) throws ServletException {
-
-    }
-
-    @Override
-    public void logout() throws ServletException {
-
-    }
-
-    @Override
-    public Collection<Part> getParts() throws IOException, IllegalStateException, ServletException {
-        return null;
-    }
-
-    @Override
-    public Part getPart(String s) throws IOException, IllegalStateException, ServletException {
-        return null;
-    }
-
-    @Override
     public Object getAttribute(String name) {
         return null;
     }
@@ -335,40 +311,5 @@ public class MockHttpServletRequest implements HttpServletRequest {
     @Override
     public int getLocalPort() {
         return 0;
-    }
-
-    @Override
-    public ServletContext getServletContext() {
-        return null;
-    }
-
-    @Override
-    public AsyncContext startAsync() {
-        return null;
-    }
-
-    @Override
-    public AsyncContext startAsync(ServletRequest servletRequest, ServletResponse servletResponse) {
-        return null;
-    }
-
-    @Override
-    public boolean isAsyncStarted() {
-        return false;
-    }
-
-    @Override
-    public boolean isAsyncSupported() {
-        return false;
-    }
-
-    @Override
-    public AsyncContext getAsyncContext() {
-        return null;
-    }
-
-    @Override
-    public DispatcherType getDispatcherType() {
-        return null;
     }
 }
