bb5d0b378db4bd0c9ba2f67606976374c83dc218
zhangchenghui <zhangchenghui.dev@gmail.com>
2020-05-19 21:37:20 +0800

    registerProcessor

--- core/src/main/java/io/seata/core/rpc/netty/RpcServer.java
+++ core/src/main/java/io/seata/core/rpc/netty/RpcServer.java
@@ -59,36 +59,10 @@ public class RpcServer extends AbstractRpcRemotingServer {
      */
     @Override
     public void init() {
-        // registry processor
-        // 1. registry on request message processor
-        ServerOnRequestProcessor onRequestProcessor =
-            new ServerOnRequestProcessor(this, getTransactionMessageHandler());
-        registerProcessor(MessageType.TYPE_BRANCH_REGISTER, onRequestProcessor, messageExecutor);
-        registerProcessor(MessageType.TYPE_BRANCH_STATUS_REPORT, onRequestProcessor, messageExecutor);
-        registerProcessor(MessageType.TYPE_GLOBAL_BEGIN, onRequestProcessor, messageExecutor);
-        registerProcessor(MessageType.TYPE_GLOBAL_COMMIT, onRequestProcessor, messageExecutor);
-        registerProcessor(MessageType.TYPE_GLOBAL_LOCK_QUERY, onRequestProcessor, messageExecutor);
-        registerProcessor(MessageType.TYPE_GLOBAL_REPORT, onRequestProcessor, messageExecutor);
-        registerProcessor(MessageType.TYPE_GLOBAL_ROLLBACK, onRequestProcessor, messageExecutor);
-        registerProcessor(MessageType.TYPE_GLOBAL_STATUS, onRequestProcessor, messageExecutor);
-        registerProcessor(MessageType.TYPE_SEATA_MERGE, onRequestProcessor, messageExecutor);
-        // 2. registry on response message processor
-        ServerOnResponseProcessor onResponseProcessor =
-            new ServerOnResponseProcessor(getTransactionMessageHandler(), getFutures());
-        registerProcessor(MessageType.TYPE_BRANCH_COMMIT_RESULT, onResponseProcessor, messageExecutor);
-        registerProcessor(MessageType.TYPE_BRANCH_ROLLBACK_RESULT, onResponseProcessor, messageExecutor);
-        // 3. registry rm message processor
-        RegRmProcessor regRmProcessor = new RegRmProcessor(this, null);
-        registerProcessor(MessageType.TYPE_REG_RM, regRmProcessor, messageExecutor);
-        // 4. registry tm message processor
-        RegTmProcessor regTmProcessor = new RegTmProcessor(this, null);
-        registerProcessor(MessageType.TYPE_REG_CLT, regTmProcessor, null);
-        // 5. registry heartbeat message processor
-        ServerHeartbeatProcessor heartbeatMessageProcessor = new ServerHeartbeatProcessor(this);
-        registerProcessor(MessageType.TYPE_HEARTBEAT_MSG, heartbeatMessageProcessor, null);
-
-        super.setChannelHandlers(new ServerHandler());
         super.init();
+        setChannelHandlers(new ServerHandler());
+        // registry processor
+        registerProcessor();
     }
 
     /**
@@ -212,5 +186,34 @@ public class RpcServer extends AbstractRpcRemotingServer {
     public void registerProcessor(int messageType, RemotingProcessor processor, ExecutorService executor) {
         Pair<RemotingProcessor, ExecutorService> pair = new Pair<>(processor, executor);
         this.processorTable.put(messageType, pair);
+    }
+
+    private void registerProcessor() {
+        // 1. registry on request message processor
+        ServerOnRequestProcessor onRequestProcessor =
+            new ServerOnRequestProcessor(this, getTransactionMessageHandler());
+        registerProcessor(MessageType.TYPE_BRANCH_REGISTER, onRequestProcessor, messageExecutor);
+        registerProcessor(MessageType.TYPE_BRANCH_STATUS_REPORT, onRequestProcessor, messageExecutor);
+        registerProcessor(MessageType.TYPE_GLOBAL_BEGIN, onRequestProcessor, messageExecutor);
+        registerProcessor(MessageType.TYPE_GLOBAL_COMMIT, onRequestProcessor, messageExecutor);
+        registerProcessor(MessageType.TYPE_GLOBAL_LOCK_QUERY, onRequestProcessor, messageExecutor);
+        registerProcessor(MessageType.TYPE_GLOBAL_REPORT, onRequestProcessor, messageExecutor);
+        registerProcessor(MessageType.TYPE_GLOBAL_ROLLBACK, onRequestProcessor, messageExecutor);
+        registerProcessor(MessageType.TYPE_GLOBAL_STATUS, onRequestProcessor, messageExecutor);
+        registerProcessor(MessageType.TYPE_SEATA_MERGE, onRequestProcessor, messageExecutor);
+        // 2. registry on response message processor
+        ServerOnResponseProcessor onResponseProcessor =
+            new ServerOnResponseProcessor(getTransactionMessageHandler(), getFutures());
+        registerProcessor(MessageType.TYPE_BRANCH_COMMIT_RESULT, onResponseProcessor, messageExecutor);
+        registerProcessor(MessageType.TYPE_BRANCH_ROLLBACK_RESULT, onResponseProcessor, messageExecutor);
+        // 3. registry rm message processor
+        RegRmProcessor regRmProcessor = new RegRmProcessor(this, null);
+        registerProcessor(MessageType.TYPE_REG_RM, regRmProcessor, messageExecutor);
+        // 4. registry tm message processor
+        RegTmProcessor regTmProcessor = new RegTmProcessor(this, null);
+        registerProcessor(MessageType.TYPE_REG_CLT, regTmProcessor, null);
+        // 5. registry heartbeat message processor
+        ServerHeartbeatProcessor heartbeatMessageProcessor = new ServerHeartbeatProcessor(this);
+        registerProcessor(MessageType.TYPE_HEARTBEAT_MSG, heartbeatMessageProcessor, null);
     }
 }
