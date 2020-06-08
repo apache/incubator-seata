ee25aa5f4d5f3162fd423c18a56db86b1533088c
will <349071347@qq.com>
2020-06-08 14:06:50 +0800

    fix could not commit when set commit retry count was zero.

--- tm/src/main/java/io/seata/tm/api/DefaultGlobalTransaction.java
+++ tm/src/main/java/io/seata/tm/api/DefaultGlobalTransaction.java
@@ -120,7 +120,7 @@ public class DefaultGlobalTransaction implements GlobalTransaction {
             return;
         }
         assertXIDNotNull();
-        int retry = COMMIT_RETRY_COUNT;
+        int retry = COMMIT_RETRY_COUNT <= 0 ? 1 : COMMIT_RETRY_COUNT;;
         try {
             while (retry > 0) {
                 try {
