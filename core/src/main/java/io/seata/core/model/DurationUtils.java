ba481ad6eebad1d3659f027d8be5f1c008ba3159
王良 <841369634@qq.com>
2020-06-15 23:06:52 +0800

    fix style

--- core/src/main/java/io/seata/core/model/DurationUtils.java
+++ core/src/main/java/io/seata/core/model/DurationUtils.java
@@ -1,3 +1,18 @@
+/*
+ *  Copyright 1999-2019 Seata.io Group.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package io.seata.core.model;
 
 import io.seata.common.util.StringUtils;
@@ -11,111 +26,111 @@ import java.time.Duration;
  */
 public final class DurationUtils {
 
-	private DurationUtils() {
-	}
+    private DurationUtils() {
+    }
 
-	/**
-	 * duration string to duration
-	 *
-	 * @param durationStr
-	 * @return duration
-	 */
-	public static Duration toDuration(String durationStr) {
-		if (StringUtils.isBlank(durationStr)) {
-			throw new IllegalArgumentException("Illegal time string: " + durationStr);
-		}
+    /**
+     * duration string to duration
+     *
+     * @param durationStr
+     * @return duration
+     */
+    public static Duration toDuration(String durationStr) {
+        if (StringUtils.isBlank(durationStr)) {
+            throw new IllegalArgumentException("Illegal time string: " + durationStr);
+        }
 
-		durationStr = durationStr.replaceAll("\\s", "");
+        durationStr = durationStr.replaceAll("\\s", "");
 
-		try {
-			char unit = durationStr.charAt(durationStr.length() - 1);
-			if (unit >= '0' && unit <= '9') {
-				long millis = Long.valueOf(durationStr);
-				return Duration.ofMillis(millis);
-			} else if (unit == 's' || unit == 'S') {
-				long seconds = Long.valueOf(durationStr.substring(0, durationStr.length() - 1));
-				return Duration.ofSeconds(seconds);
-			} else if (unit == 'm' || unit == 'M') {
-				long minutes = Long.valueOf(durationStr.substring(0, durationStr.length() - 1));
-				return Duration.ofMinutes(minutes);
-			} else if (unit == 'h' || unit == 'H') {
-				long hours = Long.valueOf(durationStr.substring(0, durationStr.length() - 1));
-				return Duration.ofHours(hours);
-			} else if (unit == 'd' || unit == 'D') {
-				long days = Long.valueOf(durationStr.substring(0, durationStr.length() - 1));
-				return Duration.ofDays(days);
-			} else {
-				//unknown unit
-				throw new Exception();
-			}
-		} catch (Exception e) {
-			throw new IllegalArgumentException("Illegal time string: " + durationStr);
-		}
-	}
+        try {
+            char unit = durationStr.charAt(durationStr.length() - 1);
+            if (unit >= '0' && unit <= '9') {
+                long millis = Long.valueOf(durationStr);
+                return Duration.ofMillis(millis);
+            } else if (unit == 's' || unit == 'S') {
+                long seconds = Long.valueOf(durationStr.substring(0, durationStr.length() - 1));
+                return Duration.ofSeconds(seconds);
+            } else if (unit == 'm' || unit == 'M') {
+                long minutes = Long.valueOf(durationStr.substring(0, durationStr.length() - 1));
+                return Duration.ofMinutes(minutes);
+            } else if (unit == 'h' || unit == 'H') {
+                long hours = Long.valueOf(durationStr.substring(0, durationStr.length() - 1));
+                return Duration.ofHours(hours);
+            } else if (unit == 'd' || unit == 'D') {
+                long days = Long.valueOf(durationStr.substring(0, durationStr.length() - 1));
+                return Duration.ofDays(days);
+            } else {
+                //unknown unit
+                throw new Exception();
+            }
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Illegal time string: " + durationStr);
+        }
+    }
 
-	/**
-	 * milliseconds to duration string
-	 *
-	 * @param millis milliseconds
-	 * @return duration string
-	 */
-	public static String millisToString(long millis) {
-		if (millis % 1000 != 0) {
-			return Long.toString(millis);
-		} else {
-			return secondsToString(millis / 1000);
-		}
-	}
+    /**
+     * milliseconds to duration string
+     *
+     * @param millis milliseconds
+     * @return duration string
+     */
+    public static String millisToString(long millis) {
+        if (millis % 1000 != 0) {
+            return Long.toString(millis);
+        } else {
+            return secondsToString(millis / 1000);
+        }
+    }
 
-	/**
-	 * seconds to duration string
-	 *
-	 * @param seconds seconds
-	 * @return duration string
-	 */
-	public static String secondsToString(long seconds) {
-		if (seconds % 60 != 0) {
-			return seconds + "s";
-		} else {
-			return minutesToString(seconds / 60);
-		}
-	}
+    /**
+     * seconds to duration string
+     *
+     * @param seconds seconds
+     * @return duration string
+     */
+    public static String secondsToString(long seconds) {
+        if (seconds % 60 != 0) {
+            return seconds + "s";
+        } else {
+            return minutesToString(seconds / 60);
+        }
+    }
 
-	/**
-	 * minutes to duration string
-	 *
-	 * @param minutes minutes
-	 * @return duration string
-	 */
-	public static String minutesToString(long minutes) {
-		if (minutes % 60 != 0) {
-			return minutes + "m";
-		} else {
-			return hoursToString(minutes / 60);
-		}
-	}
+    /**
+     * minutes to duration string
+     *
+     * @param minutes minutes
+     * @return duration string
+     */
+    public static String minutesToString(long minutes) {
+        if (minutes % 60 != 0) {
+            return minutes + "m";
+        } else {
+            return hoursToString(minutes / 60);
+        }
+    }
 
-	/**
-	 * hours to duration string
-	 *
-	 * @param hours hours
-	 * @return duration string
-	 */
-	public static String hoursToString(long hours) {
-		if (hours % 60 != 0) {
-			return hours + "h";
-		} else {
-			return daysToString(hours / 24);
-		}
-	}
+    /**
+     * hours to duration string
+     *
+     * @param hours hours
+     * @return duration string
+     */
+    public static String hoursToString(long hours) {
+        if (hours % 60 != 0) {
+            return hours + "h";
+        } else {
+            return daysToString(hours / 24);
+        }
+    }
 
-	/**
-	 * days to duration string
-	 *
-	 * @param days days
-	 * @return duration string
-	 */
-	public static String daysToString(long days) {
-		return days + "d";
-	}
+    /**
+     * days to duration string
+     *
+     * @param days days
+     * @return duration string
+     */
+    public static String daysToString(long days) {
+        return days + "d";
+    }
 }
--- core/src/test/java/io/seata/core/model/DurationUtilsTest.java
+++ core/src/test/java/io/seata/core/model/DurationUtilsTest.java
@@ -27,67 +27,67 @@ import java.time.Duration;
  */
 public class DurationUtilsTest {
 
-	@Test
-	public void testToDuration() {
-		Duration duration = DurationUtils.toDuration("1");
-		Assertions.assertEquals(duration.toMillis(), 1);
+    @Test
+    public void testToDuration() {
+        Duration duration = DurationUtils.toDuration("1");
+        Assertions.assertEquals(duration.toMillis(), 1);
 
-		duration = DurationUtils.toDuration("2s");
-		Assertions.assertEquals(duration.toMillis(), 2 * 1000);
+        duration = DurationUtils.toDuration("2s");
+        Assertions.assertEquals(duration.toMillis(), 2 * 1000);
 
-		duration = DurationUtils.toDuration("3m");
-		Assertions.assertEquals(duration.toMillis(), 3 * 60 * 1000);
+        duration = DurationUtils.toDuration("3m");
+        Assertions.assertEquals(duration.toMillis(), 3 * 60 * 1000);
 
-		duration = DurationUtils.toDuration("4h");
-		Assertions.assertEquals(duration.toMillis(), 4 * 60 * 60 * 1000);
+        duration = DurationUtils.toDuration("4h");
+        Assertions.assertEquals(duration.toMillis(), 4 * 60 * 60 * 1000);
 
-		duration = DurationUtils.toDuration("5d");
-		Assertions.assertEquals(duration.toMillis(), 5 * 24 * 60 * 60 * 1000);
+        duration = DurationUtils.toDuration("5d");
+        Assertions.assertEquals(duration.toMillis(), 5 * 24 * 60 * 60 * 1000);
 
-		Assertions.assertThrows(IllegalArgumentException.class, () -> {
-			DurationUtils.toDuration(null);
-		});
-		Assertions.assertThrows(IllegalArgumentException.class, () -> {
-			DurationUtils.toDuration("   ");
-		});
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            DurationUtils.toDuration(null);
+        });
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            DurationUtils.toDuration("   ");
+        });
 
-		Assertions.assertThrows(IllegalArgumentException.class, () -> {
-			DurationUtils.toDuration("1x");
-		});
-	}
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            DurationUtils.toDuration("1x");
+        });
+    }
 
-	@Test
-	public void testMillisToString() {
-		Assertions.assertEquals(DurationUtils.millisToString(1L), "1");
-		Assertions.assertEquals(DurationUtils.millisToString(22L * 1000), "22s");
-		Assertions.assertEquals(DurationUtils.millisToString(333L * 60 * 1000), "333m");
-		Assertions.assertEquals(DurationUtils.millisToString(4444L * 60 * 60 * 1000), "4444h");
-		Assertions.assertEquals(DurationUtils.millisToString(55555L * 24 * 60 * 60 * 1000), "55555d");
-	}
+    @Test
+    public void testMillisToString() {
+        Assertions.assertEquals(DurationUtils.millisToString(1L), "1");
+        Assertions.assertEquals(DurationUtils.millisToString(22L * 1000), "22s");
+        Assertions.assertEquals(DurationUtils.millisToString(333L * 60 * 1000), "333m");
+        Assertions.assertEquals(DurationUtils.millisToString(4444L * 60 * 60 * 1000), "4444h");
+        Assertions.assertEquals(DurationUtils.millisToString(55555L * 24 * 60 * 60 * 1000), "55555d");
+    }
 
-	@Test
-	public void testSecondsToString() {
-		Assertions.assertEquals(DurationUtils.secondsToString(22), "22s");
-		Assertions.assertEquals(DurationUtils.secondsToString(333L * 60), "333m");
-		Assertions.assertEquals(DurationUtils.secondsToString(4444L * 60 * 60), "4444h");
-		Assertions.assertEquals(DurationUtils.secondsToString(55555L * 24 * 60 * 60), "55555d");
-	}
+    @Test
+    public void testSecondsToString() {
+        Assertions.assertEquals(DurationUtils.secondsToString(22), "22s");
+        Assertions.assertEquals(DurationUtils.secondsToString(333L * 60), "333m");
+        Assertions.assertEquals(DurationUtils.secondsToString(4444L * 60 * 60), "4444h");
+        Assertions.assertEquals(DurationUtils.secondsToString(55555L * 24 * 60 * 60), "55555d");
+    }
 
-	@Test
-	public void testMinutesToString() {
-		Assertions.assertEquals(DurationUtils.minutesToString(333), "333m");
-		Assertions.assertEquals(DurationUtils.minutesToString(4444L * 60), "4444h");
-		Assertions.assertEquals(DurationUtils.minutesToString(55555L * 24 * 60), "55555d");
-	}
+    @Test
+    public void testMinutesToString() {
+        Assertions.assertEquals(DurationUtils.minutesToString(333), "333m");
+        Assertions.assertEquals(DurationUtils.minutesToString(4444L * 60), "4444h");
+        Assertions.assertEquals(DurationUtils.minutesToString(55555L * 24 * 60), "55555d");
+    }
 
-	@Test
-	public void testHoursToString() {
-		Assertions.assertEquals(DurationUtils.hoursToString(4444), "4444h");
-		Assertions.assertEquals(DurationUtils.hoursToString(55555L * 24), "55555d");
-	}
+    @Test
+    public void testHoursToString() {
+        Assertions.assertEquals(DurationUtils.hoursToString(4444), "4444h");
+        Assertions.assertEquals(DurationUtils.hoursToString(55555L * 24), "55555d");
+    }
 
-	@Test
-	public void testDaysToString() {
-		Assertions.assertEquals(DurationUtils.daysToString(55555), "55555d");
-	}
+    @Test
+    public void testDaysToString() {
+        Assertions.assertEquals(DurationUtils.daysToString(55555), "55555d");
+    }
 }
