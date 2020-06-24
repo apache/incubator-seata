ba59eeee2b6d1e01aa6e2c7abbdecb8fd81605b7
lorne.cl <long_187@126.com>
2020-06-24 18:05:43 +0800

    saga support jackson json parser

--- rm-datasource/src/main/java/io/seata/rm/datasource/exec/InsertExecutor.java
+++ rm-datasource/src/main/java/io/seata/rm/datasource/exec/InsertExecutor.java
@@ -13,6 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
+
 package io.seata.rm.datasource.exec;
 
 import java.sql.SQLException;
