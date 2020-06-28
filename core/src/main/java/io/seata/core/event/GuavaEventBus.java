335bee01e00c42b3eb1d6bc8bf89bfe1fee8884b
jimin <slievrly@gmail.com>
2020-06-28 11:05:43 +0800

    optimize: downgrade check unlock and asynchronous (#2829)

--- core/src/main/java/io/seata/core/event/GuavaEventBus.java
+++ core/src/main/java/io/seata/core/event/GuavaEventBus.java
@@ -15,16 +15,39 @@
  */
 package io.seata.core.event;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import io.seata.common.thread.NamedThreadFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Default event bus implement with Guava EventBus.
  *
  * @author zhengyangyong
  */
 public class GuavaEventBus implements EventBus {
+    private static final Logger LOGGER = LoggerFactory.getLogger(GuavaEventBus.class);
     private final com.google.common.eventbus.EventBus eventBus;
 
     public GuavaEventBus(String identifier) {
-        this.eventBus = new com.google.common.eventbus.EventBus(identifier);
+        this(identifier, false);
+    }
+
+    public GuavaEventBus(String identifier, boolean async) {
+        if (!async) {
+            this.eventBus = new com.google.common.eventbus.EventBus(identifier);
+        } else {
+            final ExecutorService eventExecutor = new ThreadPoolExecutor(1, 1, Integer.MAX_VALUE, TimeUnit.MILLISECONDS,
+                new ArrayBlockingQueue<>(2048), new NamedThreadFactory(identifier, 1), (r, executor) -> {
+
+                LOGGER.warn("eventBus executor queue is full, size:{}", executor.getQueue().size());
+            });
+            this.eventBus = new com.google.common.eventbus.AsyncEventBus(identifier, eventExecutor);
+        }
     }
 
     @Override
