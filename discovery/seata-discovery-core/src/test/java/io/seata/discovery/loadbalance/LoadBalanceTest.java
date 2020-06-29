bd7a068befe9f84c27b165609358fcc7bbda5d4e
ph3636 <tianxingguang@kanzhun.com>
2020-06-29 23:55:30 +0800

    feature: support ConsistentHashLoadBalance

--- discovery/seata-discovery-core/src/test/java/io/seata/discovery/loadbalance/LoadBalanceTest.java
+++ discovery/seata-discovery-core/src/test/java/io/seata/discovery/loadbalance/LoadBalanceTest.java
@@ -21,7 +21,6 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.net.InetSocketAddress;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
