676f43b355f95643d4c7b450813219b472b3dab3
王良 <841369634@qq.com>
2020-06-30 09:52:42 +0800

    db模式整形数据字段改为不能为空，且默认值为0

--- script/server/db/oracle.sql
+++ script/server/db/oracle.sql
@@ -2,14 +2,14 @@
 -- the table to store GlobalSession data
 CREATE TABLE global_table
 (
-    xid                       VARCHAR2(128) NOT NULL,
-    transaction_id            NUMBER(19),
-    status                    NUMBER(3)     NOT NULL,
+    xid                       VARCHAR2(128)        NOT NULL,
+    transaction_id            NUMBER(19) DEFAULT 0 NOT NULL,
+    status                    NUMBER(3)            NOT NULL,
     application_id            VARCHAR2(32),
     transaction_service_group VARCHAR2(32),
     transaction_name          VARCHAR2(128),
-    timeout                   NUMBER(10),
-    begin_time                NUMBER(19),
+    timeout                   NUMBER(10) DEFAULT 0 NOT NULL,
+    begin_time                NUMBER(19) DEFAULT 0 NOT NULL,
     application_data          VARCHAR2(2000),
     gmt_create                TIMESTAMP(0),
     gmt_modified              TIMESTAMP(0),
@@ -22,13 +22,13 @@ CREATE INDEX idx_transaction_id ON global_table (transaction_id);
 -- the table to store BranchSession data
 CREATE TABLE branch_table
 (
-    branch_id         NUMBER(19)    NOT NULL,
-    xid               VARCHAR2(128) NOT NULL,
-    transaction_id    NUMBER(19),
+    branch_id         NUMBER(19)           NOT NULL,
+    xid               VARCHAR2(128)        NOT NULL,
+    transaction_id    NUMBER(19) DEFAULT 0 NOT NULL,
     resource_group_id VARCHAR2(32),
     resource_id       VARCHAR2(256),
     branch_type       VARCHAR2(8),
-    status            NUMBER(3),
+    status            NUMBER(3)            NOT NULL,
     client_id         VARCHAR2(64),
     application_data  VARCHAR2(2000),
     gmt_create        TIMESTAMP(6),
@@ -41,10 +41,10 @@ CREATE INDEX idx_xid ON branch_table (xid);
 -- the table to store lock data
 CREATE TABLE lock_table
 (
-    row_key        VARCHAR2(128) NOT NULL,
+    row_key        VARCHAR2(128)        NOT NULL,
     xid            VARCHAR2(96),
-    transaction_id NUMBER(19),
-    branch_id      NUMBER(19)    NOT NULL,
+    transaction_id NUMBER(19) DEFAULT 0 NOT NULL,
+    branch_id      NUMBER(19)           NOT NULL,
     resource_id    VARCHAR2(256),
     table_name     VARCHAR2(32),
     pk             VARCHAR2(36),
--- script/server/db/postgresql.sql
+++ script/server/db/postgresql.sql
@@ -2,14 +2,14 @@
 -- the table to store GlobalSession data
 CREATE TABLE IF NOT EXISTS public.global_table
 (
-    xid                       VARCHAR(128) NOT NULL,
-    transaction_id            BIGINT,
-    status                    SMALLINT     NOT NULL,
+    xid                       VARCHAR(128)       NOT NULL,
+    transaction_id            BIGINT   DEFAULT 0 NOT NULL,
+    status                    SMALLINT           NOT NULL,
     application_id            VARCHAR(32),
     transaction_service_group VARCHAR(32),
     transaction_name          VARCHAR(128),
-    timeout                   INT,
-    begin_time                BIGINT,
+    timeout                   INT      DEFAULT 0 NOT NULL,
+    begin_time                BIGINT   DEFAULT 0 NOT NULL,
     application_data          VARCHAR(2000),
     gmt_create                TIMESTAMP(0),
     gmt_modified              TIMESTAMP(0),
@@ -22,13 +22,13 @@ CREATE INDEX idx_transaction_id ON public.global_table (transaction_id);
 -- the table to store BranchSession data
 CREATE TABLE IF NOT EXISTS public.branch_table
 (
-    branch_id         BIGINT       NOT NULL,
-    xid               VARCHAR(128) NOT NULL,
-    transaction_id    BIGINT,
+    branch_id         BIGINT             NOT NULL,
+    xid               VARCHAR(128)       NOT NULL,
+    transaction_id    BIGINT   DEFAULT 0 NOT NULL,
     resource_group_id VARCHAR(32),
     resource_id       VARCHAR(256),
     branch_type       VARCHAR(8),
-    status            SMALLINT,
+    status            SMALLINT           NOT NULL,
     client_id         VARCHAR(64),
     application_data  VARCHAR(2000),
     gmt_create        TIMESTAMP(6),
@@ -43,8 +43,8 @@ CREATE TABLE IF NOT EXISTS public.lock_table
 (
     row_key        VARCHAR(128) NOT NULL,
     xid            VARCHAR(96),
-    transaction_id BIGINT,
-    branch_id      BIGINT       NOT NULL,
+    transaction_id BIGINT DEFAULT 0 NOT NULL,
+    branch_id      BIGINT           NOT NULL,
     resource_id    VARCHAR(256),
     table_name     VARCHAR(32),
     pk             VARCHAR(36),
