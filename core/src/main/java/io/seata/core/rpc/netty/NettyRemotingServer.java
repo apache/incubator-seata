451d58652d3ce8348e1ebd06e5154b37352bacad
zhangchenghui <zhangchenghui.dev@gmail.com>
2020-07-10 14:12:33 +0800

    add abstract remoting remoting server.

--- core/src/main/java/io/seata/core/rpc/netty/NettyRemotingServer.java
+++ core/src/main/java/io/seata/core/rpc/netty/NettyRemotingServer.java
@@ -16,23 +16,9 @@
 package io.seata.core.rpc.netty;
 
 import io.netty.channel.Channel;
-import io.netty.channel.ChannelDuplexHandler;
 import io.netty.channel.ChannelHandler;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelPromise;
-import io.netty.handler.timeout.IdleState;
-import io.netty.handler.timeout.IdleStateEvent;
-import io.seata.common.util.NetUtil;
-import io.seata.core.protocol.HeartbeatMessage;
 import io.seata.core.protocol.MessageType;
-import io.seata.core.protocol.ProtocolConstants;
-import io.seata.core.protocol.RpcMessage;
-import io.seata.core.rpc.RegisterCheckAuthHandler;
-import io.seata.core.rpc.RemotingServer;
-import io.seata.core.rpc.RpcContext;
 import io.seata.core.rpc.TransactionMessageHandler;
-import io.seata.core.rpc.processor.RemotingProcessor;
-import io.seata.core.rpc.processor.Pair;
 import io.seata.core.rpc.processor.server.RegRmProcessor;
 import io.seata.core.rpc.processor.server.RegTmProcessor;
 import io.seata.core.rpc.processor.server.ServerHeartbeatProcessor;
@@ -41,9 +27,7 @@ import io.seata.core.rpc.processor.server.ServerOnResponseProcessor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeoutException;
 
 /**
  * The netty remoting server.
@@ -52,15 +36,13 @@ import java.util.concurrent.TimeoutException;
  * @author xingfudeshi@gmail.com
  * @author zhangchenghui.dev@gmail.com
  */
-public class NettyRemotingServer extends AbstractNettyRemoting implements RemotingServer {
+public class NettyRemotingServer extends AbstractNettyRemotingServer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(NettyRemotingServer.class);
 
     private final NettyServerBootstrap serverBootstrap;
 
     private TransactionMessageHandler transactionMessageHandler;
-
-    private RegisterCheckAuthHandler checkAuthHandler;
 
     @Override
     public void init() {
@@ -70,6 +52,7 @@ public class NettyRemotingServer extends AbstractNettyRemoting implements Remoti
         super.init();
         serverBootstrap.start();
     }
+
     /**
      * Instantiates a new Rpc remoting server.
      *
@@ -78,50 +61,6 @@ public class NettyRemotingServer extends AbstractNettyRemoting implements Remoti
     public NettyRemotingServer(ThreadPoolExecutor messageExecutor) {
         super(messageExecutor);
         serverBootstrap = new NettyServerBootstrap(new NettyServerConfig());
-    }
-
-    @Override
-    public Object sendSyncRequest(String resourceId, String clientId, Object msg) throws TimeoutException {
-        Channel channel = ChannelManager.getChannel(resourceId, clientId);
-        if (channel == null) {
-            throw new RuntimeException("rm client is not connected. dbkey:" + resourceId + ",clientId:" + clientId);
-        }
-        RpcMessage rpcMessage = buildRequestMessage(msg, ProtocolConstants.MSGTYPE_RESQUEST_SYNC);
-        return super.sendSync(channel, rpcMessage, NettyServerConfig.getRpcRequestTimeout());
-    }
-
-    @Override
-    public Object sendSyncRequest(Channel channel, Object msg) throws TimeoutException {
-        if (channel == null) {
-            throw new RuntimeException("client is not connected");
-        }
-        RpcMessage rpcMessage = buildRequestMessage(msg, ProtocolConstants.MSGTYPE_RESQUEST_SYNC);
-        return super.sendSync(channel, rpcMessage, NettyServerConfig.getRpcRequestTimeout());
-    }
-
-    @Override
-    public void sendAsyncRequest(Channel channel, Object msg) {
-        if (channel == null) {
-            throw new RuntimeException("client is not connected");
-        }
-        RpcMessage rpcMessage = buildRequestMessage(msg, ProtocolConstants.MSGTYPE_RESQUEST_ONEWAY);
-        super.sendAsync(channel, rpcMessage);
-    }
-
-    @Override
-    public void sendAsyncResponse(RpcMessage rpcMessage, Channel channel, Object msg) {
-        Channel clientChannel = channel;
-        if (!(msg instanceof HeartbeatMessage)) {
-            clientChannel = ChannelManager.getSameClientChannel(channel);
-        }
-        if (clientChannel != null) {
-            RpcMessage rpcMsg = buildResponseMessage(rpcMessage, msg, msg instanceof HeartbeatMessage
-                ? ProtocolConstants.MSGTYPE_HEARTBEAT_RESPONSE
-                : ProtocolConstants.MSGTYPE_RESPONSE);
-            super.sendAsync(clientChannel, rpcMsg);
-        } else {
-            throw new RuntimeException("channel is error.");
-        }
     }
 
     /**
@@ -133,17 +72,16 @@ public class NettyRemotingServer extends AbstractNettyRemoting implements Remoti
         this.transactionMessageHandler = transactionMessageHandler;
     }
 
-    public TransactionMessageHandler getTransactionMessageHandler() {
+    public TransactionMessageHandler getHandler() {
         return transactionMessageHandler;
     }
-
 
     /**
      * Sets channel handlers.
      *
      * @param handlers the handlers
      */
-    public void setChannelHandlers(ChannelHandler... handlers) {
+    private void setChannelHandlers(ChannelHandler... handlers) {
         serverBootstrap.setChannelHandlers(handlers);
     }
 
@@ -171,17 +109,6 @@ public class NettyRemotingServer extends AbstractNettyRemoting implements Remoti
         super.destroy();
     }
 
-    /**
-     * Debug log.
-     *
-     * @param info the info
-     */
-    public void debugLog(String info) {
-        if (LOGGER.isDebugEnabled()) {
-            LOGGER.debug(info);
-        }
-    }
-
     @Override
     public void destroyChannel(String serverAddress, Channel channel) {
         if (LOGGER.isInfoEnabled()) {
@@ -191,164 +118,33 @@ public class NettyRemotingServer extends AbstractNettyRemoting implements Remoti
         channel.close();
     }
 
-    @Override
-    public void registerProcessor(int messageType, RemotingProcessor processor, ExecutorService executor) {
-        Pair<RemotingProcessor, ExecutorService> pair = new Pair<>(processor, executor);
-        this.processorTable.put(messageType, pair);
-    }
-
     private void registerProcessor() {
         // 1. registry on request message processor
         ServerOnRequestProcessor onRequestProcessor =
-            new ServerOnRequestProcessor(this, getTransactionMessageHandler());
-        registerProcessor(MessageType.TYPE_BRANCH_REGISTER, onRequestProcessor, messageExecutor);
-        registerProcessor(MessageType.TYPE_BRANCH_STATUS_REPORT, onRequestProcessor, messageExecutor);
-        registerProcessor(MessageType.TYPE_GLOBAL_BEGIN, onRequestProcessor, messageExecutor);
-        registerProcessor(MessageType.TYPE_GLOBAL_COMMIT, onRequestProcessor, messageExecutor);
-        registerProcessor(MessageType.TYPE_GLOBAL_LOCK_QUERY, onRequestProcessor, messageExecutor);
-        registerProcessor(MessageType.TYPE_GLOBAL_REPORT, onRequestProcessor, messageExecutor);
-        registerProcessor(MessageType.TYPE_GLOBAL_ROLLBACK, onRequestProcessor, messageExecutor);
-        registerProcessor(MessageType.TYPE_GLOBAL_STATUS, onRequestProcessor, messageExecutor);
-        registerProcessor(MessageType.TYPE_SEATA_MERGE, onRequestProcessor, messageExecutor);
+            new ServerOnRequestProcessor(this, getHandler());
+        super.registerProcessor(MessageType.TYPE_BRANCH_REGISTER, onRequestProcessor, messageExecutor);
+        super.registerProcessor(MessageType.TYPE_BRANCH_STATUS_REPORT, onRequestProcessor, messageExecutor);
+        super.registerProcessor(MessageType.TYPE_GLOBAL_BEGIN, onRequestProcessor, messageExecutor);
+        super.registerProcessor(MessageType.TYPE_GLOBAL_COMMIT, onRequestProcessor, messageExecutor);
+        super.registerProcessor(MessageType.TYPE_GLOBAL_LOCK_QUERY, onRequestProcessor, messageExecutor);
+        super.registerProcessor(MessageType.TYPE_GLOBAL_REPORT, onRequestProcessor, messageExecutor);
+        super.registerProcessor(MessageType.TYPE_GLOBAL_ROLLBACK, onRequestProcessor, messageExecutor);
+        super.registerProcessor(MessageType.TYPE_GLOBAL_STATUS, onRequestProcessor, messageExecutor);
+        super.registerProcessor(MessageType.TYPE_SEATA_MERGE, onRequestProcessor, messageExecutor);
         // 2. registry on response message processor
         ServerOnResponseProcessor onResponseProcessor =
-            new ServerOnResponseProcessor(getTransactionMessageHandler(), getFutures());
-        registerProcessor(MessageType.TYPE_BRANCH_COMMIT_RESULT, onResponseProcessor, messageExecutor);
-        registerProcessor(MessageType.TYPE_BRANCH_ROLLBACK_RESULT, onResponseProcessor, messageExecutor);
+            new ServerOnResponseProcessor(getHandler(), getFutures());
+        super.registerProcessor(MessageType.TYPE_BRANCH_COMMIT_RESULT, onResponseProcessor, messageExecutor);
+        super.registerProcessor(MessageType.TYPE_BRANCH_ROLLBACK_RESULT, onResponseProcessor, messageExecutor);
         // 3. registry rm message processor
         RegRmProcessor regRmProcessor = new RegRmProcessor(this);
-        registerProcessor(MessageType.TYPE_REG_RM, regRmProcessor, messageExecutor);
+        super.registerProcessor(MessageType.TYPE_REG_RM, regRmProcessor, messageExecutor);
         // 4. registry tm message processor
         RegTmProcessor regTmProcessor = new RegTmProcessor(this);
-        registerProcessor(MessageType.TYPE_REG_CLT, regTmProcessor, null);
+        super.registerProcessor(MessageType.TYPE_REG_CLT, regTmProcessor, null);
         // 5. registry heartbeat message processor
         ServerHeartbeatProcessor heartbeatMessageProcessor = new ServerHeartbeatProcessor(this);
-        registerProcessor(MessageType.TYPE_HEARTBEAT_MSG, heartbeatMessageProcessor, null);
-    }
-
-    private void closeChannelHandlerContext(ChannelHandlerContext ctx) {
-        if (LOGGER.isInfoEnabled()) {
-            LOGGER.info("closeChannelHandlerContext channel:" + ctx.channel());
-        }
-        ctx.disconnect();
-        ctx.close();
-    }
-
-    /**
-     * The type ServerHandler.
-     */
-    @ChannelHandler.Sharable
-    class ServerHandler extends ChannelDuplexHandler {
-
-        /**
-         * Channel read.
-         *
-         * @param ctx the ctx
-         * @param msg the msg
-         * @throws Exception the exception
-         */
-        @Override
-        public void channelRead(final ChannelHandlerContext ctx, Object msg) throws Exception {
-            if (!(msg instanceof RpcMessage)) {
-                return;
-            }
-            processMessage(ctx, (RpcMessage) msg);
-        }
-
-        @Override
-        public void channelWritabilityChanged(ChannelHandlerContext ctx) {
-            synchronized (lock) {
-                if (ctx.channel().isWritable()) {
-                    lock.notifyAll();
-                }
-            }
-            ctx.fireChannelWritabilityChanged();
-        }
-
-        /**
-         * Channel inactive.
-         *
-         * @param ctx the ctx
-         * @throws Exception the exception
-         */
-        @Override
-        public void channelInactive(ChannelHandlerContext ctx) throws Exception {
-            debugLog("inactive:" + ctx);
-            if (messageExecutor.isShutdown()) {
-                return;
-            }
-            handleDisconnect(ctx);
-            super.channelInactive(ctx);
-        }
-
-        private void handleDisconnect(ChannelHandlerContext ctx) {
-            final String ipAndPort = NetUtil.toStringAddress(ctx.channel().remoteAddress());
-            RpcContext rpcContext = ChannelManager.getContextFromIdentified(ctx.channel());
-            if (LOGGER.isInfoEnabled()) {
-                LOGGER.info(ipAndPort + " to server channel inactive.");
-            }
-            if (rpcContext != null && rpcContext.getClientRole() != null) {
-                rpcContext.release();
-                if (LOGGER.isInfoEnabled()) {
-                    LOGGER.info("remove channel:" + ctx.channel() + "context:" + rpcContext);
-                }
-            } else {
-                if (LOGGER.isInfoEnabled()) {
-                    LOGGER.info("remove unused channel:" + ctx.channel());
-                }
-            }
-        }
-
-        /**
-         * Exception caught.
-         *
-         * @param ctx   the ctx
-         * @param cause the cause
-         * @throws Exception the exception
-         */
-        @Override
-        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-            if (LOGGER.isInfoEnabled()) {
-                LOGGER.info("channel exx:" + cause.getMessage() + ",channel:" + ctx.channel());
-            }
-            ChannelManager.releaseRpcContext(ctx.channel());
-            super.exceptionCaught(ctx, cause);
-        }
-
-        /**
-         * User event triggered.
-         *
-         * @param ctx the ctx
-         * @param evt the evt
-         * @throws Exception the exception
-         */
-        @Override
-        public void userEventTriggered(ChannelHandlerContext ctx, Object evt) {
-            if (evt instanceof IdleStateEvent) {
-                debugLog("idle:" + evt);
-                IdleStateEvent idleStateEvent = (IdleStateEvent) evt;
-                if (idleStateEvent.state() == IdleState.READER_IDLE) {
-                    if (LOGGER.isInfoEnabled()) {
-                        LOGGER.info("channel:" + ctx.channel() + " read idle.");
-                    }
-                    handleDisconnect(ctx);
-                    try {
-                        closeChannelHandlerContext(ctx);
-                    } catch (Exception e) {
-                        LOGGER.error(e.getMessage());
-                    }
-                }
-            }
-        }
-
-        @Override
-        public void close(ChannelHandlerContext ctx, ChannelPromise future) throws Exception {
-            if (LOGGER.isInfoEnabled()) {
-                LOGGER.info(ctx + " will closed");
-            }
-            super.close(ctx, future);
-        }
-
+        super.registerProcessor(MessageType.TYPE_HEARTBEAT_MSG, heartbeatMessageProcessor, null);
     }
 
 }
