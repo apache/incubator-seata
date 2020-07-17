76cbd9e7de8ee44682a3caa1f20cb98108d1b2c4
wangwei-ying <519160764@qq.com>
2020-07-17 09:09:32 +0800

    codestyle check

--- config/seata-config-core/src/main/java/io/seata/config/FileConfigFactory.java
+++ config/seata-config-core/src/main/java/io/seata/config/FileConfigFactory.java
@@ -17,9 +17,6 @@ package io.seata.config;
 
 import io.seata.common.loader.EnhancedServiceLoader;
 import io.seata.config.file.FileConfig;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.File;
 import java.util.LinkedHashMap;
 import java.util.Set;
