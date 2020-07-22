65b6ab612240be5591b59967ebbdd271bfd83b7c
王良 <841369634@qq.com>
2020-07-22 12:48:41 +0800

    添加apache和alibaba的DubboTransactionPropagationFilterTest

---
+++ integration/dubbo/src/test/java/io/seata/integration/dubbo/mock/MockInvocation.java
@@ -0,0 +1,61 @@
+/*
+ *  Copyright 1999-2019 Seata.io Group.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package io.seata.integration.dubbo.mock;
+
+import org.apache.dubbo.rpc.Invocation;
+import org.apache.dubbo.rpc.Invoker;
+
+import java.util.Map;
+
+/**
+ * @author wang.liang
+ */
+public class MockInvocation implements Invocation {
+    @Override
+    public String getMethodName() {
+        return null;
+    }
+
+    @Override
+    public Class<?>[] getParameterTypes() {
+        return new Class[0];
+    }
+
+    @Override
+    public Object[] getArguments() {
+        return new Object[0];
+    }
+
+    @Override
+    public Map<String, String> getAttachments() {
+        return null;
+    }
+
+    @Override
+    public String getAttachment(String key) {
+        return null;
+    }
+
+    @Override
+    public String getAttachment(String key, String defaultValue) {
+        return null;
+    }
+
+    @Override
+    public Invoker<?> getInvoker() {
+        return null;
+    }
+}
