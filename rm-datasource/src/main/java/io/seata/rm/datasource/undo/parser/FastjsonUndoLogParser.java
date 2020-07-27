01ee669444bdec6acbc5d6f430680c2413b6bef4
jsbxyyx <26616196+jsbxyyx@users.noreply.github.com>
2020-07-27 11:46:38 +0800

    remove autotype true

--- rm-datasource/src/main/java/io/seata/rm/datasource/undo/parser/FastjsonUndoLogParser.java
+++ rm-datasource/src/main/java/io/seata/rm/datasource/undo/parser/FastjsonUndoLogParser.java
@@ -16,7 +16,6 @@
 package io.seata.rm.datasource.undo.parser;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.parser.ParserConfig;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.serializer.SimplePropertyPreFilter;
 import io.seata.common.Constants;
@@ -39,7 +38,6 @@ public class FastjsonUndoLogParser implements UndoLogParser, Initialize {
 
     @Override
     public void init() {
-        ParserConfig.getGlobalInstance().setAutoTypeSupport(true);
         filter.getExcludes().add("tableMeta");
     }
 
