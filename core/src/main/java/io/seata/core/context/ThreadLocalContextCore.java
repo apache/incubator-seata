e3e2b5fa4e2c9a047d0a4f516a88eb5307c7a42c
heyaping388 <774630093@qq.com>
2020-08-04 17:53:37 +0800

    optimize: replace with lambda (#2967)

--- core/src/main/java/io/seata/core/context/ThreadLocalContextCore.java
+++ core/src/main/java/io/seata/core/context/ThreadLocalContextCore.java
@@ -17,7 +17,6 @@ package io.seata.core.context;
 
 import java.util.HashMap;
 import java.util.Map;
-
 import io.seata.common.loader.LoadLevel;
 
 /**
@@ -28,13 +27,7 @@ import io.seata.common.loader.LoadLevel;
 @LoadLevel(name = "ThreadLocalContextCore", order = Integer.MIN_VALUE)
 public class ThreadLocalContextCore implements ContextCore {
 
-    private ThreadLocal<Map<String, String>> threadLocal = new ThreadLocal<Map<String, String>>() {
-        @Override
-        protected Map<String, String> initialValue() {
-            return new HashMap<String, String>();
-        }
-
-    };
+    private ThreadLocal<Map<String, String>> threadLocal = ThreadLocal.withInitial(() -> new HashMap<>());
 
     @Override
     public String put(String key, String value) {
