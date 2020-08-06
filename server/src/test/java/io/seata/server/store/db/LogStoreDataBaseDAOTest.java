005213acaf1a4a0841e72c3cdb1c9d42dc7af64d
王良 <841369634@qq.com>
2020-08-06 11:31:57 +0800

    测试用例，缺少字段。

--- server/src/test/java/io/seata/server/store/db/LogStoreDataBaseDAOTest.java
+++ server/src/test/java/io/seata/server/store/db/LogStoreDataBaseDAOTest.java
@@ -78,7 +78,7 @@ public class LogStoreDataBaseDAOTest {
             } catch (Exception e) {
             }
 //            xid, transaction_id, branch_id, resource_group_id, resource_id, lock_key, branch_type, status, client_id, application_data, gmt_create, gmt_modified
-            s.execute("CREATE TABLE branch_table ( xid varchar(96),  transaction_id long , branch_id long primary key, resource_group_id varchar(32), resource_id varchar(32) ,lock_key varchar(64) ,branch_type varchar(32) ,  status int , client_id varchar(128),  application_data varchar(500),  gmt_create TIMESTAMP(6) ,gmt_modified TIMESTAMP(6) ) ");
+            s.execute("CREATE TABLE branch_table ( xid varchar(96),  transaction_id long , branch_id long primary key, resource_group_id varchar(32), resource_id varchar(32) ,lock_key varchar(64) ,branch_type varchar(32) ,can_be_committed_async int, status int , client_id varchar(128),  application_data varchar(500),  gmt_create TIMESTAMP(6) ,gmt_modified TIMESTAMP(6) ) ");
             System.out.println("create table branch_table success.");
 
         } catch (Exception e) {
