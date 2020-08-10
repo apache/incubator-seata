84317cc3fa357d65048d49882a6f5bf20c8f9ab5
王良 <841369634@qq.com>
2020-08-10 10:33:24 +0800

    调整部分类的变更。

--- core/src/main/java/io/seata/core/rpc/processor/server/BatchLogHandler.java
+++ core/src/main/java/io/seata/core/rpc/processor/server/BatchLogHandler.java
@@ -13,6 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
+
 package io.seata.core.rpc.processor.server;
 
 import io.seata.common.thread.NamedThreadFactory;
--- rm-datasource/src/main/java/io/seata/rm/datasource/sql/serial/SerialArray.java
+++ rm-datasource/src/main/java/io/seata/rm/datasource/sql/serial/SerialArray.java
@@ -13,6 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
+
 package io.seata.rm.datasource.sql.serial;
 
 import javax.sql.rowset.serial.SerialBlob;
