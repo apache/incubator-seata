c9624405e01ac029c579747c1ca07126517516d0
a364176773 <364176773@qq.com>
2020-08-14 17:43:03 +0800

    bug fix

--- core/src/main/java/io/seata/core/store/redis/JedisPooledFactory.java
+++ core/src/main/java/io/seata/core/store/redis/JedisPooledFactory.java
@@ -16,8 +16,6 @@
 package io.seata.core.store.redis;
 
 import io.seata.common.loader.EnhancedServiceLoader;
-import io.seata.config.Configuration;
-import io.seata.config.ExtConfigurationProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import redis.clients.jedis.Jedis;
