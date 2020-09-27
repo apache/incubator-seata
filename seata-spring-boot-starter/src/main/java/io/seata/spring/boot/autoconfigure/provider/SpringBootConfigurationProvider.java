f32ea8f677c60414a048e3d597cd5b43912a7b6e
王良 <841369634@qq.com>
2020-09-27 18:37:55 +0800

    fix bug for SpringBootConfigurationProvider

--- seata-spring-boot-starter/src/main/java/io/seata/spring/boot/autoconfigure/provider/SpringBootConfigurationProvider.java
+++ seata-spring-boot-starter/src/main/java/io/seata/spring/boot/autoconfigure/provider/SpringBootConfigurationProvider.java
@@ -17,10 +17,14 @@ package io.seata.spring.boot.autoconfigure.provider;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Stream;
+
 import io.seata.common.holder.ObjectHolder;
+import io.seata.common.util.EnumUtils;
 import io.seata.config.Configuration;
 import io.seata.config.ExtConfigurationProvider;
 import org.apache.commons.lang.StringUtils;
@@ -28,7 +32,6 @@ import org.springframework.cglib.proxy.Enhancer;
 import org.springframework.cglib.proxy.MethodInterceptor;
 import org.springframework.cglib.proxy.MethodProxy;
 import org.springframework.context.ApplicationContext;
-
 
 import static io.seata.common.util.StringFormatUtils.DOT;
 import static io.seata.spring.boot.autoconfigure.StarterConstants.PROPERTY_MAP;
@@ -48,7 +51,7 @@ public class SpringBootConfigurationProvider implements ExtConfigurationProvider
         return (Configuration) Enhancer.create(originalConfiguration.getClass(), new MethodInterceptor() {
             @Override
             public Object intercept(Object proxy, Method method, Object[] args, MethodProxy methodProxy)
-                throws Throwable {
+                    throws Throwable {
                 if (method.getName().startsWith(INTERCEPT_METHOD_PREFIX) && args.length > 0) {
                     Object result = null;
                     String rawDataId = (String) args[0];
@@ -60,9 +63,15 @@ public class SpringBootConfigurationProvider implements ExtConfigurationProvider
                         result = get(convertDataId(rawDataId), args[1], (Long) args[2]);
                     }
                     if (result != null) {
-                        //If the return type is String,need to convert the object to string
+                        //If the return type is String, need to convert the object to string
                         if (method.getReturnType().equals(String.class)) {
                             return String.valueOf(result);
+                        } else if (method.getReturnType().isArray()) {
+                            return String.valueOf(result).split((String) args[1]);
+                        } else if (List.class.equals(method.getReturnType())) {
+                            return Arrays.asList(String.valueOf(result).split((String) args[1]));
+                        } else if (method.getReturnType().isEnum()) {
+                            return EnumUtils.getEnum((Class<Enum>) method.getReturnType(), String.valueOf(result));
                         }
                         return result;
                     }
@@ -92,7 +101,7 @@ public class SpringBootConfigurationProvider implements ExtConfigurationProvider
         if (propertyClass != null) {
             Object propertyObject = ObjectHolder.INSTANCE.getObject(ApplicationContext.class).getBean(propertyClass);
             Optional<Field> fieldOptional = Stream.of(propertyObject.getClass().getDeclaredFields()).filter(
-                f -> f.getName().equalsIgnoreCase(propertySuffix)).findAny();
+                    f -> f.getName().equalsIgnoreCase(propertySuffix)).findAny();
             if (fieldOptional.isPresent()) {
                 Field field = fieldOptional.get();
                 field.setAccessible(true);
@@ -162,9 +171,9 @@ public class SpringBootConfigurationProvider implements ExtConfigurationProvider
      */
     private Class getPropertyClass(String propertyPrefix) {
         return PROPERTY_MAP.entrySet().stream()
-            .filter(e -> propertyPrefix.equals(e.getKey()))
-            .findAny()
-            .map(Map.Entry::getValue)
-            .orElse(null);
+                .filter(e -> propertyPrefix.equals(e.getKey()))
+                .findAny()
+                .map(Map.Entry::getValue)
+                .orElse(null);
     }
 }
