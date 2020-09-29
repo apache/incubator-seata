bc11ac673fba43260a58a394bff7b512bb590145
liubin01 <liubin01@fenbi.com>
2020-09-29 15:21:07 +0800

    optimize: remove repeated conditional tests (#3161)

--- integration/http/src/main/java/io/seata/integration/http/AbstractHttpExecutor.java
+++ integration/http/src/main/java/io/seata/integration/http/AbstractHttpExecutor.java
@@ -37,7 +37,6 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 /**
  * Abstract http executor.
