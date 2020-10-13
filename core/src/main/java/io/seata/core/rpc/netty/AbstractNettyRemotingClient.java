5fd12fe577b248b1cd9ac05eff9161358fc94eff
liubin01 <liubin01@fenbi.com>
2020-10-13 21:12:09 +0800

    optimize: Local variable 'map' is redundant and check queue offer return value #3187

--- core/src/main/java/io/seata/core/rpc/netty/AbstractNettyRemotingClient.java
+++ core/src/main/java/io/seata/core/rpc/netty/AbstractNettyRemotingClient.java
@@ -152,13 +152,14 @@ public abstract class AbstractNettyRemotingClient extends AbstractNettyRemoting 
             futures.put(rpcMessage.getId(), messageFuture);
 
             // put message into basketMap
-            ConcurrentHashMap<String, BlockingQueue<RpcMessage>> map = basketMap;
-            BlockingQueue<RpcMessage> basket = map.get(serverAddress);
-            if (basket == null) {
-                map.putIfAbsent(serverAddress, new LinkedBlockingQueue<>());
-                basket = map.get(serverAddress);
+            basketMap.computeIfAbsent(serverAddress, k -> new LinkedBlockingQueue<>());
+            if (!basketMap.get(serverAddress).offer(rpcMessage)) {
+                if(LOGGER.isErrorEnabled()) {
+                    LOGGER.error("put message into basketMap offer failed, serverAddress:{},rpcMessage:{}",
+                            serverAddress, rpcMessage);
+                }
+                return null;
             }
-            basket.offer(rpcMessage);
             if (LOGGER.isDebugEnabled()) {
                 LOGGER.debug("offer message: {}", rpcMessage.getBody());
             }
