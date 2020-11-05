43f5890320947d5c76627fd35fff61462212ac94
jimin <slievrly@gmail.com>
2020-11-05 10:06:10 +0800

    version: change version to 1.5.0-SNAPSHOT (#3257)

--- core/src/main/java/io/seata/core/protocol/Version.java
+++ core/src/main/java/io/seata/core/protocol/Version.java
@@ -32,7 +32,7 @@ public class Version {
     /**
      * The constant CURRENT.
      */
-    private static final String CURRENT = "1.4.0";
+    private static final String CURRENT = "1.5.0-SNAPSHOT";
     private static final String VERSION_0_7_1 = "0.7.1";
     private static final int MAX_VERSION_DOT = 3;
 
