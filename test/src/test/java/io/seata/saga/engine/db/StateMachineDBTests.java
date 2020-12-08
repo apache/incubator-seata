3530aa0685f55d0b1030b9399e9ac500f1b9b631
leo <long_187@126.com>
2020-12-08 13:24:46 +0800

    bugfix: Saga statemachine definition json cannot enable jackson parser, and when no choice matched in choice state will throw NPE (#3309)
    * fix saga statemachine json cannot enable jackson parser
    
    * fix saga when no choice matched throw NPE bug

--- test/src/test/java/io/seata/saga/engine/db/StateMachineDBTests.java
+++ test/src/test/java/io/seata/saga/engine/db/StateMachineDBTests.java
@@ -15,6 +15,7 @@
  */
 package io.seata.saga.engine.db;
 
+import io.seata.common.exception.FrameworkErrorCode;
 import io.seata.core.exception.TransactionException;
 import io.seata.core.model.GlobalStatus;
 import io.seata.saga.engine.AsyncCallback;
@@ -104,6 +105,26 @@ public class StateMachineDBTests extends AbstractServerTest {
         stateMachineEngine.start(stateMachineName, null, paramMap);
 
         cost = System.currentTimeMillis() - start;
+        System.out.println("====== cost :" + cost);
+    }
+
+    @Test
+    public void testSimpleStateMachineWithChoiceNoDefault() {
+
+        long start = System.currentTimeMillis();
+
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("a", 3);
+
+        String stateMachineName = "simpleChoiceNoDefaultTestStateMachine";
+
+        try {
+            stateMachineEngine.start(stateMachineName, null, paramMap);
+        } catch (EngineExecutionException e) {
+            Assertions.assertTrue(FrameworkErrorCode.StateMachineNoChoiceMatched.equals(e.getErrcode()));
+            e.printStackTrace(System.out);
+        }
+        long cost = System.currentTimeMillis() - start;
         System.out.println("====== cost :" + cost);
     }
 
