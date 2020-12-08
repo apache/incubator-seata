c7843d947f97b6f6b96dec53d0dfc75879980ef7
JerryYin <superywj@foxmail.com>
2020-12-08 13:33:02 +0800

    bugfix:throw exception when update pk  (#3287)

--- rm-datasource/src/main/java/io/seata/rm/datasource/exec/AbstractDMLBaseExecutor.java
+++ rm-datasource/src/main/java/io/seata/rm/datasource/exec/AbstractDMLBaseExecutor.java
@@ -26,7 +26,6 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import io.seata.common.exception.NotSupportYetException;
-import io.seata.common.exception.ShouldNeverHappenException;
 import io.seata.common.util.CollectionUtils;
 import io.seata.rm.datasource.AbstractConnectionProxy;
 import io.seata.rm.datasource.ConnectionContext;
@@ -35,7 +34,6 @@ import io.seata.rm.datasource.StatementProxy;
 import io.seata.rm.datasource.sql.struct.TableRecords;
 import io.seata.sqlparser.SQLRecognizer;
 import io.seata.sqlparser.util.JdbcConstants;
-import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -200,15 +198,6 @@ public abstract class AbstractDMLBaseExecutor<T, S extends Statement> extends Ba
 
         public static boolean isLockRetryPolicyBranchRollbackOnConflict() {
             return LOCK_RETRY_POLICY_BRANCH_ROLLBACK_ON_CONFLICT;
-        }
-    }
-
-    protected void assertContainsPKColumnName(List<String> updateColumns) {
-        for (String columnName : updateColumns) {
-            String standardColumnName = getStandardPkColumnName(columnName);
-            if (StringUtils.isNotEmpty(standardColumnName)) {
-                throw new ShouldNeverHappenException("Sorry, update pk value is not supported!");
-            }
         }
     }
 }
