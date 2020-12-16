b82ef137c92f247eb2553609714133e4903c391b
anselleeyy <573459407@qq.com>
2020-12-16 17:41:12 +0800

    remove gmt_updated column

--- saga/seata-saga-statelang/src/main/java/io/seata/saga/statelang/domain/StateInstance.java
+++ saga/seata-saga-statelang/src/main/java/io/seata/saga/statelang/domain/StateInstance.java
@@ -155,20 +155,6 @@ public interface StateInstance {
      *
      * @return
      */
-    Date getGmtUpdated();
-
-    /**
-     * set end time
-     *
-     * @param gmtUpdated
-     */
-    void setGmtUpdated(Date gmtUpdated);
-
-    /**
-     * get end time
-     *
-     * @return
-     */
     Date getGmtEnd();
 
     /**
