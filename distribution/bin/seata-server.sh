4473f139ec10a6002af50992793ec8437faffba1
luoxy <luoxy@tuya.com>
2021-01-10 17:09:41 +0800

    feature #3009: build package fix

--- distribution/bin/seata-server.sh
+++ distribution/bin/seata-server.sh
@@ -117,19 +117,6 @@ if $cygwin; then
   [ -n "$REPO" ] && REPO=`cygpath --path --windows "$REPO"`
 fi
 
-#exec "$JAVACMD" $JAVA_OPTS -server -Xmx2048m -Xms2048m -Xmn1024m -Xss512k -XX:SurvivorRatio=10 -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=256m -XX:MaxDirectMemorySize=1024m -XX:-OmitStackTraceInFastThrow -XX:-UseAdaptiveSizePolicy -XX:+HeapDumpOnOutOfMemoryError
-#-XX:HeapDumpPath="$BASEDIR"/logs/java_heapdump.hprof -XX:+DisableExplicitGC -XX:+CMSParallelRemarkEnabled -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=75 -Xloggc:"$BASEDIR"/logs/seata_gc.log -verbose:gc -Dio.netty.leakDetectionLevel=advanced
-#-Dlogback.color.disable-for-bat=true \
-#  -classpath "$CLASSPATH" \
-#   \
-#  -Dapp.pid="$$" \
-#  -Dapp.repo="$REPO" \
-#  -Dapp.home="$BASEDIR" \
-#  -Dbasedir="$BASEDIR" \
-#  --spring.config.location=${$BASEDIR}/conf/application.yaml \
-#  --logging.config=${BASE_DIR}/conf/logback.xml \
-#  "$@"
-
 JAVA_OPT="${JAVA_OPT} -server -Xmx2048m -Xms2048m -Xmn1024m -Xss512k -XX:SurvivorRatio=10 -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=256m -XX:MaxDirectMemorySize=1024m -XX:-OmitStackTraceInFastThrow -XX:-UseAdaptiveSizePolicy"
 JAVA_OPT="${JAVA_OPT} -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=${$BASEDIR}/logs/java_heapdump.hprof -XX:+DisableExplicitGC -XX:+CMSParallelRemarkEnabled -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=75 -Xloggc:${$BASEDIR}/logs/seata_gc.log -verbose:gc"
 JAVA_OPT="${JAVA_OPT} -Dio.netty.leakDetectionLevel=advanced -Dlogback.color.disable-for-bat=true"
