76d8d4678403d3a01561ababfb0c46aea4b621e7
王良 <841369634@qq.com>
2021-01-13 18:51:24 +0800

    debug

--- server/src/main/java/io/seata/server/coordinator/AbstractCore.java
+++ server/src/main/java/io/seata/server/coordinator/AbstractCore.java
@@ -192,8 +192,13 @@ public abstract class AbstractCore implements Core {
 
     protected BranchStatus branchRollbackSend(BranchRollbackRequest request, GlobalSession globalSession,
                                               BranchSession branchSession) throws IOException, TimeoutException {
+        LOGGER.info("test log1:{}", request);
+        LOGGER.info("test log2:{}", globalSession);
+        LOGGER.info("test log3:{}", branchSession);
+        LOGGER.info("test log4:{}", remotingServer);
         BranchRollbackResponse response = (BranchRollbackResponse) remotingServer.sendSyncRequest(
                 branchSession.getResourceId(), branchSession.getClientId(), request);
+        LOGGER.info("test log5:{}", response);
         return response.getBranchStatus();
     }
 
