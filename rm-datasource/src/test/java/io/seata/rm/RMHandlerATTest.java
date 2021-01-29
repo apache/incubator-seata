2b1e6b5028eaf2581715c7ec7f80d1dbe5dbfea0
selfishlover <1581926496@qq.com>
2021-01-29 17:07:22 +0800

    adjust style

--- rm-datasource/src/test/java/io/seata/rm/RMHandlerATTest.java
+++ rm-datasource/src/test/java/io/seata/rm/RMHandlerATTest.java
@@ -23,10 +23,21 @@ import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.anyString;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+/**
+ * @author selfishlover
+ */
 class RMHandlerATTest {
 
     @Test
