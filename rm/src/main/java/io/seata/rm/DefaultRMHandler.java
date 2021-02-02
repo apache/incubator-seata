197468b1ec0ca9834a8ff81bc9e4c67e8bbe41d7
Zhibei Hao <837948266@qq.com>
2021-02-02 21:00:27 +0800

    restore extra and useless spaces

--- rm/src/main/java/io/seata/rm/DefaultRMHandler.java
+++ rm/src/main/java/io/seata/rm/DefaultRMHandler.java
@@ -37,7 +37,7 @@ import java.util.concurrent.ConcurrentHashMap;
  *
  * @author zhangsen
  */
-public class    DefaultRMHandler extends AbstractRMHandler {
+public class DefaultRMHandler extends AbstractRMHandler {
 
     protected static Map<BranchType, AbstractRMHandler> allRMHandlersMap = new ConcurrentHashMap<>();
 
