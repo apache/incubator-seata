a268c6969708648dd8e44890dba398e25bc4fdc2
a364176773 <364176773@qq.com>
2021-02-20 14:53:55 +0800

    update

--- server/src/main/java/io/seata/server/storage/redis/store/RedisTransactionStoreManager.java
+++ server/src/main/java/io/seata/server/storage/redis/store/RedisTransactionStoreManager.java
@@ -469,15 +469,15 @@ public class RedisTransactionStoreManager extends AbstractTransactionStoreManage
         int limit = 20;
         int start = 0;
         int stop = limit;
-        do {
+        for (;;) {
             values = jedis.lrange(key, start, stop);
             keys.addAll(values);
-            if (values == null || values.size() < limit) {
+            if (CollectionUtils.isEmpty(values) || values.size() < limit) {
                 break;
             }
             start = keys.size();
             stop = start + limit;
-        } while (CollectionUtils.isNotEmpty(values));
+        }
         return keys;
     }
 
