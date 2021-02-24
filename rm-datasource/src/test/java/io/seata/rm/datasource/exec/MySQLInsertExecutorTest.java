7416af549b865db50edad9ef3c4b7a68ea559207
chd <1170050364@qq.com>
2021-02-24 17:50:53 +0800

    ci ex

--- rm-datasource/src/test/java/io/seata/rm/datasource/exec/MySQLInsertExecutorTest.java
+++ rm-datasource/src/test/java/io/seata/rm/datasource/exec/MySQLInsertExecutorTest.java
@@ -92,7 +92,7 @@ public class MySQLInsertExecutorTest {
         when(statementProxy.getTargetStatement()).thenReturn(statementProxy);
 
         MockResultSet resultSet = new MockResultSet(statementProxy);
-        resultSet.mockResultSet(Arrays.asList("Variable_name", "Value"), new Object[][]{{"auto_increment_increment", 1L}});
+        resultSet.mockResultSet(Arrays.asList("Variable_name", "Value"), new Object[][]{{"auto_increment_increment", BigDecimal.ONE}});
         when(statementProxy.getTargetStatement().executeQuery("SHOW VARIABLES LIKE 'auto_increment_increment'")).thenReturn(resultSet);
 
         StatementCallback statementCallback = mock(StatementCallback.class);
