c8ad5fb0880665691ddca17d411219f52c7b74e5
chd <1170050364@qq.com>
2021-02-25 15:20:19 +0800

    ci

--- core/src/main/java/io/seata/core/store/db/sql/distribute/lock/DistributeLockSqlFactory.java
+++ core/src/main/java/io/seata/core/store/db/sql/distribute/lock/DistributeLockSqlFactory.java
@@ -24,7 +24,7 @@ public class DistributeLockSqlFactory {
     /**
      * get the lock store sql
      *
-     * @param dbType the dbType, support mysql/oracle/h2/postgre/oceanbase，it's useless now, but maybe useful later
+     * @param dbType the dbType, support mysql/oracle/h2/postgre/oceanbase, it's useless now, but maybe useful later
      * @return lock store sql
      */
     public static DistributeLockSql getDistributeLogStoreSql(String dbType) {
