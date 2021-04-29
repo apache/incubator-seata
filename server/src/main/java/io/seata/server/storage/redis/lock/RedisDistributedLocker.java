b265552df85560f1009e43db79b2d81ac86da316
wangzhongxiang <1186355422@qq.com>
2021-04-29 13:42:55 +0800

    add:添加readme.md

--- server/src/main/java/io/seata/server/storage/redis/lock/RedisDistributedLocker.java
+++ server/src/main/java/io/seata/server/storage/redis/lock/RedisDistributedLocker.java
@@ -1,3 +1,18 @@
+/*
+ *  Copyright 1999-2019 Seata.io Group.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package io.seata.server.storage.redis.lock;
 
 import io.seata.server.storage.redis.JedisPooledFactory;
@@ -8,7 +23,7 @@ import redis.clients.jedis.Transaction;
 import redis.clients.jedis.params.SetParams;
 
 /**
- * @description: redis 分布式锁
+ * @description: Redis distributed lock
  * @author: zhongxiang.wang
  * @date: 2021-03-02 11:34
  */
