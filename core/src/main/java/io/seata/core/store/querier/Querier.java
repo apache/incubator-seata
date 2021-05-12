c1440aad12bb986221a2cd8ab7f8ee4fbc5d656d
王良 <841369634@qq.com>
2021-05-13 01:13:41 +0800

    optimize code

--- core/src/main/java/io/seata/core/store/querier/Querier.java
+++ core/src/main/java/io/seata/core/store/querier/Querier.java
@@ -26,96 +26,96 @@ import io.seata.common.util.CollectionUtils;
  */
 public interface Querier<T> {
 
-	/**
-	 * Match data
-	 *
-	 * @param data the data
-	 * @return the boolean
-	 */
-	<D extends T> boolean isMatch(D data);
+    /**
+     * Match data
+     *
+     * @param data the data
+     * @return the boolean
+     */
+    <D extends T> boolean isMatch(D data);
 
-	/**
-	 * Do count.
-	 *
-	 * @param list the list
-	 * @return the count
-	 */
-	default <D extends T> int doCount(List<D> list) {
-		int count = 0;
-		if (CollectionUtils.isNotEmpty(list)) {
-			for (D t : list) {
-				if (this.isMatch(t)) {
-					count++;
-				}
-			}
-		}
-		return count;
-	}
+    /**
+     * Do count.
+     *
+     * @param list the list
+     * @return the count
+     */
+    default <D extends T> int doCount(List<D> list) {
+        int count = 0;
+        if (CollectionUtils.isNotEmpty(list)) {
+            for (D t : list) {
+                if (this.isMatch(t)) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
 
-	/**
-	 * Do filter.
-	 *
-	 * @param list the list
-	 * @return the list after filter
-	 */
-	default <D extends T> List<D> doFilter(List<D> list) {
-		if (CollectionUtils.isEmpty(list)) {
-			return Collections.emptyList();
-		}
+    /**
+     * Do filter.
+     *
+     * @param list the list
+     * @return the list after filter
+     */
+    default <D extends T> List<D> doFilter(List<D> list) {
+        if (CollectionUtils.isEmpty(list)) {
+            return Collections.emptyList();
+        }
 
-		List<D> found = new ArrayList<>();
-		for (D t : list) {
-			if (this.isMatch(t)) {
-				found.add(t);
-			}
-		}
-		return found;
-	}
+        List<D> found = new ArrayList<>();
+        for (D t : list) {
+            if (this.isMatch(t)) {
+                found.add(t);
+            }
+        }
+        return found;
+    }
 
-	/**
-	 * Do sort.
-	 *
-	 * @param list the list
-	 * @return the list after sort
-	 */
-	<D extends T> List<D> doSort(List<D> list);
+    /**
+     * Do sort.
+     *
+     * @param list the list
+     * @return the list after sort
+     */
+    <D extends T> List<D> doSort(List<D> list);
 
-	/**
-	 * Do paging.
-	 *
-	 * @param list the list
-	 * @return the list after paging
-	 */
-	<D extends T> List<D> doPaging(List<D> list);
+    /**
+     * Do paging.
+     *
+     * @param list the list
+     * @return the list after paging
+     */
+    <D extends T> List<D> doPaging(List<D> list);
 
-	/**
-	 * Do query.
-	 * doFilter + doSort + doPaging
-	 *
-	 * @param list the list
-	 * @return the list after query
-	 */
-	default <D extends T> List<D> doQuery(List<D> list) {
-		if (list == null) {
-			return Collections.emptyList();
-		}
+    /**
+     * Do query.
+     * doFilter + doSort + doPaging
+     *
+     * @param list the list
+     * @return the list after query
+     */
+    default <D extends T> List<D> doQuery(List<D> list) {
+        if (list == null) {
+            return Collections.emptyList();
+        }
 
-		if (list.isEmpty()) {
-			return list;
-		}
+        if (list.isEmpty()) {
+            return list;
+        }
 
-		// do filter
-		list = this.doFilter(list);
-		if (CollectionUtils.isEmpty(list)) {
-			return list;
-		}
+        // do filter
+        list = this.doFilter(list);
+        if (CollectionUtils.isEmpty(list)) {
+            return list;
+        }
 
-		// do sort
-		list = this.doSort(list);
+        // do sort
+        list = this.doSort(list);
 
-		// do paging
-		list = this.doPaging(list);
+        // do paging
+        list = this.doPaging(list);
 
-		return list;
-	}
+        return list;
+    }
 }
