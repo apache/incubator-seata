08c2c28bfaeb0676103270bfac7ae614225453bd
FUNKYE <364176773@qq.com>
2021-05-12 23:22:36 +0800

    optimize code

--- server/src/main/java/io/seata/server/storage/raft/lock/RaftLockManager.java
+++ server/src/main/java/io/seata/server/storage/raft/lock/RaftLockManager.java
@@ -15,18 +15,24 @@
  */
 package io.seata.server.storage.raft.lock;
 
-import java.util.ArrayList;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import com.alipay.sofa.jraft.Closure;
+import io.seata.common.exception.StoreException;
+import io.seata.common.loader.EnhancedServiceLoader;
 import io.seata.common.loader.LoadLevel;
 import io.seata.core.exception.TransactionException;
 import io.seata.core.lock.Locker;
 import io.seata.core.raft.RaftServerFactory;
 import io.seata.core.store.BranchTransactionDO;
 import io.seata.core.store.GlobalTransactionDO;
+import io.seata.core.store.StoreMode;
 import io.seata.server.lock.AbstractLockManager;
+import io.seata.server.lock.LockManager;
 import io.seata.server.session.BranchSession;
 import io.seata.server.session.GlobalSession;
 import io.seata.server.storage.SessionConverter;
-import io.seata.server.storage.file.lock.FileLocker;
+import io.seata.server.storage.file.lock.FileLockManager;
 import io.seata.server.storage.raft.RaftSessionSyncMsg;
 import io.seata.server.storage.raft.RaftTaskUtil;
 
@@ -40,17 +46,35 @@ import static io.seata.core.raft.msg.RaftSyncMsg.MsgType.RELEASE_GLOBAL_SESSION_
 @LoadLevel(name = "raft")
 public class RaftLockManager extends AbstractLockManager {
 
+    private FileLockManager fileLockManager =
+        (FileLockManager)EnhancedServiceLoader.load(LockManager.class, StoreMode.FILE.getName());
+
     @Override
     public Locker getLocker(BranchSession branchSession) {
-        return new FileLocker(branchSession);
+        return fileLockManager.getLocker(branchSession);
     }
 
     @Override
     public boolean acquireLock(BranchSession branchSession) throws TransactionException {
         if (RaftServerFactory.getInstance().isLeader()) {
+            CompletableFuture<Boolean> completableFuture = new CompletableFuture<>();
             BranchTransactionDO branchTransactionDO = SessionConverter.convertBranchTransactionDO(branchSession);
             RaftSessionSyncMsg raftSyncMsg = new RaftSessionSyncMsg(ACQUIRE_LOCK, branchTransactionDO);
-            RaftTaskUtil.createTask(raftSyncMsg);
+            Closure closure = status -> {
+                if (status.isOk()) {
+                    try {
+                        completableFuture.complete(super.acquireLock(branchSession));
+                    } catch (TransactionException e) {
+                        completableFuture.completeExceptionally(e);
+                    }
+                }
+            };
+            RaftTaskUtil.createTask(closure, raftSyncMsg);
+            try {
+                return completableFuture.get();
+            } catch (InterruptedException | ExecutionException e) {
+                throw new StoreException(e);
+            }
         }
         return super.acquireLock(branchSession);
     }
@@ -60,15 +84,23 @@ public class RaftLockManager extends AbstractLockManager {
         if (RaftServerFactory.getInstance().isLeader()) {
             GlobalTransactionDO globalTransactionDO = SessionConverter.convertGlobalTransactionDO(globalSession);
             RaftSessionSyncMsg raftSyncMsg = new RaftSessionSyncMsg(RELEASE_GLOBAL_SESSION_LOCK, globalTransactionDO);
-            RaftTaskUtil.createTask(raftSyncMsg);
-        }
-        ArrayList<BranchSession> branchSessions = globalSession.getBranchSessions();
-        boolean releaseLockResult = true;
-        for (BranchSession branchSession : branchSessions) {
-            if (!this.releaseLock(branchSession)) {
-                releaseLockResult = false;
+            CompletableFuture<Boolean> completableFuture = new CompletableFuture<>();
+            Closure closure = status -> {
+                if (status.isOk()) {
+                    try {
+                        completableFuture.complete(fileLockManager.releaseGlobalSessionLock(globalSession));
+                    } catch (TransactionException e) {
+                        completableFuture.completeExceptionally(e);
+                    }
+                }
+            };
+            RaftTaskUtil.createTask(closure, raftSyncMsg);
+            try {
+                return completableFuture.get();
+            } catch (InterruptedException | ExecutionException e) {
+                throw new StoreException(e);
             }
         }
-        return releaseLockResult;
+        return fileLockManager.releaseGlobalSessionLock(globalSession);
     }
 }
