56e140dd6db86a8f2402d7a6c2aec9ecbaeb35dc
FUNKYE <364176773@qq.com>
2021-05-12 22:22:26 +0800

    fix bug

--- server/src/main/java/io/seata/server/session/GlobalSession.java
+++ server/src/main/java/io/seata/server/session/GlobalSession.java
@@ -224,6 +224,7 @@ public class GlobalSession implements SessionLifecycle, SessionStorable {
 
         for (SessionLifecycleListener lifecycleListener : lifecycleListeners) {
             lifecycleListener.onEnd(this);
+            return;
         }
     }
 
