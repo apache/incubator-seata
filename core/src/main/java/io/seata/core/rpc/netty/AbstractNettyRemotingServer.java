5429b2d5dfb8b4967bba77f538c3d0e86d968fba
jsbxyyx <jsbxyyx@163.com>
2021-07-02 14:09:51 +0800

    optimize invalid string concat

--- core/src/main/java/io/seata/core/rpc/netty/AbstractNettyRemotingServer.java
+++ core/src/main/java/io/seata/core/rpc/netty/AbstractNettyRemotingServer.java
@@ -138,11 +138,11 @@ public abstract class AbstractNettyRemotingServer extends AbstractNettyRemoting 
     /**
      * Debug log.
      *
-     * @param info the info
+     * @param format the info
      */
-    public void debugLog(String info) {
+    protected void debugLog(String format, Object... arguments) {
         if (LOGGER.isDebugEnabled()) {
-            LOGGER.debug(info);
+            LOGGER.debug(format, arguments);
         }
     }
 
@@ -193,7 +193,7 @@ public abstract class AbstractNettyRemotingServer extends AbstractNettyRemoting 
          */
         @Override
         public void channelInactive(ChannelHandlerContext ctx) throws Exception {
-            debugLog("inactive:" + ctx);
+            debugLog("inactive:{}", ctx);
             if (messageExecutor.isShutdown()) {
                 return;
             }
@@ -245,7 +245,7 @@ public abstract class AbstractNettyRemotingServer extends AbstractNettyRemoting 
         @Override
         public void userEventTriggered(ChannelHandlerContext ctx, Object evt) {
             if (evt instanceof IdleStateEvent) {
-                debugLog("idle:" + evt);
+                debugLog("idle:{}", evt);
                 IdleStateEvent idleStateEvent = (IdleStateEvent) evt;
                 if (idleStateEvent.state() == IdleState.READER_IDLE) {
                     if (LOGGER.isInfoEnabled()) {
