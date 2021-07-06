05e4b2814c6dbe82daef30d56c817c6865406148
yujianfei <daredevil2@126.com>
2021-07-06 19:34:42 +0800

    添加注释

--- tcc/src/test/java/io/seata/rm/tcc/Param.java
+++ tcc/src/test/java/io/seata/rm/tcc/Param.java
@@ -5,6 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * customized annotation
+ *
+ * @author Yujianfei
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.PARAMETER, ElementType.FIELD})
 public @interface Param {
