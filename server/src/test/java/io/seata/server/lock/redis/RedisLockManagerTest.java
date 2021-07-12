ec6758b62faba747ada617d824e49ec876470b61
FUNKYE <364176773@qq.com>
2021-07-12 13:22:42 +0800

    optimize: update redis Server mock (#3699)

--- server/src/test/java/io/seata/server/lock/redis/RedisLockManagerTest.java
+++ server/src/test/java/io/seata/server/lock/redis/RedisLockManagerTest.java
@@ -16,24 +16,21 @@
 
 package io.seata.server.lock.redis;
 
-import io.seata.server.storage.redis.lock.RedisLockManager;
 import java.io.IOException;
-
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-
-import com.github.fppt.jedismock.RedisServer;
-
 import io.seata.core.exception.TransactionException;
 import io.seata.core.lock.Locker;
 import io.seata.server.lock.LockManager;
 import io.seata.server.session.BranchSession;
 import io.seata.server.storage.redis.JedisPooledFactory;
+import io.seata.server.storage.redis.lock.RedisLockManager;
 import io.seata.server.storage.redis.lock.RedisLocker;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
+import redis.embedded.RedisServer;
 
 /**
  * @author funkye
@@ -44,12 +41,14 @@ public class RedisLockManagerTest {
 
     @BeforeAll
     public static void start() throws IOException {
-        server = RedisServer.newRedisServer(6789);
+        int port = 6789;
+        server = RedisServer.builder().setting("maxheap 8M").setting("maxmemory 8M").port(port)
+            .setting("bind localhost").build();
         server.start();
         JedisPoolConfig poolConfig = new JedisPoolConfig();
         poolConfig.setMinIdle(1);
         poolConfig.setMaxIdle(10);
-        JedisPooledFactory.getJedisPoolInstance(new JedisPool(poolConfig, "127.0.0.1", 6789, 60000));
+        JedisPooledFactory.getJedisPoolInstance(new JedisPool(poolConfig, "127.0.0.1", port, 60000));
         lockManager = new RedisLockManagerForTest();
     }
 
