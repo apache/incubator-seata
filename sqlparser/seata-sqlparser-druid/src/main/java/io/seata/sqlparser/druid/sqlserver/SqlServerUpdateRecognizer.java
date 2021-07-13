c87382f45cf1515b95d2fbcfdc5df96c5ca87cb4
GoodBoyCoder <13682768293@163.com>
2021-07-13 12:46:04 +0800

    feature: add SqlServerUpdateRecognizer and test for it

---
+++ sqlparser/seata-sqlparser-druid/src/main/java/io/seata/sqlparser/druid/sqlserver/SqlServerUpdateRecognizer.java
@@ -0,0 +1,143 @@
+package io.seata.sqlparser.druid.sqlserver;
+
+import com.alibaba.druid.sql.ast.SQLExpr;
+import com.alibaba.druid.sql.ast.SQLStatement;
+import com.alibaba.druid.sql.ast.expr.*;
+import com.alibaba.druid.sql.ast.statement.SQLExprTableSource;
+import com.alibaba.druid.sql.ast.statement.SQLJoinTableSource;
+import com.alibaba.druid.sql.ast.statement.SQLTableSource;
+import com.alibaba.druid.sql.ast.statement.SQLUpdateSetItem;
+import com.alibaba.druid.sql.dialect.mysql.visitor.MySqlOutputVisitor;
+import com.alibaba.druid.sql.dialect.sqlserver.ast.stmt.SQLServerUpdateStatement;
+import com.alibaba.druid.sql.dialect.sqlserver.visitor.SQLServerOutputVisitor;
+import io.seata.common.exception.NotSupportYetException;
+import io.seata.sqlparser.ParametersHolder;
+import io.seata.sqlparser.SQLType;
+import io.seata.sqlparser.SQLUpdateRecognizer;
+import io.seata.sqlparser.struct.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The type SqlServer update recognizer.
+ *
+ * @author GoodBoyCoder
+ */
+public class SqlServerUpdateRecognizer extends BaseSqlServerRecognizer implements SQLUpdateRecognizer {
+    private SQLServerUpdateStatement ast;
+
+    /**
+     * Instantiates a new sqlserver update recognizer.
+     *
+     * @param originalSql the original sql
+     */
+    public SqlServerUpdateRecognizer(String originalSql, SQLStatement ast) {
+        super(originalSql);
+        this.ast = (SQLServerUpdateStatement) ast;
+    }
+
+    @Override
+    public SQLType getSQLType() {
+        return SQLType.UPDATE;
+    }
+
+    @Override
+    public String getTableAlias() {
+        return ast.getTableSource().getAlias();
+    }
+
+    @Override
+    public String getTableName() {
+        StringBuilder sb = new StringBuilder();
+        SQLServerOutputVisitor visitor = new SQLServerOutputVisitor(sb) {
+            @Override
+            public boolean visit(SQLExprTableSource x) {
+                printTableSourceExpr(x.getExpr());
+                return false;
+            }
+
+            @Override
+            public boolean visit(SQLJoinTableSource x) {
+                throw new NotSupportYetException("not support the syntax of update with join table");
+            }
+        };
+
+        SQLTableSource tableSource = ast.getTableSource();
+        if (tableSource instanceof SQLExprTableSource) {
+            visitor.visit((SQLExprTableSource) tableSource);
+        } else if (tableSource instanceof SQLJoinTableSource) {
+            visitor.visit((SQLJoinTableSource) tableSource);
+        } else {
+            throw new NotSupportYetException("not support the syntax of update with unknow");
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public List<String> getUpdateColumns() {
+        List<SQLUpdateSetItem> updateSetItems = ast.getItems();
+        List<String> list = new ArrayList<>(updateSetItems.size());
+        for (SQLUpdateSetItem updateSetItem : updateSetItems) {
+            SQLExpr expr = updateSetItem.getColumn();
+            if (expr instanceof SQLIdentifierExpr) {
+                list.add(((SQLIdentifierExpr)expr).getName());
+            } else if (expr instanceof SQLPropertyExpr) {
+                // This is alias case, like UPDATE xxx_tbl a SET a.name = ? WHERE a.id = ?
+                SQLExpr owner = ((SQLPropertyExpr)expr).getOwner();
+                if (owner instanceof SQLIdentifierExpr) {
+                    list.add(((SQLIdentifierExpr)owner).getName() + "." + ((SQLPropertyExpr)expr).getName());
+                    //This is table Field Full path, like update xxx_database.xxx_tbl set xxx_database.xxx_tbl.xxx_field...
+                } else if (((SQLPropertyExpr)expr).getOwnernName().split("\\.").length > 1) {
+                    list.add(((SQLPropertyExpr)expr).getOwnernName() + "." + ((SQLPropertyExpr)expr).getName());
+                }
+            } else {
+                wrapSQLParsingException(expr);
+            }
+        }
+        return list;
+    }
+
+    @Override
+    public List<Object> getUpdateValues() {
+        List<SQLUpdateSetItem> updateSetItems = ast.getItems();
+        List<Object> list = new ArrayList<>(updateSetItems.size());
+        for (SQLUpdateSetItem updateSetItem : updateSetItems) {
+            SQLExpr expr = updateSetItem.getValue();
+            if (expr instanceof SQLNullExpr) {
+                list.add(Null.get());
+            } else if (expr instanceof SQLValuableExpr) {
+                list.add(((SQLValuableExpr) expr).getValue());
+            } else if (expr instanceof SQLVariantRefExpr) {
+                //add '?'
+                list.add(((SQLVariantRefExpr) expr).getName());
+            } else if (expr instanceof SQLMethodInvokeExpr) {
+                list.add(SqlMethodExpr.get());
+            } else if (expr instanceof SQLDefaultExpr) {
+                list.add(SqlDefaultExpr.get());
+            }else if (expr instanceof SQLSequenceExpr) {
+                //Supported only since 2012 version of SQL Server,use next value for
+                SQLSequenceExpr sequenceExpr = (SQLSequenceExpr) expr;
+                String sequence = sequenceExpr.getSequence().getSimpleName();
+                String function = sequenceExpr.getFunction().name;
+                list.add(new SqlSequenceExpr(sequence, function));
+            } else {
+                wrapSQLParsingException(expr);
+            }
+        }
+        return list;
+    }
+
+    @Override
+    public String getWhereCondition(final ParametersHolder parametersHolder,
+                                    final ArrayList<List<Object>> paramAppenderList) {
+        SQLExpr where = ast.getWhere();
+        return super.getWhereCondition(where, parametersHolder, paramAppenderList);
+    }
+
+    @Override
+    public String getWhereCondition() {
+        SQLExpr where = ast.getWhere();
+        return super.getWhereCondition(where);
+    }
+}
