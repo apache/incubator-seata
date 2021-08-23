f6897ba3d9e3d6b760c20779987f07e7e9d68345
ShiJh <838239178@qq.com>
2021-08-23 23:05:19 +0800

    修复分割选项可能造成内容丢失的问题，更改参数名称于nacos-config.sh脚本一致

--- script/config-center/nacos/nacos-config.py
+++ script/config-center/nacos/nacos-config.py
@@ -5,27 +5,32 @@ import http.client
 import sys
 import getopt as opts
 import urllib.parse
+import re
 
 
 def get_params() -> dict:
     params = {
         '-h': '127.0.0.1',
-        '-o': '8848',
-        '-n': '',
+        '-p': '8848',
+        '-t': '',
         '-g': 'SEATA_GROUP',
         '-u': '',
-        '-p': ''
+        '-w': ''
     }
-    inputs, args = opts.getopt(sys.argv[1:], shortopts='h:o:n:g:u:p:')
-    # print(inputs)
+    inputs, args = opts.getopt(sys.argv[1:], shortopts='h:p:t:g:u:w:')
     for k, v in inputs:
         params[k] = v
     print(params)
     return params
 
 def error_exit():
-    print('python nacos-config.py [-h host] [-o port] [-n namespace] [-g group] [-u username] [-p password]')
+    print('python nacos-config.py [-h host] [-p port] [-t tenant] [-g group] [-u username] [-w password]')
     exit()
+
+def get_pair(line: str) -> tuple:
+    res = re.match(r"([\.\w]+)=(.*)",line)
+    return res.groups() if res is not None else ['','']
+
 
 headers = {
     'content-type': "application/x-www-form-urlencoded"
@@ -35,12 +40,12 @@ hasError = False
 
 params = get_params()
 
-url_prefix = f"{params['-h']}:{params['-o']}"
-namespace = params['-n']
+url_prefix = f"{params['-h']}:{params['-p']}"
+tenant = params['-t']
 username = params['-u']
-password = params['-p']
+password = params['-w']
 group = params['-g']
-url_postfix_base = f'/nacos/v1/cs/configs?group={group}&tenant={namespace}'
+url_postfix_base = f'/nacos/v1/cs/configs?group={group}&tenant={tenant}'
 
 if username != '' and password != '':
     url_postfix_base += f'&username={username}&password={password}'
@@ -49,16 +54,16 @@ if url_prefix == ':':
     error_exit()
 
 for line in open('../config.txt'):
-    pair = line.rstrip("\n").split('=')
+    pair = get_pair(line.rstrip("\n"))
     if len(pair) < 2 or pair[0] == '' or pair[1] == '':
         continue
-    url_postfix = url_postfix_base + f'&dataId={urllib.parse.quote(str(pair[0]))}&content={urllib.parse.quote(str(pair[1])).strip()}'
-    conn = http.client.HTTPConnection(url_prefix)
-    conn.request("POST", url_postfix, headers=headers)
-    res = conn.getresponse()
-    data = res.read().decode("utf-8")
-    if data != "true":
-        hasError = True
+    # url_postfix = url_postfix_base + f'&dataId={urllib.parse.quote(str(pair[0]))}&content={urllib.parse.quote(str(pair[1])).strip()}'
+    # conn = http.client.HTTPConnection(url_prefix)
+    # conn.request("POST", url_postfix, headers=headers)
+    # res = conn.getresponse()
+    # data = res.read().decode("utf-8")
+    # if data != "true":
+    #     hasError = True
     print(f"{pair[0]}={pair[1]} {data if hasError else 'success'}")
 
 if hasError:
