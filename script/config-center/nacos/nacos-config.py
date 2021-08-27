374055d2fde632646e1ea1aa9ff1ef2cc24b6e56
ShiJh⭐ <838239178@qq.com>
2021-08-27 17:35:02 +0800

    optimize: add options to nacos-config.py and fix some option bugs (#3949)

--- script/config-center/nacos/nacos-config.py
+++ script/config-center/nacos/nacos-config.py
@@ -3,35 +3,70 @@
 
 import http.client
 import sys
+import getopt as opts
 import urllib.parse
+import re
 
-if len(sys.argv) < 2:
-    print ('python nacos-config.py nacosAddr')
+
+def get_params() -> dict:
+    params = {
+        '-h': '127.0.0.1',
+        '-p': '8848',
+        '-t': '',
+        '-g': 'SEATA_GROUP',
+        '-u': '',
+        '-w': ''
+    }
+    inputs, args = opts.getopt(sys.argv[1:], shortopts='h:p:t:g:u:w:')
+    for k, v in inputs:
+        params[k] = v
+    print(params)
+    return params
+
+def error_exit():
+    print('python nacos-config.py [-h host] [-p port] [-t tenant] [-g group] [-u username] [-w password]')
     exit()
+
+def get_pair(line: str) -> tuple:
+    res = re.match(r"([\.\w]+)=(.*)",line)
+    return res.groups() if res is not None else ['','']
+
 
 headers = {
     'content-type': "application/x-www-form-urlencoded"
 }
 
 hasError = False
+
+params = get_params()
+
+url_prefix = f"{params['-h']}:{params['-p']}"
+tenant = params['-t']
+username = params['-u']
+password = params['-w']
+group = params['-g']
+url_postfix_base = f'/nacos/v1/cs/configs?group={group}&tenant={tenant}'
+
+if username != '' and password != '':
+    url_postfix_base += f'&username={username}&password={password}'
+
+if url_prefix == ':':
+    error_exit()
+
 for line in open('../config.txt'):
-    pair = line.rstrip("\n").split('=')
-    if len(pair) < 2:
+    pair = get_pair(line.rstrip("\n"))
+    if len(pair) < 2 or pair[0] == '' or pair[1] == '':
         continue
-    print (line),
-    url_prefix = sys.argv[1]
+    url_postfix = url_postfix_base + f'&dataId={urllib.parse.quote(str(pair[0]))}&content={urllib.parse.quote(str(pair[1])).strip()}'
     conn = http.client.HTTPConnection(url_prefix)
-    if len(sys.argv) == 3:
-        namespace=sys.argv[2]
-        url_postfix = '/nacos/v1/cs/configs?dataId={0}&group=SEATA_GROUP&content={1}&tenant={2}'.format(urllib.parse.quote(str(pair[0])),urllib.parse.quote(str(pair[1])).strip(),namespace)
-    else:
-        url_postfix = '/nacos/v1/cs/configs?dataId={}&group=SEATA_GROUP&content={}'.format(urllib.parse.quote(str(pair[0])),urllib.parse.quote(str(pair[1]))).strip()
     conn.request("POST", url_postfix, headers=headers)
     res = conn.getresponse()
-    data = res.read()
-    if data.decode("utf-8") != "true":
+    data = res.read().decode("utf-8")
+    if data != "true":
         hasError = True
+    print(f"{pair[0]}={pair[1]} {data if hasError else 'success'}")
+
 if hasError:
-    print ("init nacos config fail.")
+    print("init nacos config fail.")
 else:
-    print ("init nacos config finished, please start seata-server.")
+    print("init nacos config finished, please start seata-server.")
