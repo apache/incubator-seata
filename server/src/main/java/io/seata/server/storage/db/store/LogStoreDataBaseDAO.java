c10b6a7431ec407a998fbd66ef9b1140663e009b
GoodBoyCoder <13682768293@163.com>
2021-09-19 16:36:25 +0800

    bugfix:fix the limit param position set error

--- server/src/main/java/io/seata/server/storage/db/store/LogStoreDataBaseDAO.java
+++ server/src/main/java/io/seata/server/storage/db/store/LogStoreDataBaseDAO.java
@@ -173,6 +173,12 @@ public class LogStoreDataBaseDAO implements LogStore {
                 ps.setInt(i + 1, status);
             }
             ps.setInt(statuses.length + 1, limit);
+
+            //modify for the change of limit position in sqlserver
+            if ("sqlserver".equalsIgnoreCase(dbType)) {
+                ps.setInt(1, limit);
+                ps.setInt(statuses.length + 1, statuses[0]);
+            }
             rs = ps.executeQuery();
             while (rs.next()) {
                 ret.add(convertGlobalTransactionDO(rs));
