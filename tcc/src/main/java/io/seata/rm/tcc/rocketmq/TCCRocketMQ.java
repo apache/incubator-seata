65b1d0dcd89b5741f330b705aecd9ea0398f9bae
潘许飞 <1142812525@qq.com>
2021-09-23 14:14:26 +0800

    feature-初步实现TCC集成RocketMQ的目标

---
+++ tcc/src/main/java/io/seata/rm/tcc/rocketmq/TCCRocketMQ.java
@@ -0,0 +1,24 @@
+
+package io.seata.rm.tcc.rocketmq;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.common.message.Message;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+import java.util.Map;
+
+import io.seata.rm.tcc.api.BusinessActionContext;
+import io.seata.rm.tcc.api.BusinessActionContextParameter;
+import io.seata.rm.tcc.api.LocalTCC;
+import io.seata.rm.tcc.api.TwoPhaseBusinessAction;
+
+@LocalTCC
+public interface TCCRocketMQ {
+    @TwoPhaseBusinessAction(name = "tccRocketMQ", commitMethod = "commit", rollbackMethod = "rollback")
+    boolean prepare(BusinessActionContext context, Map<String, Object> params);
+
+    boolean commit(BusinessActionContext context);
+
+    boolean rollback(BusinessActionContext context);
+}
