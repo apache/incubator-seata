ba705ac171412718ab4c8c15b8c8642b6ef13d20
唐鑫 <562806107@qq.com>
2021-09-24 16:52:17 +0800

    import alter

--- server/src/main/java/io/seata/server/storage/elasticsearch/store/ElasticSearchTransactionStoreManager.java
+++ server/src/main/java/io/seata/server/storage/elasticsearch/store/ElasticSearchTransactionStoreManager.java
@@ -42,7 +42,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.function.ToLongBiFunction;
 import java.util.stream.Collectors;
 
