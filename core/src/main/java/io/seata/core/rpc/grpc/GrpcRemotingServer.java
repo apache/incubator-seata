4697c294b9acd75dc79fe0d69b3b8c76d775b0ee
xilou31 <xilou31@gmail.com>
2021-09-28 16:59:46 +0800

    feature: Create grpc Server

---
+++ core/src/main/java/io/seata/core/rpc/grpc/GrpcRemotingServer.java
@@ -0,0 +1,28 @@
+package io.seata.core.rpc.grpc;
+
+import io.grpc.Channel;
+import io.seata.core.protocol.grpc.TransactionManagerServiceGrpc;
+import io.seata.core.protocol.grpc.TransactionManagerServiceGrpc.TransactionManagerServiceBlockingStub;
+
+import java.net.InetSocketAddress;
+
+/**
+ * @author xilou31
+ **/
+public class GrpcRemotingServer {
+    private static TransactionManagerServiceBlockingStub stub;
+    private static Channel channel;
+
+    public static void init(String serverAddress, Integer port) {
+        channel = GrpcServerBootstrap.getNewChannel(new InetSocketAddress(serverAddress, port));
+        stub = TransactionManagerServiceGrpc.newBlockingStub(channel);
+    }
+
+    public static TransactionManagerServiceBlockingStub getInstance() {
+        if (stub != null) {
+            return stub;
+        }
+        stub = TransactionManagerServiceGrpc.newBlockingStub(channel);
+        return stub;
+    }
+}
