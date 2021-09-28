23fbdecc54a4a95f4e22c393246767e2a580af5d
xilou31 <xilou31@gmail.com>
2021-09-28 12:40:45 +0800

    bugfix : Move Service to CLientBootstrap

--- core/src/main/java/io/seata/core/rpc/grpc/GrpcServerBootstrap.java
+++ core/src/main/java/io/seata/core/rpc/grpc/GrpcServerBootstrap.java
@@ -1,5 +1,8 @@
 package io.seata.core.rpc.grpc;
 
+import io.grpc.Channel;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.seata.core.rpc.RemotingBootstrap;
@@ -7,6 +10,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -16,6 +21,20 @@ public class GrpcServerBootstrap implements RemotingBootstrap {
     private static final Logger LOGGER = LoggerFactory.getLogger(GrpcServerBootstrap.class);
     private GrpcServerConfig serverConfig;
     private Server server;
+
+    private ConcurrentHashMap<InetSocketAddress, Channel> channelMap = new ConcurrentHashMap<>();
+
+    public Channel getNewChannel(InetSocketAddress address) {
+        if (channelMap.contains(address)) {
+            return channelMap.get(address);
+        }
+        ManagedChannel channel = null;
+        channel = ManagedChannelBuilder.forAddress(address.getHostName(), address.getPort())
+                .usePlaintext()
+                .build();
+        channelMap.put(address, channel);
+        return channel;
+    }
 
     @Override
     public void start() {
@@ -27,7 +46,6 @@ public class GrpcServerBootstrap implements RemotingBootstrap {
             server = ServerBuilder.forPort(port)
                     .addService(new ResourceManagerService())
                     .addService(new TransactionManagerService())
-                    .addService(new BranchTransactionService())
                     .build()
                     .start();
             LOGGER.info("Seata Grpc Server Started,Listening On Port:" + port);
