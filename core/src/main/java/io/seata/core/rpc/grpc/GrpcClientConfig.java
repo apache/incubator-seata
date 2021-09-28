459c276c3244d2b2cd10edbc3320ada27b3f4f5f
xilou31 <xilou31@gmail.com>
2021-09-28 12:42:37 +0800

    feature : Create grpc Client,Server Config

---
+++ core/src/main/java/io/seata/core/rpc/grpc/GrpcClientConfig.java
@@ -0,0 +1,14 @@
+package io.seata.core.rpc.grpc;
+
+/**
+ * @author xilou31
+ **/
+public class GrpcClientConfig {
+    public int getPort() {
+        return 50051;
+    }
+
+    public String getHost() {
+        return "";
+    }
+}
---
+++ core/src/main/java/io/seata/core/rpc/grpc/GrpcServerConfig.java
@@ -0,0 +1,14 @@
+package io.seata.core.rpc.grpc;
+
+/**
+ * @author xilou31
+ **/
+public class GrpcServerConfig {
+    public String getHost() {
+        return "";
+    }
+
+    public Integer getPort() {
+        return 50051;
+    }
+}
