d95d92987ca28206b1c6c63c4b1fa23b799a3423
YSF-A <1315903489@qq.com>
2021-09-30 14:09:09 +0800

    optimize: optimize TokenBucketLimiterTest

--- server/src/test/java/io/seata/server/ratelimit/TokenBucketRateLimiterTest.java
+++ server/src/test/java/io/seata/server/ratelimit/TokenBucketRateLimiterTest.java
@@ -15,6 +15,11 @@
  */
 package io.seata.server.ratelimit;
 
+import java.io.IOException;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 import io.seata.common.thread.NamedThreadFactory;
 import io.seata.server.ServerApplication;
 import org.slf4j.Logger;
@@ -23,12 +28,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.util.StopWatch;
 
-import java.io.IOException;
-import java.util.concurrent.SynchronousQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
-
-@SpringBootApplication
 public class TokenBucketRateLimiterTest {
 
     /**
@@ -36,25 +35,23 @@ public class TokenBucketRateLimiterTest {
      **/
     private static final Logger LOGGER = LoggerFactory.getLogger(TokenBucketRateLimiterTest.class);
 
+    // can test performance
     public static void main(String[] args) throws IOException {
         // Init the CONFIG in the TokenBucketRateLimiter
         SpringApplication.run(ServerApplication.class, args);
-        TokenBucketRateLimiterTest test = new TokenBucketRateLimiterTest();
         // test the performance
-//        test.testPerformanceOfTokenBucketLimiter();
+        testPerformanceOfTokenBucketLimiter();
         // test the performance with warmup and default value of burst
-//        test.testPerformanceOfTokenBucketLimiterWithWarmup();
+        //testPerformanceOfTokenBucketLimiterWithWarmup();
         // test the performance with delay and default value of burst
-//        test.testPerformanceOfTokenBucketLimiterWithDelay();
-        test.testPerformanceOfGUAVATokenBucketLimiterWithDelay();
+        //testPerformanceOfTokenBucketLimiterWithDelay();
     }
 
-    public void testPerformanceOfTokenBucketLimiter() {
+    public static void testPerformanceOfTokenBucketLimiter() {
         RateLimiter rateLimiter = new TokenBucketLimiter(1000);
-        com.google.common.util.concurrent.RateLimiter rateLimiter1 = com.google.common.util.concurrent.RateLimiter.create(1000);
         int threads = 50;
         final ThreadPoolExecutor service1 = new ThreadPoolExecutor(threads, threads, 0L, TimeUnit.MILLISECONDS,
-                new SynchronousQueue<Runnable>(), new NamedThreadFactory("test1-", false));
+                new SynchronousQueue<Runnable>(), new NamedThreadFactory("test1", false));
         for (int i = 0; i < threads; i++) {
             service1.execute(() -> {
                 int count = 1000;
@@ -69,7 +66,6 @@ public class TokenBucketRateLimiterTest {
                         e.printStackTrace();
                     }
                     boolean result = rateLimiter.canPass();
-//                    boolean result = rateLimiter1.tryAcquire();
                     if (result) {
                         pass++;
                     } else {
@@ -83,8 +79,9 @@ public class TokenBucketRateLimiterTest {
         }
     }
 
-    public void testPerformanceOfTokenBucketLimiterWithWarmup() {
+    public static void testPerformanceOfTokenBucketLimiterWithWarmup() {
         RateLimiter rateLimiter = new TokenBucketLimiter(1000);
+        // warm up
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
@@ -92,7 +89,7 @@ public class TokenBucketRateLimiterTest {
         }
         int threads = 50;
         final ThreadPoolExecutor service1 = new ThreadPoolExecutor(threads, threads, 0L, TimeUnit.MILLISECONDS,
-                new SynchronousQueue<Runnable>(), new NamedThreadFactory("test2-", false));
+                new SynchronousQueue<Runnable>(), new NamedThreadFactory("test2", false));
         for (int i = 0; i < threads; i++) {
             service1.execute(() -> {
                 int count = 1000;
@@ -115,11 +112,11 @@ public class TokenBucketRateLimiterTest {
         }
     }
 
-    public void testPerformanceOfTokenBucketLimiterWithDelay() {
+    public static void testPerformanceOfTokenBucketLimiterWithDelay() {
         RateLimiter rateLimiter = new TokenBucketLimiter(1000,true);
         int threads = 50;
         final ThreadPoolExecutor service1 = new ThreadPoolExecutor(threads, threads, 0L, TimeUnit.MILLISECONDS,
-                new SynchronousQueue<Runnable>(), new NamedThreadFactory("test3-", false));
+                new SynchronousQueue<Runnable>(), new NamedThreadFactory("test3", false));
         for (int i = 0; i < threads; i++) {
             service1.execute(() -> {
                 int count = 100;
@@ -130,31 +127,6 @@ public class TokenBucketRateLimiterTest {
                 while (count > 0) {
                     boolean result = rateLimiter.canPass();
                     if (result) {
-                        pass++;
-                    }
-                    count--;
-                }
-                w.stop();
-                LOGGER.info("total time:{}ms, pass:{}, reject:{}", w.getLastTaskTimeMillis(), pass, reject);
-            });
-        }
-    }
-
-    public void testPerformanceOfGUAVATokenBucketLimiterWithDelay() {
-        com.google.common.util.concurrent.RateLimiter rateLimiter1 = com.google.common.util.concurrent.RateLimiter.create(1000);
-        int threads = 50;
-        final ThreadPoolExecutor service1 = new ThreadPoolExecutor(threads, threads, 0L, TimeUnit.MILLISECONDS,
-                new SynchronousQueue<Runnable>(), new NamedThreadFactory("test3-", false));
-        for (int i = 0; i < threads; i++) {
-            service1.execute(() -> {
-                int count = 100;
-                int pass = 0;
-                int reject = 0;
-                StopWatch w = new StopWatch();
-                w.start();
-                while (count > 0) {
-                    double result1 = rateLimiter1.acquire();
-                    if (true) {
                         pass++;
                     }
                     count--;
