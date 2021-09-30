ed30a81143f5bca56acf7d7a0ed77b218ef5c753
王良 <841369634@qq.com>
2021-09-30 14:46:59 +0800

    fix style

--- seata-spring-autoconfigure/seata-spring-autoconfigure-client/src/main/java/io/seata/spring/boot/autoconfigure/SeataClientEnvironmentPostProcessor.java
+++ seata-spring-autoconfigure/seata-spring-autoconfigure-client/src/main/java/io/seata/spring/boot/autoconfigure/SeataClientEnvironmentPostProcessor.java
@@ -15,9 +15,9 @@
  */
 package io.seata.spring.boot.autoconfigure;
 
+import io.seata.rm.tcc.config.TCCFenceConfig;
 import io.seata.saga.engine.StateMachineConfig;
 import io.seata.spring.boot.autoconfigure.properties.SagaAsyncThreadPoolProperties;
-import io.seata.rm.tcc.config.TCCFenceConfig;
 import io.seata.spring.boot.autoconfigure.properties.SeataProperties;
 import io.seata.spring.boot.autoconfigure.properties.client.LoadBalanceProperties;
 import io.seata.spring.boot.autoconfigure.properties.client.LockProperties;
@@ -26,9 +26,6 @@ import io.seata.spring.boot.autoconfigure.properties.client.ServiceProperties;
 import io.seata.spring.boot.autoconfigure.properties.client.TmProperties;
 import io.seata.spring.boot.autoconfigure.properties.client.UndoCompressProperties;
 import io.seata.spring.boot.autoconfigure.properties.client.UndoProperties;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.core.Ordered;
@@ -40,13 +37,12 @@ import static io.seata.spring.boot.autoconfigure.StarterConstants.COMPRESS_PREFI
 import static io.seata.spring.boot.autoconfigure.StarterConstants.LOAD_BALANCE_PREFIX;
 import static io.seata.spring.boot.autoconfigure.StarterConstants.LOCK_PREFIX;
 import static io.seata.spring.boot.autoconfigure.StarterConstants.PROPERTY_BEAN_MAP;
-import static io.seata.spring.boot.autoconfigure.StarterConstants.PROPERTY_BEAN_MAP;
 import static io.seata.spring.boot.autoconfigure.StarterConstants.SAGA_ASYNC_THREAD_POOL_PREFIX;
 import static io.seata.spring.boot.autoconfigure.StarterConstants.SAGA_STATE_MACHINE_PREFIX;
 import static io.seata.spring.boot.autoconfigure.StarterConstants.SEATA_PREFIX;
 import static io.seata.spring.boot.autoconfigure.StarterConstants.SERVICE_PREFIX;
-import static io.seata.spring.boot.autoconfigure.StarterConstants.UNDO_PREFIX;
 import static io.seata.spring.boot.autoconfigure.StarterConstants.TCC_FENCE_PREFIX;
+import static io.seata.spring.boot.autoconfigure.StarterConstants.UNDO_PREFIX;
 
 /**
  * @author xingfudeshi@gmail.com
@@ -54,24 +50,24 @@ import static io.seata.spring.boot.autoconfigure.StarterConstants.TCC_FENCE_PREF
  */
 public class SeataClientEnvironmentPostProcessor implements EnvironmentPostProcessor, Ordered {
 
-	@Override
-	public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
-		PROPERTY_BEAN_MAP.put(SEATA_PREFIX, SeataProperties.class);
+    @Override
+    public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
+        PROPERTY_BEAN_MAP.put(SEATA_PREFIX, SeataProperties.class);
 
-		PROPERTY_BEAN_MAP.put(CLIENT_RM_PREFIX, RmProperties.class);
-		PROPERTY_BEAN_MAP.put(CLIENT_TM_PREFIX, TmProperties.class);
-		PROPERTY_BEAN_MAP.put(LOCK_PREFIX, LockProperties.class);
-		PROPERTY_BEAN_MAP.put(SERVICE_PREFIX, ServiceProperties.class);
-		PROPERTY_BEAN_MAP.put(UNDO_PREFIX, UndoProperties.class);
-		PROPERTY_BEAN_MAP.put(COMPRESS_PREFIX, UndoCompressProperties.class);
-		PROPERTY_BEAN_MAP.put(LOAD_BALANCE_PREFIX, LoadBalanceProperties.class);
-		PROPERTY_BEAN_MAP.put(TCC_FENCE_PREFIX, TCCFenceConfig.class);
-		PROPERTY_BEAN_MAP.put(SAGA_STATE_MACHINE_PREFIX, StateMachineConfig.class);
-		PROPERTY_BEAN_MAP.put(SAGA_ASYNC_THREAD_POOL_PREFIX, SagaAsyncThreadPoolProperties.class);
-	}
+        PROPERTY_BEAN_MAP.put(CLIENT_RM_PREFIX, RmProperties.class);
+        PROPERTY_BEAN_MAP.put(CLIENT_TM_PREFIX, TmProperties.class);
+        PROPERTY_BEAN_MAP.put(LOCK_PREFIX, LockProperties.class);
+        PROPERTY_BEAN_MAP.put(SERVICE_PREFIX, ServiceProperties.class);
+        PROPERTY_BEAN_MAP.put(UNDO_PREFIX, UndoProperties.class);
+        PROPERTY_BEAN_MAP.put(COMPRESS_PREFIX, UndoCompressProperties.class);
+        PROPERTY_BEAN_MAP.put(LOAD_BALANCE_PREFIX, LoadBalanceProperties.class);
+        PROPERTY_BEAN_MAP.put(TCC_FENCE_PREFIX, TCCFenceConfig.class);
+        PROPERTY_BEAN_MAP.put(SAGA_STATE_MACHINE_PREFIX, StateMachineConfig.class);
+        PROPERTY_BEAN_MAP.put(SAGA_ASYNC_THREAD_POOL_PREFIX, SagaAsyncThreadPoolProperties.class);
+    }
 
-	@Override
-	public int getOrder() {
-		return Ordered.HIGHEST_PRECEDENCE;
-	}
+    @Override
+    public int getOrder() {
+        return Ordered.HIGHEST_PRECEDENCE;
+    }
 }
