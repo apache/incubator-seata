cba81b7f2cc133a7489f4bb8c75c74164074e385
GoodBoyCoder <13682768293@163.com>
2021-10-08 20:43:34 +0800

    optimize:remove the id field in the undo_log table of sqlserver

--- script/client/at/db/sqlserver.sql
+++ script/client/at/db/sqlserver.sql
@@ -1,7 +1,6 @@
 -- for AT mode you must to init this sql for you business database. the seata server not need it.
 CREATE TABLE [undo_log]
 (
-    [id]            bigint Identity (1,1) NOT NULL,
     [branch_id]     bigint                NOT NULL,
     [xid]           varchar(128)          NOT NULL,
     [context]       varchar(128)          NOT NULL,
@@ -9,8 +8,6 @@ CREATE TABLE [undo_log]
     [log_status]    int                   NOT NULL,
     [log_created]   datetime              NOT NULL,
     [log_modified]  datetime              NOT NULL,
-    PRIMARY KEY CLUSTERED ([id])
-        WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON),
     CONSTRAINT [ux_undo_log] UNIQUE NONCLUSTERED ([xid], [branch_id])
         WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
 )
