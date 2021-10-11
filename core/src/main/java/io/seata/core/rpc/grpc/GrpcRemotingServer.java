0495ffa6c2f32ac730722f75dbb19f7c8042f0e6
xilou31 <xilou31@gmail.com>
2021-10-11 11:50:16 +0800

    server initital

--- core/src/main/java/io/seata/core/rpc/grpc/GrpcRemotingServer.java
+++ core/src/main/java/io/seata/core/rpc/grpc/GrpcRemotingServer.java
@@ -1,28 +1,68 @@
 package io.seata.core.rpc.grpc;
 
 import io.grpc.Channel;
-import io.seata.core.model.grpc.BranchTransactionServiceGrpc;
-import io.seata.core.model.grpc.BranchTransactionServiceGrpc.BranchTransactionServiceBlockingStub;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.seata.core.rpc.RemotingBootstrap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author xilou31
  **/
-public class GrpcRemotingServer {
-    private static BranchTransactionServiceBlockingStub stub;
-    private static Channel channel;
+public class GrpcRemotingServer implements RemotingBootstrap {
+    private static final Logger LOGGER = LoggerFactory.getLogger(GrpcRemotingServer.class);
+    private Server server;
+    private static final ConcurrentHashMap<InetSocketAddress, Channel> IDENTIFIED_CHANNELS = new ConcurrentHashMap<>();
+    private final Integer port;
 
-    public static void init(String serverAddress, Integer port) {
-        channel = GrpcServerBootstrap.getNewChannel(new InetSocketAddress(serverAddress, port));
-        stub = BranchTransactionServiceGrpc.newBlockingStub(channel);
+    public GrpcRemotingServer(Integer port) {
+        this.port = port;
     }
 
-    public static BranchTransactionServiceBlockingStub getInstance() {
-        if (stub != null) {
-            return stub;
+
+    public static Channel getChannel(InetSocketAddress address) {
+        if (IDENTIFIED_CHANNELS.containsKey(address)) {
+            return IDENTIFIED_CHANNELS.get(address);
         }
-        stub = BranchTransactionServiceGrpc.newBlockingStub(channel);
-        return stub;
+        ManagedChannel channel = null;
+        channel = ManagedChannelBuilder.forAddress(address.getHostName(), address.getPort())
+                .usePlaintext()
+                .build();
+        IDENTIFIED_CHANNELS.put(address, channel);
+        return channel;
+    }
+
+    @Override
+    public void start() {
+        try {
+            server = ServerBuilder.forPort(port)
+                    .addService(new ResourceManagerService())
+                    .addService(new TransactionManagerService())
+                    .build()
+                    .start();
+            LOGGER.info("Seata Grpc Server Started,Listening On Port:" + port);
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+    }
+
+    @Override
+    public void shutdown() {
+        if (server != null) {
+            try {
+                server.shutdown()
+                        .awaitTermination(30, TimeUnit.SECONDS);
+            } catch (InterruptedException exception) {
+                exception.printStackTrace();
+            }
+        }
     }
 }
