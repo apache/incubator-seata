fdad32f156396f39836fbbb290b75fa5631e3c22
jimin <slievrly@gmail.com>
2021-11-04 22:00:12 +0800

    Revert "feature: support console management" (#4112)
    This reverts commit fae0e589e36962719fb5a08716c73486c77082fe.

--- console/src/main/resources/static/console-fe/src/components/Page/PageContent.tsx
+++
@@ -1,8 +0,0 @@
-import React, { PureComponent } from 'react';
-
-export default class PageContent extends PureComponent<any> {
-  render() {
-    const { children } = this.props;
-    return <div>{children}</div>;
-  }
-}
