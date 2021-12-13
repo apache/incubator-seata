c679b68e077b90ea73d9f1f577b2c26e11a7a7b6
IT云清 <33415199+lightClouds917@users.noreply.github.com>
2021-12-13 19:50:19 +0800

    feature: init the console basic code (#4134)

---
+++ server/src/main/java/io/seata/server/console/impl/file/GlobalLockFileServiceImpl.java
@@ -0,0 +1,46 @@
+/*
+ *  Copyright 1999-2019 Seata.io Group.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package io.seata.server.console.impl.file;
+
+import io.seata.common.exception.NotSupportYetException;
+import io.seata.core.store.db.vo.GlobalLockVO;
+import io.seata.server.console.result.PageResult;
+import io.seata.server.console.service.GlobalLockService;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.stereotype.Component;
+
+/**
+ * Global Lock File ServiceImpl
+ *
+ * @author: zhongxiang.wang
+ */
+@Component
+@org.springframework.context.annotation.Configuration
+@ConditionalOnExpression("#{'file'.equals('${lockMode}')}")
+public class GlobalLockFileServiceImpl implements GlobalLockService {
+
+    @Override
+    public PageResult<GlobalLockVO> queryByTable(String tableName) {
+        throw new NotSupportYetException();
+    }
+
+    @Override
+    public PageResult<GlobalLockVO> queryByXid(String xid) {
+        throw new NotSupportYetException();
+    }
+
+
+}
