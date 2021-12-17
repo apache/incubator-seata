c8dcec0fef2cedf1d03f0bf41c2ad24179e77e2a
zhangchenghui <zhangchenghui.dev@gmail.com>
2021-12-17 22:19:34 +0800

    optimize: tc batch response to client.

--- core/src/main/java/io/seata/core/rpc/processor/server/ServerOnRequestProcessor.java
+++ core/src/main/java/io/seata/core/rpc/processor/server/ServerOnRequestProcessor.java
@@ -347,7 +347,7 @@ public class ServerOnRequestProcessor implements RemotingProcessor, Disposable {
         public boolean equals(Object o) {
             if (this == o) {
                 return true;
-            };
+            }
             if (o == null || getClass() != o.getClass()) {
                 return false;
             }
