a69addea64f3e6abc9d8a2aab3390db1f50ab4e5
a364176773 <364176773@qq.com>
2022-01-27 09:56:54 +0800

    fix

--- server/src/main/java/io/seata/server/coordinator/DefaultCoordinator.java
+++ server/src/main/java/io/seata/server/coordinator/DefaultCoordinator.java
@@ -24,7 +24,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ArrayBlockingQueue;
