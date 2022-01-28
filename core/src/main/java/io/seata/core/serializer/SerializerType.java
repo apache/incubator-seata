a8e133526060ff68c98a549a1326973ac96705e5
王良 <841369634@qq.com>
2022-01-28 14:32:37 +0800

    fix style

--- core/src/main/java/io/seata/core/serializer/SerializerType.java
+++ core/src/main/java/io/seata/core/serializer/SerializerType.java
@@ -30,7 +30,7 @@ public enum SerializerType {
     SEATA((byte)0x1),
 
     /**
-     * The protobuf， 'io.seata:seata-serializer-protobuf' dependency must be referenced manually.
+     * The protobuf, 'io.seata:seata-serializer-protobuf' dependency must be referenced manually.
      * <p>
      * Math.pow(2, 1)
      */
