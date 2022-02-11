b362bdd38a2ce038cc6ef58973dd69ba3023f91e
王良 <841369634@qq.com>
2022-02-11 14:27:35 +0800

    revert sql parser友好提示。

--- rm-datasource/src/main/java/io/seata/rm/datasource/sql/SQLVisitorFactory.java
+++ rm-datasource/src/main/java/io/seata/rm/datasource/sql/SQLVisitorFactory.java
@@ -16,9 +16,11 @@
 package io.seata.rm.datasource.sql;
 
 import io.seata.common.loader.EnhancedServiceLoader;
+import io.seata.config.ConfigurationFactory;
+import io.seata.core.constants.ConfigurationKeys;
 import io.seata.sqlparser.SQLRecognizer;
 import io.seata.sqlparser.SQLRecognizerFactory;
-import io.seata.sqlparser.SqlParserConfigurationFactory;
+import io.seata.sqlparser.SqlParserType;
 
 import java.util.List;
 
@@ -32,7 +34,7 @@ public class SQLVisitorFactory {
     private final static SQLRecognizerFactory SQL_RECOGNIZER_FACTORY;
 
     static {
-        String sqlParserType = SqlParserConfigurationFactory.getSqlParserType();
+        String sqlParserType = ConfigurationFactory.getInstance().getConfig(ConfigurationKeys.SQL_PARSER_TYPE, SqlParserType.SQL_PARSER_TYPE_DRUID);
         SQL_RECOGNIZER_FACTORY = EnhancedServiceLoader.load(SQLRecognizerFactory.class, sqlParserType);
     }
 
