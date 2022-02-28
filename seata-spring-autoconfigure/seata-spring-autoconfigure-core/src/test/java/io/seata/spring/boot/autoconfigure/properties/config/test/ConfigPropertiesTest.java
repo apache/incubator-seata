326353d5b57b5eeaa639fd9cbf5a5894e12d79ea
doubleDimple <96671314+doubleDimple@users.noreply.github.com>
2022-02-28 14:31:27 +0800

    optimize: add some copyright header for test classes (#4409)

--- seata-spring-autoconfigure/seata-spring-autoconfigure-core/src/test/java/io/seata/spring/boot/autoconfigure/properties/config/test/ConfigPropertiesTest.java
+++ seata-spring-autoconfigure/seata-spring-autoconfigure-core/src/test/java/io/seata/spring/boot/autoconfigure/properties/config/test/ConfigPropertiesTest.java
@@ -13,7 +13,6 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-
 package io.seata.spring.boot.autoconfigure.properties.config.test;
 
 import io.seata.common.loader.EnhancedServiceLoader;
