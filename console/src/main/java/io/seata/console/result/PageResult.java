6dbba435648fbea2e6bdcf9501e83fa2c1b589b6
liuqiufeng <775038282@qq.com>
2022-03-05 14:23:25 +0800

    console api code optimize

--- console/src/main/java/io/seata/console/result/PageResult.java
+++ console/src/main/java/io/seata/console/result/PageResult.java
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package io.seata.core.console.result;
+package io.seata.console.result;
 
 package io.seata.console.result;
 
