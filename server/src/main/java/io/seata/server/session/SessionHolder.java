a98cf19deb5d7b2cdb99f9942b0e3742ecc0ea67
chd <1170050364@qq.com>
2022-03-09 00:13:22 +0800

    UnusedImports

--- server/src/main/java/io/seata/server/session/SessionHolder.java
+++ server/src/main/java/io/seata/server/session/SessionHolder.java
@@ -16,7 +16,6 @@
 package io.seata.server.session;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
