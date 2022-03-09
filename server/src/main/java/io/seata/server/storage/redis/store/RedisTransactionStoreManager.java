e832d8707d6a7d294e8f34c61725261a8d215493
doubleDimple <lovele.cn@gmail.com>
2022-03-09 18:37:37 +0800

    remove no use method

--- server/src/main/java/io/seata/server/storage/redis/store/RedisTransactionStoreManager.java
+++ server/src/main/java/io/seata/server/storage/redis/store/RedisTransactionStoreManager.java
@@ -20,7 +20,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Date;
-import java.util.Set;
 import java.util.Optional;
 import java.util.Collections;
 import java.util.concurrent.ConcurrentHashMap;
