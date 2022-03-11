007cbfceda451fec80615a390a5d466b28c0ec2f
18739969807 <t529791649@163.com>
2022-03-11 09:15:15 +0800

    future:support oracle and postgresql multi primary key

--- rm-datasource/src/main/java/io/seata/rm/datasource/exec/oracle/OracleInsertExecutor.java
+++ rm-datasource/src/main/java/io/seata/rm/datasource/exec/oracle/OracleInsertExecutor.java
@@ -30,7 +30,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.SQLException;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
