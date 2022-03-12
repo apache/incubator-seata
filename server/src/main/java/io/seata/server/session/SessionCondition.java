095e36da62e2d29b9a71b7d2d9043262c8b2d308
chd <1170050364@qq.com>
2022-03-13 00:04:12 +0800

    optimize: skip reload for redis & db mode (#4393)

--- server/src/main/java/io/seata/server/session/SessionCondition.java
+++ server/src/main/java/io/seata/server/session/SessionCondition.java
@@ -27,7 +27,7 @@ public class SessionCondition {
     private String xid;
     private GlobalStatus status;
     private GlobalStatus[] statuses;
-    private long overTimeAliveMills;
+    private Long overTimeAliveMills;
     private boolean lazyLoadBranch;
 
     /**
@@ -60,7 +60,7 @@ public class SessionCondition {
      *
      * @param statuses the statuses
      */
-    public SessionCondition(GlobalStatus[] statuses) {
+    public SessionCondition(GlobalStatus... statuses) {
         this.statuses = statuses;
     }
 
@@ -96,7 +96,7 @@ public class SessionCondition {
      *
      * @return the over time alive mills
      */
-    public long getOverTimeAliveMills() {
+    public Long getOverTimeAliveMills() {
         return overTimeAliveMills;
     }
 
@@ -105,7 +105,7 @@ public class SessionCondition {
      *
      * @param overTimeAliveMills the over time alive mills
      */
-    public void setOverTimeAliveMills(long overTimeAliveMills) {
+    public void setOverTimeAliveMills(Long overTimeAliveMills) {
         this.overTimeAliveMills = overTimeAliveMills;
     }
 
@@ -129,7 +129,7 @@ public class SessionCondition {
         return statuses;
     }
 
-    public void setStatuses(GlobalStatus[] statuses) {
+    public void setStatuses(GlobalStatus... statuses) {
         this.statuses = statuses;
     }
 
