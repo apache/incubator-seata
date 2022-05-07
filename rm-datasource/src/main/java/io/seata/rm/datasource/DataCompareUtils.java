26e25715ab7e0c96b3410ca7e8fd80907bc7becd
minghua.xie <minghua.xie@shopee.com>
2022-05-07 10:28:46 +0800

    import optimized

--- rm-datasource/src/main/java/io/seata/rm/datasource/DataCompareUtils.java
+++ rm-datasource/src/main/java/io/seata/rm/datasource/DataCompareUtils.java
@@ -31,7 +31,12 @@ import java.sql.Time;
 import java.sql.Timestamp;
 import java.sql.Types;
 import java.time.LocalDateTime;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 /**
