913f0a5d4f4e914b79947fe0480f38f27e93dd75
wulingxiao <1251605638@qqcom>
2022-05-22 12:44:10 +0800

    remove UnusedImports in EnhancedServiceLoader.java

--- common/src/main/java/io/seata/common/loader/EnhancedServiceLoader.java
+++ common/src/main/java/io/seata/common/loader/EnhancedServiceLoader.java
@@ -21,7 +21,6 @@ import java.io.InputStreamReader;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Enumeration;
 import java.util.List;
 import java.util.Map;
