3d3d6e310eabf4aaf662afcf3c79a181369f89e2
minghua.xie <minghua.xie@shopee.com>
2022-05-25 18:46:53 +0800

    white space

--- core/src/main/java/io/seata/core/event/ExceptionEvent.java
+++ core/src/main/java/io/seata/core/event/ExceptionEvent.java
@@ -1,3 +1,15 @@
+/*
+ * Copyright 1999-2019 Seata.io Group.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package io.seata.core.event;
 
 /**
@@ -5,15 +17,15 @@ package io.seata.core.event;
  *
  * @author Bughue
  */
-public class ExceptionEvent implements Event{
+public class ExceptionEvent implements Event {
 
     private String name;
 
-    public ExceptionEvent(String code){
+    public ExceptionEvent(String code) {
         this.name = code;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 }
