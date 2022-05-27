038e7eac249434f4f3ecc52b2f37556ddedd6c96
Gasol <1142812525@qq.com>
2022-05-27 18:02:34 +0800

    bug fix

--- tcc/src/main/java/io/seata/rm/tcc/rocketmq/RocketMQAspect.java
+++ tcc/src/main/java/io/seata/rm/tcc/rocketmq/RocketMQAspect.java
@@ -1,3 +1,18 @@
+/*
+ *  Copyright 1999-2019 Seata.io Group.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package io.seata.rm.tcc.rocketmq;
 
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
@@ -23,7 +38,7 @@ public class RocketMQAspect implements BeanPostProcessor {
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
         if (bean instanceof DefaultMQProducer) {
-            LOGGER.warn("生成代理类");
+            LOGGER.info("Generate RocketMQ Producer Proxy");
             tccRocketMQ.setDefaultMQProducer((DefaultMQProducer) bean);
             return new SeataMQProducer((DefaultMQProducer) bean, tccRocketMQ);
         }
