5675d8521d88839f266013881b2e349f1eb360cd
a364176773 <364176773@qq.com>
2022-06-17 18:13:37 +0800

    feature: support xid load balance

---
+++ discovery/seata-discovery-core/src/main/java/io/seata/discovery/loadbalance/XIDLoadBalance.java
@@ -0,0 +1,49 @@
+/*
+ *  Copyright 1999-2019 Seata.io Group.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package io.seata.discovery.loadbalance;
+
+import static io.seata.discovery.loadbalance.LoadBalanceFactory.XID_LOAD_BALANCE;
+
+
+import java.net.InetSocketAddress;
+import java.util.List;
+
+import io.seata.common.loader.LoadLevel;
+import io.seata.common.util.StringUtils;
+
+/**
+ * The type xid load balance.
+ *
+ * @author funkye
+ */
+@LoadLevel(name = XID_LOAD_BALANCE)
+public class XIDLoadBalance implements LoadBalance {
+
+    @Override
+    public <T> T select(List<T> invokers, String xid) {
+        String[] xidArray = xid.split(":");
+        int port = Integer.parseInt(xidArray[1]);
+        String ip = xidArray[0];
+        for (T invoker : invokers) {
+            InetSocketAddress inetSocketAddress = (InetSocketAddress)invoker;
+            if (StringUtils.equals(ip,inetSocketAddress.getHostName()) && inetSocketAddress.getPort() == port) {
+                return (T)inetSocketAddress;
+            }
+        }
+        throw new RuntimeException("not found seata-server channel");
+    }
+
+}
