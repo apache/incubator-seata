8dea756b158da36fb0306734e14ed23a6e7dbc4a
SherZhou <1433875305@qq.com>
2022-07-19 14:46:37 +0800

    optimise: add tests for service

--- server/src/test/java/io/seata/server/console/service/GlobalLockServiceTest.java
+++ server/src/test/java/io/seata/server/console/service/GlobalLockServiceTest.java
@@ -1,13 +1,26 @@
+/*
+ *  Copyright 1999-2019 Seata.io Group.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package io.seata.server.console.service;
 
 import io.seata.server.console.param.GlobalLockParam;
-import io.seata.server.console.param.GlobalSessionParam;
 import io.seata.server.console.result.PageResult;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 /**
@@ -17,12 +30,10 @@ import static org.mockito.Mockito.when;
  */
 @SpringBootTest
 public class GlobalLockServiceTest {
-    @MockBean
-    GlobalLockService globalLockService;
+    GlobalLockService globalLockService = mock(GlobalLockService.class);
 
     @Test
     public void testGlobalSessionQuery(){
-//        GlobalSessionParam globalSessionParam = new GlobalSessionParam();
         GlobalLockParam globalLockParam = new GlobalLockParam();
         globalLockParam.setPageNum(1);
 
@@ -31,6 +42,5 @@ public class GlobalLockServiceTest {
 
         when(globalLockService.query(globalLockParam)).thenReturn(pageResult);
         assertThat(globalLockService.query(globalLockParam)).isEqualTo(pageResult);
-//        Mockito.verify(globalSessionService).query(globalSessionParam);
     }
 }
--- server/src/test/java/io/seata/server/console/service/GlobalSessionServiceTest.java
+++ server/src/test/java/io/seata/server/console/service/GlobalSessionServiceTest.java
@@ -1,12 +1,26 @@
+/*
+ *  Copyright 1999-2019 Seata.io Group.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package io.seata.server.console.service;
 
 import io.seata.server.console.param.GlobalSessionParam;
 import io.seata.server.console.result.PageResult;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 /**
@@ -18,20 +32,22 @@ import static org.mockito.Mockito.when;
 @SpringBootTest
 public class GlobalSessionServiceTest {
 
-    @MockBean
-    GlobalSessionService globalSessionService;
+
+
+    GlobalSessionService globalSessionService =  mock(GlobalSessionService.class);
 
     @Test
     public void testGlobalSessionQuery(){
         GlobalSessionParam globalSessionParam = new GlobalSessionParam();
         globalSessionParam.setXid("01");
         globalSessionParam.setPageNum(1);
+        globalSessionParam.setPageSize(1);
 
         PageResult pageResult = new PageResult();
         pageResult.isSuccess();
 
         when(globalSessionService.query(globalSessionParam)).thenReturn(pageResult);
         assertThat(globalSessionService.query(globalSessionParam)).isEqualTo(pageResult);
-//        Mockito.verify(globalSessionService).query(globalSessionParam);
+
     }
 }
