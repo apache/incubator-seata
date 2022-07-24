afcfeef5f0420219f5776d8aca8ff0dddd605f2f
wulingxiao <1251605638@qqcom>
2022-07-24 17:42:27 +0800

    optimize:optimize logging collection config add loggerContextListener to append appender when context reset

--- server/src/test/java/io/seata/server/logging/logback/extend/DestinationConnectionStrategyCreatorTest.java
+++ server/src/test/java/io/seata/server/logging/logback/extend/DestinationConnectionStrategyCreatorTest.java
@@ -1,3 +1,18 @@
+/*
+ *  Copyright 1999-2019 Seata.io Group.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package io.seata.server.logging.logback.extend;
 
 import ch.qos.logback.core.util.Duration;
@@ -12,7 +27,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author wlx
- * @date 2022/6/14 10:56 下午
  */
 public class DestinationConnectionStrategyCreatorTest {
 
