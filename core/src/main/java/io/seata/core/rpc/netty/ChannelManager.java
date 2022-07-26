cea200d7140019dd925bc50bafb92dabbce7f28d
TheR1sing3un <ther1sing3un@163.com>
2022-07-27 02:13:54 +0800

    style(console): format changed code
    1. format changed code
    
    Signed-off-by: TheR1sing3un <ther1sing3un@163.com>

--- core/src/main/java/io/seata/core/rpc/netty/ChannelManager.java
+++ core/src/main/java/io/seata/core/rpc/netty/ChannelManager.java
@@ -29,7 +29,13 @@ import io.seata.core.rpc.RpcContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
