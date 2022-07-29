0bf268ca5b91a6ffc1e49ae4c11668301b39cf4a
hsien <yhsienwang@163.com>
2022-07-29 21:44:46 +0800

    feature: support log and lock store for OceanBase(Oracle mode)

--- rm-datasource/src/main/java/io/seata/rm/datasource/DataSourceProxy.java
+++ rm-datasource/src/main/java/io/seata/rm/datasource/DataSourceProxy.java
@@ -62,7 +62,7 @@ public class DataSourceProxy extends AbstractDataSourceProxy implements Resource
     /**
      * Enable the table meta checker
      */
-    private static final boolean ENABLE_TABLE_META_CHECKER_ENABLE = ConfigurationFactory.getInstance().getBoolean(
+    private static boolean ENABLE_TABLE_META_CHECKER_ENABLE = ConfigurationFactory.getInstance().getBoolean(
         ConfigurationKeys.CLIENT_TABLE_META_CHECK_ENABLE, DEFAULT_CLIENT_TABLE_META_CHECK_ENABLE);
 
     private final ScheduledExecutorService tableMetaExecutor = new ScheduledThreadPoolExecutor(1,
