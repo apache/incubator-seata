ea2ee64209da3a110bf4786e2e834ab6ed5c5924
huangr <huangr@pingpongx.com>
2022-08-03 19:50:20 +0800

    bugfix: when use Statement.executeBatch() api can not generate after image and undo log

--- rm-datasource/src/main/java/io/seata/rm/datasource/exec/mysql/MySQLInsertExecutor.java
+++ rm-datasource/src/main/java/io/seata/rm/datasource/exec/mysql/MySQLInsertExecutor.java
@@ -113,6 +113,11 @@ public class MySQLInsertExecutor extends BaseInsertExecutor implements Defaultab
         return pkValuesMap;
     }
 
+    @Override
+    protected boolean needUndoLog() throws Exception {
+        return true;
+    }
+
     /**
      * the modify for test
      */
