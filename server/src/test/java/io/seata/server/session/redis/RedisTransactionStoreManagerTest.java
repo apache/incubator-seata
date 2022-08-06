7b4367fa4e98806f78999dad70e4b8b6e89f278a
FUNKYE <364176773@qq.com>
2022-08-07 01:58:05 +0800

    test

--- server/src/test/java/io/seata/server/session/redis/RedisTransactionStoreManagerTest.java
+++ server/src/test/java/io/seata/server/session/redis/RedisTransactionStoreManagerTest.java
@@ -70,7 +70,7 @@ public class RedisTransactionStoreManagerTest {
 
     @Test
     public synchronized void testBeginSortByTimeoutQuery() throws TransactionException, InterruptedException {
-        GlobalSession session1 = GlobalSession.createGlobalSession("test1", "test2", "test001", 1100);
+        GlobalSession session1 = GlobalSession.createGlobalSession("test1", "test2", "test001", 1500);
         String xid1 = XID.generateXID(session1.getTransactionId());
         session1.setXid(xid1);
         session1.setTransactionId(session1.getTransactionId());
@@ -79,7 +79,7 @@ public class RedisTransactionStoreManagerTest {
         session1.setStatus(GlobalStatus.Begin);
         sessionManager.addGlobalSession(session1);
         Thread.sleep(10);
-        GlobalSession session2 = GlobalSession.createGlobalSession("test3", "test4", "test002", 1100);
+        GlobalSession session2 = GlobalSession.createGlobalSession("test3", "test4", "test002", 1500);
         String xid2 = XID.generateXID(session2.getTransactionId());
         session2.setXid(xid2);
         session2.setTransactionId(session2.getTransactionId());
@@ -91,7 +91,7 @@ public class RedisTransactionStoreManagerTest {
         for (GlobalSession globalSession : list2) {
             LOGGER.info("xid: {},timeout: {}",globalSession.getXid(),globalSession.getTimeout()+globalSession.getBeginTime());
         }
-        Thread.sleep(100);
+        Thread.sleep(10);
         SessionCondition sessionCondition = new SessionCondition(GlobalStatus.Begin);
         List<GlobalSession> list = sessionManager.findGlobalSessions(sessionCondition);
         for (GlobalSession globalSession : list) {
