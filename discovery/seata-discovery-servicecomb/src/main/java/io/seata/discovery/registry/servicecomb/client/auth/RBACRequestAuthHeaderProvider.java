28c05d598dc42825fed3ab0dc628a900539daaae
zhaozw <zhaozhongwei22@163.com>
2022-08-16 17:18:48 +0800

    format code

--- discovery/seata-discovery-servicecomb/src/main/java/io/seata/discovery/registry/servicecomb/client/auth/RBACRequestAuthHeaderProvider.java
+++ discovery/seata-discovery-servicecomb/src/main/java/io/seata/discovery/registry/servicecomb/client/auth/RBACRequestAuthHeaderProvider.java
@@ -54,143 +54,139 @@ import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class RBACRequestAuthHeaderProvider implements AuthHeaderProvider {
-  private static final Logger LOGGER = LoggerFactory.getLogger(RBACRequestAuthHeaderProvider.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(RBACRequestAuthHeaderProvider.class);
 
-  // special token used for special conditions
-  // e.g. un-authorized: will query token after token expired period
-  // e.g. not found:  will query token after token expired period
-  public static final String INVALID_TOKEN = "invalid";
+    // special token used for special conditions
+    // e.g. un-authorized: will query token after token expired period
+    // e.g. not found: will query token after token expired period
+    public static final String INVALID_TOKEN = "invalid";
 
-  private static final String UN_AUTHORIZED_CODE_HALF_OPEN = "401302";
+    private static final String UN_AUTHORIZED_CODE_HALF_OPEN = "401302";
 
-  public static final String CACHE_KEY = "token";
+    public static final String CACHE_KEY = "token";
 
-  public static final String AUTH_HEADER = "Authorization";
+    public static final String AUTH_HEADER = "Authorization";
 
-  private static final long TOKEN_REFRESH_TIME_IN_SECONDS = 20 * 60 * 1000;
+    private static final long TOKEN_REFRESH_TIME_IN_SECONDS = 20 * 60 * 1000;
 
-  private ExecutorService executorService;
+    private ExecutorService executorService;
 
-  private LoadingCache<String, String> cache;
+    private LoadingCache<String, String> cache;
 
-  private String lastErrorCode = "401302";
+    private String lastErrorCode = "401302";
 
-  private int lastStatusCode = 401;
+    private int lastStatusCode = 401;
 
-  private ServiceCenterClient client;
+    private ServiceCenterClient client;
 
-  private Configuration properties = ConfigurationFactory.CURRENT_FILE_INSTANCE;
+    private Configuration properties = ConfigurationFactory.CURRENT_FILE_INSTANCE;
 
-  public RBACRequestAuthHeaderProvider() {
-    EventManager.register(this);
+    public RBACRequestAuthHeaderProvider() {
+        EventManager.register(this);
 
-    if (enabled()) {
-      client = createServiceCenterClient();
-      executorService = new ThreadPoolExecutor(1, 1,
-              0L, TimeUnit.MILLISECONDS,
-              new LinkedBlockingQueue<Runnable>(),
-              t -> new Thread(t, "rbac-executor"));
-      ListeningExecutorService service = MoreExecutors.listeningDecorator(executorService);
-      cache = CacheBuilder.newBuilder()
-          .maximumSize(1)
-          .refreshAfterWrite(refreshTime(), TimeUnit.MILLISECONDS)
-          .build(new CacheLoader<String, String>() {
-            @Override
-            public String load(String key) {
-              return createHeaders();
+        if (enabled()) {
+            client = createServiceCenterClient();
+            executorService = new ThreadPoolExecutor(1, 1, 0L, TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<Runnable>(), t -> new Thread(t, "rbac-executor"));
+            ListeningExecutorService service = MoreExecutors.listeningDecorator(executorService);
+            cache = CacheBuilder.newBuilder().maximumSize(1).refreshAfterWrite(refreshTime(), TimeUnit.MILLISECONDS)
+                .build(new CacheLoader<String, String>() {
+                    @Override
+                    public String load(String key) {
+                        return createHeaders();
+                    }
+
+                    @Override
+                    public ListenableFuture<String> reload(String key, String oldValue) {
+                        return Futures.submitAsync(Callables.asAsyncCallable(() -> createHeaders(), service),
+                            executorService);
+                    }
+                });
+        }
+    }
+
+    private ServiceCenterClient createServiceCenterClient() {
+        AddressManager addressManager = createAddressManager();
+        HttpConfiguration.SSLProperties sslProperties = AuthHeaderProviders.createSslProperties(properties);
+        return new ServiceCenterClient(addressManager, sslProperties, signRequest -> Collections.emptyMap(),
+            SeataServicecombKeys.DEFAULT, null);
+    }
+
+    private AddressManager createAddressManager() {
+        String address =
+            properties.getConfig(SeataServicecombKeys.KEY_REGISTRY_ADDRESS, SeataServicecombKeys.DEFAULT_REGISTRY_URL);
+        String project = properties.getConfig(SeataServicecombKeys.KEY_SERVICE_PROJECT, SeataServicecombKeys.DEFAULT);
+        LOGGER.info("Using service center, address={}.", address);
+        return new AddressManager(project, Arrays.asList(address.split(SeataServicecombKeys.COMMA)));
+    }
+
+    @Subscribe
+    public void onNotPermittedEvent(OperationEvents.UnAuthorizedOperationEvent event) {
+        this.executorService.submit(this::retryRefresh);
+    }
+
+    protected String createHeaders() {
+        LOGGER.info("start to create RBAC headers");
+
+        RbacTokenResponse rbacTokenResponse = callCreateHeaders();
+        lastErrorCode = rbacTokenResponse.getErrorCode();
+        lastStatusCode = rbacTokenResponse.getStatusCode();
+
+        if (Status.UNAUTHORIZED.getStatusCode() == rbacTokenResponse.getStatusCode()
+            || Status.FORBIDDEN.getStatusCode() == rbacTokenResponse.getStatusCode()) {
+            // password wrong, do not try anymore
+            LOGGER.warn("username or password may be wrong, stop trying to query tokens.");
+            return INVALID_TOKEN;
+        } else if (Status.NOT_FOUND.getStatusCode() == rbacTokenResponse.getStatusCode()) {
+            // service center not support, do not try
+            LOGGER.warn("service center do not support RBAC token, you should not config account info");
+            return INVALID_TOKEN;
+        }
+
+        LOGGER.info("refresh token successfully {}", rbacTokenResponse.getStatusCode());
+        return rbacTokenResponse.getToken();
+    }
+
+    protected RbacTokenResponse callCreateHeaders() {
+        RbacTokenRequest request = new RbacTokenRequest();
+        request.setName(properties.getConfig(SeataServicecombKeys.KEY_RBAC_NAME));
+        request.setPassword(properties.getConfig(SeataServicecombKeys.KEY_RBAC_PASSWORD));
+
+        return client.queryToken(request);
+    }
+
+    protected long refreshTime() {
+        return TOKEN_REFRESH_TIME_IN_SECONDS;
+    }
+
+    @Override
+    public Map<String, String> authHeaders() {
+        if (!enabled()) {
+            return Collections.emptyMap();
+        }
+
+        try {
+            String header = cache.get(CACHE_KEY);
+            if (!StringUtils.isEmpty(header)) {
+                Map<String, String> tokens = new HashMap<>(1);
+                tokens.put(AUTH_HEADER, "Bearer " + header);
+                return tokens;
             }
-
-            @Override
-            public ListenableFuture<String> reload(String key, String oldValue) {
-              return Futures.submitAsync(Callables.asAsyncCallable(() -> createHeaders(), service), executorService);
-            }
-          });
-    }
-  }
-
-
-  private ServiceCenterClient createServiceCenterClient() {
-    AddressManager addressManager = createAddressManager();
-    HttpConfiguration.SSLProperties sslProperties =
-        AuthHeaderProviders.createSslProperties(properties);
-    return new ServiceCenterClient(addressManager, sslProperties, signRequest -> Collections.emptyMap()
-        , SeataServicecombKeys.DEFAULT, null);
-  }
-
-  private AddressManager createAddressManager() {
-    String address =
-        properties.getConfig(SeataServicecombKeys.KEY_REGISTRY_ADDRESS, SeataServicecombKeys.DEFAULT_REGISTRY_URL);
-    String project = properties.getConfig(SeataServicecombKeys.KEY_SERVICE_PROJECT, SeataServicecombKeys.DEFAULT);
-    LOGGER.info("Using service center, address={}.", address);
-    return new AddressManager(project, Arrays.asList(address.split(SeataServicecombKeys.COMMA)));
-  }
-
-  @Subscribe
-  public void onNotPermittedEvent(OperationEvents.UnAuthorizedOperationEvent event) {
-    this.executorService.submit(this::retryRefresh);
-  }
-
-  protected String createHeaders() {
-    LOGGER.info("start to create RBAC headers");
-
-    RbacTokenResponse rbacTokenResponse = callCreateHeaders();
-    lastErrorCode = rbacTokenResponse.getErrorCode();
-    lastStatusCode = rbacTokenResponse.getStatusCode();
-
-    if (Status.UNAUTHORIZED.getStatusCode() == rbacTokenResponse.getStatusCode()
-        || Status.FORBIDDEN.getStatusCode() == rbacTokenResponse.getStatusCode()) {
-      // password wrong, do not try anymore
-      LOGGER.warn("username or password may be wrong, stop trying to query tokens.");
-      return INVALID_TOKEN;
-    } else if (Status.NOT_FOUND.getStatusCode() == rbacTokenResponse.getStatusCode()) {
-      // service center not support, do not try
-      LOGGER.warn("service center do not support RBAC token, you should not config account info");
-      return INVALID_TOKEN;
+        } catch (Exception e) {
+            LOGGER.error("Get auth headers failed", e);
+        }
+        return Collections.emptyMap();
     }
 
-    LOGGER.info("refresh token successfully {}", rbacTokenResponse.getStatusCode());
-    return rbacTokenResponse.getToken();
-  }
-
-  protected RbacTokenResponse callCreateHeaders() {
-    RbacTokenRequest request = new RbacTokenRequest();
-    request.setName(properties.getConfig(SeataServicecombKeys.KEY_RBAC_NAME));
-    request.setPassword(properties.getConfig(SeataServicecombKeys.KEY_RBAC_PASSWORD));
-
-    return client.queryToken(request);
-  }
-
-  protected long refreshTime() {
-    return TOKEN_REFRESH_TIME_IN_SECONDS;
-  }
-
-  @Override
-  public Map<String, String> authHeaders() {
-    if (!enabled()) {
-      return Collections.emptyMap();
+    private boolean enabled() {
+        return !StringUtils.isEmpty(properties.getConfig(SeataServicecombKeys.KEY_RBAC_NAME))
+            && !StringUtils.isEmpty(properties.getConfig(SeataServicecombKeys.KEY_RBAC_PASSWORD));
     }
 
-    try {
-      String header = cache.get(CACHE_KEY);
-      if (!StringUtils.isEmpty(header)) {
-        Map<String, String> tokens = new HashMap<>(1);
-        tokens.put(AUTH_HEADER, "Bearer " + header);
-        return tokens;
-      }
-    } catch (Exception e) {
-      LOGGER.error("Get auth headers failed", e);
+    private void retryRefresh() {
+        if (Status.UNAUTHORIZED.getStatusCode() == lastStatusCode
+            && UN_AUTHORIZED_CODE_HALF_OPEN.equals(lastErrorCode)) {
+            cache.refresh(properties.getConfig(SeataServicecombKeys.KEY_SERVICE_NAME));
+        }
     }
-    return Collections.emptyMap();
-  }
-
-  private boolean enabled() {
-    return !StringUtils.isEmpty(properties.getConfig(SeataServicecombKeys.KEY_RBAC_NAME)) && !StringUtils
-        .isEmpty(properties.getConfig(SeataServicecombKeys.KEY_RBAC_PASSWORD));
-  }
-
-  private void retryRefresh() {
-    if (Status.UNAUTHORIZED.getStatusCode() == lastStatusCode && UN_AUTHORIZED_CODE_HALF_OPEN.equals(lastErrorCode)) {
-      cache.refresh(properties.getConfig(SeataServicecombKeys.KEY_SERVICE_NAME));
-    }
-  }
 }
