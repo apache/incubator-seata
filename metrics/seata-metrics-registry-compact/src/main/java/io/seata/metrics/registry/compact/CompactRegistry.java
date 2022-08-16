b6e1dffc11ebe60b4024f51f71ce43324bd13c8f
liurong <liurong.liu@alibaba-inc.com>
2022-08-16 10:23:03 +0800

    fix

--- metrics/seata-metrics-registry-compact/src/main/java/io/seata/metrics/registry/compact/CompactRegistry.java
+++ metrics/seata-metrics-registry-compact/src/main/java/io/seata/metrics/registry/compact/CompactRegistry.java
@@ -18,7 +18,6 @@ package io.seata.metrics.registry.compact;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Supplier;
 
