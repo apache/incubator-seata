9abbe557bd8c60346294567f5e024bc6d947dcac
ph3636 <>
2022-08-17 19:46:10 +0800

    feature: add pipeline processing thread pool, mainly deal with non-business logic such as codec etc

--- common/src/main/java/io/seata/common/ConfigurationKeys.java
+++ common/src/main/java/io/seata/common/ConfigurationKeys.java
@@ -854,4 +854,23 @@ public interface ConfigurationKeys {
      */
     String ENABLE_PARALLEL_REQUEST_HANDLE_KEY = SERVER_PREFIX + "enableParallelRequestHandle";
 
+    /**
+     * The constant SERVER_PIPELINE_THREAD_PREFIX
+     */
+    String SERVER_PIPELINE_THREAD_PREFIX = THREAD_FACTORY_PREFIX + "serverPipelineThreadPrefix";
+
+    /**
+     * The constant SERVER_PIPELINE_THREAD_SIZE
+     */
+    String SERVER_PIPELINE_THREAD_SIZE = THREAD_FACTORY_PREFIX + "serverPipelineThreadSize";
+
+    /**
+     * The constant CLIENT_PIPELINE_THREAD_PREFIX
+     */
+    String CLIENT_PIPELINE_THREAD_PREFIX = THREAD_FACTORY_PREFIX + "clientPipelineThreadPrefix";
+
+    /**
+     * The constant CLIENT_PIPELINE_THREAD_SIZE
+     */
+    String CLIENT_PIPELINE_THREAD_SIZE = THREAD_FACTORY_PREFIX + "clientPipelineThreadSize";
 }
