1582e4514b7e743af38bf6123a185670d7f45236
liurong <liurong.liu@alibaba-inc.com>
2022-08-17 18:36:34 +0800

    add assert in UT

--- server/src/test/java/io/seata/server/metrics/RegistryMeterKeyTest.java
+++ server/src/test/java/io/seata/server/metrics/RegistryMeterKeyTest.java
@@ -1,8 +1,10 @@
 package io.seata.server.metrics;
 
-import io.seata.metrics.Counter;
 import io.seata.metrics.Id;
 import io.seata.metrics.IdConstants;
+import io.seata.metrics.Counter;
+import io.seata.metrics.registry.RegistryFactory;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static io.seata.server.metrics.MeterIdConstants.COUNTER_ACTIVE;
@@ -10,9 +12,20 @@ import static io.seata.server.metrics.MeterIdConstants.COUNTER_ACTIVE;
 public class RegistryMeterKeyTest {
     @Test
     public void testGetIdMeterKey() {
-        System.out.println(COUNTER_ACTIVE.getMeterKey());
-        Id id = COUNTER_ACTIVE.withTag(IdConstants.ROLE_KEY, IdConstants.ROLE_VALUE_TM);
-        System.out.println(id.getMeterKey());
+        Id id1 = COUNTER_ACTIVE.withTag(IdConstants.ROLE_KEY, IdConstants.ROLE_VALUE_TM);
+        Id id2 = COUNTER_ACTIVE.withTag(IdConstants.ROLE_KEY, IdConstants.ROLE_VALUE_TC);
+        Id id3 = COUNTER_ACTIVE.withTag(IdConstants.ROLE_KEY, IdConstants.ROLE_VALUE_TM);
+
+        Assertions.assertNotEquals(id1.getMeterKey(), id2.getMeterKey());
+        Assertions.assertEquals(id1.getMeterKey(),id3.getMeterKey());
+
+        Counter c1 = RegistryFactory.getInstance().getCounter(id1);
+        Counter c2 = RegistryFactory.getInstance().getCounter(id2);
+        Counter c3 = RegistryFactory.getInstance().getCounter(id3);
+
+        Assertions.assertNotEquals(c1, c2);
+        Assertions.assertEquals(c2, c3);
+
     }
 
 }
