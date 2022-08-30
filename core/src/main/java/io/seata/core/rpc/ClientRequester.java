970972318656ff46e78f140bd3d8c1005e01c2b6
GoodBoyCoder <goodboycoder@outlook.com>
2022-08-30 20:01:11 +0800

    refactor:code style

--- core/src/main/java/io/seata/core/rpc/ClientRequester.java
+++ core/src/main/java/io/seata/core/rpc/ClientRequester.java
@@ -1,3 +1,18 @@
+/*
+ *  Copyright 1999-2019 Seata.io Group.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package io.seata.core.rpc;
 
 import java.util.Map;
@@ -7,7 +22,6 @@ import java.util.function.Supplier;
 import io.seata.common.ConfigurationKeys;
 import io.seata.common.DefaultValues;
 import io.seata.common.exception.NotSupportYetException;
-import io.seata.common.util.CollectionUtils;
 import io.seata.config.ConfigurationFactory;
 import io.seata.core.rpc.grpc.RmGrpcRemotingClient;
 import io.seata.core.rpc.grpc.TmGrpcRemotingClient;
