d1dabea4513ef8edbd26dd9988d9e459f47da02d
MOTMlsc <lsc13581765971@163.com>
2022-09-06 10:20:24 +0800

    add header and remove unused import

--- core/src/main/java/io/seata/core/rpc/netty/gts/message/TxcMessageCodec.java
+++ core/src/main/java/io/seata/core/rpc/netty/gts/message/TxcMessageCodec.java
@@ -1,3 +1,18 @@
+/*
+ *  Copyright 1999-2019 Seata.io Group.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package io.seata.core.rpc.netty.gts.message;
 
 import io.netty.buffer.ByteBuf;
@@ -10,13 +25,6 @@ import io.seata.core.rpc.netty.gts.exception.TxcException;
 import io.seata.core.serializer.Serializer;
 import io.seata.core.serializer.SerializerServiceLoader;
 import io.seata.core.serializer.SerializerType;
-
-/**
- * @author liusicheng
- * @website seata
- * @description /
- * @date 2022/9/6
- **/
 
 public class TxcMessageCodec {
     public static TxcCodec getTxcCodecInstance(short typeCode) {
