e38e56d7b83966cbac8a2e881b96392d7f821401
jimin <slievrly@163.com>
2022-09-08 20:12:08 +0800

    optimize: fix some vulnerabilities in console and upgrade skywalking-eyes (#4921)

--- distribution/bin/seata-server.bat
+++ distribution/bin/seata-server.bat
@@ -2,7 +2,7 @@
 @REM
 @REM Licensed under the Apache License, Version 2.0 (the "License");
 @REM you may not use this file except in compliance with the License.
-@REM You may obtain a copy of the License at、
+@REM You may obtain a copy of the License at
 @REM
 @REM      http://www.apache.org/licenses/LICENSE-2.0
 @REM
