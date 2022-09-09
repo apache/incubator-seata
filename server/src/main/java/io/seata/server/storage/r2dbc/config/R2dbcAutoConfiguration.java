aec5f055cfdf1a6993a07637a11ea456041c4fac
FUNKYE <364176773@qq.com>
2022-09-09 21:47:00 +0800

    update

--- server/src/main/java/io/seata/server/storage/r2dbc/config/R2dbcAutoConfiguration.java
+++ server/src/main/java/io/seata/server/storage/r2dbc/config/R2dbcAutoConfiguration.java
@@ -1,3 +1,18 @@
+/*
+ *  Copyright 1999-2019 Seata.io Group.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package io.seata.server.storage.r2dbc.config;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
@@ -5,6 +20,9 @@ import org.springframework.boot.autoconfigure.data.r2dbc.R2dbcDataAutoConfigurat
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+/**
+ * @author jianbin.chen
+ */
 @ConditionalOnExpression("#{'db'.equals('${sessionMode}')||'db'.equals('${lockMode}')}")
 @Configuration
 @Import(R2dbcDataAutoConfiguration.class)
