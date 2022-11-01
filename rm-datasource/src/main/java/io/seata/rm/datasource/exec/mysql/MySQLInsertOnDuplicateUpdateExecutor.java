13db88f6a73c71fe087c76d5d909d93d78c54b96
renliangyu857 <2918490262@qq.com>
2022-11-01 15:01:23 +0800

    resolve test failures

--- rm-datasource/src/main/java/io/seata/rm/datasource/exec/mysql/MySQLInsertOnDuplicateUpdateExecutor.java
+++ rm-datasource/src/main/java/io/seata/rm/datasource/exec/mysql/MySQLInsertOnDuplicateUpdateExecutor.java
@@ -385,9 +385,9 @@ public class MySQLInsertOnDuplicateUpdateExecutor extends MySQLInsertExecutor im
         for (ColumnMeta columnMeta : indexMeta.getValues()) {
             String columnName = columnMeta.getColumnName();
             List<Object> imageParameters = imageParameterMap.get(columnName);
-            if ((imageParameters == null && columnMeta.getColumnDef() == null)
-                || imageParameters.get(rowIndex) == null
-                || imageParameters.get(rowIndex) instanceof Null) {
+            if (imageParameters == null && columnMeta.getColumnDef() == null) {
+                return false;
+            } else if (imageParameters != null && (imageParameters.get(rowIndex) == null || imageParameters.get(rowIndex) instanceof Null)){
                 return false;
             }
         }
