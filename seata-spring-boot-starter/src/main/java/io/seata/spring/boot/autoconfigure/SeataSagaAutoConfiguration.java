d0c41e97c083230365c350db51a79441c5c42e7b
a364176773 <364176773@qq.com>
2022-11-01 18:12:26 +0800

    update

--- seata-spring-boot-starter/src/main/java/io/seata/spring/boot/autoconfigure/SeataSagaAutoConfiguration.java
+++ seata-spring-boot-starter/src/main/java/io/seata/spring/boot/autoconfigure/SeataSagaAutoConfiguration.java
@@ -36,7 +36,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolExecutorFactoryBean;
