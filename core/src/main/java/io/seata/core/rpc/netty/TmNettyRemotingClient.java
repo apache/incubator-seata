115ec7df440054be2237629231d1e1da4f05be92
renliangyu857 <2918490262@qq.com>
2022-11-24 09:53:23 +0800

    Update TmNettyRemotingClient.java

--- core/src/main/java/io/seata/core/rpc/netty/TmNettyRemotingClient.java
+++ core/src/main/java/io/seata/core/rpc/netty/TmNettyRemotingClient.java
@@ -118,7 +118,7 @@ public final class TmNettyRemotingClient extends AbstractNettyRemotingClient {
                             new NamedThreadFactory(nettyClientConfig.getTmDispatchThreadPrefix(),
                                     nettyClientConfig.getClientWorkerThreads()),
                             RejectedPolicies.runsOldestTaskPolicy());
-                    instance = new TmNettyRemotingClient(nettyClientConfig, null, messageExecutor,applicationId,transactionServiceGroup,accessKey,secretKey);
+                    instance = new TmNettyRemotingClient(nettyClientConfig, null, messageExecutor, applicationId, transactionServiceGroup, accessKey, secretKey);
                 }
             }
         }
