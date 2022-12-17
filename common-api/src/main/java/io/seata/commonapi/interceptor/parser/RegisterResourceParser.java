e54fb846f18385cc6c435e5a602a5b28e021aa53
leezongjie <leezongjie.lzj@antgroup.com>
2022-12-17 17:47:50 +0800

    添加分支注册接口

---
+++ common-api/src/main/java/io/seata/commonapi/interceptor/parser/RegisterResourceParser.java
@@ -0,0 +1,11 @@
+package io.seata.commonapi.interceptor.parser;
+
+/**
+ * @author leezongjie
+ * @date 2022/12/17
+ */
+public interface RegisterResourceParser {
+
+    void registerResource(Object target);
+
+}
