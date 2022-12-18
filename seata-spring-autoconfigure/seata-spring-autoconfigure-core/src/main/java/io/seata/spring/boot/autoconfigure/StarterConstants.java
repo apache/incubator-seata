52541c5afd8fe99aaea07761d08644cc68c19de3
WangLiang/王良 <841369634@qq.com>
2022-12-18 13:41:05 +0800

    optimize: First support `spring:6.x` and `spring-boot:3.x` (#5115)

--- seata-spring-autoconfigure/seata-spring-autoconfigure-core/src/main/java/io/seata/spring/boot/autoconfigure/StarterConstants.java
+++ seata-spring-autoconfigure/seata-spring-autoconfigure-core/src/main/java/io/seata/spring/boot/autoconfigure/StarterConstants.java
@@ -38,6 +38,7 @@ public interface StarterConstants {
     String UNDO_PREFIX = CLIENT_PREFIX + ".undo";
     String LOAD_BALANCE_PREFIX_KEBAB_STYLE = CLIENT_PREFIX + ".load-balance";
     String LOAD_BALANCE_PREFIX = CLIENT_PREFIX + ".loadBalance";
+    String HTTP_PREFIX = CLIENT_PREFIX + ".http";
     String LOG_PREFIX = SEATA_PREFIX + ".log";
     String COMPRESS_PREFIX = UNDO_PREFIX + ".compress";
     String TCC_PREFIX = SEATA_PREFIX + ".tcc";
