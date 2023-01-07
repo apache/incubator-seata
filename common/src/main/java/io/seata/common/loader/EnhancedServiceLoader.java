8dc2b88f5c8764f1bed13d295fbdd6f0defe6c8d
yuruixin <ruixinyu.yrx@alibaba-inc.com>
2023-01-07 17:15:25 +0800

    optimize: print Stack Trace in EnhancedServiceLoader.loadFile#cahtch

--- common/src/main/java/io/seata/common/loader/EnhancedServiceLoader.java
+++ common/src/main/java/io/seata/common/loader/EnhancedServiceLoader.java
@@ -540,7 +540,7 @@ public class EnhancedServiceLoader {
                                     }
                                     extensions.add(extensionDefinition);
                                 } catch (LinkageError | ClassNotFoundException e) {
-                                    LOGGER.warn("Load [{}] class fail. {}", line, e.getMessage());
+                                    LOGGER.warn("Load [{}] class fail. ", line, e);
                                 } catch (ClassCastException e) {
                                     LOGGER.error("Load [{}] class fail, please make sure the extension" +
                                             " config in {} implements {}.", line, fileName, type.getName());
