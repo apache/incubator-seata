dc8ab09df6c5dcec8c5c5b9dfd5c1cc9d4d3b81c
leezongjie <leezongjie.lzj@antgroup.com>
2023-02-13 12:05:55 +0800

    fix，recovery BeforeTransaction

--- integration-tx-api/src/main/java/io/seata/integration/tx/api/interceptor/handler/GlobalTransactionalInterceptorHandler.java
+++ integration-tx-api/src/main/java/io/seata/integration/tx/api/interceptor/handler/GlobalTransactionalInterceptorHandler.java
@@ -350,7 +350,7 @@ public class GlobalTransactionalInterceptorHandler extends AbstractProxyInvocati
 
     @Override
     public SeataInterceptorPosition getPosition() {
-        return SeataInterceptorPosition.AfterTransaction;
+        return SeataInterceptorPosition.BeforeTransaction;
     }
 
 }
