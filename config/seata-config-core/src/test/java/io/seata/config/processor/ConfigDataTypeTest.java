c1902713916a926cf1fdf5017aaf1948fdb69a11
liuqiufeng <775038282@qq.com>
2023-02-19 11:43:45 +0800

    add unit test

---
+++ config/seata-config-core/src/test/java/io/seata/config/processor/ConfigDataTypeTest.java
@@ -0,0 +1,54 @@
+/*
+ *  Copyright 1999-2019 Seata.io Group.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package io.seata.config.processor;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author liuqiufeng
+ */
+class ConfigDataTypeTest {
+
+    @Test
+    void getType() {
+        ConfigDataType configDataType = ConfigDataType.getType("yaml");
+        Assertions.assertEquals(ConfigDataType.yaml, configDataType);
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            ConfigDataType.getType("test");
+        });
+    }
+
+    @Test
+    void getTypeBySuffix() {
+        ConfigDataType configDataType = ConfigDataType.getTypeBySuffix("yml");
+        Assertions.assertEquals(ConfigDataType.yaml, configDataType);
+        
+        configDataType = ConfigDataType.getTypeBySuffix("yaml");
+        Assertions.assertEquals(ConfigDataType.yaml, configDataType);
+        
+        configDataType = ConfigDataType.getTypeBySuffix("properties");
+        Assertions.assertEquals(ConfigDataType.properties, configDataType);
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            ConfigDataType.getTypeBySuffix("test");
+        });
+    }
+}
