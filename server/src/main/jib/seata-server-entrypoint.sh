87588b0a3676114c425ac94edb5de23c0b3cea30
xingfudeshi <xingfudeshi@gmail.com>
2023-04-23 17:22:45 +0800

    docker image supports JVM parameter injection

---
+++ server/src/main/jib/seata-server-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Copyright 1999-2019 Seata.io Group.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at、
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# entrypoint for server
+
+exec java $JAVA_OPTS \
+  -cp $( cat /app/jib-classpath-file ) \
+  $( cat /app/jib-main-class-file )
