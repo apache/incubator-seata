165dee7070179f350d375d2b967c25d853253a7a
jimin <slievrly@163.com>
2023-05-07 17:44:11 +0800

    bugfix: fix oracle insert undolog failed (#5556)
    Signed-off-by: slievrly <slievrly@163.com>

--- script/client/at/db/oracle.sql
+++ script/client/at/db/oracle.sql
@@ -1,6 +1,7 @@
 -- for AT mode you must to init this sql for you business database. the seata server not need it.
 CREATE TABLE undo_log
 (
+    id            NUMBER(19)    NOT NULL,
     branch_id     NUMBER(19)    NOT NULL,
     xid           VARCHAR2(128) NOT NULL,
     context       VARCHAR2(128) NOT NULL,
@@ -8,6 +9,7 @@ CREATE TABLE undo_log
     log_status    NUMBER(10)    NOT NULL,
     log_created   TIMESTAMP(0)  NOT NULL,
     log_modified  TIMESTAMP(0)  NOT NULL,
+    PRIMARY KEY (id),
     CONSTRAINT ux_undo_log UNIQUE (xid, branch_id)
 );
 CREATE INDEX ix_log_created ON undo_log(log_created);
@@ -19,3 +21,6 @@ COMMENT ON COLUMN undo_log.rollback_info is 'rollback info';
 COMMENT ON COLUMN undo_log.log_status is '0:normal status,1:defense status';
 COMMENT ON COLUMN undo_log.log_created is 'create datetime';
 COMMENT ON COLUMN undo_log.log_modified is 'modify datetime';
+
+-- Generate ID using sequence and trigger
+CREATE SEQUENCE UNDO_LOG_SEQ START WITH 1 INCREMENT BY 1;
