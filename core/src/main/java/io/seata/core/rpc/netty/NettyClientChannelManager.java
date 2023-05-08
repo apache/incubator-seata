f324e23465c1ddcdee3bb151772c728ee977a104
Yikun Chen <1197896848@qq.com>
2023-05-08 11:31:08 +0800

    optimize: optimize channel availability thread log output

--- core/src/main/java/io/seata/core/rpc/netty/NettyClientChannelManager.java
+++ core/src/main/java/io/seata/core/rpc/netty/NettyClientChannelManager.java
@@ -16,7 +16,13 @@
 package io.seata.core.rpc.netty;
 
 import java.net.InetSocketAddress;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.function.Function;
