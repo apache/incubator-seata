486d2338ca5a303f8b52f81a72b9048ac6c9bd01
fengzhenhai <87805798+vonfun@users.noreply.github.com>
2023-06-12 22:55:43 +0800

    feature: support trace the transaction link according to the XID (#5600)

--- ext/apm-seata-skywalking-plugin/src/main/java/io/seata/apm/skywalking/plugin/RemotingProcessorProcessInterceptor.java
+++ ext/apm-seata-skywalking-plugin/src/main/java/io/seata/apm/skywalking/plugin/RemotingProcessorProcessInterceptor.java
@@ -13,29 +13,37 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
+
 package io.seata.apm.skywalking.plugin;
 
+import com.alipay.sofa.common.profile.StringUtil;
+import io.seata.apm.skywalking.plugin.common.SWSeataUtils;
+import io.seata.core.protocol.AbstractMessage;
 import io.seata.core.protocol.RpcMessage;
 import org.apache.skywalking.apm.agent.core.context.CarrierItem;
 import org.apache.skywalking.apm.agent.core.context.ContextCarrier;
 import org.apache.skywalking.apm.agent.core.context.ContextManager;
+import org.apache.skywalking.apm.agent.core.context.tag.StringTag;
 import org.apache.skywalking.apm.agent.core.context.trace.AbstractSpan;
 import org.apache.skywalking.apm.agent.core.context.trace.SpanLayer;
 import org.apache.skywalking.apm.agent.core.plugin.interceptor.enhance.EnhancedInstance;
 import org.apache.skywalking.apm.agent.core.plugin.interceptor.enhance.InstanceMethodsAroundInterceptor;
 import org.apache.skywalking.apm.agent.core.plugin.interceptor.enhance.MethodInterceptResult;
-import io.seata.apm.skywalking.plugin.common.SWSeataUtils;
 import org.apache.skywalking.apm.network.trace.component.ComponentsDefine;
 
 import java.lang.reflect.Method;
 
 /**
+ * The RemoteProcessor deal.
+ *
  * @author zhaoyuguang
  */
-public class RemotingProcessorProcessInterceptor implements InstanceMethodsAroundInterceptor {
+public class RemotingProcessorProcessInterceptor implements
+        InstanceMethodsAroundInterceptor {
 
     @Override
-    public void beforeMethod(EnhancedInstance objInst, Method method, Object[] allArguments, Class<?>[] argumentsTypes,
+    public void beforeMethod(EnhancedInstance objInst, Method method,
+                             Object[] allArguments, Class<?>[] argumentsTypes,
                              MethodInterceptResult result) throws Throwable {
         RpcMessage rpcMessage = (RpcMessage) allArguments[1];
         String operationName = SWSeataUtils.convertOperationName(rpcMessage);
@@ -48,17 +56,27 @@ public class RemotingProcessorProcessInterceptor implements InstanceMethodsAroun
         AbstractSpan activeSpan = ContextManager.createEntrySpan(operationName, contextCarrier);
         SpanLayer.asRPCFramework(activeSpan);
         activeSpan.setComponent(ComponentsDefine.SEATA);
+
+        String xid = SWSeataUtils.convertXid(rpcMessage);
+        if (StringUtil.isNotBlank(xid)) {
+            activeSpan.tag(new StringTag(20, "Seata.xid"), xid);
+        }
     }
 
     @Override
-    public Object afterMethod(EnhancedInstance objInst, Method method, Object[] allArguments, Class<?>[] argumentsTypes,
+    public Object afterMethod(EnhancedInstance objInst, Method method,
+                              Object[] allArguments, Class<?>[] argumentsTypes,
                               Object ret) throws Throwable {
-        ContextManager.stopSpan();
+        RpcMessage rpcMessage = (RpcMessage) allArguments[0];
+        if (rpcMessage.getBody() instanceof AbstractMessage) {
+            ContextManager.stopSpan();
+        }
         return ret;
     }
 
     @Override
-    public void handleMethodException(EnhancedInstance objInst, Method method, Object[] allArguments,
+    public void handleMethodException(EnhancedInstance objInst,
+                                      Method method, Object[] allArguments,
                                       Class<?>[] argumentsTypes, Throwable t) {
     }
 }
