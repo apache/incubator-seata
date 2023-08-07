8c21373e6ba2489079a9ed285ee369d2ff0377e0
slievrly <slievrly@163.com>
2023-08-07 20:19:09 +0800

    fix style
    Signed-off-by: slievrly <slievrly@163.com>

--- core/src/main/java/io/seata/core/serializer/SerializerSecurityRegistry.java
+++ core/src/main/java/io/seata/core/serializer/SerializerSecurityRegistry.java
@@ -27,8 +27,6 @@ import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
