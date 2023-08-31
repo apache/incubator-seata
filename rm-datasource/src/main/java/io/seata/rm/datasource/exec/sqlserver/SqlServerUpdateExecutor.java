c8e208667ca535190aa9aa202cd3c8cf909dde77
FUNKYE <364176773@qq.com>
2023-08-31 15:11:46 +0800

    bugfix: insert executor keywords unescape (#5821)

--- rm-datasource/src/main/java/io/seata/rm/datasource/exec/sqlserver/SqlServerUpdateExecutor.java
+++ rm-datasource/src/main/java/io/seata/rm/datasource/exec/sqlserver/SqlServerUpdateExecutor.java
@@ -61,7 +61,7 @@ public class SqlServerUpdateExecutor<T, S extends Statement> extends UpdateExecu
             suffix.append(WHERE).append(whereCondition);
         }
         StringJoiner selectSQLJoin = new StringJoiner(", ", prefix.toString(), suffix.toString());
-        List<String> needUpdateColumns = getNeedUpdateColumns(tableMeta.getTableName(), sqlRecognizer.getTableAlias(), recognizer.getUpdateColumnsUnEscape());
+        List<String> needUpdateColumns = getNeedColumns(tableMeta.getTableName(), sqlRecognizer.getTableAlias(), recognizer.getUpdateColumnsUnEscape());
         for (String needUpdateColumn : needUpdateColumns) {
             selectSQLJoin.add(needUpdateColumn);
         }
