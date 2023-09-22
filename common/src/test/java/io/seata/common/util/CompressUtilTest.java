7401934eb96879b38031950eb7f02449b671b989
王良 <841369634@qq.com>
2023-09-22 14:41:27 +0800

    test-java21 build.yml

--- common/src/test/java/io/seata/common/util/CompressUtilTest.java
+++ common/src/test/java/io/seata/common/util/CompressUtilTest.java
@@ -36,15 +36,15 @@ public class CompressUtilTest {
 
 
     @Test
-    @DisabledOnJre(JRE.JAVA_17)
-    public void testCompress() throws IOException {
+    @EnabledOnJre(JRE.JAVA_8)
+    public void testCompressForJava8() throws IOException {
         Assertions.assertArrayEquals(compressedBytes1,
                 CompressUtil.compress(originBytes));
     }
 
     @Test
-    @EnabledOnJre(JRE.JAVA_17)
-    public void testCompressForJava17() throws IOException {
+    @DisabledOnJre(JRE.JAVA_8)
+    public void testCompressForNotJava8() throws IOException {
         Assertions.assertArrayEquals(compressedBytes2,
                 CompressUtil.compress(originBytes));
     }
