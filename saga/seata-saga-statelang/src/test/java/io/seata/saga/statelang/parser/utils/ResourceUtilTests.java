b7bef337f06ea54c88f9657a2bf919601254cad8
PTYin <peteryin1604@gmail.com>
2023-10-12 15:35:32 +0800

    bugfix: modify tests

--- saga/seata-saga-statelang/src/test/java/io/seata/saga/statelang/parser/utils/ResourceUtilTests.java
+++ saga/seata-saga-statelang/src/test/java/io/seata/saga/statelang/parser/utils/ResourceUtilTests.java
@@ -30,9 +30,9 @@ public class ResourceUtilTests {
     @Test
     public void getResources_test() {
         Resource[] resources = ResourceUtil.getResources("classpath*:statelang/*.json");
-        assertThat(resources.length).isEqualTo(2);
+        assertThat(resources.length).isEqualTo(6);
 
         Resource[] resources2 = ResourceUtil.getResources(new String[]{"classpath*:statelang/*.json"});
-        assertThat(resources2.length).isEqualTo(2);
+        assertThat(resources2.length).isEqualTo(6);
     }
 }
