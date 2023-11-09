e10bd1deccc74eef0c21f19602c950dc08e42389
jianbin <jianbin@apache.org>
2023-11-09 17:27:32 +0800

    test: Adding test cases for RaftServerFactory

--- server/src/test/java/io/seata/server/raft/RaftServerTest.java
+++ server/src/test/java/io/seata/server/raft/RaftServerTest.java
@@ -1,3 +1,18 @@
+/*
+ *  Copyright 1999-2019 Seata.io Group.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package io.seata.server.raft;
 
 import io.seata.common.ConfigurationKeys;
@@ -15,14 +30,26 @@ import org.springframework.context.ApplicationContext;
 public class RaftServerTest {
 
     @BeforeAll
-    public static void setUp(ApplicationContext context) {}
+    public static void setUp(ApplicationContext context) {
+    }
 
     @AfterAll
     public static void destroy() {
-        RaftServerFactory.getInstance().destroy();
         System.setProperty("server.raftPort", "0");
         System.setProperty(ConfigurationKeys.SERVER_RAFT_SERVER_ADDR, "");
         StoreConfig.setStartupParameter("file", "file", "file");
+    }
+
+    @Test
+    public void initRaftServerStart() {
+        System.setProperty("server.raftPort", "9091");
+        System.setProperty(ConfigurationKeys.SERVER_RAFT_SERVER_ADDR,
+            XID.getIpAddress() + ":9091" + "," + XID.getIpAddress() + ":9092" + "," + XID.getIpAddress() + ":9093");
+        StoreConfig.setStartupParameter("raft", "raft", "raft");
+        Assertions.assertDoesNotThrow(() -> RaftServerFactory.getInstance().init());
+        Assertions.assertNotNull(RaftServerFactory.getInstance().getRaftServer("default"));
+        RaftServerFactory.getInstance().start();
+        RaftServerFactory.getInstance().destroy();
     }
 
     @Test
@@ -38,17 +65,6 @@ public class RaftServerTest {
             XID.getIpAddress() + ":9091" + "," + XID.getIpAddress() + ":9092" + "," + XID.getIpAddress() + ":9093");
         StoreConfig.setStartupParameter("raft", "raft", "raft");
         Assertions.assertThrows(IllegalArgumentException.class, () -> RaftServerFactory.getInstance().init());
-    }
-
-    @Test
-    public void initRaftServerStart() {
-        System.setProperty("server.raftPort", "9091");
-        System.setProperty(ConfigurationKeys.SERVER_RAFT_SERVER_ADDR,
-            XID.getIpAddress() + ":9091" + "," + XID.getIpAddress() + ":9092" + "," + XID.getIpAddress() + ":9093");
-        StoreConfig.setStartupParameter("raft", "raft", "raft");
-        Assertions.assertDoesNotThrow(() -> RaftServerFactory.getInstance().init());
-        Assertions.assertNotNull(RaftServerFactory.getInstance().getRaftServer("default"));
-        RaftServerFactory.getInstance().start();
     }
 
 }
