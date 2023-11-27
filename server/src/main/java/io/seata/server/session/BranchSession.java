0a7359517ad6050d2a2d958dde007741fa00b795
funkye <jianbin@apache.org>
2023-11-27 14:39:38 +0800

    bugfix: support jdk9+ compile code (#6085)

--- server/src/main/java/io/seata/server/session/BranchSession.java
+++ server/src/main/java/io/seata/server/session/BranchSession.java
@@ -22,7 +22,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
-
+import io.seata.common.util.BufferUtils;
 import io.seata.common.util.CompressUtil;
 import io.seata.core.exception.TransactionException;
 import io.seata.core.model.BranchStatus;
@@ -407,7 +407,7 @@ public class BranchSession implements Lockable, Comparable<BranchSession>, Sessi
 
         byteBuffer.put((byte)status.getCode());
         byteBuffer.put((byte)lockStatus.getCode());
-        byteBuffer.flip();
+        BufferUtils.flip(byteBuffer);
         byte[] result = new byte[byteBuffer.limit()];
         byteBuffer.get(result);
         return result;
