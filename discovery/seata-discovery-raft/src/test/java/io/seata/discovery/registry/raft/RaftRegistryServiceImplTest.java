4daf69aa03741da72ef5ecb6ed9232582311dcba
ggbocoder <1832877748@qq.com>
2023-11-28 23:00:09 +0800

    fix

--- discovery/seata-discovery-raft/src/test/java/io/seata/discovery/registry/raft/RaftRegistryServiceImplTest.java
+++ discovery/seata-discovery-raft/src/test/java/io/seata/discovery/registry/raft/RaftRegistryServiceImplTest.java
@@ -22,6 +22,7 @@ import org.apache.http.HttpStatus;
 import org.apache.http.StatusLine;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.entity.StringEntity;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
@@ -52,9 +53,12 @@ class RaftRegistryServiceImplTest {
         System.setProperty("registry.raft.serverAddr", "127.0.0.1:8092");
         System.setProperty("registry.raft.tokenValidityInMilliseconds", "10000");
         ConfigurationFactory.getInstance();
-
     }
 
+    @AfterAll
+    public static void adAfterClass() throws Exception {
+        System.clearProperty("service.vgroupMapping.tx");
+    }
     /**
      * test whether throws exception when login failed
      */
