2d06c65d747e48050ccd71ffff5bf4d17c2d574c
leezongjie <leezongjie.lzj@antgroup.com>
2023-12-01 10:37:40 +0800

    bugfix: The TCC aspect does not handle internal invocation exceptions and directly throws them externally. (#6090)

---
+++ spring/src/test/java/io/seata/spring/annotation/AdapterSpringSeataInterceptorTest.java
@@ -0,0 +1,103 @@
+/*
+ *  Copyright 1999-2019 Seata.io Group.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package io.seata.spring.annotation;
+
+import java.lang.reflect.AccessibleObject;
+import java.lang.reflect.Method;
+import java.util.concurrent.Callable;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import io.seata.integration.tx.api.interceptor.handler.ProxyInvocationHandler;
+import io.seata.integration.tx.api.interceptor.parser.DefaultInterfaceParser;
+import io.seata.rm.tcc.api.BusinessActionContext;
+import io.seata.spring.tcc.NormalTccAction;
+import io.seata.spring.tcc.NormalTccActionImpl;
+import org.aopalliance.intercept.MethodInvocation;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author leezongjie
+ * @date 2023/11/29
+ */
+class AdapterSpringSeataInterceptorTest {
+
+    @Test
+    void should_throw_raw_exception_when_call_prepareWithException() throws Throwable {
+        //given
+        NormalTccActionImpl normalTccAction = new NormalTccActionImpl();
+        ProxyInvocationHandler proxyInvocationHandler = DefaultInterfaceParser.get().parserInterfaceToProxy(normalTccAction, "proxyTccAction");
+        AdapterSpringSeataInterceptor adapterSpringSeataInterceptor = new AdapterSpringSeataInterceptor(proxyInvocationHandler);
+        MyMockMethodInvocation myMockMethodInvocation = new MyMockMethodInvocation(NormalTccAction.class.getMethod("prepareWithException", BusinessActionContext.class), () -> normalTccAction.prepareWithException(null));
+
+        //when then
+        Assertions.assertThrows(IllegalArgumentException.class, () -> adapterSpringSeataInterceptor.invoke(myMockMethodInvocation));
+    }
+
+    @Test
+    void should_success_when_call_prepare_with_ProxyInvocationHandler() throws Throwable {
+        //given
+        NormalTccActionImpl normalTccAction = new NormalTccActionImpl();
+        ProxyInvocationHandler proxyInvocationHandler = DefaultInterfaceParser.get().parserInterfaceToProxy(normalTccAction, "proxyTccAction");
+        AdapterSpringSeataInterceptor adapterSpringSeataInterceptor = new AdapterSpringSeataInterceptor(proxyInvocationHandler);
+        MyMockMethodInvocation myMockMethodInvocation = new MyMockMethodInvocation(NormalTccAction.class.getMethod("prepare", BusinessActionContext.class), () -> normalTccAction.prepare(null));
+
+        //when then
+        Assertions.assertTrue((Boolean) adapterSpringSeataInterceptor.invoke(myMockMethodInvocation));
+    }
+
+    static class MyMockMethodInvocation implements MethodInvocation {
+
+        private Callable callable;
+        private Method method;
+
+        public MyMockMethodInvocation(Method method, Callable callable) {
+            this.method = method;
+            this.callable = callable;
+        }
+
+        @Nullable
+        @Override
+        public Object proceed() throws Throwable {
+            return callable.call();
+        }
+
+        @Nonnull
+        @Override
+        public Method getMethod() {
+            return method;
+        }
+
+        @Nonnull
+        @Override
+        public Object[] getArguments() {
+            return new Object[0];
+        }
+
+        @Nullable
+        @Override
+        public Object getThis() {
+            return null;
+        }
+
+        @Nonnull
+        @Override
+        public AccessibleObject getStaticPart() {
+            return null;
+        }
+    }
+}
--- tcc/src/test/java/io/seata/rm/tcc/interceptor/ProxyUtilsTccTest.java
+++ tcc/src/test/java/io/seata/rm/tcc/interceptor/ProxyUtilsTccTest.java
@@ -15,24 +15,24 @@
  */
 package io.seata.rm.tcc.interceptor;
 
-import io.seata.integration.tx.api.util.ProxyUtil;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
+
 import io.seata.core.context.RootContext;
 import io.seata.core.exception.TransactionException;
 import io.seata.core.model.BranchStatus;
 import io.seata.core.model.BranchType;
 import io.seata.core.model.Resource;
 import io.seata.core.model.ResourceManager;
+import io.seata.integration.tx.api.util.ProxyUtil;
 import io.seata.rm.DefaultResourceManager;
 import io.seata.rm.tcc.NormalTccAction;
 import io.seata.rm.tcc.NormalTccActionImpl;
 import io.seata.rm.tcc.TccParam;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * @author leezongjie
@@ -41,68 +41,68 @@ public class ProxyUtilsTccTest {
 
     private final String DEFAULT_XID = "default_xid";
 
+    AtomicReference<String> branchReference = new AtomicReference<String>();
+
+
+    ResourceManager resourceManager = new ResourceManager() {
+
+        @Override
+        public Long branchRegister(BranchType branchType, String resourceId, String clientId, String xid, String applicationData, String lockKeys) throws TransactionException {
+            branchReference.set(resourceId);
+            return System.currentTimeMillis();
+        }
+
+        @Override
+        public void branchReport(BranchType branchType, String xid, long branchId, BranchStatus status, String applicationData) throws TransactionException {
+
+        }
+
+        @Override
+        public boolean lockQuery(BranchType branchType, String resourceId, String xid, String lockKeys) throws TransactionException {
+            return false;
+        }
+
+        @Override
+        public BranchStatus branchCommit(BranchType branchType, String xid, long branchId, String resourceId, String applicationData) throws TransactionException {
+            return null;
+        }
+
+        @Override
+        public BranchStatus branchRollback(BranchType branchType, String xid, long branchId, String resourceId, String applicationData) throws TransactionException {
+            return null;
+        }
+
+        @Override
+        public void registerResource(Resource resource) {
+
+        }
+
+        @Override
+        public void unregisterResource(Resource resource) {
+
+        }
+
+        @Override
+        public Map<String, Resource> getManagedResources() {
+            return null;
+        }
+
+        @Override
+        public BranchType getBranchType() {
+            return null;
+        }
+    };
+
+
     @Test
     public void testTcc() {
         //given
         NormalTccActionImpl tccAction = new NormalTccActionImpl();
-
         NormalTccAction tccActionProxy = ProxyUtil.createProxy(tccAction);
-
         RootContext.bind(DEFAULT_XID);
 
         TccParam tccParam = new TccParam(1, "abc@163.com");
         List<String> listB = Arrays.asList("b");
-
-        AtomicReference<String> branchReference = new AtomicReference<String>();
-
-        ResourceManager resourceManager = new ResourceManager() {
-
-            @Override
-            public Long branchRegister(BranchType branchType, String resourceId, String clientId, String xid, String applicationData, String lockKeys) throws TransactionException {
-                branchReference.set(resourceId);
-                return System.currentTimeMillis();
-            }
-
-            @Override
-            public void branchReport(BranchType branchType, String xid, long branchId, BranchStatus status, String applicationData) throws TransactionException {
-
-            }
-
-            @Override
-            public boolean lockQuery(BranchType branchType, String resourceId, String xid, String lockKeys) throws TransactionException {
-                return false;
-            }
-
-            @Override
-            public BranchStatus branchCommit(BranchType branchType, String xid, long branchId, String resourceId, String applicationData) throws TransactionException {
-                return null;
-            }
-
-            @Override
-            public BranchStatus branchRollback(BranchType branchType, String xid, long branchId, String resourceId, String applicationData) throws TransactionException {
-                return null;
-            }
-
-            @Override
-            public void registerResource(Resource resource) {
-
-            }
-
-            @Override
-            public void unregisterResource(Resource resource) {
-
-            }
-
-            @Override
-            public Map<String, Resource> getManagedResources() {
-                return null;
-            }
-
-            @Override
-            public BranchType getBranchType() {
-                return null;
-            }
-        };
 
         DefaultResourceManager.mockResourceManager(BranchType.TCC, resourceManager);
 
@@ -113,7 +113,23 @@ public class ProxyUtilsTccTest {
         Assertions.assertEquals("a", result);
         Assertions.assertNotNull(result);
         Assertions.assertEquals("tccActionForTest", branchReference.get());
+    }
 
+    @Test
+    public void testTccThrowRawException() {
+        //given
+        NormalTccActionImpl tccAction = new NormalTccActionImpl();
+        NormalTccAction tccActionProxy = ProxyUtil.createProxy(tccAction);
+        RootContext.bind(DEFAULT_XID);
+
+        TccParam tccParam = new TccParam(1, "abc@163.com");
+        List<String> listB = Arrays.asList("b");
+
+        DefaultResourceManager.mockResourceManager(BranchType.TCC, resourceManager);
+
+        //when
+        //then
+        Assertions.assertThrows(IllegalArgumentException.class, () -> tccActionProxy.prepareWithException(null, 0, listB, tccParam));
     }
 
     @Test
