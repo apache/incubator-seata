2de90c0eba685cbdc6a1c9c30b49e13d10e2a3e1
zhangjiawei <349071347@qq.com>
2023-12-19 00:11:03 +0800

    fix ci

--- common/src/main/java/io/seata/common/util/ConfigTools.java
+++ common/src/main/java/io/seata/common/util/ConfigTools.java
@@ -25,7 +25,6 @@ import java.security.PublicKey;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
-import java.util.Scanner;
 import javax.crypto.Cipher;
 
 /**
