9ee0a6d4f700c634d7fef406274bba995458b902
laywin <laywin@yeah.net>
2023-12-21 11:48:40 +0800

    optimize: redis registry push empty protection optimize (#6164)

--- discovery/seata-discovery-nacos/src/test/java/io/seata/discovery/registry/nacos/NacosRegistryServiceImplTest.java
+++ discovery/seata-discovery-nacos/src/test/java/io/seata/discovery/registry/nacos/NacosRegistryServiceImplTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.seata.config.nacos;
+package io.seata.discovery.registry.nacos;
 
 import java.lang.reflect.Method;
 import java.util.Properties;
