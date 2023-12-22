ad32f7234a3dda03f19d69f3b15a283186662bbc
jsbxyyx <jsbxyyx@163.com>
2023-12-22 15:14:55 +0800

    fail fast default true

--- common/src/main/java/io/seata/common/DefaultValues.java
+++ common/src/main/java/io/seata/common/DefaultValues.java
@@ -60,7 +60,7 @@ public interface DefaultValues {
     boolean DEFAULT_ENABLE_RM_CLIENT_BATCH_SEND_REQUEST = true;
     boolean DEFAULT_ENABLE_TC_SERVER_BATCH_SEND_RESPONSE = false;
 
-    boolean DEFAULT_CLIENT_CHANNEL_CHECK_FAIL_FAST = false;
+    boolean DEFAULT_CLIENT_CHANNEL_CHECK_FAIL_FAST = true;
 
     String DEFAULT_BOSS_THREAD_PREFIX = "NettyBoss";
     String DEFAULT_NIO_WORKER_THREAD_PREFIX = "NettyServerNIOWorker";
