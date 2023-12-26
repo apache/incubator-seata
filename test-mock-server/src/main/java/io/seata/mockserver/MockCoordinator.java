34ae9f71d8c42c7d3360b521ea1d1842fe6e9f95
minghua.xie <minghua.xie@shopee.com>
2023-12-26 14:43:20 +0800

    license

--- test-mock-server/src/main/java/io/seata/mockserver/MockCoordinator.java
+++ test-mock-server/src/main/java/io/seata/mockserver/MockCoordinator.java
@@ -1,17 +1,18 @@
 /*
- *  Copyright 1999-2019 Seata.io Group.
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package io.seata.mockserver;
 
@@ -28,18 +29,12 @@ import io.seata.core.rpc.Disposable;
 import io.seata.core.rpc.RemotingServer;
 import io.seata.core.rpc.RpcContext;
 import io.seata.core.rpc.TransactionMessageHandler;
-import io.seata.core.rpc.netty.ChannelManager;
 import io.seata.mockserver.call.CallRm;
-import io.seata.serializer.seata.MessageCodecFactory;
-import io.seata.serializer.seata.MessageSeataCodec;
-import io.seata.serializer.seata.protocol.v1.MessageCodecFactoryV1;
 import io.seata.server.AbstractTCInboundHandler;
 
 /**
  * Mock Coordinator
  *
- * @author minghua.xie
- * @date 2023/11/14
  **/
 public class MockCoordinator extends AbstractTCInboundHandler implements TransactionMessageHandler, Disposable {
 
@@ -96,9 +91,9 @@ public class MockCoordinator extends AbstractTCInboundHandler implements Transac
                 Thread.sleep(1000);
                 BranchStatus commit = CallRm.branchCommit(remotingServer, resourceId, clientId);
                 BranchStatus rollback = CallRm.branchRollback(remotingServer, resourceId, clientId);
-                if (ProtocolConstants.VERSION_0 != Version.calcProtocolVersion(rpcContext.getVersion())) {
-                    CallRm.deleteUndoLog(remotingServer, resourceId, clientId);
-                }
+//                if (ProtocolConstants.VERSION_0 != Version.calcProtocolVersion(rpcContext.getVersion())) {
+//                    CallRm.deleteUndoLog(remotingServer, resourceId, clientId);
+//                }
             } catch (Exception e) {
                 e.printStackTrace();
             }
