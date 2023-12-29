8c197af15dbf1faee4f6ef09f794339878d0f02b
minghua.xie <minghua.xie@shopee.com>
2023-12-29 10:52:13 +0800

    v0

--- core/src/main/java/io/seata/core/rpc/netty/AbstractNettyRemoting.java
+++ core/src/main/java/io/seata/core/rpc/netty/AbstractNettyRemoting.java
@@ -43,6 +43,7 @@ import io.seata.core.protocol.MessageType;
 import io.seata.core.protocol.MessageTypeAware;
 import io.seata.core.protocol.ProtocolConstants;
 import io.seata.core.protocol.RpcMessage;
+import io.seata.core.protocol.Version;
 import io.seata.core.rpc.Disposable;
 import io.seata.core.rpc.hook.RpcHook;
 import io.seata.core.rpc.processor.Pair;
@@ -62,7 +63,7 @@ public abstract class AbstractNettyRemoting implements Disposable {
      * The Timer executor.
      */
     protected final ScheduledExecutorService timerExecutor = new ScheduledThreadPoolExecutor(1,
-        new NamedThreadFactory("timeoutChecker", 1, true));
+            new NamedThreadFactory("timeoutChecker", 1, true));
     /**
      * The Message executor.
      */
@@ -112,7 +113,7 @@ public abstract class AbstractNettyRemoting implements Disposable {
                         futures.remove(entry.getKey());
                         RpcMessage rpcMessage = future.getRequestMessage();
                         future.setResultMessage(new TimeoutException(String
-                            .format("msgId: %s ,msgType: %s ,msg: %s ,request timeout", rpcMessage.getId(), String.valueOf(rpcMessage.getMessageType()), rpcMessage.getBody().toString())));
+                                .format("msgId: %s ,msgType: %s ,msg: %s ,request timeout", rpcMessage.getId(), String.valueOf(rpcMessage.getMessageType()), rpcMessage.getBody().toString())));
                         if (LOGGER.isDebugEnabled()) {
                             LOGGER.debug("timeout clear future: {}", entry.getValue().getRequestMessage().getBody());
                         }
@@ -199,7 +200,7 @@ public abstract class AbstractNettyRemoting implements Disposable {
             return result;
         } catch (Exception exx) {
             LOGGER.error("wait response error:{},ip:{},request:{}", exx.getMessage(), channel.remoteAddress(),
-                rpcMessage.getBody());
+                    rpcMessage.getBody());
             if (exx instanceof TimeoutException) {
                 throw (TimeoutException) exx;
             } else {
@@ -218,7 +219,7 @@ public abstract class AbstractNettyRemoting implements Disposable {
         channelWritableCheck(channel, rpcMessage.getBody());
         if (LOGGER.isDebugEnabled()) {
             LOGGER.debug("write message:" + rpcMessage.getBody() + ", channel:" + channel + ",active?"
-                + channel.isActive() + ",writable?" + channel.isWritable() + ",isopen?" + channel.isOpen());
+                    + channel.isActive() + ",writable?" + channel.isWritable() + ",isopen?" + channel.isOpen());
         }
 
         doBeforeRpcHooks(ChannelUtil.getAddressFromChannel(channel), rpcMessage);
@@ -231,22 +232,32 @@ public abstract class AbstractNettyRemoting implements Disposable {
     }
 
     protected RpcMessage buildRequestMessage(Object msg, byte messageType) {
+        return buildRequestMessage(msg, messageType, Version.getCurrent());
+    }
+
+    protected RpcMessage buildRequestMessage(Object msg, byte messageType, String version) {
         RpcMessage rpcMessage = new RpcMessage();
         rpcMessage.setId(getNextMessageId());
         rpcMessage.setMessageType(messageType);
         rpcMessage.setCodec(ProtocolConstants.CONFIGURED_CODEC);
         rpcMessage.setCompressor(ProtocolConstants.CONFIGURED_COMPRESSOR);
         rpcMessage.setBody(msg);
+        rpcMessage.setVersion(version);
         return rpcMessage;
     }
 
     protected RpcMessage buildResponseMessage(RpcMessage rpcMessage, Object msg, byte messageType) {
+        return buildResponseMessage(rpcMessage, msg, messageType, Version.getCurrent());
+    }
+
+    protected RpcMessage buildResponseMessage(RpcMessage rpcMessage, Object msg, byte messageType, String version) {
         RpcMessage rpcMsg = new RpcMessage();
         rpcMsg.setMessageType(messageType);
         rpcMsg.setCodec(rpcMessage.getCodec()); // same with request
         rpcMsg.setCompressor(rpcMessage.getCompressor());
         rpcMsg.setBody(msg);
         rpcMsg.setId(rpcMessage.getId());
+        rpcMsg.setVersion(version);
         return rpcMsg;
     }
 
