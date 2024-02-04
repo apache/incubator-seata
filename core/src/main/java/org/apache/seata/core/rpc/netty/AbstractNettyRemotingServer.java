a333430946d7e4df27d796b1e4df0546c073d5c7
minghua.xie <minghua.xie@shopee.com>
2024-02-04 14:06:30 +0800

    style

--- core/src/main/java/org/apache/seata/core/rpc/netty/AbstractNettyRemotingServer.java
+++ core/src/main/java/org/apache/seata/core/rpc/netty/AbstractNettyRemotingServer.java
@@ -269,7 +269,7 @@ public abstract class AbstractNettyRemotingServer extends AbstractNettyRemoting 
 
     }
 
-    protected void setListenPort(int listenPort){
+    protected void setListenPort(int listenPort) {
         this.serverBootstrap.setListenPort(listenPort);
     }
 }
