3b9d1f8b6ec080676ddf0124eff9ec84a375053e
wangte <m18091785089@163.com>
2024-03-02 14:06:59 +0800

    remove unuse import

--- integration-tx-api/src/main/java/org/apache/seata/rm/tcc/api/BusinessActionContextParameter.java
+++ integration-tx-api/src/main/java/org/apache/seata/rm/tcc/api/BusinessActionContextParameter.java
@@ -20,7 +20,6 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import java.util.Map;
 
 /**
  * the TCC parameters that need to be passed to the action context;
