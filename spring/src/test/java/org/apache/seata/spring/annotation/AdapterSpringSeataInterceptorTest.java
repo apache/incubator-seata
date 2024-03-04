fc7d5a48e8eaf83b839d3c0b7b9536945727ca61
以奇 <wangmingjun.wmj@alibaba-inc.com>
2024-03-05 00:09:45 +0800

    move from 6091

--- spring/src/test/java/org/apache/seata/spring/annotation/AdapterSpringSeataInterceptorTest.java
+++ spring/src/test/java/org/apache/seata/spring/annotation/AdapterSpringSeataInterceptorTest.java
@@ -15,13 +15,11 @@
  * limitations under the License.
  */
 package org.apache.seata.spring.annotation;
-
 import java.lang.reflect.AccessibleObject;
 import java.lang.reflect.Method;
 import java.util.concurrent.Callable;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-
 import org.apache.seata.integration.tx.api.interceptor.handler.ProxyInvocationHandler;
 import org.apache.seata.integration.tx.api.interceptor.parser.DefaultInterfaceParser;
 import org.apache.seata.rm.tcc.api.BusinessActionContext;
@@ -29,6 +27,7 @@ import org.apache.seata.spring.tcc.NormalTccAction;
 import org.apache.seata.spring.tcc.NormalTccActionImpl;
 import org.aopalliance.intercept.MethodInvocation;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -36,64 +35,58 @@ import org.junit.jupiter.api.Test;
  */
 class AdapterSpringSeataInterceptorTest {
 
+    private static NormalTccAction normalTccAction;
+
+    private static AdapterSpringSeataInterceptor adapterSpringSeataInterceptor;
+
+    @BeforeAll
+    static void init() throws Throwable {
+        //given
+        normalTccAction = new NormalTccActionImpl();
+        ProxyInvocationHandler proxyInvocationHandler = DefaultInterfaceParser.get().parserInterfaceToProxy(normalTccAction, "proxyTccAction");
+        adapterSpringSeataInterceptor = new AdapterSpringSeataInterceptor(proxyInvocationHandler);
+    }
+
     @Test
     void should_throw_raw_exception_when_call_prepareWithException() throws Throwable {
-        //given
-        NormalTccActionImpl normalTccAction = new NormalTccActionImpl();
-        ProxyInvocationHandler proxyInvocationHandler = DefaultInterfaceParser.get().parserInterfaceToProxy(normalTccAction, "proxyTccAction");
-        AdapterSpringSeataInterceptor adapterSpringSeataInterceptor = new AdapterSpringSeataInterceptor(proxyInvocationHandler);
         MyMockMethodInvocation myMockMethodInvocation = new MyMockMethodInvocation(NormalTccAction.class.getMethod("prepareWithException", BusinessActionContext.class), () -> normalTccAction.prepareWithException(null));
-
         //when then
         Assertions.assertThrows(IllegalArgumentException.class, () -> adapterSpringSeataInterceptor.invoke(myMockMethodInvocation));
     }
 
     @Test
     void should_success_when_call_prepare_with_ProxyInvocationHandler() throws Throwable {
-        //given
-        NormalTccActionImpl normalTccAction = new NormalTccActionImpl();
-        ProxyInvocationHandler proxyInvocationHandler = DefaultInterfaceParser.get().parserInterfaceToProxy(normalTccAction, "proxyTccAction");
-        AdapterSpringSeataInterceptor adapterSpringSeataInterceptor = new AdapterSpringSeataInterceptor(proxyInvocationHandler);
         MyMockMethodInvocation myMockMethodInvocation = new MyMockMethodInvocation(NormalTccAction.class.getMethod("prepare", BusinessActionContext.class), () -> normalTccAction.prepare(null));
-
         //when then
         Assertions.assertTrue((Boolean) adapterSpringSeataInterceptor.invoke(myMockMethodInvocation));
     }
-
     static class MyMockMethodInvocation implements MethodInvocation {
-
         private Callable callable;
         private Method method;
-
         public MyMockMethodInvocation(Method method, Callable callable) {
             this.method = method;
             this.callable = callable;
         }
-
         @Nullable
         @Override
         public Object proceed() throws Throwable {
             return callable.call();
         }
-
         @Nonnull
         @Override
         public Method getMethod() {
             return method;
         }
-
         @Nonnull
         @Override
         public Object[] getArguments() {
             return new Object[0];
         }
-
         @Nullable
         @Override
         public Object getThis() {
             return null;
         }
-
         @Nonnull
         @Override
         public AccessibleObject getStaticPart() {
