5b4fc772fb00712e8bf203e37fe589b5a20c44df
以奇 <wangmingjun.wmj@alibaba-inc.com>
2024-03-05 17:48:32 +0800

    move from 6091

--- spring/src/test/java/org/apache/seata/spring/annotation/AdapterSpringSeataInterceptorTest.java
+++ spring/src/test/java/org/apache/seata/spring/annotation/AdapterSpringSeataInterceptorTest.java
@@ -15,11 +15,13 @@
  * limitations under the License.
  */
 package org.apache.seata.spring.annotation;
+
 import java.lang.reflect.AccessibleObject;
 import java.lang.reflect.Method;
 import java.util.concurrent.Callable;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+
 import org.apache.seata.integration.tx.api.interceptor.handler.ProxyInvocationHandler;
 import org.apache.seata.integration.tx.api.interceptor.parser.DefaultInterfaceParser;
 import org.apache.seata.rm.tcc.api.BusinessActionContext;
@@ -50,6 +52,7 @@ class AdapterSpringSeataInterceptorTest {
     @Test
     void should_throw_raw_exception_when_call_prepareWithException() throws Throwable {
         MyMockMethodInvocation myMockMethodInvocation = new MyMockMethodInvocation(NormalTccAction.class.getMethod("prepareWithException", BusinessActionContext.class), () -> normalTccAction.prepareWithException(null));
+
         //when then
         Assertions.assertThrows(IllegalArgumentException.class, () -> adapterSpringSeataInterceptor.invoke(myMockMethodInvocation));
     }
@@ -57,36 +60,45 @@ class AdapterSpringSeataInterceptorTest {
     @Test
     void should_success_when_call_prepare_with_ProxyInvocationHandler() throws Throwable {
         MyMockMethodInvocation myMockMethodInvocation = new MyMockMethodInvocation(NormalTccAction.class.getMethod("prepare", BusinessActionContext.class), () -> normalTccAction.prepare(null));
+
         //when then
         Assertions.assertTrue((Boolean) adapterSpringSeataInterceptor.invoke(myMockMethodInvocation));
     }
+
     static class MyMockMethodInvocation implements MethodInvocation {
+
         private Callable callable;
         private Method method;
+
         public MyMockMethodInvocation(Method method, Callable callable) {
             this.method = method;
             this.callable = callable;
         }
+
         @Nullable
         @Override
         public Object proceed() throws Throwable {
             return callable.call();
         }
+
         @Nonnull
         @Override
         public Method getMethod() {
             return method;
         }
+
         @Nonnull
         @Override
         public Object[] getArguments() {
             return new Object[0];
         }
+
         @Nullable
         @Override
         public Object getThis() {
             return null;
         }
+
         @Nonnull
         @Override
         public AccessibleObject getStaticPart() {
