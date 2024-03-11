e7b1e1d311f10716772e3e33f4ff158decb366fa
funkye <jianbin@apache.org>
2024-03-11 21:49:47 +0800

    optimize: rm-datasource compatible (#6402)

--- rm-datasource/src/test/java/org/apache/seata/rm/datasource/xa/XAXidBuilderTest.java
+++ rm-datasource/src/test/java/org/apache/seata/rm/datasource/xa/XAXidBuilderTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.seata.rm.xa;
+package org.apache.seata.rm.datasource.xa;
 
 import org.apache.seata.rm.datasource.xa.XAXid;
 import org.apache.seata.rm.datasource.xa.XAXidBuilder;
