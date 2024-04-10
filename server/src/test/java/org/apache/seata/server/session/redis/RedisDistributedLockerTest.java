a7116a94e147f41cd068570fdf666a5a5857affd
funky-eyes <jianbin@apache.org>
2024-04-10 23:50:48 +0800

    optimize

--- server/src/test/java/org/apache/seata/server/session/redis/RedisDistributedLockerTest.java
+++ server/src/test/java/org/apache/seata/server/session/redis/RedisDistributedLockerTest.java
@@ -121,7 +121,7 @@ public class RedisDistributedLockerTest {
 
        //sleep 60s
         try {
-            Thread.sleep(2000);
+            Thread.sleep(2100);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
