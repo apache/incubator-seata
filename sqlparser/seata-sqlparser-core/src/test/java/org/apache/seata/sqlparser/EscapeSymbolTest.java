8fabcdc67d6e37f69efea240dac67f92c79777ad
traitsisgiorgos <t8210233@aueb.gr>
2024-06-12 16:01:15 +0300

    fix assert import

--- sqlparser/seata-sqlparser-core/src/test/java/org/apache/seata/sqlparser/EscapeSymbolTest.java
+++ sqlparser/seata-sqlparser-core/src/test/java/org/apache/seata/sqlparser/EscapeSymbolTest.java
@@ -17,7 +17,7 @@
 package org.apache.seata.sqlparser;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class EscapeSymbolTest {
 
