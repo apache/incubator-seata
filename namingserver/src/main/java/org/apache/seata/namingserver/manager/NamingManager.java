0d0ed41fcec58b383efd6f5210025331d96fb08e
jianbin <jianbin@apache.org>
2024-07-12 16:43:27 +0800

    bugfix: thread safety issue when adding and removing instances

--- namingserver/src/main/java/org/apache/seata/namingserver/manager/NamingManager.java
+++ namingserver/src/main/java/org/apache/seata/namingserver/manager/NamingManager.java
@@ -42,7 +42,6 @@ import org.springframework.util.CollectionUtils;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.ArrayList;
