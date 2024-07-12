eb3490519a3d5ce46e76d18ca2507eecbbed205f
jianbin <jianbin@apache.org>
2024-07-12 10:41:13 +0800

    fix testcase

--- namingserver/src/main/java/org/apache/seata/namingserver/pojo/ClusterData.java
+++ namingserver/src/main/java/org/apache/seata/namingserver/pojo/ClusterData.java
@@ -26,7 +26,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
