5c5ba4a813bb81814139517b7b0088e029f1581d
leggasai <1642271413@qq.com>
2024-07-18 19:09:59 +0800

    bugfix: add log and tcc prefix config items

--- config/seata-config-core/src/main/java/org/apache/seata/config/store/rocksdb/RocksDBConfigStoreManager.java
+++ config/seata-config-core/src/main/java/org/apache/seata/config/store/rocksdb/RocksDBConfigStoreManager.java
@@ -63,7 +63,7 @@ public class RocksDBConfigStoreManager extends AbstractConfigStoreManager {
     private static volatile RocksDBConfigStoreManager instance;
     private final RocksDB rocksdb;
     private static final List<String> prefixList = Arrays.asList(FILE_ROOT_PREFIX_CONFIG, FILE_ROOT_PREFIX_REGISTRY, SERVER_PREFIX, CLIENT_PREFIX, SERVICE_PREFIX,
-            STORE_PREFIX, METRICS_PREFIX, TRANSPORT_PREFIX);
+            STORE_PREFIX, METRICS_PREFIX, TRANSPORT_PREFIX, LOG_PREFIX, TCC_PREFIX);
 
     public static RocksDBConfigStoreManager getInstance() {
         if (instance == null) {
