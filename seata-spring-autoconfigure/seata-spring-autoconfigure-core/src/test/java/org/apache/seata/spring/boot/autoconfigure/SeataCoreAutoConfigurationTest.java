314078447db87e0cd099e09fdccd218a36664623
lyl2008dsg <lyl2008dsg@163.com>
2024-09-08 20:55:24 +0800

    Add unit tests for ConfigurationFactory to verify configuration loading from file.conf and registry.conf

--- seata-spring-autoconfigure/seata-spring-autoconfigure-core/src/test/java/org/apache/seata/spring/boot/autoconfigure/SeataCoreAutoConfigurationTest.java
+++ seata-spring-autoconfigure/seata-spring-autoconfigure-core/src/test/java/org/apache/seata/spring/boot/autoconfigure/SeataCoreAutoConfigurationTest.java
@@ -16,6 +16,10 @@
  */
 package org.apache.seata.spring.boot.autoconfigure;
 
+import org.apache.seata.config.Configuration;
+import org.apache.seata.config.ConfigurationFactory;
+import org.apache.seata.config.FileConfiguration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,7 +28,10 @@ import org.springframework.core.env.Environment;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = SeataCoreAutoConfiguration.class)
@@ -33,6 +40,11 @@ public class SeataCoreAutoConfigurationTest {
 
     @Autowired
     private Environment environment;
+
+    @BeforeAll
+    public static void init() {
+        ConfigurationFactory.reload();
+    }
 
     @Test
     public void testSeataPropertiesLoaded() {
@@ -47,5 +59,34 @@ public class SeataCoreAutoConfigurationTest {
         // overridden by application-test.properties
         String seataNamespaces = environment.getProperty("seata.config.nacos.namespace");
         assertEquals("seata-test-application.yml", seataNamespaces, "The Nacos namespace should be 'seata-test-application.yml'");
+    }
+
+    @Test
+    public void testConfigFromFileUsingGetInstance() {
+        Configuration configFromFile = ConfigurationFactory.getInstance();
+        String dbUrlFromFile = configFromFile.getConfig("store.db.url");
+        assertEquals("jdbc:mysql://127.0.0.1:3306/seata?rewriteBatchedStatements=true", dbUrlFromFile, "The DB URL should be correctly loaded from file.conf");
+        String storeFileDirFromFile = configFromFile.getConfig("store.file.dir");
+        assertEquals("sessionStore", storeFileDirFromFile, "The storeFileDir should be 'sessionStore' in file.conf");
+    }
+
+    @Test
+    public void testConfigFromFileUsingGetOriginFileInstance() {
+        Optional<FileConfiguration> optionalConfigFromFile = ConfigurationFactory.getOriginFileInstance();
+        assertTrue(optionalConfigFromFile.isPresent(), "The configuration from file.conf should be present");
+        FileConfiguration configFromFile = optionalConfigFromFile.get();
+        String dbUrlFromFile = configFromFile.getConfig("store.db.url");
+        assertEquals("jdbc:mysql://127.0.0.1:3306/seata?rewriteBatchedStatements=true", dbUrlFromFile, "The DB URL should be correctly loaded from file.conf");
+        String storeFileDirFromFile = configFromFile.getConfig("store.file.dir");
+        assertEquals("sessionStore", storeFileDirFromFile, "The storeFileDir should be 'sessionStore' in file.conf");
+    }
+
+    @Test
+    public void testConfigFromRegistryUsingGetOriginFileInstanceRegistry() {
+        Configuration configFromRegistry = ConfigurationFactory.getOriginFileInstanceRegistry();
+        String registryTypeFromRegistry = configFromRegistry.getConfig("config.type");
+        assertEquals("file", registryTypeFromRegistry, "The config type should be 'file' in registry.conf");
+        String seataNamespaceFromRegistry = configFromRegistry.getConfig("config.nacos.namespace");
+        assertEquals("seata-test", seataNamespaceFromRegistry, "The Nacos namespace should be 'seata-test' in registry.conf");
     }
 }
