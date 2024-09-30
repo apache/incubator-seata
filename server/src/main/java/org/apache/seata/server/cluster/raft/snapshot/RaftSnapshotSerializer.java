52d35fc8c0da27ee127522a67ba20afdc7ae1c3d
funkye <364176773@qq.com>
2024-09-30 09:47:28 +0800

    Update server/src/main/java/org/apache/seata/server/cluster/raft/snapshot/RaftSnapshotSerializer.java

--- server/src/main/java/org/apache/seata/server/cluster/raft/snapshot/RaftSnapshotSerializer.java
+++ server/src/main/java/org/apache/seata/server/cluster/raft/snapshot/RaftSnapshotSerializer.java
@@ -21,6 +21,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.ObjectStreamClass;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
