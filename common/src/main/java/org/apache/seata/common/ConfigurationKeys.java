181127f503bdba1a55d31d21d074f85697dd3f08
yiqi <77573225+PleaseGiveMeTheCoke@users.noreply.github.com>
2024-10-05 13:14:21 +0800

    feature: support grpc protocol (#6881)

--- common/src/main/java/org/apache/seata/common/ConfigurationKeys.java
+++ common/src/main/java/org/apache/seata/common/ConfigurationKeys.java
@@ -628,6 +628,8 @@ public interface ConfigurationKeys {
     @Deprecated
     String ENABLE_CLIENT_BATCH_SEND_REQUEST = TRANSPORT_PREFIX + "enableClientBatchSendRequest";
 
+    String TRANSPORT_PROTOCOL = TRANSPORT_PREFIX + "protocol";
+
     /**
      * The constant ENABLE_TM_CLIENT_BATCH_SEND_REQUEST
      */
