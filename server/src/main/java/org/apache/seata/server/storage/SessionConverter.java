9c01441ea56d6a4ecc6893e75173a5082d913644
psxjoy <psxjoy@outlook.com>
2024-10-12 15:46:13 +0800

    optimize: fix some typos in project (#6911)

--- server/src/main/java/org/apache/seata/server/storage/SessionConverter.java
+++ server/src/main/java/org/apache/seata/server/storage/SessionConverter.java
@@ -149,13 +149,13 @@ public class SessionConverter {
                 BeanUtils.copyProperties(globalSession,globalSessionVO);
                 globalSessionVO.setStatus(globalSession.getStatus().getCode());
                 globalSessionVO.setTimeout(Long.valueOf(globalSession.getTimeout()));
-                globalSessionVO.setBranchSessionVOs(converToBranchSession(globalSession.getBranchSessions()));
+                globalSessionVO.setBranchSessionVOs(convertToBranchSession(globalSession.getBranchSessions()));
                 result.add(globalSessionVO);
             }
         }
     }
 
-    public static Set<BranchSessionVO> converToBranchSession(List<BranchSession> branchSessions) {
+    public static Set<BranchSessionVO> convertToBranchSession(List<BranchSession> branchSessions) {
         Set<BranchSessionVO> branchSessionVOs = new HashSet<>(branchSessions.size());
         if (CollectionUtils.isNotEmpty(branchSessions)) {
             for (BranchSession branchSession : branchSessions) {
