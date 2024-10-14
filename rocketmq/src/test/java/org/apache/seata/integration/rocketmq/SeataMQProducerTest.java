d07d9f2bfc2103c1ccfd153e5d45d1513f95d661
psxjoy <psxjoy@outlook.com>
2024-10-14 10:22:35 +0800

    feature: add unit-test for rocketmq module

--- rocketmq/src/test/java/org/apache/seata/integration/rocketmq/SeataMQProducerTest.java
+++ rocketmq/src/test/java/org/apache/seata/integration/rocketmq/SeataMQProducerTest.java
@@ -16,16 +16,91 @@
  */
 package org.apache.seata.integration.rocketmq;
 
+import org.apache.rocketmq.client.exception.MQBrokerException;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.client.producer.TransactionListener;
+import org.apache.rocketmq.client.producer.TransactionMQProducer;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.exception.RemotingException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 /**
  * seata mq producer test
  **/
 public class SeataMQProducerTest {
 
-    @Test
-    public void testCreate(){
-        new SeataMQProducer("testProducerGroup");
-        new SeataMQProducer("testNamespace", "testProducerGroup",null);
+    @Mock
+    private TransactionMQProducer transactionMQProducer;
+
+
+    @InjectMocks
+    private SeataMQProducer producer;
+
+    @BeforeEach
+    void setUp()  {
+        producer = Mockito.spy(new SeataMQProducer("testGroup"));
+
     }
+
+    @Test
+    public void testCreate() {
+        new SeataMQProducer("testProducerGroup");
+        new SeataMQProducer("testNamespace", "testProducerGroup", null);
+    }
+
+    @Test
+    void getTransactionListener_ShouldReturnNonNullTransactionListener() {
+        TransactionListener transactionListener = producer.getTransactionListener();
+        assertNotNull(transactionListener, "TransactionListener should not be null");
+    }
+
+    @Test
+    void testSend() throws MQClientException, RemotingException, MQBrokerException, InterruptedException {
+        // Arrange
+        Message msg = new Message("testTopic", "testBody".getBytes());
+        SendResult expectedResult = mock(SendResult.class);
+        int expectedTimeout = 3000; // 假设默认超时时间是3000毫秒
+
+        doReturn(expectedTimeout).when(producer).getSendMsgTimeout();
+        doReturn(expectedResult).when(producer).send(any(Message.class), anyLong());
+
+        // Act
+        SendResult result = producer.send(msg);
+
+        // Assert
+        assertSame(expectedResult, result);
+        verify(producer).send(msg, expectedTimeout);
+    }
+
+    @Test
+    void testSendWithException() throws MQClientException, RemotingException, MQBrokerException, InterruptedException {
+        // Arrange
+        Message msg = new Message("testTopic", "testBody".getBytes());
+        int expectedTimeout = 3000;
+
+        doReturn(expectedTimeout).when(producer).getSendMsgTimeout();
+        doThrow(new MQClientException("Test exception", null))
+            .when(producer).send(any(Message.class), anyInt());
+
+        // Act & Assert
+        assertThrows(MQClientException.class, () -> producer.send(msg));
+        verify(producer).send(msg, expectedTimeout);
+    }
+
 }
