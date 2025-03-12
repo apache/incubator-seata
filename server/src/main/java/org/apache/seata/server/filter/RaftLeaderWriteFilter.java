61c6f45b32d917f7bedc9fe6ce0bd1e640150a38
Jingliu Xiong <928124786@qq.com>
2025-03-12 23:23:04 +0800

    feat: add raft console trx operation

--- server/src/main/java/org/apache/seata/server/filter/RaftLeaderWriteFilter.java
+++ server/src/main/java/org/apache/seata/server/filter/RaftLeaderWriteFilter.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.seata.server.filter;
 
 import org.apache.seata.common.store.SessionMode;
@@ -5,10 +21,10 @@ import org.apache.seata.core.exception.TransactionException;
 import org.apache.seata.core.exception.TransactionExceptionCode;
 import org.apache.seata.server.cluster.listener.ClusterChangeEvent;
 import org.apache.seata.server.cluster.raft.context.SeataClusterContext;
+import org.apache.seata.server.console.exception.ConsoleException;
 import org.apache.seata.server.store.StoreConfig;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Conditional;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.Filter;
@@ -43,8 +59,9 @@ public class RaftLeaderWriteFilter implements Filter, ApplicationListener<Cluste
         if (!"GET".equalsIgnoreCase(method)) {
             String group = SeataClusterContext.bindGroup();
             if (!isPass(group)) {
-                throw new ServletException(new TransactionException(TransactionExceptionCode.NotRaftLeader,
-                        " The current TC is not a leader node, interrupt processing !"));
+                throw new ConsoleException(new TransactionException(TransactionExceptionCode.NotRaftLeader,
+                        " The current TC is not a leader node, interrupt processing of transactions!"),
+                        " The current TC is not a leader node, interrupt processing of transactions!");
             }
         }
 
