6b97a6a2c191eca0f309d861510a7b7adb1c55cb
mehedikhan72 <lopingcard@gmail.com>
2025-03-17 03:11:37 +0600

    test: added unit tests for NacosRegistryServiceImple

--- discovery/seata-discovery-nacos/src/test/java/org/apache/seata/discovery/registry/nacos/NacosRegistryServiceImplTest.java
+++ discovery/seata-discovery-nacos/src/test/java/org/apache/seata/discovery/registry/nacos/NacosRegistryServiceImplTest.java
@@ -17,11 +17,17 @@
 package org.apache.seata.discovery.registry.nacos;
 
 import java.lang.reflect.Method;
+import java.net.InetSocketAddress;
 import java.util.Properties;
+import com.alibaba.nacos.api.naming.listener.EventListener;
 
 import org.apache.seata.common.util.ReflectionUtil;
+import org.apache.seata.discovery.registry.RegistryService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 
 /**
@@ -40,5 +46,34 @@ public class NacosRegistryServiceImplTest {
         Assertions.assertThat(properties.getProperty("contextPath")).isEqualTo("/bar");
     }
 
+    @Test
+    public void testRegister() throws Exception {
+        RegistryService registryService = mock(NacosRegistryServiceImpl.class);
+        InetSocketAddress inetSocketAddress = new InetSocketAddress("127.0.0.1", 8091);
+        registryService.register(inetSocketAddress);
+        verify(registryService).register(inetSocketAddress);
+    }
 
+    @Test
+    public void testUnregister() throws Exception {
+        RegistryService registryService = mock(NacosRegistryServiceImpl.class);
+        InetSocketAddress inetSocketAddress = new InetSocketAddress("127.0.0.1", 8091);
+        registryService.unregister(inetSocketAddress);
+        verify(registryService).unregister(inetSocketAddress);
+    }
+
+    @Test
+    public void testSubscribe() throws Exception {
+        RegistryService registryService = mock(NacosRegistryServiceImpl.class);
+        EventListener eventListener = mock(EventListener.class);
+        registryService.subscribe("test", eventListener);
+        verify(registryService).subscribe("test", eventListener);
+    }
+
+    @Test
+    public void testLookup() throws Exception {
+        RegistryService registryService = mock(NacosRegistryServiceImpl.class);
+        registryService.lookup("test-key");
+        verify(registryService).lookup("test-key");
+    }
 }
