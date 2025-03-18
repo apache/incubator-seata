492cc8b20560b6cdd05329c2b20404fa82410fc4
jimin <slievrly@163.com>
2025-03-18 12:04:52 +0800

    reactor: refactor the code that does not comply with license requirements (#7145)

--- server/src/main/java/org/apache/seata/server/logging/logback/ExtendedArrowThrowableProxyConverter.java
+++ server/src/main/java/org/apache/seata/server/logging/logback/ExtendedArrowThrowableProxyConverter.java
@@ -18,18 +18,25 @@ package org.apache.seata.server.logging.logback;
 
 import ch.qos.logback.classic.pattern.ExtendedThrowableProxyConverter;
 import ch.qos.logback.classic.spi.IThrowableProxy;
-import ch.qos.logback.core.CoreConstants;
 
 /**
  * {@link ExtendedThrowableProxyConverter} that adds some additional whitespace around the
  * stack trace.
- * Copied from spring-boot-xxx.jar by wang.liang
  */
-public class ExtendedWhitespaceThrowableProxyConverter extends ExtendedThrowableProxyConverter {
+public class ExtendedArrowThrowableProxyConverter extends ExtendedThrowableProxyConverter {
 
+    public static final String SEPARATOR = System.getProperty("line.separator");
+    public static final String START_ARROW = "==> ";
+    public static final String END_ARROW = " <==";
     @Override
     protected String throwableProxyToString(IThrowableProxy tp) {
-        return "==>" + CoreConstants.LINE_SEPARATOR + super.throwableProxyToString(tp)
-                + "<==" + CoreConstants.LINE_SEPARATOR + CoreConstants.LINE_SEPARATOR;
+        StringBuilder throwBuilder = new StringBuilder();
+        throwBuilder.append(START_ARROW)
+            .append(SEPARATOR)
+            .append(super.throwableProxyToString(tp))
+            .append(END_ARROW)
+            .append(SEPARATOR);
+        return throwBuilder.toString();
     }
+
 }
