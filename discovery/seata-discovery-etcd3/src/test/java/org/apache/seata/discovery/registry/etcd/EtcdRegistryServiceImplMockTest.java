76b98f96b2b113d2268d493f550d90580bb85e30
Hokkine <xs273845155@outlook.com>
2025-03-19 11:05:22 +0800

    docs: add license header

--- discovery/seata-discovery-etcd3/src/test/java/org/apache/seata/discovery/registry/etcd/EtcdRegistryServiceImplMockTest.java
+++ discovery/seata-discovery-etcd3/src/test/java/org/apache/seata/discovery/registry/etcd/EtcdRegistryServiceImplMockTest.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.seata.discovery.registry.etcd;
 
 import io.etcd.jetcd.*;
@@ -71,13 +87,13 @@ public class EtcdRegistryServiceImplMockTest {
     }
 
     @BeforeAll
-    public static void beforeClass() throws Exception {
+    public static void beforeClass() {
         String endPoint = String.format("http://%s:%s", HOST, PORT);
         System.setProperty(EtcdRegistryServiceImpl.TEST_ENDPONT, endPoint);
     }
 
     @AfterAll
-    public static void afterClass() throws Exception {
+    public static void afterClass() {
         System.setProperty(EtcdRegistryServiceImpl.TEST_ENDPONT, "");
     }
 
