a11a52afde11a790ff8d7801660879576bfce41e
Monil <moniln@uci.edu>
2025-04-13 22:48:52 -0700

    Update CodeTest.java
    updated value sets

--- common/src/test/java/org/apache/seata/common/code/CodeTest.java
+++ common/src/test/java/org/apache/seata/common/code/CodeTest.java
@@ -48,8 +48,7 @@ public class CodeTest {
     static Stream<Arguments> codeSetterProvider() {
         return Stream.of(
                 Arguments.of(Code.SUCCESS, "201", "Created"),
-                Arguments.of(Code.ERROR, "500", "Something went wrong"),
-                Arguments.of(Code.LOGIN_FAILED, "403", "Unauthorized")
+                Arguments.of(Code.ERROR, "500", "Something went wrong")
         );
     }
 
