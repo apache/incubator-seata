6546234c302bab1a606c8090ee9dbe649b1061f5
徐博 洪 <3123004787@mail2.gdut.edu.cn>
2025-07-11 20:35:37 +0800

    feat: Implement dynamic parameter queries for undo_log, including parameter queries such as creating and modifying time periods

--- console/src/main/java/org/apache/seata/mcp/controller/tools/UndoLogTools.java
+++ console/src/main/java/org/apache/seata/mcp/controller/tools/UndoLogTools.java
@@ -2,6 +2,7 @@ package org.apache.seata.mcp.controller.tools;
 
 import org.apache.seata.mcp.annotation.Tool;
 import org.apache.seata.mcp.annotation.ToolParam;
+import org.apache.seata.mcp.entity.param.UndoLogParam;
 import org.apache.seata.mcp.service.UndoLogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,11 +13,9 @@ public class UndoLogTools {
     @Autowired
     private UndoLogService undoLogService;
 
-    @Tool(description = "Based on the branchId, xid analyzes the undo_log data of rm")
-    public String analyzeUndoLog(@ToolParam(description = "Unique identifier of the data source",required = true) String resourceId,
-                                        @ToolParam(description = "Branch transaction ID",required = true) String branchId,
-                                        @ToolParam(description = "Global transaction ID",required = true) String xid){
-        return undoLogService.queryAndAnalyzeUndoLog(resourceId, branchId, xid);
+    @Tool(description = "analyze the undo_log data of rm")
+    public String analyzeUndoLog(@ToolParam(description = "UndoLog Query parameters",required = true) UndoLogParam undoLogParam) {
+        return undoLogService.queryAndAnalyzeUndoLog(undoLogParam);
     }
 
 }
