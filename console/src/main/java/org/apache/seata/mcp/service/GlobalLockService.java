380e0b1f8e9891f7b23290fe87ce90c25105c237
徐博 洪 <2012753288@qq.com>
2025-07-11 11:53:36 +0800

    feat: Implement the service and tool APIs of the global lock management function

---
+++ console/src/main/java/org/apache/seata/mcp/service/GlobalLockService.java
@@ -0,0 +1,11 @@
+package org.apache.seata.mcp.service;
+
+import org.apache.seata.mcp.entity.param.GlobalLockParam;
+
+public interface GlobalLockService {
+    String queryGlobalLock(GlobalLockParam param);
+
+    String deleteGlobalLock(GlobalLockParam param);
+
+    String checkGlobalLock(String xid,String branchId);
+}
