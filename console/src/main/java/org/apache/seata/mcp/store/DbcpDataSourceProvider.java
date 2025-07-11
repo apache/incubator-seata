7160549222b0e892dff7e820f154d2a94b143983
徐博 洪 <3123004787@mail2.gdut.edu.cn>
2025-07-11 12:03:02 +0800

    feat: Implement the DbcpDataSource instance creation function

---
+++ console/src/main/java/org/apache/seata/mcp/store/DbcpDataSourceProvider.java
@@ -0,0 +1,72 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.seata.mcp.store;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.seata.common.loader.LoadLevel;
+import org.apache.seata.mcp.store.db.AbstractMCPDataSourceProvider;
+
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+/**
+ * The dbcp datasource provider
+ */
+@LoadLevel(name = "dbcp")
+public class DbcpDataSourceProvider extends AbstractMCPDataSourceProvider {
+
+    @Override
+    public DataSource doGenerate() {
+        BasicDataSource ds = new BasicDataSource();
+        ds.setDriverClassName(getDriverClassName());
+        ds.setDriverClassLoader(getDriverClassLoader());
+        ds.setUrl(getUrl());
+        ds.setUsername(getUser());
+        ds.setPassword(getPassword());
+        ds.setInitialSize(getMinConn());
+        ds.setMaxTotal(getMaxConn());
+        ds.setMinIdle(getMinConn());
+        ds.setMaxIdle(getMinConn());
+        ds.setMaxWaitMillis(getMaxWait());
+
+        // Connection retry control
+        ds.setMaxConnLifetimeMillis(300000);              // Maximum connection lifetime (5 minutes)
+        ds.setLogExpiredConnections(true);                // Log expired connections
+        ds.setConnectionProperties("useUnicode=yes;characterEncoding=utf8;socketTimeout=5000;connectTimeout=500;autoReconnect=true;maxReconnects=3;retriesAllDown=3");
+
+        // Failed connection handling
+        ds.setTestOnCreate(true);                         // Validate connection on creation
+        ds.setTestOnBorrow(true);                         // Validate connection on borrow
+        ds.setTestWhileIdle(true);                        // Validate idle connections
+        ds.setValidationQuery(getValidationQuery(getDBType()));
+        ds.setValidationQueryTimeout(5);                  // Validation query timeout (seconds)
+
+        // Fast failure after connection acquisition attempts
+        ds.setFastFailValidation(true);                   // Fast validation failure
+        ds.setDefaultTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
+
+        // Key settings: Connection pool exhaustion strategy
+        ds.setMaxWaitMillis(5000);                        // Maximum wait time of 5 seconds
+        ds.setAbandonedUsageTracking(true);               // Track connection usage
+        ds.setRemoveAbandonedOnBorrow(true);              // Check for abandoned connections on borrow
+        ds.setRemoveAbandonedOnMaintenance(true);         // Remove abandoned connections during maintenance
+        ds.setRemoveAbandonedTimeout(60);                 // Mark as abandoned after 60 seconds
+
+        return ds;
+    }
+}
