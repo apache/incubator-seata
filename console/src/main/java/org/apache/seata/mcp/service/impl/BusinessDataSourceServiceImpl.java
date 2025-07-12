af85802ce7d9eac9f9fcaf3527da39c5aa0062fd
徐博 洪 <3123004787@mail2.gdut.edu.cn>
2025-07-12 12:59:26 +0800

    optimize: Put statements related to SQL queries into the Constant constant class

--- console/src/main/java/org/apache/seata/mcp/service/impl/BusinessDataSourceServiceImpl.java
+++ console/src/main/java/org/apache/seata/mcp/service/impl/BusinessDataSourceServiceImpl.java
@@ -2,6 +2,7 @@ package org.apache.seata.mcp.service.impl;
 
 import org.apache.seata.common.exception.StoreException;
 import org.apache.seata.common.util.StringUtils;
+import org.apache.seata.mcp.constant.SqlConstant;
 import org.apache.seata.mcp.entity.param.UndoLogParam;
 import org.apache.seata.mcp.service.BusinessDataSourceService;
 import org.apache.seata.mcp.store.SqlExecutionTemplate;
@@ -20,30 +21,13 @@ public class BusinessDataSourceServiceImpl implements BusinessDataSourceService 
     @Autowired
     private SqlExecutionTemplate sqlExecutionTemplate;
 
-    private static final String GET_TABLE_NAME_SQL = "SELECT TABLE_NAME, TABLE_COMMENT FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = ? ";
-
-    private static final String GET_SCHEMA_SQL = "SELECT COLUMN_NAME, DATA_TYPE, COLUMN_COMMENT FROM INFORMATION_SCHEMA.COLUMNS " +
-            "WHERE TABLE_SCHEMA = ? AND TABLE_NAME = ?";
-
-    private static final String GET_UNDO_LOG_SQL = "SELECT rollback_info FROM undo_log WHERE";
-
-    private final String PARAM_BRANCH_ID = " branch_id = ?";
-
-    private final String PARAM_XID = " xid = ?";
-
-    private final String UNDO_LOG_STATUS = " log_status = ?";
-
-    private final String UNDO_LOG_CREATE_TIME = " log_created BETWEEN ? AND ?";
-
-    private final String UNDO_LOG_MODIFY_TIME = " log_modified BETWEEN ? AND ?";
-
     @Override
     public List<String> getTableNamesBySchema(String resourceId) {
         String schema = getSchemaNameByResourceId(resourceId);
         if (StringUtils.isBlank(schema)) {
             throw new StoreException("failed to get schema by resourceId: " + resourceId);
         }else{
-            List<Map<String, Object>> maps = sqlExecutionTemplate.query(resourceId,GET_TABLE_NAME_SQL,schema);
+            List<Map<String, Object>> maps = sqlExecutionTemplate.query(resourceId, SqlConstant.GET_TABLE_NAME_SQL,schema);
             return maps.stream().map(map -> {
                 String tableName = String.valueOf(map.get("TABLE_NAME"));
                 String tableComment = String.valueOf(map.get("TABLE_COMMENT"));
@@ -58,7 +42,7 @@ public class BusinessDataSourceServiceImpl implements BusinessDataSourceService 
         if (StringUtils.isBlank(schema)) {
             throw new StoreException("failed to get schema by resourceId: " + resourceId);
         }else{
-            return sqlExecutionTemplate.query(resourceId,GET_SCHEMA_SQL,schema,tableName);
+            return sqlExecutionTemplate.query(resourceId,SqlConstant.GET_SCHEMA_SQL,schema,tableName);
         }
     }
 
@@ -69,7 +53,7 @@ public class BusinessDataSourceServiceImpl implements BusinessDataSourceService 
 
     @Override
     public List<byte[]> getUndoLogInfo(UndoLogParam param) {
-        String sql = GET_UNDO_LOG_SQL;
+        String sql = SqlConstant.GET_UNDO_LOG_SQL;
         List<Object> params = new ArrayList<>();
         String branchId = param.getBranchId();
         String xid = param.getXid();
@@ -82,22 +66,22 @@ public class BusinessDataSourceServiceImpl implements BusinessDataSourceService 
             throw new StoreException("you cannot query without resourceId");
         }
         if(StringUtils.isNotBlank(branchId)){
-            sql += PARAM_BRANCH_ID;
+            sql += SqlConstant.PARAM_BRANCH_ID_SQL;
             params.add(branchId);
         }
         if(StringUtils.isNotBlank(xid)){
-            sql += PARAM_XID;
+            sql += SqlConstant.PARAM_XID_SQL;
             params.add(xid);
         }
         if(logStatus != null){
-            sql += UNDO_LOG_STATUS;
+            sql += SqlConstant.UNDO_LOG_STATUS_SQL;
             params.add(logStatus);
         }
         if(logCreateTime != null){
             String startTime = logCreateTime.getStartTime();
             String endTime = logCreateTime.getEndTime();
             if(startTime != null && endTime != null){
-                sql += UNDO_LOG_CREATE_TIME;
+                sql += SqlConstant.UNDO_LOG_CREATE_TIME_SQL;
             }
             if(startTime != null){
                 params.add(startTime);
@@ -110,7 +94,7 @@ public class BusinessDataSourceServiceImpl implements BusinessDataSourceService 
             String startTime = logModifiedTime.getStartTime();
             String endTime = logModifiedTime.getEndTime();
             if(startTime != null && endTime != null){
-                sql += UNDO_LOG_MODIFY_TIME;
+                sql += SqlConstant.UNDO_LOG_MODIFY_TIME_SQL;
             }
             if(startTime != null){
                 params.add(startTime);
