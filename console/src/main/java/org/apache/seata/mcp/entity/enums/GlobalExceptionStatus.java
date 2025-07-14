2f2e330456d616b48cf26400e2ffa42df54f6bc3
徐博 洪 <3123004787@mail2.gdut.edu.cn>
2025-07-14 21:44:43 +0800

    optimize: Add Licensed

--- console/src/main/java/org/apache/seata/mcp/entity/enums/GlobalExceptionStatus.java
+++ console/src/main/java/org/apache/seata/mcp/entity/enums/GlobalExceptionStatus.java
@@ -1,4 +1,4 @@
-package org.apache.seata.mcp.entity.enums;/*
+/*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -14,7 +14,7 @@ package org.apache.seata.mcp.entity.enums;/*
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+package org.apache.seata.mcp.entity.enums;
 
 import java.util.ArrayList;
 import java.util.List;
