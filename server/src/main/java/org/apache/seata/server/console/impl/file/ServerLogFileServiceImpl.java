f9262452718053c5829b23e9232be201813f59ad
徐博 洪 <3123004787@mail2.gdut.edu.cn>
2025-07-24 15:13:14 +0800

    fix: add license

--- server/src/main/java/org/apache/seata/server/console/impl/file/ServerLogFileServiceImpl.java
+++ server/src/main/java/org/apache/seata/server/console/impl/file/ServerLogFileServiceImpl.java
@@ -1,4 +1,20 @@
-package org.apache.seata.server.console.impl.log;
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.seata.server.console.impl.file;
 
 import org.apache.commons.io.input.ReversedLinesFileReader;
 import org.apache.seata.common.result.SingleResult;
@@ -13,14 +29,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 import java.io.File;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class ServerLogServiceImpl implements ServerLogService {
+public class ServerLogFileServiceImpl implements ServerLogService {
 
     @Autowired
     private Environment env;
@@ -35,7 +50,7 @@ public class ServerLogServiceImpl implements ServerLogService {
 
     private static final Integer maxLines = 50;
 
-    private final Logger LOGGER = LoggerFactory.getLogger(ServerLogServiceImpl.class);
+    private final Logger LOGGER = LoggerFactory.getLogger(ServerLogFileServiceImpl.class);
 
     @Override
     public SingleResult<?> getServerLog(ServerLogParam serverLogParam) {
