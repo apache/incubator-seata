227a72b7707e5d17b259a6258176d646df9d2c12
徐博 洪 <3123004787@mail2.gdut.edu.cn>
2025-07-24 15:11:05 +0800

    feat: Implement the log file reading interface on the server side

--- server/src/main/java/org/apache/seata/server/console/entity/param/ServerLogParam.java
+++ server/src/main/java/org/apache/seata/server/console/entity/param/ServerLogParam.java
@@ -6,9 +6,23 @@ public class ServerLogParam implements Serializable {
 
     private static final long serialVersionUID = 225478653801012285L;
 
+    private Integer cursor;
+
+    private Integer nextLines;
+
+    private Long costedTime;
+
     private String logType;
 
     private String logTime;
+
+    public Integer getNextLines() {
+        return nextLines;
+    }
+
+    public void setNextLines(Integer nextLines) {
+        this.nextLines = nextLines;
+    }
 
     public String getLogType() {
         return logType;
@@ -26,10 +40,29 @@ public class ServerLogParam implements Serializable {
         this.logTime = logTime;
     }
 
+    public Integer getCursor() {
+        return cursor;
+    }
+
+    public void setCursor(Integer cursor) {
+        this.cursor = cursor;
+    }
+
+    public Long getCostedTime() {
+        return costedTime;
+    }
+
+    public void setCostedTime(Long costedTime) {
+        this.costedTime = costedTime;
+    }
+
     @Override
     public String toString() {
         return "ServerLogParam{" +
-                "logType='" + logType + '\'' +
+                "cursor=" + cursor +
+                ", nextLines=" + nextLines +
+                ", costedTime=" + costedTime +
+                ", logType='" + logType + '\'' +
                 ", logTime='" + logTime + '\'' +
                 '}';
     }
