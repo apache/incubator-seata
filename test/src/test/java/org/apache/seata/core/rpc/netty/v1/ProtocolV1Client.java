e8fc250e4da2801246ec7425130e095d0bcfa3ec
minghua.xie <minghua.xie@npt.sg>
2025-08-07 11:07:37 +0800

    merge

--- test/src/test/java/org/apache/seata/core/rpc/netty/v1/ProtocolV1Client.java
+++ test/src/test/java/org/apache/seata/core/rpc/netty/v1/ProtocolV1Client.java
@@ -55,14 +55,16 @@ import org.slf4j.LoggerFactory;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
- *
  */
 public class ProtocolV1Client {
 
