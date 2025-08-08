a022614dd7cd8acd833033d63bcc26edb8246b3c
keepConcentration <phm543@gmail.com>
2025-08-08 15:26:32 +0900

    test: specify assertEquals import in DefaultInterfaceParserTest

--- integration-tx-api/src/test/java/org/apache/seata/integration/tx/api/interceptor/parser/DefaultInterfaceParserTest.java
+++ integration-tx-api/src/test/java/org/apache/seata/integration/tx/api/interceptor/parser/DefaultInterfaceParserTest.java
@@ -29,7 +29,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DefaultInterfaceParserTest {
 
