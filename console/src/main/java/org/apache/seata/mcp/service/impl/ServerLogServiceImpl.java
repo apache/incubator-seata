c8b6b196e4fae131ad4a6c481b846ed67ba7053d
徐博 洪 <2012753288@qq.com>
2025-08-11 15:48:41 +0800

    feat: All tools that call interfaces from TC nodes specify the namespace to correspond to the TC node

--- console/src/main/java/org/apache/seata/mcp/service/impl/ServerLogServiceImpl.java
+++ console/src/main/java/org/apache/seata/mcp/service/impl/ServerLogServiceImpl.java
@@ -4,6 +4,7 @@ import org.apache.seata.common.util.StringUtils;
 import org.apache.seata.mcp.entity.constant.RPCConstant;
 import org.apache.seata.mcp.entity.param.ServerLogIndexParam;
 import org.apache.seata.mcp.entity.param.ServerLogParam;
+import org.apache.seata.mcp.entity.pojo.NameSpaceDetail;
 import org.apache.seata.mcp.service.MCPRPCService;
 import org.apache.seata.mcp.service.ServerLogService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,8 +17,8 @@ public class ServerLogServiceImpl implements ServerLogService {
     private MCPRPCService mcpRPCService;
 
     @Override
-    public String analyseServerLog(ServerLogParam param) {
-        String result = mcpRPCService.getCallTC(RPCConstant.SERVER_LOG_BASE_URL + "/getServerLog", param, null, null);
+    public String analyseServerLog(NameSpaceDetail nameSpaceDetail, ServerLogParam param) {
+        String result = mcpRPCService.getCallTC(nameSpaceDetail,RPCConstant.SERVER_LOG_BASE_URL + "/getServerLog", param, null, null);
         if (StringUtils.isBlank(result)) {
             return "analyse server log failed";
         } else {
@@ -26,8 +27,8 @@ public class ServerLogServiceImpl implements ServerLogService {
     }
 
     @Override
-    public String getHistoryServerLogNums(ServerLogIndexParam param) {
-        String result = mcpRPCService.getCallTC(RPCConstant.SERVER_LOG_BASE_URL + "/getHistoryServerLogNums", param, null, null);
+    public String getHistoryServerLogNums(NameSpaceDetail nameSpaceDetail, ServerLogIndexParam param) {
+        String result = mcpRPCService.getCallTC(nameSpaceDetail,RPCConstant.SERVER_LOG_BASE_URL + "/getHistoryServerLogNums", param, null, null);
         if (StringUtils.isBlank(result)) {
             return "get history server log nums failed";
         } else {
