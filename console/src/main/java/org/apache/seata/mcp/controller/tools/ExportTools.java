8fd3059893ca3bfc9dde604a1e5b927720cbf0b3
徐博 洪 <3123004787@mail2.gdut.edu.cn>
2025-08-21 21:18:32 +0800

    optimize: Integrate report parameters into a param to directly export query data reports when querying a business database

--- console/src/main/java/org/apache/seata/mcp/controller/tools/ExportTools.java
+++ console/src/main/java/org/apache/seata/mcp/controller/tools/ExportTools.java
@@ -3,11 +3,11 @@ package org.apache.seata.mcp.controller.tools;
 import org.apache.seata.common.result.SingleResult;
 import org.apache.seata.mcp.annotation.Tool;
 import org.apache.seata.mcp.annotation.ToolParam;
+import org.apache.seata.mcp.entity.param.ExportSheetParam;
 import org.apache.seata.mcp.utils.ExcelExportUtil;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -16,14 +16,12 @@ public class ExportTools {
 
     @Tool(description = "Automatically convert the queried data into an Excel table and store it in the specified location with custom headers, Please show it directly to the user when it is successful")
     public SingleResult<?> exportExcelFromDataWithCustomHeaders(
-            @ToolParam(description = "The input data is of type List<Map<String, Object>>",required = true) List<Map<String,Object>> data,
-            @ToolParam(description = "Sheet Name",required = true) String sheetName,
-            @ToolParam(description = "Where the file is stored", required = true) String filePath,
+            @ToolParam(description = "The export sheet param",required = true) ExportSheetParam param,
             @ToolParam(description = "Customize the excel header is of type List<Map<String, String>>," +
                     "key is the name of the field used in the data, value is the name that appears on the excel header",required = true) List<Map<String, String>> headers
                                                                 ) throws IOException {
-        ExcelExportUtil.exportExcelWithCustomHeaders(sheetName,filePath,data,headers);
-        return SingleResult.success("Conversion is successful, please check the specified file location: "+filePath);
+        ExcelExportUtil.exportExcelWithCustomHeaders(param,headers);
+        return SingleResult.success("Conversion is successful, please check the specified file location: "+param.getFilePath());
     }
 
 }
