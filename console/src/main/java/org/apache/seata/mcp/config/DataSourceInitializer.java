41f8c130559caa37d66e4ea6cd5c04650a4cfebb
徐博 洪 <3123004787@mail2.gdut.edu.cn>
2025-08-22 16:07:27 +0800

    optimize: A business data source automatically creates a corresponding datasource instance when the application is launched

---
+++ console/src/main/java/org/apache/seata/mcp/config/DataSourceInitializer.java
@@ -0,0 +1,16 @@
+package org.apache.seata.mcp.config;
+
+import org.apache.seata.mcp.store.DataSourceFactory;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class DataSourceInitializer {
+
+    @PostConstruct
+    public void init(){
+        DataSourceFactory.initAllDataSources();
+    }
+
+}
