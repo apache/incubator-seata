93147ecf46fb2589ad69b46a54056285687bee0d
徐博 洪 <2012753288@qq.com>
2025-08-26 11:46:39 +0800

    fix: user to start with a capital letter

--- console/src/main/java/org/apache/seata/mcp/controller/tools/BusinessDataSourceTools.java
+++ console/src/main/java/org/apache/seata/mcp/controller/tools/BusinessDataSourceTools.java
@@ -42,7 +42,7 @@ public class BusinessDataSourceTools {
                     "Get the identity and name of the business data source. Important!!!: key is name, value is resourceId")
     public Map<String, String> getResourceIds() {
         if (LOGGER.isInfoEnabled()) {
-            LOGGER.info("user try to get resource ids");
+            LOGGER.info("User try to get resource ids");
         }
         return BusinessDataSourcesProperties.getDataSourcesNamesAndResourceIds();
     }
@@ -52,7 +52,7 @@ public class BusinessDataSourceTools {
             @ToolParam(description = "The identity of the data source, start with jdbc://", required = true)
                     String resourceId) {
         if (LOGGER.isInfoEnabled()) {
-            LOGGER.info("user try to get all table names, resource id {}", resourceId);
+            LOGGER.info("User try to get all table names, resource id {}", resourceId);
         }
         return dataSourceService.getTableNamesBySchema(resourceId);
     }
@@ -63,7 +63,7 @@ public class BusinessDataSourceTools {
             @ToolParam(description = "The identity of the data source, start with jdbc://", required = true)
                     String resourceId) {
         if (LOGGER.isInfoEnabled()) {
-            LOGGER.info("user try to get table schema, tableName: {}, resourceId: {}", tableName, resourceId);
+            LOGGER.info("User try to get table schema, tableName: {}, resourceId: {}", tableName, resourceId);
         }
         return dataSourceService.getTableSchemaByTableName(resourceId, tableName);
     }
@@ -73,7 +73,7 @@ public class BusinessDataSourceTools {
             @ToolParam(description = "SQL statement, String type", required = true) String sql,
             @ToolParam(description = "The identity of the data source, start with jdbc://", required = true) String resourceId) {
         if (LOGGER.isInfoEnabled()) {
-            LOGGER.info("user try to run sql: {}, resourceId: {}", sql, resourceId);
+            LOGGER.info("User try to run sql: {}, resourceId: {}", sql, resourceId);
         }
         List<Map<String, Object>> result = dataSourceService.runSql(sql, resourceId);
         result.add(Collections.singletonMap("Important!!!","If it is related to data analysis, statistics, etc., Please generate a table and attach an analysis statement to the user for viewing"));
