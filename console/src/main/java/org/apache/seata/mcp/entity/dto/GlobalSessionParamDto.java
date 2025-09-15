702ef6fc575efc909df1d24623de8c1491de172d
徐博 洪 <3123004787@mail2.gdut.edu.cn>
2025-09-15 16:32:58 +0800

    fix: Deletes the vGroup invalid parameter of the global transaction query

--- console/src/main/java/org/apache/seata/mcp/entity/dto/GlobalSessionParamDto.java
+++ console/src/main/java/org/apache/seata/mcp/entity/dto/GlobalSessionParamDto.java
@@ -33,12 +33,6 @@ public class GlobalSessionParamDto implements Serializable {
     private String transactionName;
 
     /**
-     * the vgroup
-     */
-    @ToolParam(description = "Belong to the group")
-    private String vgroup;
-
-    /**
      * if with branch
      * true: with branch session
      * false: no branch session
@@ -130,14 +124,6 @@ public class GlobalSessionParamDto implements Serializable {
         this.withBranch = withBranch;
     }
 
-    public String getVgroup() {
-        return vgroup;
-    }
-
-    public void setVgroup(String vgroup) {
-        this.vgroup = vgroup;
-    }
-
     @Override
     public String toString() {
         return "GlobalSessionParamDto{" + "xid='"
@@ -145,7 +131,6 @@ public class GlobalSessionParamDto implements Serializable {
                 + applicationId + '\'' + ", status="
                 + status + ", transactionName='"
                 + transactionName + '\'' + ", vgroup='"
-                + vgroup + '\'' + ", withBranch="
                 + withBranch + ", pageNum="
                 + pageNum + ", pageSize="
                 + pageSize + ", timeStart='"
