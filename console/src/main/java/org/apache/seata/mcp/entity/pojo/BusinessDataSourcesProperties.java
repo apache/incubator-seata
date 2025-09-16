ec7e1ecca4616100ea5f8c68be1b4b943e89f63f
徐博 洪 <3123004787@mail2.gdut.edu.cn>
2025-09-16 15:51:08 +0800

    optimize: Add a parameter size limit for the number of connections to a business data source

--- console/src/main/java/org/apache/seata/mcp/entity/pojo/BusinessDataSourcesProperties.java
+++ console/src/main/java/org/apache/seata/mcp/entity/pojo/BusinessDataSourcesProperties.java
@@ -72,9 +72,17 @@ public class BusinessDataSourcesProperties implements InitializingBean {
             props.setUsername(env.getProperty(prefix + "username"));
             props.setPassword(env.getProperty(prefix + "password"));
             props.setDatasource(env.getProperty(prefix + "datasource", "druid"));
-            props.setMinConn(env.getProperty(prefix + "minConn", Integer.class, DEFAULT_DB_MIN_CONN));
-            props.setMaxConn(env.getProperty(prefix + "maxConn", Integer.class, DEFAULT_DB_MAX_CONN));
-            props.setMaxWait(env.getProperty(prefix + "maxWait", Long.class, 5000L));
+            props.setMinConn(env.getProperty(prefix + "min-conn", Integer.class, DEFAULT_DB_MIN_CONN));
+            if(props.getMinConn()<=0 || props.getMinConn()>DEFAULT_DB_MIN_CONN){
+                LOGGER.warn("The minimum number of connections for a data source: {} is not compliant",name);
+                continue;
+            }
+            props.setMaxConn(env.getProperty(prefix + "max-conn", Integer.class, DEFAULT_DB_MAX_CONN));
+            if(props.getMaxConn()<=0 || props.getMaxConn()>DEFAULT_DB_MAX_CONN){
+                LOGGER.warn("The maximum number of connections for a data source: {} is not compliant",name);
+                continue;
+            }
+            props.setMaxWait(env.getProperty(prefix + "max-wait", Long.class, 5000L));
 
             // Check whether the parameters are perfect
             if (!validateDataSourceProperties(props, name)) {
