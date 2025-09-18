02aa035b31390fb43dc9e6a8d9919cc3377d9ccf
jongmin-chung <chungjm0711@gmail.com>
2025-09-18 16:32:09 +0900

    chore: apply code when spotless:apply

--- tm/src/main/java/org/apache/seata/tm/api/TransactionalTemplate.java
+++ tm/src/main/java/org/apache/seata/tm/api/TransactionalTemplate.java
@@ -290,7 +290,7 @@ public class TransactionalTemplate {
                 break;
             case TimeoutRollbacked:
             case Rollbacked:
-                // rollback transactions but do not exist are usually considered completed
+            // rollback transactions but do not exist are usually considered completed
             case Finished:
                 code = TransactionalExecutor.Code.RollbackDone;
                 break;
