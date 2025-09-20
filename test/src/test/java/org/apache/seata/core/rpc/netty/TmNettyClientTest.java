dd58398662fa4c60316c6a05e47f9c7f6b79bfd1
minghua.xie <minghua.xie@npt.sg>
2025-09-20 17:40:34 +0800

    style

--- test/src/test/java/org/apache/seata/core/rpc/netty/TmNettyClientTest.java
+++ test/src/test/java/org/apache/seata/core/rpc/netty/TmNettyClientTest.java
@@ -36,6 +36,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import java.lang.management.ManagementFactory;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
