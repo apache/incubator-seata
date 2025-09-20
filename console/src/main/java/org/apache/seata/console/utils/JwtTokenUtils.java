23e655f69af510d89c8ff19f8ee0343d28715556
徐博 洪 <2012753288@qq.com>
2025-09-20 20:54:28 +0800

    fix: fix the use of .* in JwtTokenUtils

--- console/src/main/java/org/apache/seata/console/utils/JwtTokenUtils.java
+++ console/src/main/java/org/apache/seata/console/utils/JwtTokenUtils.java
@@ -16,7 +16,12 @@
  */
 package org.apache.seata.console.utils;
 
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.SignatureException;
 import org.slf4j.Logger;
