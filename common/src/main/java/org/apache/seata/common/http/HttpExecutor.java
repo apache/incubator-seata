dd105cdb9738aa1735f92191ab6ee7314da60a23
xiaoyu <zytuatua@163.com>
2025-10-09 18:43:28 +0800

    unify api

--- common/src/main/java/org/apache/seata/common/http/HttpExecutor.java
+++ common/src/main/java/org/apache/seata/common/http/HttpExecutor.java
@@ -1,4 +1,15 @@
 package org.apache.seata.common.http;
 
+import java.io.IOException;
+import java.util.Map;
+
 public interface HttpExecutor {
+
+    HttpResult doPost(String url, Map<String, String> params, Map<String, String> header, int timeout) throws IOException;
+
+    HttpResult doPost(String url, String body, Map<String, String> header, int timeout) throws IOException;
+
+    HttpResult doGet(String url, Map<String, String> param, Map<String, String> header, int timeout) throws IOException;
+
+    HttpResult doPostJson(String url, String jsonBody, Map<String, String> headers, int timeout) throws IOException;
 }
