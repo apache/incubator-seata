cb1b72b9fd68790a62365b0ab0b2d4f75f8f1a21
maple <14118288+gao-shengkai525866@user.noreply.gitee.com>
2025-11-11 21:40:06 +0800

    add UT

---
+++ rm-datasource/src/test/java/org/apache/seata/rm/datasource/xa/AbstractDataSourceProxyXATest.java
@@ -0,0 +1,173 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.seata.rm.datasource.xa;
+
+import org.apache.seata.core.model.BranchType;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import javax.sql.PooledConnection;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Tests for AbstractDataSourceProxyXA
+ *
+ */
+public class AbstractDataSourceProxyXATest {
+
+    private TestDataSourceProxyXA dataSourceProxy;
+    private XAXid xaXid;
+
+    @BeforeEach
+    public void setUp() {
+        dataSourceProxy = new TestDataSourceProxyXA();
+        xaXid = XAXidBuilder.build("testXid", 123L);
+    }
+
+    @Test
+    public void testGetConnectionForXAFinish_WithExistingConnection() throws SQLException {
+        // Mock existing connection
+        ConnectionProxyXA mockConnection = mock(ConnectionProxyXA.class);
+        Connection mockWrappedConnection = mock(Connection.class);
+        when(mockConnection.getWrappedConnection()).thenReturn(mockWrappedConnection);
+        when(mockWrappedConnection.isClosed()).thenReturn(false);
+
+        // Put connection in the lookup
+        dataSourceProxy.hold(xaXid.toString(), mockConnection);
+
+        // Get connection for XA finish
+        ConnectionProxyXA result = dataSourceProxy.getConnectionForXAFinish(xaXid);
+
+        Assertions.assertEquals(mockConnection, result);
+        verify(mockConnection).getWrappedConnection();
+        verify(mockWrappedConnection).isClosed();
+    }
+
+    @Test
+    public void testGetConnectionForXAFinish_WithClosedConnection() throws SQLException {
+        // Mock closed connection
+        ConnectionProxyXA mockConnection = mock(ConnectionProxyXA.class);
+        Connection mockWrappedConnection = mock(Connection.class);
+        when(mockConnection.getWrappedConnection()).thenReturn(mockWrappedConnection);
+        when(mockWrappedConnection.isClosed()).thenReturn(true);
+
+        // Put connection in the lookup
+        dataSourceProxy.hold(xaXid.toString(), mockConnection);
+
+        // Get connection for XA finish
+        ConnectionProxyXA result = dataSourceProxy.getConnectionForXAFinish(xaXid);
+
+        // Should return a new connection from getConnectionProxyXA
+        Assertions.assertNotNull(result);
+        verify(mockConnection).getWrappedConnection();
+        verify(mockWrappedConnection).isClosed();
+    }
+
+    @Test
+    public void testGetConnectionForXAFinish_NoExistingConnection() throws SQLException {
+        // Get connection for XA finish without existing connection
+        ConnectionProxyXA result = dataSourceProxy.getConnectionForXAFinish(xaXid);
+
+        // Should return a new connection from getConnectionProxyXA
+        Assertions.assertNotNull(result);
+    }
+
+    @Test
+    public void testForceClosePhysicalConnection_WithExistingConnection() throws SQLException {
+        // Mock connection
+        ConnectionProxyXA mockConnection = mock(ConnectionProxyXA.class);
+        Connection mockWrappedConnection = mock(Connection.class);
+        when(mockConnection.getWrappedConnection()).thenReturn(mockWrappedConnection);
+
+        // Put connection in the lookup
+        dataSourceProxy.hold(xaXid.toString(), mockConnection);
+
+        // Force close physical connection
+        dataSourceProxy.forceClosePhysicalConnection(xaXid);
+
+        verify(mockConnection).close();
+        verify(mockConnection, times(2)).getWrappedConnection();
+        verify(mockWrappedConnection).close();
+    }
+
+    @Test
+    public void testForceClosePhysicalConnection_WithPooledConnection() throws SQLException {
+        // Mock connection with PooledConnection
+        ConnectionProxyXA mockConnection = mock(ConnectionProxyXA.class);
+        PooledConnection mockPooledConnection = mock(PooledConnection.class);
+        Connection mockPhysicalConnection = mock(Connection.class);
+        
+        when(mockConnection.getWrappedConnection()).thenReturn(mockPooledConnection);
+        when(mockPooledConnection.getConnection()).thenReturn(mockPhysicalConnection);
+
+        // Put connection in the lookup
+        dataSourceProxy.hold(xaXid.toString(), mockConnection);
+
+        // Force close physical connection
+        dataSourceProxy.forceClosePhysicalConnection(xaXid);
+
+        verify(mockConnection).close();
+        verify(mockPooledConnection).getConnection();
+        verify(mockPhysicalConnection).close();
+    }
+
+    @Test
+    public void testForceClosePhysicalConnection_NoExistingConnection() throws SQLException {
+        // Force close physical connection without existing connection
+        // Should not throw exception
+        Assertions.assertDoesNotThrow(() -> dataSourceProxy.forceClosePhysicalConnection(xaXid));
+    }
+
+    @Test
+    public void testDefaultResourceGroupId() {
+        Assertions.assertEquals("DEFAULT_XA", TestDataSourceProxyXA.DEFAULT_RESOURCE_GROUP_ID);
+    }
+
+    /**
+     * Test implementation of AbstractDataSourceProxyXA for testing purposes
+     */
+    private static class TestDataSourceProxyXA extends AbstractDataSourceProxyXA {
+        
+        private ConnectionProxyXA mockConnectionProxy;
+
+        public TestDataSourceProxyXA() {
+            this.branchType = BranchType.XA;
+            this.resourceId = "test-resource-id";
+        }
+
+        @Override
+        protected Connection getConnectionProxyXA() throws SQLException {
+            // Return a mock connection for testing
+            if (mockConnectionProxy == null) {
+                mockConnectionProxy = mock(ConnectionProxyXA.class);
+                Connection mockWrappedConnection = mock(Connection.class);
+                when(mockConnectionProxy.getWrappedConnection()).thenReturn(mockWrappedConnection);
+                when(mockWrappedConnection.isClosed()).thenReturn(false);
+            }
+            return mockConnectionProxy;
+        }
+    }
+}
+
