1830e0498021fb3f4e88ccaa83c56ac2e86061e6
Loki <loki16@qq.com>
2025-11-12 19:46:59 +0800

    fix ci

--- core/src/main/java/org/apache/seata/core/rpc/netty/AbstractNettyRemotingClient.java
+++ core/src/main/java/org/apache/seata/core/rpc/netty/AbstractNettyRemotingClient.java
@@ -107,6 +107,7 @@ public abstract class AbstractNettyRemotingClient extends AbstractNettyRemoting 
 
     /**
      * Global reconnect timer reference and started flag.
+     * Use AtomicReference so we can shut down and recreate the executor if needed.
      */
     private static final AtomicReference<ScheduledExecutorService> GLOBAL_RECONNECT_TIMER_REF = new AtomicReference<>();
 
