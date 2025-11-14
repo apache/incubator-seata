b889f96b71b0d094e21d16c4eba07f48ee986d06
Loki <loki16@qq.com>
2025-11-14 13:41:22 +0800

    run mvn

--- namingserver/src/test/java/org/apache/seata/namingserver/ClusterWatcherManagerTest.java
+++ namingserver/src/test/java/org/apache/seata/namingserver/ClusterWatcherManagerTest.java
@@ -34,7 +34,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
