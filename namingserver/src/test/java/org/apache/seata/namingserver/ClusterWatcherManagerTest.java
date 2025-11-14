38c21d71012765969d7c5d0670eacbee7b4e7f01
Loki <loki16@qq.com>
2025-11-14 18:45:49 +0800

    fix:fix the wrong test to make ci successfully

--- namingserver/src/test/java/org/apache/seata/namingserver/ClusterWatcherManagerTest.java
+++ namingserver/src/test/java/org/apache/seata/namingserver/ClusterWatcherManagerTest.java
@@ -34,6 +34,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -218,6 +219,25 @@ public class ClusterWatcherManagerTest {
         Mockito.verify(response).setStatus(HttpStatus.NOT_MODIFIED.value());
         Mockito.verify(asyncContext).complete();
         assertTrue(watcher.isDone());
+    }
+
+    @Test
+    void testScheduledTaskReRegisterNonTimeoutWatcher() throws InterruptedException {
+        int timeoutDuration = 3000;
+        Watcher<AsyncContext> watcher =
+                new Watcher<>(TEST_GROUP, asyncContext, timeoutDuration, TEST_TERM, TEST_CLIENT_ENDPOINT);
+        clusterWatcherManager.registryWatcher(watcher);
+
+        clusterWatcherManager.init();
+        TimeUnit.SECONDS.sleep(2);
+
+        Mockito.verify(response, Mockito.never()).setStatus(Mockito.anyInt());
+        Mockito.verify(asyncContext, Mockito.never()).complete();
+        assertFalse(watcher.isDone());
+        Map<String, Queue<Watcher<?>>> watchers =
+                (Map<String, Queue<Watcher<?>>>) ReflectionTestUtils.getField(clusterWatcherManager, "WATCHERS");
+        assertTrue(watchers.containsKey(TEST_GROUP));
+        assertEquals(1, watchers.get(TEST_GROUP).size());
     }
 
     @Test
