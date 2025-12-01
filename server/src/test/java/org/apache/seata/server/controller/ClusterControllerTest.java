e6a22ff289697237be0c7b73421fab52985d1bfe
xiaoyu <zhangyv@apache.org>
2025-12-01 21:46:44 +0800

    feature:Support HTTP/2 response handling for the Watch API in Server Raft mode

--- server/src/test/java/org/apache/seata/server/controller/ClusterControllerTest.java
+++ server/src/test/java/org/apache/seata/server/controller/ClusterControllerTest.java
@@ -115,9 +115,7 @@ class ClusterControllerTest extends BaseSpringBootTest {
 
         HttpClientUtil.doPostWithHttp2(
                 "http://127.0.0.1:" + port + "/metadata/v1/watch?timeout=3000", params, headers, callback);
-        // Currently, the server side does not have the ability to send http2 responses,
-        // so if no response is received here, it will definitely time out
-        Assertions.assertFalse(latch.await(5, TimeUnit.SECONDS));
+        Assertions.assertTrue(latch.await(5, TimeUnit.SECONDS));
     }
 
     @Test
@@ -141,7 +139,7 @@ class ClusterControllerTest extends BaseSpringBootTest {
         });
         thread.start();
         try (CloseableHttpResponse response =
-                HttpClientUtil.doPost("http://127.0.0.1:" + port + "/metadata/v1/watch", param, header, 30000)) {
+                     HttpClientUtil.doPost("http://127.0.0.1:" + port + "/metadata/v1/watch", param, header, 30000)) {
             if (response != null) {
                 StatusLine statusLine = response.getStatusLine();
                 Assertions.assertEquals(HttpStatus.SC_OK, statusLine.getStatusCode());
@@ -153,6 +151,56 @@ class ClusterControllerTest extends BaseSpringBootTest {
 
     @Test
     @Order(4)
+    void watch_withHttp2() throws Exception {
+        CountDownLatch latch = new CountDownLatch(1);
+
+        Map<String, String> headers = new HashMap<>();
+        headers.put(HTTP.CONTENT_TYPE, ContentType.APPLICATION_FORM_URLENCODED.getMimeType());
+
+        Map<String, String> params = new HashMap<>();
+        params.put("default-test", "1");
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                ((ApplicationEventPublisher) ObjectHolder.INSTANCE.getObject(OBJECT_KEY_SPRING_APPLICATION_CONTEXT))
+                        .publishEvent(new ClusterChangeEvent(this, "default-test", 2, true));
+            }
+        });
+        thread.start();
+
+        HttpCallback<Response> callback = new HttpCallback<Response>() {
+            @Override
+            public void onSuccess(Response response) {
+                Assertions.assertNotNull(response);
+                Assertions.assertEquals(Protocol.H2_PRIOR_KNOWLEDGE, response.protocol());
+                Assertions.assertEquals(HttpStatus.SC_OK, response.code());
+                latch.countDown();
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                Assertions.fail("Should not fail: " + t.getMessage());
+            }
+
+            @Override
+            public void onCancelled() {
+                Assertions.fail("Should not be cancelled");
+            }
+        };
+
+        HttpClientUtil.doPostWithHttp2(
+                "http://127.0.0.1:" + port + "/metadata/v1/watch", params, headers, callback, 30);
+        Assertions.assertTrue(latch.await(35, TimeUnit.SECONDS));
+    }
+
+    @Test
+    @Order(5)
     void testXssFilterBlocked_queryParam() throws Exception {
         String malicious = "<script>alert('xss')</script>";
         Map<String, String> header = new HashMap<>();
@@ -169,7 +217,7 @@ class ClusterControllerTest extends BaseSpringBootTest {
     }
 
     @Test
-    @Order(5)
+    @Order(6)
     void testXssFilterBlocked_queryParam_withGetHttp2() throws Exception {
         CountDownLatch latch = new CountDownLatch(1);
 
@@ -208,7 +256,7 @@ class ClusterControllerTest extends BaseSpringBootTest {
     }
 
     @Test
-    @Order(6)
+    @Order(7)
     void testXssFilterBlocked_formParam_withPostHttp2() throws Exception {
         CountDownLatch latch = new CountDownLatch(1);
 
@@ -245,7 +293,7 @@ class ClusterControllerTest extends BaseSpringBootTest {
     }
 
     @Test
-    @Order(7)
+    @Order(8)
     void testXssFilterBlocked_bodyParam_withPostHttp2() throws Exception {
         CountDownLatch latch = new CountDownLatch(1);
 
@@ -280,7 +328,7 @@ class ClusterControllerTest extends BaseSpringBootTest {
     }
 
     @Test
-    @Order(8)
+    @Order(9)
     void testXssFilterBlocked_formParam() throws Exception {
         Map<String, String> headers = new HashMap<>();
         headers.put(HTTP.CONTENT_TYPE, ContentType.APPLICATION_FORM_URLENCODED.getMimeType());
@@ -296,7 +344,7 @@ class ClusterControllerTest extends BaseSpringBootTest {
     }
 
     @Test
-    @Order(9)
+    @Order(10)
     void testXssFilterBlocked_jsonBody() throws Exception {
         Map<String, String> headers = new HashMap<>();
         headers.put(HTTP.CONTENT_TYPE, ContentType.APPLICATION_JSON.getMimeType());
@@ -311,7 +359,7 @@ class ClusterControllerTest extends BaseSpringBootTest {
     }
 
     @Test
-    @Order(10)
+    @Order(11)
     void testXssFilterBlocked_headerParam() throws Exception {
         Map<String, String> headers = new HashMap<>();
         headers.put(HTTP.CONTENT_TYPE, ContentType.APPLICATION_FORM_URLENCODED.getMimeType());
@@ -328,7 +376,7 @@ class ClusterControllerTest extends BaseSpringBootTest {
     }
 
     @Test
-    @Order(11)
+    @Order(12)
     void testXssFilterBlocked_multiSource() throws Exception {
         Map<String, String> headers = new HashMap<>();
         headers.put(HTTP.CONTENT_TYPE, ContentType.APPLICATION_JSON.getMimeType());
@@ -348,7 +396,7 @@ class ClusterControllerTest extends BaseSpringBootTest {
     }
 
     @Test
-    @Order(12)
+    @Order(13)
     void testXssFilterBlocked_formParamWithUserCustomKeyWords() throws Exception {
         Map<String, String> headers = new HashMap<>();
         headers.put(HTTP.CONTENT_TYPE, ContentType.APPLICATION_FORM_URLENCODED.getMimeType());
