e6965773e56ae84af2252539f3012b8746fc966c
徐博 洪 <2012753288@qq.com>
2025-12-23 23:05:00 +0800

    feature: Implement the GlobalLock tool

---
+++ console/src/main/java/org/apache/seata/mcp/entity/param/GlobalLockParam.java
@@ -0,0 +1,146 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.seata.mcp.entity.param;
+
+import org.apache.seata.common.util.PageUtil;
+import org.apache.seata.common.util.StringUtils;
+import org.apache.seata.mcp.core.utils.DateUtils;
+import org.apache.seata.mcp.entity.dto.GlobalLockParamDto;
+import org.springframework.beans.BeanUtils;
+
+import java.io.Serializable;
+
+/**
+ * Global lock param
+ */
+public class GlobalLockParam implements Serializable {
+
+    private static final long serialVersionUID = 615412528070131284L;
+
+    private String xid;
+
+    private String tableName;
+
+    private String transactionId;
+
+    private String branchId;
+
+    private String pk;
+
+    private String resourceId;
+
+    private int pageNum;
+
+    private int pageSize;
+
+    private Long timeStart;
+
+    private Long timeEnd;
+
+    public int getPageNum() {
+        return pageNum;
+    }
+
+    public void setPageNum(int pageNum) {
+        this.pageNum = pageNum;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public Long getTimeStart() {
+        return timeStart;
+    }
+
+    public void setTimeStart(Long timeStart) {
+        this.timeStart = timeStart;
+    }
+
+    public Long getTimeEnd() {
+        return timeEnd;
+    }
+
+    public void setTimeEnd(Long timeEnd) {
+        this.timeEnd = timeEnd;
+    }
+
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public String getBranchId() {
+        return branchId;
+    }
+
+    public void setBranchId(String branchId) {
+        this.branchId = branchId;
+    }
+
+    public String getXid() {
+        return xid;
+    }
+
+    public void setXid(String xid) {
+        this.xid = xid;
+    }
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+    }
+
+    public String getPk() {
+        return pk;
+    }
+
+    public void setPk(String pk) {
+        this.pk = pk;
+    }
+
+    public String getResourceId() {
+        return resourceId;
+    }
+
+    public void setResourceId(String resourceId) {
+        this.resourceId = resourceId;
+    }
+
+    public static GlobalLockParam convertFromParamDto(GlobalLockParamDto paramDto) {
+        PageUtil.checkParam(paramDto.getPageNum(), paramDto.getPageSize());
+        GlobalLockParam param = new GlobalLockParam();
+        BeanUtils.copyProperties(paramDto, param);
+        if (StringUtils.isNotBlank(paramDto.getTimeStart())) {
+            param.setTimeStart(DateUtils.convertToTimeStampFromDateTime(paramDto.getTimeStart()));
+        }
+        if (StringUtils.isNotBlank(paramDto.getTimeEnd())) {
+            param.setTimeEnd(DateUtils.convertToTimeStampFromDateTime(paramDto.getTimeEnd()));
+        }
+        return param;
+    }
+}
---
+++ console/src/main/java/org/apache/seata/mcp/tools/GlobalLockTools.java
@@ -0,0 +1,128 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.seata.mcp.tools;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.seata.common.result.PageResult;
+import org.apache.seata.common.util.StringUtils;
+import org.apache.seata.mcp.core.constant.RPCConstant;
+import org.apache.seata.mcp.core.props.MCPProperties;
+import org.apache.seata.mcp.core.props.NameSpaceDetail;
+import org.apache.seata.mcp.core.utils.DateUtils;
+import org.apache.seata.mcp.entity.dto.GlobalLockParamDto;
+import org.apache.seata.mcp.entity.param.GlobalLockDeleteParam;
+import org.apache.seata.mcp.entity.param.GlobalLockParam;
+import org.apache.seata.mcp.entity.vo.GlobalLockVO;
+import org.apache.seata.mcp.service.MCPRPCService;
+import org.apache.seata.mcp.service.ModifyConfirmService;
+import org.springaicommunity.mcp.annotation.McpTool;
+import org.springaicommunity.mcp.annotation.McpToolParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class GlobalLockTools {
+    @Autowired
+    private MCPRPCService mcpRPCService;
+
+    @Autowired
+    private MCPProperties configuration;
+
+    @Autowired
+    private ModifyConfirmService modifyConfirmService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @McpTool(description = "Query the global lock information")
+    public PageResult<GlobalLockVO> queryGlobalLock(
+            @McpToolParam(description = "Specify the namespace of the TC node", required = true)
+            NameSpaceDetail nameSpaceDetail,
+            @McpToolParam(description = "Global lock parameters", required = true) GlobalLockParamDto paramDto) {
+        GlobalLockParam param = GlobalLockParam.convertFromParamDto(paramDto);
+        if (param.getTimeStart() != null) {
+            if (param.getTimeEnd() != null) {
+                if (DateUtils.judgeExceedTimeDuration(
+                        param.getTimeStart(), param.getTimeEnd(), configuration.getQueryDuration())) {
+                    return PageResult.failure(
+                            "",
+                            "The query time span is not allowed to exceed the max query duration : "
+                                    + DateUtils.convertToHourFromTimeStamp(configuration.getQueryDuration()) + " hour");
+                }
+            } else {
+                param.setTimeEnd(param.getTimeStart() + DateUtils.ONE_DAY_TIMESTAMP);
+            }
+        } else {
+            param.setTimeEnd(null);
+            param.setTimeStart(null);
+        }
+        PageResult<GlobalLockVO> result;
+        String response = mcpRPCService.getCallTC(
+                nameSpaceDetail, RPCConstant.GLOBAL_LOCK_BASE_URL + "/query", param, null, null);
+        try {
+            result = objectMapper.readValue(response, new TypeReference<PageResult<GlobalLockVO>>() {});
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        if (result == null) {
+            return PageResult.failure("", "query global lock failed");
+        } else {
+            return result;
+        }
+    }
+
+    @McpTool(description = "Delete the global lock, Get the modify key before you delete")
+    public String deleteGlobalLock(
+            @McpToolParam(description = "Specify the namespace of the TC node", required = true)
+                    NameSpaceDetail nameSpaceDetail,
+            @McpToolParam(description = "Global lock delete parameters", required = true) GlobalLockDeleteParam param,
+            @McpToolParam(description = "Modify key", required = true) String modifyKey) {
+        if (!modifyConfirmService.isValidKey(modifyKey)) {
+            return "The modify key is not available";
+        }
+        String result = mcpRPCService.deleteCallTC(
+                nameSpaceDetail, RPCConstant.GLOBAL_LOCK_BASE_URL + "/delete", param, null, null);
+        if (StringUtils.isBlank(result)) {
+            return "delete global lock failed";
+        } else {
+            return result;
+        }
+    }
+
+    @McpTool(description = "Check if the lock exist the branch session")
+    public String checkGlobalLock(
+            @McpToolParam(description = "Specify the namespace of the TC node", required = true)
+                    NameSpaceDetail nameSpaceDetail,
+            @McpToolParam(description = "Global transaction id", required = true) String xid,
+            @McpToolParam(description = "Branch transaction id", required = true) String branchId) {
+        Map<String, String> pathParams = new HashMap<>();
+        pathParams.put("xid", xid);
+        pathParams.put("branchId", branchId);
+        String result = mcpRPCService.getCallTC(
+                nameSpaceDetail, RPCConstant.GLOBAL_LOCK_BASE_URL + "/check", null, pathParams, null);
+        if (StringUtils.isBlank(result)) {
+            return String.format("check global lock failed, xid: %s, branchId: %s", xid, branchId);
+        } else {
+            return result;
+        }
+    }
+}
