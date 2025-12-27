82cd44adc00d58b28f8fa17c2d26c080c8689b9f
slievrly <slievrly@163.com>
2025-12-27 21:38:16 +0800

    add test

--- serializer/seata-serializer-fory/src/test/java/org/apache/seata/serializer/fory/ForySerializerTest.java
+++ serializer/seata-serializer-fory/src/test/java/org/apache/seata/serializer/fory/ForySerializerTest.java
@@ -83,15 +83,23 @@ public class ForySerializerTest {
 
     @Test
     public void testLoadSerializerWithType() {
-        Serializer serializer = null;
-        serializer = SerializerServiceLoader.load(SerializerType.FURY);
-        Assertions.assertNotNull(serializer, "FURY Serializer should be available");
+        Serializer serializerFury = SerializerServiceLoader.load(SerializerType.FURY);
+        Assertions.assertNotNull(serializerFury, "FURY Serializer should be available");
+
+        Serializer serializerFory = SerializerServiceLoader.load(SerializerType.FORY);
+        Assertions.assertNotNull(serializerFory, "FORY Serializer should be available");
+
+        Assertions.assertEquals(serializerFury, serializerFory, "FORY Serializer should be equal FURY");
     }
 
     @Test
     public void testLoadSerializerWithVersion() {
-        Serializer serializer = null;
-        serializer = SerializerServiceLoader.load(SerializerType.FURY, (byte) 0x01);
-        Assertions.assertNotNull(serializer, "FURY Serializer should be available");
+        Serializer serializerFury = SerializerServiceLoader.load(SerializerType.FURY, (byte) 0x01);
+        Assertions.assertNotNull(serializerFury, "FURY Serializer should be available");
+
+        Serializer serializerFory = SerializerServiceLoader.load(SerializerType.FORY, (byte) 0x01);
+        Assertions.assertNotNull(serializerFory, "FORY Serializer should be available");
+
+        Assertions.assertEquals(serializerFury, serializerFory, "FORY Serializer should be equal FURY");
     }
 }
