16d5981538e28cb1923cf52bf247a93da7064a84
sddtc <changhbaga@gmail.com>
2026-01-29 13:59:20 +0800

    test: fix test with specific time zone

--- rm-datasource/src/test/java/org/apache/seata/rm/datasource/util/OffsetTimeUtilsTest.java
+++ rm-datasource/src/test/java/org/apache/seata/rm/datasource/util/OffsetTimeUtilsTest.java
@@ -77,8 +77,7 @@ class OffsetTimeUtilsTest {
         bytes[5] = (byte) 31; // minute = 31 - 1 = 30
         bytes[6] = (byte) 46; // second = 46 - 1 = 45
 
-        OffsetDateTime expected =
-                MOCK_UTC_DATE.atZoneSameInstant(ZoneId.systemDefault()).toOffsetDateTime();
+        OffsetDateTime expected = MOCK_UTC_DATE.atZoneSameInstant(ZoneId.of("Asia/Shanghai")).toOffsetDateTime();
 
         OffsetDateTime actual = OffsetTimeUtils.timeToOffsetDateTime(bytes);
 
@@ -107,8 +106,7 @@ class OffsetTimeUtilsTest {
         // minutes = bytes[12] - 60; to get 0 minutes -> bytes[12] = 60
         bytes[12] = (byte) 60;
 
-        OffsetDateTime expected =
-                MOCK_UTC_DATE.atZoneSameInstant(ZoneId.systemDefault()).toOffsetDateTime();
+        OffsetDateTime expected = MOCK_UTC_DATE.atZoneSameInstant(ZoneId.of("Asia/Shanghai")).toOffsetDateTime();
 
         OffsetDateTime actual = OffsetTimeUtils.timeToOffsetDateTime(bytes);
 
