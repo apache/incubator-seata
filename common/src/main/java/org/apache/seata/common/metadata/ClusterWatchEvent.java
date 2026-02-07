e2524a8cdec72a6a462f7745a251c9025ddb26a3
xiaoyu <zhangyv@apache.com>
2026-02-07 17:08:09 +0800

    optimize: optimize format code manager

--- common/src/main/java/org/apache/seata/common/metadata/ClusterWatchEvent.java
+++ common/src/main/java/org/apache/seata/common/metadata/ClusterWatchEvent.java
@@ -66,9 +66,7 @@ public class ClusterWatchEvent {
 
     @Override
     public String toString() {
-        String metadataString = metadata == null
-                ? "null"
-                : "MetadataResponse{term=" + metadata.getTerm() + "}";
+        String metadataString = metadata == null ? "null" : "MetadataResponse{term=" + metadata.getTerm() + "}";
         return "ClusterWatchEvent{" + "group='"
                 + group + '\'' + ", timestamp="
                 + timestamp + ", metadata="
