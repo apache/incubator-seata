35b68289a7c697dfa1343d8a96d617ce8065b60b
somiljain2006 <somil16022006@gmail.com>
2026-06-01 18:50:42 +0530

    Added more tests

--- rm-datasource/src/test/java/org/apache/seata/rm/datasource/exec/MySQLInsertExecutorTest.java
+++ rm-datasource/src/test/java/org/apache/seata/rm/datasource/exec/MySQLInsertExecutorTest.java
@@ -35,6 +35,7 @@ import org.apache.seata.rm.datasource.mock.MockDriver;
 import org.apache.seata.rm.datasource.mock.MockResultSet;
 import org.apache.seata.rm.datasource.sql.struct.TableRecords;
 import org.apache.seata.sqlparser.SQLInsertRecognizer;
+import org.apache.seata.sqlparser.SQLParsingException;
 import org.apache.seata.sqlparser.druid.mysql.MySQLInsertRecognizer;
 import org.apache.seata.sqlparser.struct.ColumnMeta;
 import org.apache.seata.sqlparser.struct.Null;
@@ -65,6 +66,8 @@ import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.anyString;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
@@ -863,6 +866,66 @@ public class MySQLInsertExecutorTest {
         assertEquals(2, pkValues.size());
         assertEquals(10, pkValues.get(0));
         assertEquals(20, pkValues.get(1));
+    }
+
+    @Test
+    public void testParsePkValues_MissingParameter_ThrowsException() {
+        String sql = "INSERT INTO test_table (id, name) VALUES (?, ?)";
+
+        Map<Integer, ArrayList<Object>> mockParameters = new HashMap<>();
+
+        PreparedStatementProxy statementProxy = mock(PreparedStatementProxy.class);
+        when(statementProxy.getParameters()).thenReturn(mockParameters);
+
+        SQLStatement statement = new MySqlStatementParser(sql).parseStatement();
+        MySQLInsertRecognizer recognizer = new MySQLInsertRecognizer(sql, statement);
+
+        MySQLInsertExecutor executor = spy(new MySQLInsertExecutor(statementProxy, (st, args) -> null, recognizer));
+
+        Map<String, Integer> testPkIndexMap = new HashMap<>();
+        testPkIndexMap.put("id", 0);
+        doReturn(testPkIndexMap).when(executor).getPkIndex();
+        doReturn("mysql").when(executor).getDbType();
+
+        SQLParsingException exception = assertThrows(SQLParsingException.class, executor::parsePkValuesFromStatement);
+
+        assertTrue(exception.getMessage().contains("Failed to find PreparedStatement parameter mapping"));
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void testParsePkValues_WithSpatialFunctionBeforePk() {
+        String sql =
+                "INSERT INTO test_table (name, geo, id) VALUES (?, ST_GeomFromText(CONCAT('POINT(', ?, ' ', ?, ')')), ?)";
+
+        Map<Integer, ArrayList<Object>> mockParameters = new HashMap<>();
+        mockParameters.put(1, Lists.newArrayList("A"));
+        mockParameters.put(2, Lists.newArrayList(111));
+        mockParameters.put(3, Lists.newArrayList(222));
+        mockParameters.put(4, Lists.newArrayList(10));
+
+        PreparedStatementProxy statementProxy = mock(PreparedStatementProxy.class);
+        when(statementProxy.getParameters()).thenReturn(mockParameters);
+
+        SQLStatement statement = new MySqlStatementParser(sql).parseStatement();
+        MySQLInsertRecognizer recognizer = new MySQLInsertRecognizer(sql, statement);
+
+        MySQLInsertExecutor executor = spy(new MySQLInsertExecutor(statementProxy, (st, args) -> null, recognizer));
+
+        Map<String, Integer> testPkIndexMap = new HashMap<>();
+        testPkIndexMap.put("id", 2);
+        doReturn(testPkIndexMap).when(executor).getPkIndex();
+        doReturn("mysql").when(executor).getDbType();
+
+        Map<String, List<Object>> pkValuesMap = executor.parsePkValuesFromStatement();
+
+        assertNotNull(pkValuesMap);
+
+        List<Object> pkValues = pkValuesMap.get("id");
+        assertNotNull(pkValues);
+        assertEquals(1, pkValues.size());
+
+        assertEquals(10, pkValues.get(0));
     }
 
     private List<String> mockInsertColumns() {
