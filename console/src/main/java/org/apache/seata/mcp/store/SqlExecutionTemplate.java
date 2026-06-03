9f34fd4b4b867913cc17d6f06c36b4a293dcdc6d
“Xb2555” <2012753288@qq.com>
2026-06-03 22:43:39 +0800

    fix: allow SELECT queries with where clauses

--- console/src/main/java/org/apache/seata/mcp/store/SqlExecutionTemplate.java
+++ console/src/main/java/org/apache/seata/mcp/store/SqlExecutionTemplate.java
@@ -69,12 +69,6 @@ public class SqlExecutionTemplate {
                 throw new StoreException("The query valid failed,Only query operations are allowed：" + sql);
             }
             conn = getConnection(resourceId);
-            if (params == null || params.length == 0) {
-                if ((sql.contains("where") || sql.contains("WHERE"))) {
-                    throw new StoreException(
-                            "Query contains WHERE clause but no parameters were provided. This may lead to unintended full table scans and is not allowed.");
-                }
-            }
             ps = conn.prepareStatement(sql);
             if (params != null) {
                 for (int i = 0; i < params.length; i++) {
